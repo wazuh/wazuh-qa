ff0c9178528eb0ece1b84e9a50b825deef34165c
Zenidd <jpsaezgutierrez@gmail.com>
2019-06-17 17:44:26 +0200

    New test names and other modifications

--- integration_tests/analysisd/feeder.py
+++ integration_tests/analysisd/feeder.py
@@ -6,39 +6,27 @@
 # Standard message form: <id>:<location>:<log>
 
 from socket import socket, AF_UNIX, SOCK_DGRAM, SO_SNDBUF, SOL_SOCKET
-from sys import argv
+import sys
 
-ADDR = '/var/ossec/queue/ossec/queue'
-BLEN = 212992
+def feeding(basedir, msg, limit=200):
+    ADDR = basedir + '/queue/ossec/queue'
+    BLEN = 212992
 
-if len(argv) < 3:
-    print("Syntax: {0} [-L] <message> <N messages>")
-    exit(1)
+    sock = socket(AF_UNIX, SOCK_DGRAM)
+    sock.connect(ADDR)
 
-sock = socket(AF_UNIX, SOCK_DGRAM)
-sock.connect(ADDR)
-oldbuf = sock.getsockopt(SOL_SOCKET, SO_SNDBUF)
+    oldbuf = sock.getsockopt(SOL_SOCKET, SO_SNDBUF)
 
-if oldbuf < BLEN:
-    sock.setsockopt(SOL_SOCKET, SO_SNDBUF, BLEN)
-    newbuf = sock.getsockopt(SOL_SOCKET, SO_SNDBUF)
-    print("INFO: Buffer expended from {0} to {1}".format(oldbuf, newbuf))
+    if oldbuf < BLEN:
+        sock.setsockopt(SOL_SOCKET, SO_SNDBUF, BLEN)
+        newbuf = sock.getsockopt(SOL_SOCKET, SO_SNDBUF)
+        print("INFO: Buffer expended from {0} to {1}".format(oldbuf, newbuf))
 
-if argv[1] == '-L':
-    string = ' '.join(argv[2:])
+    string = ' '.join(msg)
     i=1
-    limit = 100
-    limit = int(argv[3])
-    try:
-        while i<limit:
-            sock.send(string.encode())
-            i += 1
-    except BaseException as e:
-        print(e)
-        print("Messages: {0}\nBytes: {1}".format(i, i * len(string)))
 
-else:
-    string = ' '.join(argv[1:])
-    sock.send(string.encode())
+    while i < limit:
+        sock.send(string.encode())
+        i += 1
 
-sock.close()
+    sock.close()
