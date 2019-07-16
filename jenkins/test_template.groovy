54256d2f5214118cde92c0a2965ce390952a2348
Okynos <pylott@gmail.com>
2019-07-16 17:35:33 +0200

    Removed template library, updated Jenkins pipeline

--- jenkins/test_template.groovy
+++ jenkins/test_template.groovy
@@ -1,28 +1,63 @@
-@Library('jenkins-qa-library') _
 @Library('jenkins-shared-library@641-add-qa-templates') _
-import org.wazuh.Template
 
 String master_node = null
+String jenkins_branch = '641-add-qa-templates'
+String agent_image = 'centos-7'
+String manager_image = 'centos-7'
+String num_agents = '1'
+String num_managers = '1'
+String deploy_node = 'Node0'
+String deploy_prefix = 'test'
+String  max_manager_version = '3.9.2'
+String verbosity = '-vvv'
+
+String hosts_deploy_path
+String hosts_config_path
+
+Map template
 
 node(master_node){
   try{
-    stage('STAGE 0: Initialize'){
-      gitHelper.lightCheckout(
-        branch: '94-add-tests-jenkins',
-        repository: vars.DEFAULT_WAZUH_QA_REPOSITORY
-      )
+    ansiColor(vars.DEFAULT_TERMINAL_COLOR){
+      stage('STAGE 0: Initialize'){
+        gitHelper.lightCheckout(
+          branch: '94-add-tests-jenkins',
+          repository: vars.DEFAULT_WAZUH_QA_REPOSITORY
+        )
+      }
+
+      stage('STAGE 1: Parse template'){
+        template = readYaml(file: 'tests/specification.yml')
+        if(template.provisioner != 'docker'){ error 'Only docker provisioner supported' }
+
+
+
+      }
+
+
+      stage('STAGE 2: Launch instances'){
+        instance.init(branch: jenkins_branch)
+
+        hosts_deploy_path = instance.createDeployData(
+          agent_image: agent_image,
+          manager_image: manager_image,
+          number_of_agents: template.total_agents,
+          number_of_managers: template.total_managers,
+          deploy_node: deploy_node,
+          deploy_prefix: deploy_prefix,
+          use_ecr: true,
+          ecr_repository: instance.ECR_BASE_REPOSITORY,
+          ecr_source_version: max_manager_version
+        )
+
+        hosts_config_path = instance.createConfigurationData(
+          deploy_prefix: deploy_prefix,
+          is_centos5: false
+        )
+
+        instance.deploy(verbosity: verbosity)
+      }
     }
-
-    stage('STAGE 1: Parse template'){
-      Template template = new Template('tests/specification.yml')
-      echo template.data.toString()
-    }
-
-
-    stage('STAGE 2: Launch instances'){
-
-    }
-
 
   }catch(exception){
     throw exception
