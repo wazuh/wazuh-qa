e4380326ecff860717c05519a0943206232530fc
Okynos <pylott@gmail.com>
2019-07-16 11:39:10 +0200

    Added methods to load yaml

--- jenkins/test_template.groovy
+++ jenkins/test_template.groovy
@@ -1,15 +1,21 @@
 @Library('jenkins-qa-library') _
+@Library('jenkins-shared-library@641-add-qa-templates') _
+import org.wazuh.Template
 
 String master_node = null
 
 node(master_node){
   try{
     stage('STAGE 0: Initialize'){
-
+      gitHelper.lightCheckout(
+        branch: '94-add-tests-jenkins',
+        repository: vars.DEFAULT_WAZUH_QA_REPOSITORY
+      )
     }
 
     stage('STAGE 1: Parse template'){
-      Template template = new Template('/path/to/template')
+      Template template = new Template('tests/specification.yml')
+      echo template.data.toString()
     }
 
 
