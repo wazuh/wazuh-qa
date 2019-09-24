b6877e1c9af1f30c9fbec17c074d613907729c2b
Alberto Marin <alberto.marin@wazuh.com>
2019-09-24 13:08:48 +0200

    Create tests for 'check' attribute

--- test_wazuh/test_fim/test_checks/test_checks.py
+++ test_wazuh/test_fim/test_checks/test_checks.py
@@ -15,10 +15,8 @@ test_directories = [os.path.join('/', 'testdir1'), os.path.join('/', 'testdir2')
                     os.path.join('/', 'testdir4'), os.path.join('/', 'testdir5'), os.path.join('/', 'testdir6'),
                     os.path.join('/', 'testdir7'), os.path.join('/', 'testdir8'), os.path.join('/', 'testdir9'),
                     os.path.join('/', 'testdir0'),
-                    os.path.join('/', 'testdir_tags'),
-                    os.path.join('/', 'testdir_report_changes')
                     ]
-testdir1, testdir2, testdir3, testdir4, testdir5, testdir6, testdir7, testdir8, testdir9, testdir0, testdir_tags, testdir_report_changes = test_directories
+testdir1, testdir2, testdir3, testdir4, testdir5, testdir6, testdir7, testdir8, testdir9, testdir0 = test_directories
 
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 
@@ -79,78 +77,3 @@ def test_fim_checks(folder, name, filetype, content, checkers, configure_environ
     # Wait until event is detected
     event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
     validate_event(event, checks=checkers)
-
-
-@pytest.mark.parametrize('name, filetype, content', [
-    ('file1', REGULAR, 'Sample content'),
-    #('file2', REGULAR, ''),
-    ('file3', REGULAR, b'Sample content')
-    #('file4', REGULAR, b'')
-])
-@pytest.mark.parametrize('folder, checkers', [
-    # <directories whodata="yes" report_changes="yes">/testdir_report_changes</directories>
-    (testdir_report_changes, REQUIRED_ATTRIBUTES[CHECK_ALL])
-])
-def test_fim_reports(folder, name, filetype, content, checkers, configure_environment, restart_wazuh, wait_for_initial_scan):
-    # Create file
-    create_file(filetype, name, folder, content)
-
-    # Wait until event is detected
-    event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
-    validate_event(event, checks=checkers)
-
-    # Modify file
-    regular_path = os.path.join(folder, name)
-    modify_file(folder, name, content)
-
-    # Wait until event is detected
-    event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
-    validate_event(event, checks=checkers)
-    assert (event['data'].get('content_changes') is not None)
-
-    # Delete file
-    regular_path = os.path.join(folder, name)
-    delete_file(folder, name)
-
-    # Wait until event is detected
-    event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
-    validate_event(event, checks=checkers)
-
-
-@pytest.mark.parametrize('name, filetype, content', [
-    ('file1', REGULAR, 'Sample content'),
-    #('file2', REGULAR, ''),
-    ('file3', REGULAR, b'Sample content')
-    #('file4', REGULAR, b'')
-])
-@pytest.mark.parametrize('folder, checkers', [
-    # <directories whodata="yes" tags="tag0,tag1,tag2,tag3,tag4,tag5,tag6,tag7,tag8,tag9">/testdir_tags</directories>
-    (testdir_tags, REQUIRED_ATTRIBUTES[CHECK_ALL])
-])
-def test_fim_tags(folder, name, filetype, content, checkers, configure_environment, restart_wazuh, wait_for_initial_scan):
-    defined_tags = 'tag0,tag1,tag2,tag3,tag4,tag5,tag6,tag7,tag8,tag9'
-    # Create file
-    create_file(filetype, name, folder, content)
-
-    # Wait until event is detected
-    event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
-    validate_event(event, checks=checkers)
-    assert (defined_tags == event['data']['tags'])
-
-    # Modify file
-    regular_path = os.path.join(folder, name)
-    modify_file(folder, name, content)
-
-    # Wait until event is detected
-    event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
-    validate_event(event, checks=checkers)
-    assert (defined_tags == event['data']['tags'])
-
-    # Delete file
-    regular_path = os.path.join(folder, name)
-    delete_file(folder, name)
-
-    # Wait until event is detected
-    event = wazuh_log_monitor.start(timeout=3, callback=callback_detect_event).result()
-    validate_event(event, checks=checkers)
-    assert (defined_tags == event['data']['tags'])
