819737ed15d2d481557efac862a9899c3afa2660
CarlosRS <carlos.ridao@wazuh.com>
2019-10-04 08:25:00 +0200

    Created a new set of tests and modified previous version to use parametrized yaml

--- test_wazuh/test_fim/test_checks/test_checks_individually.py
+++ test_wazuh/test_fim/test_checks/test_checks_individually.py
@@ -9,7 +9,8 @@ from wazuh_testing.fim import (CHECK_ALL, CHECK_GROUP, CHECK_INODE,
                                CHECK_MD5SUM, CHECK_MTIME, CHECK_OWNER,
                                CHECK_PERM, CHECK_SHA1SUM, CHECK_SHA256SUM,
                                CHECK_SIZE, CHECK_SUM, LOG_FILE_PATH, REGULAR,
-                               REQUIRED_ATTRIBUTES, regular_file_cud)
+                               REQUIRED_ATTRIBUTES, callback_detect_event,
+                               regular_file_cud,create_file, modify_file, delete_file, validate_event)
 from wazuh_testing.tools import (FileMonitor, check_apply_test,
                                  load_wazuh_configurations)
 
@@ -17,7 +18,7 @@ from wazuh_testing.tools import (FileMonitor, check_apply_test,
 # variables
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
+configurations_path = os.path.join(test_data_path, 'wazuh_individually_conf.yaml')
 test_directories = [os.path.join('/', 'testdir1'), os.path.join('/', 'testdir2'), 
                     os.path.join('/', 'testdir3'), os.path.join('/', 'testdir4'), 
                     os.path.join('/', 'testdir5'), os.path.join('/', 'testdir6'), 
@@ -51,24 +52,37 @@ def get_configuration(request):
 
 
 # tests
-
+"""
 @pytest.mark.parametrize('path, checkers', [
-    (testdir1, REQUIRED_ATTRIBUTES[CHECK_ALL] - REQUIRED_ATTRIBUTES[CHECK_SUM]),
-    (testdir2, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_MD5SUM}),
-    (testdir3, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_SHA1SUM}),
-    (testdir4, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_SHA256SUM}),
-    (testdir5, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_SIZE}),
-    (testdir6, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_OWNER}),
-    (testdir7, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_GROUP}),
-    (testdir8, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_PERM}),
-    (testdir9, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_MTIME}),
-    (testdir0, REQUIRED_ATTRIBUTES[CHECK_ALL] - {CHECK_INODE}),
+    (testdir1, REQUIRED_ATTRIBUTES[CHECK_SUM]),
+    (testdir2, REQUIRED_ATTRIBUTES[CHECK_MD5SUM]),
+    (testdir3, REQUIRED_ATTRIBUTES[CHECK_SHA1SUM]),
+    (testdir4, REQUIRED_ATTRIBUTES[CHECK_SHA256SUM]),
+    (testdir5, REQUIRED_ATTRIBUTES[CHECK_SIZE]),
+    (testdir6, REQUIRED_ATTRIBUTES[CHECK_OWNER]),
+    (testdir7, REQUIRED_ATTRIBUTES[CHECK_GROUP]),
+    (testdir8, REQUIRED_ATTRIBUTES[CHECK_PERM]),
+    (testdir9, REQUIRED_ATTRIBUTES[CHECK_MTIME]),
+    (testdir0, REQUIRED_ATTRIBUTES[CHECK_INODE])
 ])
-def test_checks(path, checkers, get_configuration, configure_environment, 
-                restart_wazuh, wait_for_initial_scan):
+"""
+@pytest.mark.parametrize('path, checkers', [
+    (testdir1, {CHECK_SUM}),
+    (testdir2, {CHECK_MD5SUM}),
+    (testdir3, {CHECK_SHA1SUM}),
+    (testdir4, {CHECK_SHA256SUM}),
+    (testdir5, {CHECK_SIZE}),
+    (testdir6, {CHECK_OWNER}),
+    (testdir7, {CHECK_GROUP}),
+    (testdir8, {CHECK_PERM}),
+    (testdir9, {CHECK_MTIME}),
+    (testdir0, {CHECK_INODE})
+])
+def test_checks_individually(path, checkers, get_configuration, configure_environment, 
+                             restart_wazuh, wait_for_initial_scan):
     #check_apply_test({'realtime'}, get_configuration['tags'])
 
     if get_configuration['metadata']['fim_mode'] == 'scheduled':
-        regular_file_cud(path, wazuh_log_monitor, min_timeout=3, options=checkers, time_travel = True)
+        regular_file_cud(path, wazuh_log_monitor, min_timeout=2, options=checkers, time_travel = True)
     else:
-        regular_file_cud(path, wazuh_log_monitor, min_timeout=3, options=checkers)
+        regular_file_cud(path, wazuh_log_monitor, min_timeout=2, options=checkers)
