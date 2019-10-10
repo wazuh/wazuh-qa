77dfe24a09bb6c4419407a9dcb15eea87f71368b
Manuel Gutierrez <manuel.gutierrez@wazuh.com>
2019-10-10 11:24:42 +0200

    Enable xpack tests

--- ansible/wazuh-ansible/run_cluster_mode.sh
+++ ansible/wazuh-ansible/run_cluster_mode.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-paths=( "molecule/default/" "molecule/worker/" "molecule/elasticsearch/" "molecule/kibana/" )
+paths=( "molecule/default/" "molecule/worker/" "molecule/elasticsearch/" "molecule/elasticsearch-xpack/" "molecule/kibana/" )
 images=( "solita/ubuntu-systemd:bionic" "solita/ubuntu-systemd:xenial" "milcom/centos7-systemd" "ubuntu:trusty" "centos:6" )
 platform=( "bionic" "xenial" "centos7" "trusty" "centos6" )
 
@@ -18,13 +18,13 @@ index=$(($REPLY - 1))
 
 if [ -z "$IMAGE" ]
 then
-      echo "Platform not selected. Please select a platform of [bionuc, xenial or centos7]. => Aborting"
+      echo "Platform not selected. Please select a platform of [bionic, xenial or centos7]. => Aborting"
       exit
 else
         for i in "${paths[@]}"
         do
             cp "$i/playbook.yml.template" "$i/playbook.yml"
-            sed -i "s/platform/${platform[$index]}/g" "$i/playbook.yml"
+            sed -i "s/%PLATFORM%/${platform[$index]}/g" "$i/playbook.yml"
 
             cp "$i/molecule.yml.template" "$i/molecule.yml"
             sed -i "s|imagename|${images[$index]}|g" "$i/molecule.yml"
@@ -36,9 +36,9 @@ fi
 pipenv run destroy_all
 
 pipenv run elasticsearch
+pipenv run elasticsearch-xpack
 pipenv run test
 pipenv run worker
 pipenv run kibana
 
 pipenv run destroy_all
-
