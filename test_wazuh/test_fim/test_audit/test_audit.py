b24190ffc8534f0ceb206e2dcd2428b46f9266ea
CarlosRS <carlos.ridao@wazuh.com>
2019-10-17 10:17:11 +0200

    Test restart_audit now implemented and working fine

--- test_wazuh/test_fim/test_audit/test_audit.py
+++ test_wazuh/test_fim/test_audit/test_audit.py
@@ -5,7 +5,9 @@
 import os
 import subprocess
 import time
+import psutil
 import pytest
+
 
 from wazuh_testing.fim import (LOG_FILE_PATH, callback_audit_added_rule,
                                callback_audit_connection,
@@ -15,9 +17,7 @@ from wazuh_testing.fim import (LOG_FILE_PATH, callback_audit_added_rule,
                                callback_audit_rules_manipulation,
                                callback_realtime_added_directory,
                                callback_audit_key, 
-                               create_file,
-                               delete_file,
-                               REGULAR,
+                               create_file, REGULAR,
                                detect_initial_scan)
 from wazuh_testing.tools import (FileMonitor, check_apply_test,
                                  load_wazuh_configurations,
@@ -148,28 +148,74 @@ def test_move_rules_realtime(tags_to_apply, get_configuration,
     p.wait()
 
 
-def test_audit_key(get_configuration, configure_environment, restart_syscheckd):
-    check_apply_test({"audit_key"}, get_configuration['tags'])
-    
-    audit_key = "custom_audit_key"
-    audit_dir = "/testdir1"
+@pytest.mark.parametrize('audit_key, path', [
+    ("custom_audit_key", "/testdir1")
+])
+def test_audit_key(audit_key, path, get_configuration, configure_environment, restart_syscheckd):
+    """Checks <audit_key> functionality by adding a audit rule and checking if alerts with that key are triggered when
+    a file is created.
+
+    This test is intended to be used with valid configurations
+
+    :param taudit_key string Name of the audit_key to monitor
+    :param path string Path of the folder to be monitored
+    """
+    check_apply_test({audit_key}, get_configuration['tags'])
 
     # Add watch rule
-    os.system("auditctl -w " + audit_dir + " -p wa -k " + audit_key)
+    os.system("auditctl -w " + path + " -p wa -k " + audit_key)
 
+    # Restart and for wazuh
     truncate_file(LOG_FILE_PATH)
-
     restart_wazuh_service()
-
     wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
     detect_initial_scan(wazuh_log_monitor)
 
-    create_file(REGULAR, audit_dir, "testfile")
-
+    # Look for audit_key word
+    create_file(REGULAR, path, "testfile")
     events = wazuh_log_monitor.start(timeout=30,
                                      callback=callback_audit_key,
                                      accum_results=1).result()
     assert (audit_key in events)
 
     # Remove watch rule
-    os.system("auditctl -W " + audit_dir + " -p wa -k " + audit_key)
+    os.system("auditctl -W " + path + " -p wa -k " + audit_key)
+
+
+@pytest.mark.parametrize('tags_to_apply, should_restart', [
+    ({'audit_key'}, True),
+    ({'restart_audit_false'}, False)
+])
+def test_restart_audit(tags_to_apply, should_restart, get_configuration, configure_environment, restart_syscheckd):
+    """Checks <restart_audit> functionality by removing the plugin and monitoring audit to see if it restart and create 
+    the file again.
+
+    This test is intended to be used with valid configurations
+
+    :param tags_to_apply set Run test if matches with a configuration identifier, skip otherwise
+    :param should_restart boolean True if Auditd should restart, False otherwise
+    """
+    def get_audit_creation_time():
+        for proc in psutil.process_iter(attrs=['name']):
+            if proc.name() == "auditd":
+                return proc.create_time()
+        pytest.fail("Auditd is not running")
+    
+    plugin_path = "/etc/audisp/plugins.d/af_wazuh.conf"
+
+    check_apply_test(tags_to_apply, get_configuration['tags'])
+
+    os.remove(plugin_path)
+
+    time_before_restart = get_audit_creation_time()
+    restart_wazuh_service()
+    time.sleep(5)
+    
+    time_after_restart = get_audit_creation_time()
+
+    if should_restart:
+        assert(time_before_restart != time_after_restart)
+    else:
+        assert(time_before_restart == time_after_restart)
+
+    assert(os.path.isfile(plugin_path))
