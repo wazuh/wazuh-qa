5082289b6ed8188c3d47e218641e7f5f5c0de633
Víctor Fernández <vicferpoy@gmail.com>
2019-10-30 10:58:07 +0100

    Improve skip_proc and skip_sys for skip tests

--- test_wazuh/test_fim/test_skip/test_skip.py
+++ test_wazuh/test_fim/test_skip/test_skip.py
@@ -5,40 +5,40 @@
 import os
 import shutil
 import subprocess
+from datetime import timedelta
 
 import pytest
 
 from wazuh_testing.fim import (LOG_FILE_PATH, callback_detect_integrity_event,
-                               regular_file_cud)
+                               regular_file_cud, detect_initial_scan, callback_detect_event)
 from wazuh_testing.tools import (FileMonitor, check_apply_test,
-                                 load_wazuh_configurations)
+                                 load_wazuh_configurations, TimeMachine,
+                                 set_section_wazuh_conf, restart_wazuh_with_new_conf)
 
 # variables
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
-test_directories = [os.path.join('/', 'testdir1')]
+testdir = os.path.join('/', 'testdir1')
+test_directories = [testdir]
 
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 
+
 # configurations
 
-configurations = load_wazuh_configurations(configurations_path, __name__,
-                                           params=[{'FIM_MODE': '', 'SKIP': 'yes'},
-                                                   {'FIM_MODE': '', 'SKIP': 'no'},
-                                                   {'FIM_MODE': {'realtime': 'yes'}, 'SKIP': 'yes'},
-                                                   {'FIM_MODE': {'realtime': 'yes'}, 'SKIP': 'no'},
-                                                   {'FIM_MODE': {'whodata': 'yes'}, 'SKIP': 'yes'},
-                                                   {'FIM_MODE': {'whodata': 'yes'}, 'SKIP': 'no'}
-                                                   ],
-                                           metadata=[{'fim_mode': 'scheduled', 'skip': 'yes'},
-                                                     {'fim_mode': 'scheduled', 'skip': 'no'},
-                                                     {'fim_mode': 'realtime', 'skip': 'yes'},
-                                                     {'fim_mode': 'realtime', 'skip': 'no'},
-                                                     {'fim_mode': 'whodata', 'skip': 'yes'},
-                                                     {'fim_mode': 'whodata', 'skip': 'no'}
-                                                     ]
-                                           )
+def change_conf(directory):
+    return load_wazuh_configurations(configurations_path, __name__,
+                                     params=[{'SKIP': 'yes', 'DIRECTORY': directory},
+                                             {'SKIP': 'no', 'DIRECTORY': directory},
+                                             ],
+                                     metadata=[{'skip': 'yes', 'directory': directory},
+                                               {'skip': 'no', 'directory': directory},
+                                               ]
+                                     )
+
+
+configurations = change_conf(testdir)
 
 
 # fixtures
@@ -51,20 +51,20 @@ def get_configuration(request):
 
 @pytest.fixture(scope='session')
 def configure_nfs():
-    # create and configure nfs mount point
-    subprocess.call(['./data/configure_nfs.sh'])
+    """ Call NFS scripts to create and configure a NFS mount point """
+    path = os.path.dirname(os.path.abspath(__file__))
+    subprocess.call([f'{path}/data/configure_nfs.sh'])
     yield
     # remove nfs
-    subprocess.call(['./data/remove_nfs.sh'])
+    subprocess.call([f'{path}/data/remove_nfs.sh'])
     shutil.rmtree(os.path.join('/', 'media', 'nfs-folder'), ignore_errors=True)
 
 
 # tests
 
-
 @pytest.mark.parametrize('directory,  tags_to_apply', [
     (os.path.join('/', 'proc'), {'skip_proc'}),
-    (os.path.join('/', 'sys'), {'skip_sys'}),
+    (os.path.join('/', 'sys', 'video'), {'skip_sys'}),
     (os.path.join('/', 'dev'), {'skip_dev'}),
     (os.path.join('/', 'nfs-mount-point'), {'skip_nfs'})
 ])
@@ -73,27 +73,65 @@ def test_skip(directory, tags_to_apply,
               restart_syscheckd, wait_for_initial_scan):
     """ Check if syscheck is skipping the directory based on its skip configuration
 
-    :param directory: Directory that will be monitored
+    :param directory: Directory that will be monitored. We only use it on skip_dev and skip_nfs
     """
     check_apply_test(tags_to_apply, get_configuration['tags'])
-    timeout = 3
-    if tags_to_apply == {'skip_proc'}:
-        timeout = 100
 
     if get_configuration['metadata']['skip'] == 'yes':
         trigger = False
     else:
         trigger = True
 
-    if tags_to_apply == {'skip_proc'} or tags_to_apply == {'skip_sys'}:
+    if tags_to_apply == {'skip_proc'}:
         if trigger:
-            event = wazuh_log_monitor.start(timeout=8, callback=callback_detect_integrity_event).result()
-            assert any(path in event['data'].get('path') for path in ['/proc', '/sys'])
+            proc = subprocess.Popen(["python3", f"{os.path.dirname(os.path.abspath(__file__))}/data/proc.py"])
+            # Change configuration, monitoring the PID path in /proc
+            new_conf = change_conf(f'/proc/{proc.pid}')
+            # new_conf[1] -> 'skip': 'no'
+            new_ossec_conf = set_section_wazuh_conf(new_conf[1].get('section'),
+                                                    new_conf[1].get('elements'))
+            restart_wazuh_with_new_conf(new_ossec_conf)
+            detect_initial_scan(wazuh_log_monitor)
+
+            # Do not expect any 'Sending event'
+            with pytest.raises(TimeoutError):
+                wazuh_log_monitor.start(timeout=5, callback=callback_detect_event)
+
+            TimeMachine.travel_to_future(timedelta(hours=13))
+
+            found_event = False
+            while not found_event:
+                event = wazuh_log_monitor.start(timeout=5, callback=callback_detect_event).result()
+                if f'/proc/{proc.pid}/' in event['data'].get('path'):
+                    found_event = True
+
+            # Kill the process
+            subprocess.Popen(["kill", "-9", str(proc.pid)])
+
         else:
             with pytest.raises(TimeoutError):
-                wazuh_log_monitor.start(timeout=5, callback=callback_detect_integrity_event)
+                wazuh_log_monitor.start(timeout=3, callback=callback_detect_integrity_event)
 
+    elif tags_to_apply == {'skip_sys'}:
+        if trigger:
+            # If /sys/module/video does not exist, use 'modprobe video'
+            assert os.path.exists('/sys/module/video'), f'/sys/module/video does not exist'
+            # Do not expect any 'Sending event'
+            with pytest.raises(TimeoutError):
+                wazuh_log_monitor.start(timeout=5, callback=callback_detect_event)
+            # Remove module video and travel to future to check alerts
+            subprocess.Popen(["modprobe", "-r", "video"])
+            TimeMachine.travel_to_future(timedelta(hours=13))
+            # Detect at least one 'delete' event in /sys/module/video path
+            event = wazuh_log_monitor.start(timeout=5, callback=callback_detect_event).result()
+            assert event['data'].get('type') == 'deleted' and '/sys/module/video' in event['data'].get('path'), \
+                f'Sys event not detected'
+            # Restore module video
+            subprocess.Popen(["modprobe", "video"])
+        else:
+            with pytest.raises(TimeoutError):
+                wazuh_log_monitor.start(timeout=3, callback=callback_detect_integrity_event)
     else:
         regular_file_cud(directory, wazuh_log_monitor,
-                         time_travel=get_configuration['metadata']['fim_mode'] == 'scheduled',
-                         min_timeout=timeout, triggers_event=trigger)
+                         time_travel=True,
+                         min_timeout=3, triggers_event=trigger)
