5fe6e72d10fe74931ede5cff2d3f888174712e0e
Rshad Zhran <rashzk95@gmail.com>
2019-11-07 12:25:23 +0100

    adapted run.sh to dynamically proceed per the selected suite

--- kitchen/wazuh-puppet/kitchen/run.sh
+++ kitchen/wazuh-puppet/kitchen/run.sh
@@ -21,15 +21,13 @@ do
     kitchen create $suite # creating new kitchen instances
 
     echo "Getting Wazuh managers IPs to the agents"
-    ubuntu_manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  ubuntu | grep manager`)"
-    centos_manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  centos | grep manager`)"
+    manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  $suite | grep manager`)"
 
     echo "getting a copy of ./manifests/site.pp.template"
-    cp ./manifests/site.pp.template ./manifests/site.pp
+    cp ./manifests/site.pp.template.$suite ./manifests/site.pp
 
     echo "Assigning Wazuh managers IPs to the corresponding agents."
-    sed -i 's/ubuntu_manager_ip/'${ubuntu_manager_ip}'/g' ./manifests/site.pp
-    sed -i 's/centos_manager_ip/'${centos_manager_ip}'/g' ./manifests/site.pp
+    sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
 
     echo "Kitchen is converging ..."
     kitchen converge $suite
