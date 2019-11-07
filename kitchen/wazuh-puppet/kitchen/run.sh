fe2b8851428d171ac0e3560ba87a774e364d8821
Rshad Zhran <rashzk95@gmail.com>
2019-11-07 17:59:31 +0100

    unified site.pp - added key word for platforms and removed site.pp per platform

--- kitchen/wazuh-puppet/kitchen/run.sh
+++ kitchen/wazuh-puppet/kitchen/run.sh
@@ -17,6 +17,15 @@ fi
 
 for suite in "${suites_platforms[@]}"
 do
+
+    if [[ $suite == *"ubuntu"* ]]; then
+        platform = "ubuntu"
+
+    elif [[ $suite == *"centos"* ]]; then
+        platform = "centos"
+	fi
+
+
     echo "$suite is selected"
     
     echo "Kitchen is creating the new instances"
@@ -26,11 +35,14 @@ do
     manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  $suite | grep manager`)"
 
     echo "getting a copy of ./manifests/site.pp.template"
-    cp ./manifests/site.pp.template.$suite ./manifests/site.pp
+    cp ./manifests/site.pp.template ./manifests/site.pp
 
     echo "Assigning Wazuh managers IPs to the corresponding agents."
     sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
 
+    echo "Setting the platform in the components names."
+    sed -i 's/platform/'${platform}'/g' ./manifests/site.pp
+    
 	if [[ $suite == *"centos"* ]]; then
 		echo "suite is a Centos one and requires OpenSSL to be installed. .. Installing .."
 		kitchen exec $suite -c "sudo yum install -y openssl"
