6d7edd3d2ea98822fa40d03ac89baf91448cbdb7
Víctor Fernández <vicferpoy@gmail.com>
2019-11-11 16:49:15 +0100

    Add test_report_changes for windows

--- test_wazuh/test_fim/test_report_changes/test_report_changes_and_diff.py
+++ test_wazuh/test_fim/test_report_changes/test_report_changes_and_diff.py
@@ -2,19 +2,26 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 import os
+import sys
 
 import pytest
 
 from wazuh_testing.fim import (CHECK_ALL, LOG_FILE_PATH, regular_file_cud, WAZUH_PATH)
-from wazuh_testing.tools import (FileMonitor, check_apply_test, load_wazuh_configurations)
+from wazuh_testing.tools import (FileMonitor, check_apply_test, load_wazuh_configurations, set_configuration)
 
 # variables
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
-test_directories = [
-    os.path.join('/', 'testdir_reports'),
-    os.path.join('/', 'testdir_nodiff')
-]
+if sys.platform == 'win32':
+    test_directories = [os.path.join('C:', os.sep, 'testdir_reports'), os.path.join('C:', os.sep, 'testdir_nodiff')]
+    directory_str = "c:\\testdir_reports,c:\\testdir_nodiff"
+    nodiff_file = os.path.join('C:', os.sep, 'testdir_nodiff', 'regular_file')
+
+
+else:
+    test_directories = [os.path.join('/', 'testdir_reports'), os.path.join('/', 'testdir_nodiff')]
+    directory_str = "/testdir_reports,/testdir_nodiff"
+    nodiff_file = "testdir_nodiff/regular_file"
 testdir_reports, testdir_nodiff = test_directories
 configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
 options = {CHECK_ALL}
@@ -23,25 +30,15 @@ wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 
 # configurations
 
-
+conf_params, conf_metadata = set_configuration({'REPORT_CHANGES': {'report_changes': 'yes'},
+                                                'TEST_DIRECTORIES': directory_str, 'NODIFF_FILE': nodiff_file,
+                                                'MODULE_NAME': __name__},
+                                               {'report_changes': 'yes',
+                                                'test_directories': directory_str, 'nodiff_file': nodiff_file,
+                                                'module_name': __name__})
 configurations = load_wazuh_configurations(configurations_path, __name__,
-                                           params=[{'FIM_MODE': '',
-                                                    'REPORT_CHANGES': {'report_changes': 'yes'},
-                                                    'MODULE_NAME': __name__},
-                                                   {'FIM_MODE': {'realtime': 'yes'},
-                                                    'REPORT_CHANGES': {'report_changes': 'yes'},
-                                                    'MODULE_NAME': __name__},
-                                                   {'FIM_MODE': {'whodata': 'yes'},
-                                                    'REPORT_CHANGES': {'report_changes': 'yes'},
-                                                    'MODULE_NAME': __name__}
-                                                   ],
-                                           metadata=[{'fim_mode': 'scheduled', 'report_changes': True,
-                                                      'module_name': __name__},
-                                                     {'fim_mode': 'realtime', 'report_changes': True,
-                                                      'module_name': __name__},
-                                                     {'fim_mode': 'whodata', 'report_changes': True,
-                                                      'module_name': __name__}
-                                                     ]
+                                           params=conf_params,
+                                           metadata=conf_metadata
                                            )
 
 
@@ -77,19 +74,23 @@ def test_reports_file_and_nodiff(folder, checkers, tags_to_apply,
     def report_changes_validator(event):
         """ Validate content_changes attribute exists in the event """
         for file in file_list:
-            diff_file = os.path.join(WAZUH_PATH, 'queue', 'diff', 'local',
-                                     folder.strip('/'), file)
-            assert(os.path.exists(diff_file)), f'{diff_file} does not exist'
-            assert(event['data'].get('content_changes') is not None), f'content_changes is empty'
+            diff_file = os.path.join(WAZUH_PATH, 'queue', 'diff', 'local')
+            if sys.platform == 'win32':
+                diff_file = os.path.join(diff_file, 'c')
+                diff_file = os.path.join(diff_file, folder.strip('C:\\'), file)
+            else:
+                diff_file = os.path.join(diff_file, folder.strip('/'), file)
+            assert (os.path.exists(diff_file)), f'{diff_file} does not exist'
+            assert (event['data'].get('content_changes') is not None), f'content_changes is empty'
 
     def no_diff_validator(event):
         """ Validate content_changes value is truncated if the file is set to no_diff """
         if is_truncated:
             assert ('<Diff truncated because nodiff option>' in event['data'].get('content_changes')), \
-                    f'content_changes is not truncated'
+                f'content_changes is not truncated'
         else:
             assert ('<Diff truncated because nodiff option>' not in event['data'].get('content_changes')), \
-                    f'content_changes is truncated'
+                f'content_changes is truncated'
 
     regular_file_cud(folder, wazuh_log_monitor, file_list=file_list,
                      time_travel=get_configuration['metadata']['fim_mode'] == 'scheduled',
