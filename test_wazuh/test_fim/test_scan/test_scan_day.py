aae6e209fa44e9cb1f7ace0a87a7dadf77a59309
Víctor Fernández <vicferpoy@gmail.com>
2019-11-12 13:45:58 +0100

    Add scan tests for windows

--- test_wazuh/test_fim/test_scan/test_scan_day.py
+++ test_wazuh/test_fim/test_scan/test_scan_day.py
@@ -7,27 +7,28 @@ from datetime import datetime, timedelta
 import pytest
 
 from wazuh_testing.fim import (LOG_FILE_PATH, callback_detect_end_scan)
-from wazuh_testing.tools import (FileMonitor, check_apply_test, load_wazuh_configurations, TimeMachine)
+from wazuh_testing.tools import (FileMonitor, check_apply_test, load_wazuh_configurations, TimeMachine, PREFIX)
 
 # variables
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 
 configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
-test_directories = [os.path.join('/', 'testdir1')]
+test_directories = [os.path.join(PREFIX, 'testdir1')]
+
+directory_str = ','.join(test_directories)
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 scan_days = ['monday', 'thursday', 'wednesday']
 
 # configurations
 
-
 configurations = load_wazuh_configurations(configurations_path, __name__,
-                                           params=[{'SCAN_DAY': scan_days[0]},
-                                                   {'SCAN_DAY': scan_days[1]},
-                                                   {'SCAN_DAY': scan_days[2]}
+                                           params=[{'SCAN_DAY': scan_days[0], 'TEST_DIRECTORIES': directory_str},
+                                                   {'SCAN_DAY': scan_days[1], 'TEST_DIRECTORIES': directory_str},
+                                                   {'SCAN_DAY': scan_days[2], 'TEST_DIRECTORIES': directory_str}
                                                    ],
-                                           metadata=[{'scan_day': scan_days[0]},
-                                                     {'scan_day': scan_days[1]},
-                                                     {'scan_day': scan_days[2]}
+                                           metadata=[{'scan_day': scan_days[0], 'test_directories': directory_str},
+                                                     {'scan_day': scan_days[1], 'test_directories': directory_str},
+                                                     {'scan_day': scan_days[2], 'test_directories': directory_str}
                                                      ]
                                            )
 
