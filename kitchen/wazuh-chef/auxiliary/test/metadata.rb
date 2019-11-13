52c857013de2ce0684677a82c2a149db09b27eeb
Rshad Zhran <rashzk95@gmail.com>
2019-11-13 17:27:22 +0100

    Add  cookbook to load cookbooks attributes in a file

--- kitchen/wazuh-chef/auxiliary/test/metadata.rb
+++ kitchen/wazuh-chef/auxiliary/test/metadata.rb
@@ -2,6 +2,6 @@ name             'test'
 maintainer       'Wazuh Inc.'
 maintainer_email 'info@wazuh.com'
 license          'Apache 2.0'
-description      'Installs and onfigures ossec'
+description      'This Cookbook is used for testing purposes - Loading Cookbooks attributes'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.9'
