6df34dcbbe5ebfac9012ed73beb72dbf927e6014
jesusjimsa <jesusjimsa@icloud.com>
2019-12-16 15:11:02 +0100

    Adapt to PEP 8

--- test_wazuh/test_fim/test_ignore/test_ignore_works_over_restrict.py
+++ test_wazuh/test_fim/test_ignore/test_ignore_works_over_restrict.py
@@ -3,20 +3,18 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 import os
-from datetime import timedelta
 
 import pytest
 import sys
-from wazuh_testing.fim import LOG_FILE_PATH, callback_detect_event, callback_ignore, create_file, \
+from wazuh_testing.fim import LOG_FILE_PATH, callback_ignore, create_file, \
                               REGULAR, generate_params, check_time_travel, DEFAULT_TIMEOUT
-from wazuh_testing.tools import FileMonitor, check_apply_test, load_wazuh_configurations, TimeMachine, \
-                                PREFIX
+from wazuh_testing.tools import FileMonitor, check_apply_test, load_wazuh_configurations, PREFIX
 
 # Variables
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-configurations_path = os.path.join(test_data_path,
-                                    'wazuh_conf_ignore_restrict_win32.yaml' if sys.platform == 'win32' else 'wazuh_conf_ignore_restrict.yaml')
+configurations_path = os.path.join(test_data_path, 'wazuh_conf_ignore_restrict_win32.yaml' if sys.platform == 'win32'
+                                   else 'wazuh_conf_ignore_restrict.yaml')
 
 
 test_directories = [os.path.join(PREFIX, 'testdir1')]
@@ -32,6 +30,7 @@ conf_params, conf_metadata = generate_params()
 configurations = load_wazuh_configurations(configurations_path, __name__, params=conf_params, metadata=conf_metadata)
 
 # Fixtures
+
 
 @pytest.fixture(scope='module', params=configurations)
 def get_configuration(request):
