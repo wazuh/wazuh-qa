ea93d09d29726b5726b5ee3af1fa3102fab6241c
Selutario <selutario@gmail.com>
2019-12-17 11:37:20 +0100

    Simplified YAML file. Priority values added from test.

--- test_wazuh/test_fim/test_process_priority/test_process_priority.py
+++ test_wazuh/test_fim/test_process_priority/test_process_priority.py
@@ -4,11 +4,12 @@
 
 import os
 import psutil
+from copy import deepcopy
 
 import pytest
 
-from wazuh_testing.fim import (LOG_FILE_PATH, generate_params)
-from wazuh_testing.tools import (FileMonitor, check_apply_test, load_wazuh_configurations, PREFIX)
+from wazuh_testing.fim import (generate_params)
+from wazuh_testing.tools import (check_apply_test, load_wazuh_configurations)
 
 
 # variables
@@ -21,8 +22,19 @@ test_directories = []
 
 # configurations
 
-conf_params, conf_metadata = generate_params(modes=monitoring_modes)
-configurations = load_wazuh_configurations(configurations_path, __name__, params=conf_params, metadata=conf_metadata)
+priority_list = ['0', '4', '-5']
+
+p, m = generate_params(modes=monitoring_modes)
+
+params, metadata = list(), list()
+for priority in priority_list:
+    for p_dict, m_dict in zip(p, m):
+        p_dict['PROCESS_PRIORITY'] = priority
+        m_dict['process_priority'] = priority
+        params.append(deepcopy(p_dict))
+        metadata.append(deepcopy(m_dict))
+
+configurations = load_wazuh_configurations(configurations_path, __name__, params=params, metadata=metadata)
 
 # fixtures
 
@@ -31,21 +43,18 @@ def get_configuration(request):
     """Get configurations from the module."""
     return request.param
 
+
 # tests
 
-@pytest.mark.parametrize('priority, tags_to_apply', [
-    (0, {'ossec_conf_1'}),
-    (4, {'ossec_conf_2'}),
-    (-5, {'ossec_conf_3'})
+@pytest.mark.parametrize('tags_to_apply', [
+    ({'ossec_conf'})
 ])
-def test_process_priority(priority, tags_to_apply, get_configuration,
+def test_process_priority(tags_to_apply, get_configuration,
                           configure_environment, restart_syscheckd,
                           wait_for_initial_scan):
+    """Check if the ossec-syscheckd service priority is updated correctly using
+       <process_priority> tag in ossec.conf.
     """
-    Check if the ossec-syscheckd service priority is updated correctly using
-    <process_priority> tag in ossec.conf.
-    """
-
     def get_process(search_name):
         """ Search process by its name """
         for proc in psutil.process_iter():
@@ -55,6 +64,8 @@ def test_process_priority(priority, tags_to_apply, get_configuration,
         return None
 
     check_apply_test(tags_to_apply, get_configuration['tags'])
+
+    priority = int(get_configuration['metadata']['process_priority'])
     process_name = 'ossec-syscheckd'
     syscheckd_process = get_process(process_name)
 
