e962ee794c37942aa508813869e9c1a62637c238
0xN3utr0n <0xN3utr0n@pm.me>
2019-12-19 14:50:02 +0100

    The third case will now use folder testdir3 rather than testdir1

--- test_wazuh/test_fim/test_basic_usage/test_basic_usage_move_dir.py
+++ test_wazuh/test_fim/test_basic_usage/test_basic_usage_move_dir.py
@@ -4,6 +4,7 @@
 
 import os
 import shutil
+import time
 import pytest
 
 from wazuh_testing.fim import LOG_FILE_PATH, generate_params, create_file, REGULAR, \
@@ -15,18 +16,17 @@ pytestmark = [pytest.mark.linux, pytest.mark.win32, pytest.mark.darwin]
 # variables
 
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
-test_directories = [os.path.join(PREFIX, 'testdir1'), os.path.join(PREFIX, 'testdir2')]
+test_directories = [os.path.join(PREFIX, 'testdir1'), os.path.join(PREFIX, 'testdir2'), os.path.join(PREFIX, 'testdir3')]
 directory_str = ','.join(test_directories)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
-testdir1, testdir2 = test_directories
-subdir = "subdir"
+testdir1, testdir2, testdir3 = test_directories
 
 # configurations
 
 conf_params = {'TEST_DIRECTORIES': directory_str, 'MODULE_NAME': __name__}
 conf_metadata = {'test_directories': directory_str, 'module_name': __name__}
-p, m = generate_params(conf_params, conf_metadata, modes=["scheduled"])
+p, m = generate_params(conf_params, conf_metadata)
 configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
 
 
@@ -40,16 +40,17 @@ def get_configuration(request):
 
 # tests
 
+def extra_configuration_before_yield():
+    create_file(REGULAR, os.path.join(testdir1, 'subdir'), 'regular1', content='')
+    create_file(REGULAR, os.path.join(PREFIX, 'subdir'), 'regular1', content='')
+    create_file(REGULAR, os.path.join(testdir3, 'subdir2'), 'regular2', content='')
 
-@pytest.mark.parametrize('file, file_content, tags_to_apply', [
-    ('regular1', '', {'ossec_conf'})
+@pytest.mark.parametrize('source_folder, target_folder, subdir, tags_to_apply, triggers_delete_event, triggers_add_event', [
+    (PREFIX, testdir2, 'subdir', {'ossec_conf'}, False, True),
+    (testdir1, PREFIX, 'subdir', {'ossec_conf'}, True, False),
+    (testdir3, testdir2, 'subdir2', {'ossec_conf'}, True, True),
 ])
-@pytest.mark.parametrize('source_folder, target_folder, triggers_delete_event, triggers_add_event', [
-    (PREFIX, testdir2, False, True),
-    (testdir1, PREFIX, True, False),
-    (testdir1, testdir2, True, True),
-])
-def test_move_file(file, file_content, tags_to_apply, source_folder, target_folder,
+def test_move_file(source_folder, target_folder, subdir, tags_to_apply,
                    triggers_delete_event, triggers_add_event,
                    get_configuration, configure_environment,
                    restart_syscheckd, wait_for_initial_scan):
@@ -65,10 +66,6 @@ def test_move_file(file, file_content, tags_to_apply, source_folder, target_fold
 
     check_apply_test(tags_to_apply, get_configuration['tags'])
     scheduled = get_configuration['metadata']['fim_mode'] == 'scheduled'
-
-    # Create file inside folder
-    create_file(REGULAR, os.path.join(source_folder, subdir), file, content=file_content)
-    shutil.rmtree(os.path.join(target_folder, subdir), ignore_errors=True)
     check_time_travel(scheduled)
 
     # Move file to target directory
