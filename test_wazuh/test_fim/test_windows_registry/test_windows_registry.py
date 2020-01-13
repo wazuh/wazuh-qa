f4057c24448e7d406381316503ebe7388e825851
Selutario <selutario@gmail.com>
2020-01-13 12:28:23 +0100

    Reduce config file, test parameter 'arch: both'

--- test_wazuh/test_fim/test_windows_registry/test_windows_registry.py
+++ test_wazuh/test_fim/test_windows_registry/test_windows_registry.py
@@ -31,20 +31,15 @@ frequency = 4
 
 # Configurations
 
-conf_params = {'WINDOWS_REGISTRY': registry,
-               'FREQUENCY': frequency}
-conf_metadata = {'WINDOWS_REGISTRY'.lower(): registry,
-                 'FREQUENCY'.lower(): frequency}
+conf_params = {'WINDOWS_REGISTRY': registry, 'FREQUENCY': frequency}
 configurations_path = os.path.join(test_data_path, 'wazuh_conf_no_attr.yaml')
-p, m = generate_params(extra_params=conf_params, extra_metadata=conf_metadata, modes=monitoring_modes)
+p, m = generate_params(extra_params=conf_params, modes=monitoring_modes)
 configurations1 = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
 
-conf_params.update({'ARCH_ATTRIBUTE': {'arch': '64bit'},
-                    'TAG_ATTRIBUTE': {'tags': 'test_tag'}})
-conf_metadata.update({'ARCH_ATTRIBUTE'.lower(): {'arch': '64bit'},
-                     'TAG_ATTRIBUTE'.lower(): {'tags': 'test_tag'}})
+attributes = [{'arch': 'both'}, {'tags': 'test_tag'}]
 configurations_path = os.path.join(test_data_path, 'wazuh_conf_attr.yaml')
-p, m = generate_params(extra_params=conf_params, extra_metadata=conf_metadata, modes=monitoring_modes)
+p, m = generate_params(extra_params=conf_params, apply_to_all=({'ATTRIBUTE': attr} for attr in attributes),
+                       modes=monitoring_modes)
 configurations2 = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
 
 configurations = configurations1 + configurations2
@@ -63,17 +58,17 @@ def get_configuration(request):
 def extra_configuration_before_yield():
     # It makes sure to delete the registry if it already exists.
     try:
-        delete_registry(winreg.HKEY_LOCAL_MACHINE, sub_key)
+        delete_registry(winreg.HKEY_LOCAL_MACHINE, sub_key, 32)
     except OSError:
         pass
 
 
-@pytest.mark.parametrize('arch, tag, tags_to_apply', [
-    (None, None, {'ossec_conf'}),
-    ("64", None, {'ossec_conf_arch64'}),
-    (None, "test_tag", {'ossec_conf_tag'})
+@pytest.mark.parametrize('arch_list, tag, tags_to_apply', [
+    (['32'], None, {'ossec_conf'}),
+    (['32', '64'], None, {'ossec_conf_2'}),
+    (['32'], "test_tag", {'ossec_conf_2'})
 ])
-def test_windows_registry(arch, tag, tags_to_apply,
+def test_windows_registry(arch_list, tag, tags_to_apply,
                           get_configuration, configure_environment, restart_syscheckd, wait_for_initial_scan):
     """Check the correct monitoring of Windows Registries.
 
@@ -86,40 +81,57 @@ def test_windows_registry(arch, tag, tags_to_apply,
 
     Parameters
     ----------
-    arch : str
-        Selected architecture.
+    arch_list : list
+        Selected architectures.
     tag : str
         Name of the tag to look for in the event.
     tags_to_apply : set
-         Run test if matches with a configuration identifier, skip otherwise.
+         Run test if matches with a configuration identifier, skip otherwise.º
     """
     check_apply_test(tags_to_apply, get_configuration['tags'])
 
+    # Check that configuration is applying to the correct test
+    if (tag and 'tags' not in get_configuration['metadata']['attribute'].keys() or
+            len(arch_list) > 1 and 'arch' not in get_configuration['metadata']['attribute'].keys()):
+        pytest.skip("Does not apply to this config file")
+
     # Check that windows_registry does not trigger alerts for new entries and empty keys
-    create_registry(winreg.HKEY_LOCAL_MACHINE, sub_key)
+    create_registry(winreg.HKEY_LOCAL_MACHINE, sub_key, 32)
     TimeMachine.travel_to_future(timedelta(seconds=frequency))
     with pytest.raises(TimeoutError):
-        wazuh_log_monitor.start(timeout=DEFAULT_TIMEOUT, callback=callback_detect_event)
+        wazuh_log_monitor.start(timeout=DEFAULT_TIMEOUT, callback=callback_detect_event, accum_results=len(arch_list))
 
     # Check that windows_registry trigger alerts when adding an entry
     modify_registry(winreg.HKEY_LOCAL_MACHINE, sub_key, 'test_add')
     TimeMachine.travel_to_future(timedelta(seconds=frequency))
-    event = wazuh_log_monitor.start(timeout=DEFAULT_TIMEOUT, callback=callback_detect_event).result()
-    assert event['data']['type'] == 'added', f'Event type not equal'
+    event = wazuh_log_monitor.start(timeout=DEFAULT_TIMEOUT, callback=callback_detect_event,
+                                    accum_results=len(arch_list)).result()
+    if not isinstance(event, list):
+        event = [event]
+    for i, arch in enumerate(arch_list):
+        assert event[i]['data']['type'] == 'added', f'Event type not equal'
 
     # Check that windows_registry trigger alerts when modifying existing keys
     # and check arch and tag values match with the ones in event
     modify_registry(winreg.HKEY_LOCAL_MACHINE, sub_key, 'test_modify')
     TimeMachine.travel_to_future(timedelta(seconds=frequency))
-    event = wazuh_log_monitor.start(timeout=5, callback=callback_detect_event).result()
-    assert event['data']['type'] == 'modified', f'Event type not equal'
-    if arch:
-        assert arch in event['data']['path'], f'Architecture is not correct'
-    if tag:
-        assert event['data']['tags'] == tag, f'{tag} not found in event'
+    event = wazuh_log_monitor.start(timeout=DEFAULT_TIMEOUT, callback=callback_detect_event,
+                                    accum_results=len(arch_list)).result()
+    if not isinstance(event, list):
+        event = [event]
+    for i, arch in enumerate(arch_list):
+        assert event[i]['data']['type'] == 'modified', f'Event type not equal'
+        if arch:
+            assert arch in event[i]['data']['path'], f'Architecture is not correct'
+        if tag:
+            assert event[i]['data']['tags'] == tag, f'{tag} not found in event'
 
     # Check that windows_registry trigger alerts when deleting a key
-    delete_registry(winreg.HKEY_LOCAL_MACHINE, sub_key)
+    delete_registry(winreg.HKEY_LOCAL_MACHINE, sub_key, 32)
     TimeMachine.travel_to_future(timedelta(seconds=frequency))
-    event = wazuh_log_monitor.start(timeout=5, callback=callback_detect_event).result()
-    assert event['data']['type'] == 'deleted', f'Event type not equal'
+    event = wazuh_log_monitor.start(timeout=DEFAULT_TIMEOUT, callback=callback_detect_event,
+                                    accum_results=len(arch_list)).result()
+    if not isinstance(event, list):
+        event = [event]
+    for i, arch in enumerate(arch_list):
+        assert event[i]['data']['type'] == 'deleted', f'Event type not equal'
