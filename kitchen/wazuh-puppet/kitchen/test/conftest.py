4a5fcc8866ff549c9557c7e7f03cd3c03d29d8b9
Rshad Zhran <rashzk95@gmail.com>
2020-01-21 13:52:57 +0100

    Remove subprocess import

--- kitchen/wazuh-puppet/kitchen/test/conftest.py
+++ kitchen/wazuh-puppet/kitchen/test/conftest.py
@@ -2,7 +2,6 @@ import functools
 import os
 import pytest
 import testinfra
-import subprocess
  
 test_host = testinfra.get_host('paramiko://{KITCHEN_USERNAME}@{KITCHEN_HOSTNAME}:{KITCHEN_PORT}'.format(**os.environ), ssh_identity_file=os.environ.get('KITCHEN_SSH_KEY'))
  
