8a5877c073cfc99ac5cda2e0e72badf84a140b65
Rshad Zhran <rashzk95@gmail.com>
2020-01-31 11:23:56 +0100

    Adapt run.sh to run the tests with ENV vars.

--- kitchen/wazuh-puppet/kitchen/run.sh
+++ kitchen/wazuh-puppet/kitchen/run.sh
@@ -13,16 +13,25 @@ echo "Kitchen is creating the new instances"
 kitchen create # creating new kitchen instances
 
 echo "Getting Wazuh managers IPs to the agents"
-manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep $PLATFORM | grep manager`)"
+manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep $PLATFORM | grep $RELEASE | grep manager`)"
 
 echo "getting a copy of ./manifests/site.pp.template"
 cp ./manifests/site.pp.template ./manifests/site.pp
+
+echo "PRINT MANAGER IP"
+echo $manager_ip
+
+echo "PRINT MANIFEST SITE.PP"
+cat ./manifests/site.pp
 
 echo "Assigning Wazuh managers IPs to the corresponding agents."
 sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
 
 echo "Setting the platform in the components names."
 sed -i 's/platform/'$PLATFORM'/g' ./manifests/site.pp
+
+echo "Setting the rlease in the components names."
+sed -i 's/release/'$RELEASE'/g' ./manifests/site.pp
 
 if [[ $PLATFORM == *"centos"* ]] || [[ $PLATFORM == *"amazon"* ]]; then
    echo "suite is a Centos one and requires OpenSSL to be installed. .. Installing .."
