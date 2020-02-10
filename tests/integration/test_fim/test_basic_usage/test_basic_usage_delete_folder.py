65acd2871b070b40bc43abedf54e39dfe17058c5
José Luis López Sánchez <selutario@gmail.com>
2020-02-10 16:39:23 +0100

    Replace list with set (more efficient)
    Co-Authored-By: Daniel Ruiz Capilla <daniel.ruiz@wazuh.com>

--- tests/integration/test_fim/test_basic_usage/test_basic_usage_delete_folder.py
+++ tests/integration/test_fim/test_basic_usage/test_basic_usage_delete_folder.py
@@ -88,7 +88,7 @@ def test_delete_folder(folder, file_list, filetype, tags_to_apply,
     # Expect deleted events
     event_list = wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=callback_detect_event,
                                     accum_results=len(file_list)).result()
-    path_list = [event['data']['path'] for event in event_list]
+    path_list = set([event['data']['path'] for event in event_list])
     counter_type = Counter([event['data']['type'] for event in event_list])
 
     assert counter_type['deleted'] == len(file_list), f'Number of "deleted" events should be {len(file_list)}'
