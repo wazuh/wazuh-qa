8ced13d78c9e25314603663c3b27feda0eb3f3cb
Víctor Fernández Poyatos <vicferpoy@gmail.com>
2020-02-12 10:14:01 +0100

    Add socket responses tests for analysisd

--- tests/integration/test_analysisd/test_all_syscheckd_events/test_check_socket_responses.py
+++ tests/integration/test_analysisd/test_all_syscheckd_events/test_check_socket_responses.py
@@ -7,7 +7,7 @@ import pytest
 import yaml
 
 from wazuh_testing import global_parameters
-from wazuh_testing.analysis import callback_analysisd_message
+from wazuh_testing.analysis import callback_analysisd_message, callback_wazuh_db_message
 from wazuh_testing.tools import WAZUH_PATH, LOG_FILE_PATH, WAZUH_LOGS_PATH
 from wazuh_testing.tools.monitoring import FileMonitor
 
@@ -24,9 +24,11 @@ wdb_path = os.path.join(os.path.join(WAZUH_PATH, 'queue', 'db', 'wdb'))
 analysis_path = os.path.join(os.path.join(WAZUH_PATH, 'queue', 'ossec', 'queue'))
 monitored_sockets, receiver_sockets = None, None  # These variables will be set in the fixture create_unix_sockets
 monitored_sockets_params = [(wdb_path, 'TCP')]
-# monitored_sockets_params = []
 receiver_sockets_params = [(analysis_path, 'UDP')]
 used_daemons = ['ossec-analysisd']
+socket_path = analysis_path
+analysis_monitor = None
+wdb_monitor = None
 
 with open(messages_path) as f:
     test_cases = yaml.safe_load(f)
@@ -41,13 +43,21 @@ testdir = 'testdir'
 @pytest.mark.parametrize('test_case',
                          [test_case['test_case'] for test_case in test_cases],
                          ids=[test_case['name'] for test_case in test_cases])
-def test_validate_socket_responses(configure_environment_standalone_daemons, create_unix_sockets,
+def test_validate_socket_responses(configure_mitm_environment_analysisd, create_unix_sockets,
                                    wait_for_analysisd_startup, test_case: list):
-    """
+    """Validate every response from the analysisd socket to the wazuh-db socket.
+
+    This test will catch every response from analysisd to wazuh-db in real-time using the yaml
+    `/data/syscheck_events.yaml`.
+
+    Parameters
+    ----------
+    test_case : dict
+        Dict with the input to inject to the analysisd socket and output to expect to be sent to the wazuh-db socket.
     """
     for stage in test_case:
         expected = callback_analysisd_message(stage['output'])
         receiver_sockets[0].send([stage['input']])
-        response = monitored_sockets[0].start(timeout=global_parameters.default_timeout,
-                                              callback=callback_analysisd_message).result()
+        response = wdb_monitor.start(timeout=global_parameters.default_timeout,
+                                     callback=callback_wazuh_db_message).result()
         assert response == expected, 'Failed test case stage {}: {}'.format(test_case.index(stage) + 1, stage['stage'])
