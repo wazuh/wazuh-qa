08cac298d8c4d3f8f28b1c47b93363615299a053
Manuel Gutierrez <manuel.gutierrez@wazuh.com>
2020-02-12 14:38:56 +0100

    Remove duplicated provisioning

--- tests/system/fim/provisioning/roles/aws/tests/ssh.py
+++
@@ -1,4 +0,0 @@
-def test_custom_key_exists(host, user, ssh_key):
-    authorized_keys_file = host.file("/home/" + user + "/.ssh/authorized_keys")
-    assert authorized_keys_file.exists
-    assert (ssh_key in authorized_keys_file.content_string)
