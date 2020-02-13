91586cb8deaece4aca606dc4ebac64a026ba603d
Manuel Gutierrez <manuel.gutierrez@wazuh.com>
2020-02-13 15:07:39 +0100

    Fix pep8

--- tests/system/fim/common_tasks/files_modify.py
+++ tests/system/fim/common_tasks/files_modify.py
@@ -8,13 +8,10 @@
 # License (version 2) as published by the FSF - Free Software
 # Foundation.
 
-import os
 import sys
-import platform
 import random
-if platform.system() == "Linux":
-    import pwd
-    import grp
+
+
 if sys.version_info.major < 3:
     print('ERROR: Python 2 is not supported.')
     sys.exit(1)
@@ -25,52 +22,32 @@ def random_mode():
     Returns a random file permission
 
     File permission in unix use octal format, but os.chmod expects a decimal.
-    Numbers 0 and 511 are the min and max (decimal) numbers which chmod accepts,
+    Numbers 0 and 511 are the min and max (decimal) numbers accepted,
     being 0 equivalent to 000 and 511 equivalent to 777 in octal.
     """
     return random.randint(0, 511)
+
 
 def modify_file_content(filepath):
     """
         Modify file content by adding a random number of bytes
     """
-    content = 'qazxswedcvbnmklpoiuytggdfert'*random.randint(1,10)
+    content = 'qazxswedcvbnmklpoiuytggdfert'*random.randint(1, 10)
     content += str(random.random())
     with open(filepath, 'ab') as f:
         f.write(bytes(content, 'utf8'))
 
 
-def modify_file(filepath, owner, group, mode):
-    """
-        Modify a file owner, group and permissions.
-
-        :param str filepath: Full path of the file
-        :param str owner: File owner
-        :param str group: File group
-        :param int mode: File permissions in decimal format
-        :return: Returns a dictionary with the change metadata
-    """
-    uid = pwd.getpwnam(owner).pw_uid
-    gid = grp.getgrnam(group).gr_gid
-    os.chown(filepath, uid, gid)
-    os.chmod(filepath, mode)
-    return {
-        'path': filepath,
-        'uid': uid,
-        'gid': gid,
-        'mode': oct(mode).split('o')[1].zfill(3) # convert to octal string
-        }
-
-
 def main():
     import argparse
-    parser =  argparse.ArgumentParser()
+    parser = argparse.ArgumentParser()
     parser.add_argument('filelist', action="store")
     args = parser.parse_args()
     filelist = args.filelist
     with open(filelist) as flist:
         for path in flist:
             modify_file_content(path[:-1])
+
 
 if __name__ == "__main__":
     main()
