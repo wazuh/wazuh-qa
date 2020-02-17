1569bb981e790c02f239be0264dd3f6d7c153e94
Selutario <joseluis.lopez@wazuh.com>
2020-02-17 16:30:21 +0100

    Run diff/fc command and check lenght.

--- tests/integration/test_fim/test_report_changes/test_large_changes.py
+++ tests/integration/test_fim/test_report_changes/test_large_changes.py
@@ -4,15 +4,16 @@
 
 import os
 import sys
-import random
-import string
+import subprocess
+import gzip
+import shutil
 
 import pytest
 
-from wazuh_testing.fim import LOG_FILE_PATH, callback_detect_event, REGULAR, create_file, delete_file, \
+from wazuh_testing.fim import LOG_FILE_PATH, callback_detect_event, REGULAR, create_file, \
     generate_params, check_time_travel
 from wazuh_testing import global_parameters
-from wazuh_testing.tools import PREFIX
+from wazuh_testing.tools import PREFIX, WAZUH_PATH
 from wazuh_testing.tools.monitoring import FileMonitor
 from wazuh_testing.tools.configuration import check_apply_test, load_wazuh_configurations
 
@@ -28,6 +29,7 @@ test_directories = [os.path.join(PREFIX, 'testdir')]
 nodiff_file = os.path.join(PREFIX, 'testdir_nodiff', 'regular_file')
 directory_str = ','.join(test_directories)
 testdir = test_directories[0]
+unzip_diff_dir = os.path.join(PREFIX, 'unzip_diff')
 
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -79,6 +81,15 @@ def generateString(stringLength=10, character='0'):
     return random_str
 
 
+def extra_configuration_before_yield():
+    """Create a folder to store diff files unzipped"""
+    os.makedirs(unzip_diff_dir, exist_ok=True)
+
+
+def extra_configuration_after_yield():
+    """Delete the folder after the test"""
+    shutil.rmtree(unzip_diff_dir, ignore_errors=True)
+
 # Tests
 
 @pytest.mark.parametrize('tags_to_apply', [
@@ -94,11 +105,11 @@ def generateString(stringLength=10, character='0'):
     ('regular_6', testdir, 70000, 10),
 ])
 def test_large_changes(filename, folder, original_size, modified_size, tags_to_apply, get_configuration,
-                            configure_environment, restart_syscheckd, wait_for_initial_scan):
+                       configure_environment, restart_syscheckd, wait_for_initial_scan):
     """Check content_changes shows the tag 'More changes' when it exceeds the maximum size.
 
     Every change in the content of the file shall produce an alert including
-    the difference. But, if the difference is greater or equal than 59370 bytes, 'More changes'
+    the difference. But, if the difference is greater or equal than 59391 bytes, 'More changes'
     appears instead.
 
     Parameters
@@ -114,9 +125,20 @@ def test_large_changes(filename, folder, original_size, modified_size, tags_to_a
     tags_to_apply : set
         Run test if matches with a configuration identifier, skip otherwise.
     """
+
+    limit = 59391
+    output_size = 0
+    original_file = os.path.join(folder, filename)
+    unzip_diff_file = os.path.join(unzip_diff_dir, filename + '-old')
+    diff_file_path = os.path.join(WAZUH_PATH, 'queue', 'diff', 'local')
+    if sys.platform == 'win32':
+        diff_file_path = os.path.join(diff_file_path, 'c')
+        diff_file_path = os.path.join(diff_file_path, folder.strip('c:\\'), filename, 'last-entry.gz')
+    else:
+        diff_file_path = os.path.join(diff_file_path, folder.strip('/'), filename, 'last-entry.gz')
+
     check_apply_test(tags_to_apply, get_configuration['tags'])
     fim_mode = get_configuration['metadata']['fim_mode']
-    limit = 58000
 
     # Create the file and and capture the event.
     original_string = generateString(original_size, '0')
@@ -124,11 +146,33 @@ def test_large_changes(filename, folder, original_size, modified_size, tags_to_a
     check_time_travel(fim_mode == 'scheduled')
     wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=callback_detect_event).result()
 
+    # Store uncompressed diff file in backup folder
+    with gzip.open(diff_file_path, 'rb') as f_in:
+        with open(unzip_diff_file, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
     # Modify the file with new content
     modified_string = generateString(modified_size, '1')
     create_file(REGULAR, folder, filename, content=modified_string)
     check_time_travel(fim_mode == 'scheduled')
     event = wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=callback_detect_event).result()
+
+    # Run the diff/fc command and get the output length
+    try:
+        if sys.platform == 'win32':
+            subprocess.check_output(['fc', '/n', original_file, unzip_diff_file])
+        else:
+            subprocess.check_output(['diff', original_file, unzip_diff_file])
+    except subprocess.CalledProcessError as e:
+        # Inputs are different
+        if e.returncode == 1:
+            output_size = len(e.output)
+
+    # Assert 'More changes' is shown when the command returns more than 'limit' characters
+    if output_size > limit:
+        assert 'More changes' in event['data']['content_changes'], '"More changes" not found within content_changes.'
+    else:
+        assert 'More changes' not in event['data']['content_changes'], '"More changes" found within content_changes.'
 
     # Assert old content is shown in content_changes
     assert '0' in event['data']['content_changes'], '"0" is the old value but it is not found within content_changes'
@@ -137,7 +181,3 @@ def test_large_changes(filename, folder, original_size, modified_size, tags_to_a
     if original_size < limit or sys.platform == 'win32':
         assert '1' in event['data']['content_changes'], '"1" is the new value but it is not found ' \
                                                         'within content_changes'
-
-    # Assert 'More changes' is shown when the sum of old and new content is greater than the limit.
-    if (original_size + modified_size) >= limit:
-        assert 'More changes' in event['data']['content_changes'], '"More changes" not found within content_changes.'
