3a1df24e22f62ab9679744cbbba4c4054bac4171
Adrián Jesús Peña Rodríguez <adrianjpr@gmail.com>
2020-02-19 17:49:50 +0100

    Add enum class

--- tests/integration/test_fim/test_stats_integrity_sync/test_FIM_performance.py
+++ tests/integration/test_fim/test_stats_integrity_sync/test_FIM_performance.py
@@ -8,6 +8,7 @@ import shutil
 import subprocess
 import time
 from multiprocessing import Process
+from enum import Enum
 
 import pandas as pd
 import pytest
@@ -20,6 +21,12 @@ from wazuh_testing.tools.services import control_service, check_daemon_status
 root_dir = '/test'
 tested_daemon = 'ossec-syscheckd'
 performance_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'stats', 'performance')
+
+
+class Types(Enum):
+    added = 'added'
+    modified = 'modified'
+    deleted = 'deleted'
 
 
 # Fixtures
@@ -196,22 +203,13 @@ def create_long_path(length, path_name):
     return path_name
 
 
-def tail_ossec_fim_print_info():
-    regex_tail = r".*during: ([0-9]+\.[0-9]+) sec"
-    cat_ossec = subprocess.Popen(["cat", LOG_FILE_PATH], stdout=subprocess.PIPE)
-    grep_ossec = subprocess.Popen(["grep", "fim_print_info"], stdin=cat_ossec.stdout, stdout=subprocess.PIPE)
-    head_ossec = subprocess.check_output(["head", "-n1"], stdin=grep_ossec.stdout).decode().strip()
-
-    return 0.0 if head_ossec == "" else float(re.match(regex_tail, head_ossec).group(1))
-
-
-def tail_ossec_fim_sync_checksum():
-    regex_tail = r".*Time: ([0-9]+\.[0-9]+) seconds."
-    cat_ossec = subprocess.Popen(["cat", LOG_FILE_PATH], stdout=subprocess.PIPE)
-    grep_ossec = subprocess.check_output(["grep", "Finished calculating FIM integrity"], stdin=cat_ossec.stdout
-                                         ).decode().strip()
-
-    return False if grep_ossec == "" else float(re.match(regex_tail, grep_ossec).group(1))
+def find_ossec_log(regex=None, strlog=None):
+    try:
+        head = subprocess.Popen(["cat", LOG_FILE_PATH], stdout=subprocess.PIPE)
+        grep = subprocess.check_output(["grep", strlog], stdin=head.stdout).decode().strip()
+        return re.search(regex, grep).group(1)
+    except (AttributeError, subprocess.CalledProcessError):
+        return None
 
 
 def create_n_files(path_name, num_files=1000, file_size=1024):
@@ -257,8 +255,8 @@ def scan_test(scan_df, length, n_files, file_size):
     while True:
         stats = calculate_stats(daemon=tested_daemon, **previous_stats)
         logger.info(stats)
-        time_fim = tail_ossec_fim_print_info()
-        if time_fim > 0:
+        time_fim = find_ossec_log(r".*during: ([0-9]+\.[0-9]+) sec", "fim_print_info")
+        if time_fim:
             scan_df.loc[len(scan_df)] = [str(time_printing), *list(stats.values()), time_fim, 'scan']
             break
 
@@ -281,7 +279,7 @@ def integrity_test(data_df, integrity_df, length, n_files, file_size):
         stats = calculate_stats(daemon=tested_daemon, **previous_stats)
         logger.info(stats)
 
-        time_integrity = tail_ossec_fim_sync_checksum()
+        time_integrity = find_ossec_log(r".*Time: ([0-9]+\.[0-9]+) seconds.", "Finished calculating FIM integrity")
         if time_integrity is not False:
             integrity_df.loc[len(integrity_df)] = [str(n_files), str(length), str(file_size), str(time_integrity)]
             break
@@ -294,16 +292,50 @@ def integrity_test(data_df, integrity_df, length, n_files, file_size):
         time.sleep(1)
 
 
-def real_test(test_name, real_df, length, n_files, file_size):
+def process_manager(test_name, path_name, n_files, file_size=0):
+    if test_name == Types.added:
+        if find_ossec_log(r".*Directory added for real time monitoring: '(/test/real)'", "realtime_adddir") != None:
+            process = Process(target=create_n_files, args=(path_name, n_files, file_size,))
+        else:
+            return
+    elif test_name == Types.modified:
+        process = Process(target=modify_n_files, args=(path_name, n_files,))
+    elif test_name == Types.deleted:
+        process = Process(target=delete_n_files, args=(path_name, n_files,))
+    else:
+        raise AttributeError(f'Invalid type detected: {test_name}')
+
+    process.start()
+    return process
+
+
+def time_manager(events, last_count, time_start, time_out, n_files):
+    count = int(events)
+    if count - last_count > 0:
+        time_out = 5
+    else:
+        time_out -= 1
+    last_count = count
+
+    time_fim = 0
+    if count >= n_files and time_out == 0:
+        time_finish = time.time()
+        time_fim = time_finish - time_start
+
+    return count, last_count, time_fim, time_out
+
+
+def real_test(test_name, real_df, length, n_files, file_size=0):
     started = False
     process, grep_name = None, None
     time_printing, time_start, time_fim, last_count = 0, 0, 0, 0
     time_out = 5
-
-    logger.info(
-        f"[REAL] Test {test_name} with {str(length)} path length, {str(n_files)} files and {str(file_size)} KB file "
-        f"size")
+    grep_name = f'"mode":"real-time","type":"{test_name.value}"'
     previous_stats = get_stats(daemon=tested_daemon)
+    logger.info(
+        f"[REAL] Test {test_name.value} with {str(length)} path length, {str(n_files)} files and {str(file_size)} KB "
+        f"file size")
+
     while True:
         stats = calculate_stats(daemon=tested_daemon, **previous_stats)
         logger.info(stats)
@@ -312,48 +344,30 @@ def real_test(test_name, real_df, length, n_files, file_size):
             started = True
             path_name = create_long_path(length, "real")
             time_start = time.time()
-            if test_name == "Insert":
-                grep_name = '"mode":"real-time","type":"added"'
-                process = Process(target=create_n_files, args=(path_name, n_files, file_size,))
-                process.start()
-            elif test_name == "Update":
-                grep_name = '"mode":"real-time","type":"modified"'
-                process = Process(target=modify_n_files, args=(path_name, n_files,))
-                process.start()
-            elif test_name == "Delete":
-                grep_name = '"mode":"real-time","type":"deleted"'
-                process = Process(target=delete_n_files, args=(path_name, n_files,))
-                process.start()
+            process = process_manager(test_name, path_name, n_files, file_size)
         else:
             head = subprocess.Popen(["cat", LOG_FILE_PATH], stdout=subprocess.PIPE)
             grep = subprocess.Popen(["grep", grep_name], stdin=head.stdout, stdout=subprocess.PIPE)
             events = subprocess.check_output(["wc", "-l"], stdin=grep.stdout).decode().strip()
-
-            count = int(events)
-            if count - last_count > 0:
-                time_out = 5
-            else:
-                time_out -= 1
-            last_count = count
-
-            if count >= n_files and time_out == 0:
-                time_finish = time.time()
-                time_fim = time_finish - time_start
+            count, last_count, time_fim, time_out = time_manager(events=events, last_count=last_count,
+                                                                 time_out=time_out,  time_start=time_start,
+                                                                 n_files=n_files)
 
             if time_out == 0:
                 logger.warning(f"Timeout: Event read {str(count)} last: {str(last_count)}")
                 break
 
-            logger.info(f"[{test_name}] Writing info {str(time_printing)} Events: {str(count)}/{str(n_files)}")
+            logger.info(f"[{test_name.value}] Writing info {str(time_printing)} Events: {str(count)}/{str(n_files)}")
 
         if any(float(stat) != 0 for stat in stats.values()):
-            real_df.loc[len(real_df)] = [str(time_printing), *list(stats.values()), time_fim, test_name]
+            real_df.loc[len(real_df)] = [str(time_printing), *list(stats.values()), time_fim, test_name.value]
 
         previous_stats = get_stats(daemon=tested_daemon)
         time_printing += 1
         time.sleep(1)
     logger.info(f'[REAL] Time FIM: {time_fim}')
-    process.join()
+    if process:
+        process.join()
 
 
 @pytest.mark.parametrize('number_files', [
@@ -406,11 +420,11 @@ def test_performance(mode, file_size, path_length, number_files, initial_clean, 
 
     # Test real-time (added, modified, deleted)
     truncate_file(LOG_FILE_PATH)
-    real_test('Insert', real_df=data_df, length=path_length, n_files=6000, file_size=file_size)
+    real_test(Types.added, real_df=data_df, length=path_length, n_files=6000, file_size=file_size)
     truncate_file(LOG_FILE_PATH)
-    real_test('Update', real_df=data_df, length=path_length, n_files=6000, file_size=file_size)
+    real_test(Types.modified, real_df=data_df, length=path_length, n_files=6000)
     truncate_file(LOG_FILE_PATH)
-    real_test('Delete', real_df=data_df, length=path_length, n_files=6000, file_size=file_size)
+    real_test(Types.deleted, real_df=data_df, length=path_length, n_files=6000)
     data_df.to_csv(data_filename, index=False)
 
     # Clean environment
