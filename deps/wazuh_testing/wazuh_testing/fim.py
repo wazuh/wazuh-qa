4518c9ad44b9b9f596cc7b45ded9009dbaf2353b
Daniel Ruiz Capilla <daniel.ruiz@wazuh.com>
2020-02-25 16:28:22 +0100

    Update deps/wazuh_testing/wazuh_testing/fim.py

--- deps/wazuh_testing/wazuh_testing/fim.py
+++ deps/wazuh_testing/wazuh_testing/fim.py
@@ -792,7 +792,6 @@ def callback_integrity_message(line):
         match = re.match(r"(\d{4}/\d{2}/\d{2} \d{2}:\d{2}:\d{2}).*({.*?})$", line)
         if match:
             return datetime.strptime(match.group(1), '%Y/%m/%d %H:%M:%S'), json.dumps(match.group(2))
-        return None
 
 
 def callback_event_message(line):
