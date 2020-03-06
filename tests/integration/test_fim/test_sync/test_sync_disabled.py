c33b3acddde0681b0fb2a353b1f768762bd88420
jesusjimsa <jesusjimsa@icloud.com>
2020-03-06 13:32:40 +0100

    Add disabled synchronization test

---
+++ tests/integration/test_fim/test_sync/test_sync_disabled.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2015-2020, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+import os
+
+import pytest
+
+from wazuh_testing import global_parameters
+from wazuh_testing.fim import LOG_FILE_PATH, callback_detect_synchronization, detect_initial_scan, generate_params
+from wazuh_testing.tools import PREFIX
+from wazuh_testing.tools.configuration import load_wazuh_configurations, check_apply_test
+from wazuh_testing.tools.file import truncate_file
+from wazuh_testing.tools.monitoring import FileMonitor
+from wazuh_testing.tools.time import TimeMachine, time_to_timedelta
+
+# Marks
+
+pytestmark = [pytest.mark.tier(level=2)]
+
+# variables
+test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+
+configurations_path = os.path.join(test_data_path, 'wazuh_disabled_sync_conf.yaml')
+test_directories = [os.path.join(PREFIX, 'testdir1')]
+wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+
+# configurations
+
+p, m = generate_params(modes=['scheduled'])
+
+configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
+
+# fixtures
+
+
+@pytest.fixture(scope='module', params=configurations)
+def get_configuration(request):
+    """Get configurations from the module."""
+    return request.param
+
+# Tests
+
+
+def test_sync_disabled(get_configuration, configure_environment, restart_syscheckd, wait_for_initial_scan):
+    """
+    Verify that synchronization is disabled when enabled is set to no in the configuration.
+    """
+    # Check if the test should be skipped
+    check_apply_test({'sync_interval'}, get_configuration['tags'])
+
+    with pytest.raises(TimeoutError):
+        event = wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
+                                        callback=callback_detect_synchronization)
+        raise AttributeError(f'Unexpected event {event}')
