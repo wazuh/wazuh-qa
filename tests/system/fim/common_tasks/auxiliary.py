747076cbacb7a08d063fe78683da371a74b99e7a
Rshad Zhran <rashzk95@gmail.com>
2020-03-13 19:02:01 +0100

    Reformat code into a function & Modify already existing result file

--- tests/system/fim/common_tasks/auxiliary.py
+++ tests/system/fim/common_tasks/auxiliary.py
@@ -1,37 +1,88 @@
 import json
+import os
 
-data = {}
-data['json_verification'] = {
-    'result': '',
-    'scenarios': {}
-}
+def generate_result(scenario, host, action, passed, expected_alerts_num, 
+                    received_alerts_num, missing_paths, output_path, host_os, host_arch):
 
-common_ = {
-    'result': '',
-    'hosts': {},
-}
+    """
+    Generates a JSON file with the related testing info for each scenario 
+    and for each agent host. If the file already exists, then it load it
+    into a dictionary, modify it and re-write it. If not, then it creates it.
 
-scenario_vars = {
-        "result": "",
-        'added': common_,
-        'deleted': common_,
-        'modified': common_
-}
+    :param str scenario: Scenario name.
+    :param str host: Agent target host.
+    :param str action: Action name. 'added', 'deleted' or 'modified'
+    :param str passed: Test passed. 'FAILED' or 'SUCCESS'.
+    :param int expected_alerts_num: Expected number of alerts to be received.
+    :param int received_alerts_num: Number of the received alerts.
+    :param list missing_paths: List of the missing paths.
+    :param str output_path: Path of the output file.
+    :param str host_os: Agent host operating system.
+    :param str host_arch: Agent host architecture.
 
-data['json_verification']['scenarios']['201_default_scenario'] = scenario_vars
+    """
+    # Setting passed variables to True by default.
+    global_passed = True
+    scenario_passed = True
+    action_passed = True
 
-host_vars = {
-        'host_os': '',
-        'host_arch': '',
-        'result': '',
-        'expected_alerts': '',
-        'received_alerts': '',
-        'missing_alerts': '',
-        'missing_paths': [],
-}
+    if not passed: # In case of negative 'passed'
+        global_passed = False
+        scenario_passed = False
+        action_passed = False
 
-data['json_verification']['scenarios']['201_default_scenario']['added']['hosts']['10.0.0.1'] = host_vars
+    data = {} # Initialize data as empty dictionary.
 
+    scenario_vars = {
+            'passed': scenario_passed,
+            action: { 
+                'passed': action_passed,
+                'hosts': {},
+            }
+    }
 
-with open('results.json', 'w') as outfile:
-    json.dump(data, outfile)
+    host_vars = {
+            'host_os': host_os,
+            'host_arch': host_arch,
+            'passed': passed,
+            'expected_alerts': expected_alerts_num,
+            'received_alerts': received_alerts_num,
+            'missing_alerts': (expected_alerts_num - received_alerts_num),
+            'missing_paths': missing_paths,
+    }
+
+    # file_exists registers if output_path exists or not
+    file_exists = os.path.exists(output_path)
+
+    if file_exists: # In case output_path exists, then load it in a dic.
+        with open(output_path) as f:
+            data = json.load(f)
+
+    else: # output_path does not exist.
+        data['json_verification'] = {
+            'passed': global_passed,
+            'scenarios': {}
+        }
+    
+    data['json_verification']['passed'] = global_passed # setting the global 'passed' result
+
+    if scenario in data['json_verification']['scenarios']: # In case 'scenario' already exists
+
+        if not passed:
+            data['json_verification']['scenarios'][scenario]['passed'] = False
+
+        if data['json_verification']['scenarios'][scenario][action]:
+            if not passed:
+                data['json_verification']['scenarios'][scenario][action]['passed'] = False
+        else:
+            data['json_verification']['scenarios'][scenario][action]['passed'] = action_passed
+
+        data['json_verification']['scenarios'][scenario][action]['hosts'][host] = host_vars
+
+    else: # In case the scenario does not exist.
+        data['json_verification']['scenarios'][scenario] = scenario_vars
+        data['json_verification']['scenarios'][scenario][action]['passed'] = action_passed
+        data['json_verification']['scenarios'][scenario][action]['hosts'][host] = host_vars
+        
+    with open(output_path, 'w') as outfile: # save data to 'output_path'
+        json.dump(data, outfile)
