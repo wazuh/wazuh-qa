8b07babf460d0997adf9292b8ab09bd1ec92e434
Manuel Gutierrez <manuel.gutierrez@wazuh.com>
2020-03-18 20:11:48 +0100

    Extract relevant data from ossec.log

---
+++ tests/system/fim/common_tasks/summarize_scenario.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2015-2020, Wazuh Inc.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License (version 2) as published by the FSF - Free Software
+# Foundation.
+
+
+def get_ossec_log_errors(scenario_name, hostname):
+    ossec_path = "/opt/fim_tests_results/{}/agent_state/{}/ossec.log".format(scenario_name, hostname)
+    watch_list = "syscheck warning error syscollector".split()
+    results = []
+    with open(ossec_path, "r") as ossec_log:
+        for line in ossec_log:
+            if any(word in line.lower() for word in watch_list):
+                results.append(line)
+    return results
+
+
+
