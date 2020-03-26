9ebd0e6b40cbb5f9c306f907c13c5ed81e9f3cd1
Manuel Gutierrez <manuel.gutierrez@wazuh.com>
2020-03-26 17:38:49 +0100

    Add function docs

--- tests/system/fim/common_tasks/complete_summarize.py
+++ tests/system/fim/common_tasks/complete_summarize.py
@@ -15,6 +15,11 @@ import copy
 def get_ossec_log_errors(scenario_name, hostname):
     """
     Return relevant lines from ossec.log
+
+    :param str scenario_name: Name of scenario
+    :param str hostname: Hostname to extract ossec.log
+
+    :return: list with the relevant lines
     """
     ossec_path = "/opt/fim_test_results/{}/agent_state/{}/ossec.log".format(
         scenario_name,
@@ -30,6 +35,11 @@ def get_ossec_log_errors(scenario_name, hostname):
 
 
 def read_verify_json():
+    """
+    Extract inner dict from result_json.json
+
+    :return: dict with json verifications
+    """
     path = "/opt/fim_test_results/result_json.json"
     with open(path, "r") as f:
         json_dict = json.load(f)
@@ -38,6 +48,11 @@ def read_verify_json():
 
 
 def read_verify_elastic():
+    """
+    Extract inner dict from result_es.json
+
+    :return: dict with elasticsearch verifications
+    """
     path = "/opt/fim_test_results/result_es.json"
     with open(path, "r") as f:
         json_dict = json.load(f)
@@ -46,6 +61,16 @@ def read_verify_elastic():
 
 
 def update_scenario(scenario, verification, content, results_dict):
+    """
+    Update scenario dict with specific content
+
+    :param str scenario: Name of the scenario
+    :param str verification: Type of verification
+    :param dict content: Content dict with actual data
+    :param dict result_dict: Dictionary to clone as result template
+
+    :return: dict cloned from json_dict with current scenario details updated
+    """
     current_dict = copy.deepcopy(results_dict)
     if content['passed']:
         # scenario SUCCESS
@@ -79,8 +104,15 @@ def update_scenario(scenario, verification, content, results_dict):
 
 def summarize_result(test_dict, verification, prebuilt_dict=None):
     """
-    Summarize a test_dict and dump results into results_dict
+    Summarize a test_dict of one type of verification and dump
+    results into results_dict.
     Use prebuilt_dict as a base if provided.
+
+    :param dict test_dict: Dict with test results
+    :param str verification: Type of verification
+    :param dict prebuilt_dict: A template dict with previous scenarios summarized, if any
+
+    :return: dict with current and previous scenarios summarized
     """
     results_dict = prebuilt_dict or {}
     for k, v in test_dict['scenarios'].items():
@@ -89,6 +121,12 @@ def summarize_result(test_dict, verification, prebuilt_dict=None):
 
 
 def final_summarize():
+    """
+    Summarize both json and elastic reports and create one global dict
+    with enough details in case of failed events.
+
+    :return: dict with final summary
+    """
     json_dict = read_verify_json()
     elastic_dict = read_verify_elastic()
     json_sum = summarize_result(json_dict, "json")
