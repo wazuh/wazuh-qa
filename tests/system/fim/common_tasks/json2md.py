83b36883f5c6a65e2309860a680bb9ba0cd32d39
Zenidd <jpsaezgutierrez@gmail.com>
2020-03-27 18:26:33 +0100

    Adding markdown summary support

---
+++ tests/system/fim/common_tasks/json2md.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2015-2020, Wazuh Inc.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License (version 2) as published by the FSF - Free Software
+# Foundation.
+
+import json
+import copy
+
+
+def read_summary_json():
+    path = "../provisioning/agents_status/fim_test_results/summary.json"
+    with open(path, "r") as f:
+        json_dict = json.load(f)
+    return json_dict
+
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--output", type=str, required=False,
+                        dest='output_file',
+                        default='./printable_summary.md',
+                        help="Output file to save the printable summary")
+    args = parser.parse_args()
+    out_path = args.output_file
+    printable_summary = read_summary_json()
+    with open(out_path, "w") as f:
+        json.dump(printable_summary, f)
