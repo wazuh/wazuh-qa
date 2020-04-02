35fbf8b114daef0cffa38c0c27a24c83daba6523
Daniel Melgarejo <danimegar@gmail.com>
2020-04-02 08:53:21 +0200

    Fixed format in pull on start test

--- tests/integration/test_gcloud/test_pull_on_start.py
+++ tests/integration/test_gcloud/test_pull_on_start.py
@@ -3,7 +3,6 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 import os
-import time
 import pytest
 
 from wazuh_testing import global_parameters
@@ -34,11 +33,11 @@ configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
 
 monitoring_modes = ['scheduled']
 conf_params = {'PROJECT_ID': project_id, 'SUBSCRIPTION_NAME': subscription_name,
-               'CREDENTIALS_FILE': credentials_file, 'INTERVAL': interval, 
+               'CREDENTIALS_FILE': credentials_file, 'INTERVAL': interval,
                'MAX_MESSAGES': max_messages, 'LOGGING': logging, 'MODULE_NAME': __name__}
 
-p, m = generate_params(extra_params=conf_params, 
-                       apply_to_all=({'PULL_ON_START': pull_on_start_value} for  pull_on_start_value in pull_on_start),
+p, m = generate_params(extra_params=conf_params,
+                       apply_to_all=({'PULL_ON_START': pull_on_start_value} for pull_on_start_value in pull_on_start),
                        modes=monitoring_modes)
 
 configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
@@ -55,34 +54,34 @@ def get_configuration(request):
 # tests
 
 def test_pull_on_start(get_configuration, configure_environment,
-                   restart_wazuh, wait_for_gcp_start):
+                       restart_wazuh, wait_for_gcp_start):
     """
     Verify the module gcp-pubsub starts if pull_on_start is set to yes and 
     the module sleeps if pull_on_start is set to no.
     In the second case, the module will start to pull messages after time interval.  
     """
-    value = get_configuration['elements'][0]['pull_on_start']['value'] == 'yes'
-    sinterval = get_configuration['elements'][4]['interval']['value']
-    interval = int(''.join(filter(str.isdigit, sinterval)))
+    pull_start = get_configuration['elements'][0]['pull_on_start']['value'] == 'yes'
+    str_interval = get_configuration['elements'][4]['interval']['value']
+    time_interval = int(''.join(filter(str.isdigit, str_interval)))
 
-    if value:
+    if pull_start:
         wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
                                 callback=callback_detect_start_fetching_logs,
                                 accum_results=1,
                                 error_message='Did not receive expected '
-                                                   '"Starting fetching of logs" event').result()
+                                              '"Starting fetching of logs" event').result()
     else:
         wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
-                            callback=callback_detect_start_gcp_sleep,
-                            error_message='Did not receive expected '
-                                          '"Sleeping for x seconds" event')
+                                callback=callback_detect_start_gcp_sleep,
+                                error_message='Did not receive expected '
+                                              '"Sleeping for x seconds" event')
         with pytest.raises(TimeoutError):
-            event = wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=callback_detect_start_fetching_logs)
+            event = wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
+                                            callback=callback_detect_start_fetching_logs)
             raise AttributeError(f'Unexpected event {event}')
 
-        time.sleep(interval)
-        wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
+        wazuh_log_monitor.start(timeout=global_parameters.default_timeout + time_interval,
                                 callback=callback_detect_start_fetching_logs,
                                 accum_results=1,
                                 error_message='Did not receive expected '
-                                                   '"Starting fetching of logs" event').result()
+                                              '"Starting fetching of logs" event').result()
