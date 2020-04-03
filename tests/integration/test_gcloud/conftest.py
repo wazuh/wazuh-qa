91df5c46f6d209b1a4c2b52b642fc03a385fce6a
Daniel Melgarejo <danimegar@gmail.com>
2020-04-03 11:58:05 +0200

    Update yaml file in pull on start test

--- tests/integration/test_gcloud/conftest.py
+++ tests/integration/test_gcloud/conftest.py
@@ -4,10 +4,7 @@
 
 import pytest
 
-from wazuh_testing import global_parameters
 from wazuh_testing.gcloud import detect_gcp_start
-from wazuh_testing.tools.configuration import get_wazuh_conf, write_wazuh_conf, set_section_wazuh_conf
-from wazuh_testing.tools.services import control_service
 
 
 @pytest.fixture(scope='module')
@@ -16,40 +13,3 @@ def wait_for_gcp_start(get_configuration, request):
     file_monitor = getattr(request.module, 'wazuh_log_monitor')
     detect_gcp_start(file_monitor)
 
-
-@pytest.fixture(scope='module')
-def configure_environment(get_configuration, request):
-    """Configure a custom environment for testing. Restart Wazuh is needed for applying the configuration."""
-
-    # save current configuration
-    backup_config = get_wazuh_conf()
-
-    # configuration for testing
-    elements = get_configuration.get('elements')
-    test_config = set_section_wazuh_conf(get_configuration.get('section'),
-                                         new_elements=elements)
-
-    # set new configuration
-    write_wazuh_conf(test_config)
-
-    # Call extra functions before yield
-    if hasattr(request.module, 'extra_configuration_before_yield'):
-        func = getattr(request.module, 'extra_configuration_before_yield')
-        func()
-
-    # Set current configuration
-    global_parameters.current_configuration = get_configuration
-
-    yield
-
-    # restore previous configuration
-    write_wazuh_conf(backup_config)
-
-    # Call extra functions after yield
-    if hasattr(request.module, 'extra_configuration_after_yield'):
-        func = getattr(request.module, 'extra_configuration_after_yield')
-        func()
-
-    if hasattr(request.module, 'force_restart_after_restoring'):
-        if getattr(request.module, 'force_restart_after_restoring'):
-            control_service('restart')
