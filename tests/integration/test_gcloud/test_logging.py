c95cbb5e6b1419f0625a05d905cd29d36cfa4a64
Daniel Melgarejo <danimegar@gmail.com>
2020-04-03 08:46:31 +0200

    Fixed format of logging test

--- tests/integration/test_gcloud/test_logging.py
+++ tests/integration/test_gcloud/test_logging.py
@@ -3,10 +3,8 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 import os
-import time
 import pytest
 from google.cloud import pubsub_v1
-from google.cloud import storage
 
 from wazuh_testing import global_parameters
 from wazuh_testing.gcloud import callback_detect_all_gcp
@@ -25,7 +23,7 @@ project_id = 'sinuous-voice-271711'
 subscription_name = 'wazuh-integration'
 topic_name = 'wazuh-pubsub'
 credentials_file = 'credentials.json'
-interval = '20s'
+interval = '25s'
 pull_on_start = 'yes'
 max_messages = 100
 logging = ['info', 'debug', 'warning', 'error', 'critical']
@@ -42,7 +40,7 @@ conf_params = {'PROJECT_ID': project_id, 'SUBSCRIPTION_NAME': subscription_name,
                'MODULE_NAME': __name__}
 
 p, m = generate_params(extra_params=conf_params,
-                       apply_to_all=({'LOGGING': logging_value} for  logging_value in logging),
+                       apply_to_all=({'LOGGING': logging_value} for logging_value in logging),
                        modes=monitoring_modes)
 
 configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
@@ -58,14 +56,14 @@ def get_configuration(request):
 
 # tests
 
-def publish(project_id, topic_name, credentials, n=5):
+def publish(id_project, name_topic, credentials, repetitions=5):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/var/ossec/{}".format(credentials)
 
     publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_name)
+    topic_path = publisher.topic_path(id_project, name_topic)
 
-    for n in range(0, n):
-        data = u"Message number {}".format(n)
+    for number in range(0, repetitions):
+        data = u"- DEBUG - Message number {}".format(number)
         # Data must be a bytestring
         data = data.encode("utf-8")
         # Add two attributes, origin and username, to the message
@@ -73,38 +71,39 @@ def publish(project_id, topic_name, credentials, n=5):
             topic_path, data, origin="python-sample", username="gcp"
         )
 
+
 def test_logging(get_configuration, configure_environment,
                  restart_wazuh, wait_for_gcp_start):
     """
     When a logging option is used, it cannot be an event that has another logging option.
     For example, events from gcp-pubusb will only have '- INFO -' if logging = info.
     """
-    sinterval = get_configuration['elements'][4]['interval']['value']
+    str_interval = get_configuration['elements'][4]['interval']['value']
     logging_opt = get_configuration['elements'][6]['logging']['value']
-    interval = int(''.join(filter(str.isdigit, sinterval)))
+    time_interval = int(''.join(filter(str.isdigit, str_interval)))
 
     if logging_opt == 'info':
-        filt = ['- DEBUG -', '- WARNING -', '- ERROR -', '- CRITICAL -']
+        key_words = ['- DEBUG -', '- WARNING -', '- ERROR -', '- CRITICAL -']
     elif logging_opt == 'debug':
-        filt = ['- INFO -', '- WARNING -', '- ERROR -', '- CRITICAL -']
+        key_words = ['- INFO -', '- WARNING -', '- ERROR -', '- CRITICAL -']
     elif logging_opt == 'warning':
-        filt = ['- INFO -', '- DEBUG -', '- ERROR -', '- CRITICAL -']
+        key_words = ['- INFO -', '- DEBUG -', '- ERROR -', '- CRITICAL -']
     elif logging_opt == 'warning':
-        filt = ['- INFO -', '- DEBUG -', '- WARNING -', '- CRITICAL -']
+        key_words = ['- INFO -', '- DEBUG -', '- WARNING -', '- CRITICAL -']
     else:
-        filt = ['- INFO -', '- DEBUG -', '- WARNING -', '- ERROR -']
+        key_words = ['- INFO -', '- DEBUG -', '- WARNING -', '- ERROR -']
 
     # Publish messages to pull them later
     publish(project_id, topic_name, credentials_file)
-    
+
     found = 0
-    for i in range (0,12):
-        n = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + interval + 5,
-                                    callback=callback_detect_all_gcp,
-                                    accum_results=1,
-                                    error_message='Did not receive expected '
-                                                  'wazuh-modulesd:gcp-pubsub[]').result()
-        for j in filt:
-            if j in n:
+    for nevents in range(0, 12):
+        event = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + time_interval + 5,
+                                        callback=callback_detect_all_gcp,
+                                        accum_results=1,
+                                        error_message='Did not receive expected '
+                                                      'wazuh-modulesd:gcp-pubsub[]').result()
+        for key in key_words:
+            if key in event:
                 found = 1
     assert found == 0
