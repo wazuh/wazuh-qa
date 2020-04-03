784b9a2348fa944e4b90708149f45f218c80c2a0
Daniel Melgarejo <danimegar@gmail.com>
2020-04-03 08:30:41 +0200

    Fixed format of max_messages test

--- tests/integration/test_gcloud/test_max_messages.py
+++ tests/integration/test_gcloud/test_max_messages.py
@@ -3,10 +3,8 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 import os
-import time
 import pytest
 from google.cloud import pubsub_v1
-from google.cloud import storage
 
 from wazuh_testing import global_parameters
 from wazuh_testing.gcloud import callback_received_messages_number
@@ -25,7 +23,7 @@ project_id = 'sinuous-voice-271711'
 subscription_name = 'wazuh-integration'
 topic_name = 'wazuh-pubsub'
 credentials_file = 'credentials.json'
-interval = '20s'
+interval = '25s'
 pull_on_start = 'no'
 max_messages = 100
 logging = 'info'
@@ -38,7 +36,7 @@ configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
 monitoring_modes = ['scheduled']
 conf_params = {'PROJECT_ID': project_id, 'SUBSCRIPTION_NAME': subscription_name,
                'CREDENTIALS_FILE': credentials_file, 'INTERVAL': interval,
-               'PULL_ON_START': pull_on_start, 'MAX_MESSAGES': max_messages, 
+               'PULL_ON_START': pull_on_start, 'MAX_MESSAGES': max_messages,
                'LOGGING': logging, 'MODULE_NAME': __name__}
 
 p, m = generate_params(extra_params=conf_params,
@@ -57,14 +55,14 @@ def get_configuration(request):
 
 # tests
 
-def publish(project_id, topic_name, credentials, n):
+def publish(id_project, name_topic, credentials, repetitions):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/var/ossec/{}".format(credentials)
 
     publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_name)
+    topic_path = publisher.topic_path(id_project, name_topic)
 
-    for n in range(0, n):
-        data = u"- DEBUG - Message number {}".format(n)
+    for number in range(0, repetitions):
+        data = u"- DEBUG - Message number {}".format(number)
         # Data must be a bytestring
         data = data.encode("utf-8")
         # Add two attributes, origin and username, to the message
@@ -72,43 +70,44 @@ def publish(project_id, topic_name, credentials, n):
             topic_path, data, origin="python-sample", username="gcp"
         )
 
+
 @pytest.mark.parametrize('nmessages', [
-    (30), (100), (120)
+    30, 100, 120
 ])
 def test_max_messages(nmessages, get_configuration, configure_environment,
-                   restart_wazuh, wait_for_gcp_start):
+                      restart_wazuh, wait_for_gcp_start):
     """
     Verify the module gcp-pubsub pull a number of messages less than or equal to max_messages. 
     If the number of messages is greater than max_messages, the module will only pull max_messages
     and the rest will be pulled in the next iteration. 
     """
-    sinterval = get_configuration['elements'][4]['interval']['value']
-    interval = int(''.join(filter(str.isdigit, sinterval)))
+    str_interval = get_configuration['elements'][4]['interval']['value']
+    time_interval = int(''.join(filter(str.isdigit, str_interval)))
 
     # Publish messages to pull them later
     publish(project_id, topic_name, credentials_file, nmessages)
 
     if nmessages <= max_messages:
-        n = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + interval + 5,
-                                    callback=callback_received_messages_number,
-                                    accum_results=1,
-                                    error_message='Did not receive expected '
-                                                  '- INFO - Received and acknowledged x messages').result()
-        assert int(n) == nmessages
+        number_pulled = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + time_interval + 5,
+                                                callback=callback_received_messages_number,
+                                                accum_results=1,
+                                                error_message='Did not receive expected '
+                                                              '- INFO - Received and acknowledged x messages').result()
+        assert int(number_pulled) == nmessages
     else:
         ntimes = int(nmessages / max_messages)
         remainder = int(nmessages % max_messages)
 
         for i in range(ntimes):
-            n = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + interval + 5,
-                                        callback=callback_received_messages_number,
-                                        accum_results=1,
-                                        error_message='Did not receive expected '
-                                                      '- INFO - Received and acknowledged x messages').result()
-            assert int(n) == max_messages
-        n = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + interval + 5,
-                                        callback=callback_received_messages_number,
-                                        accum_results=1,
-                                        error_message='Did not receive expected '
-                                                      '- INFO - Received and acknowledged x messages').result()
-        assert int(n) == remainder
+            number_pulled = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + time_interval + 5,
+                                                    callback=callback_received_messages_number,
+                                                    accum_results=1,
+                                                    error_message='Did not receive expected '
+                                                                  'Received and acknowledged x messages').result()
+            assert int(number_pulled) == max_messages
+        number_pulled = wazuh_log_monitor.start(timeout=global_parameters.default_timeout + time_interval + 5,
+                                                callback=callback_received_messages_number,
+                                                accum_results=1,
+                                                error_message='Did not receive expected '
+                                                              '- INFO - Received and acknowledged x messages').result()
+        assert int(number_pulled) == remainder
