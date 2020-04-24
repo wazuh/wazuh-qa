b5f65750a6e9a4a0589a306cd5ce1c00379a7318
Daniel Melgarejo <danimegar@gmail.com>
2020-04-24 15:21:53 +0200

    Skipped logging test for Windows systems

--- tests/integration/test_gcloud/test_functioning/test_logging.py
+++ tests/integration/test_gcloud/test_functioning/test_logging.py
@@ -4,6 +4,7 @@
 
 import os
 import pytest
+import sys
 
 from wazuh_testing import global_parameters
 from wazuh_testing.gcloud import callback_detect_all_gcp, publish
@@ -70,6 +71,7 @@ def get_configuration(request):
 
 # tests
 
+@pytest.mark.skipif(sys.platform == "win32", reason="Windows does not have support for Google Cloud integration.")
 def test_logging(get_configuration, configure_environment,
                  restart_wazuh, wait_for_gcp_start):
     """
