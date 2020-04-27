0c332e08db26c64e786f835fc5356ffbbecba7c7
Nicolas Papp <nicolaspapp@gmail.com>
2020-04-27 19:26:58 +0000

    Single Group tests

--- tests/integration/test_authd/test_authd.py
+++ tests/integration/test_authd/test_authd.py
@@ -7,11 +7,12 @@ import os
 import pytest
 import socket
 import ssl
+import subprocess
 import yaml
 
 from wazuh_testing import global_parameters
 from wazuh_testing.tools import WAZUH_PATH
-
+from wazuh_testing.tools.configuration import load_wazuh_configurations
 # Marks
 
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0), pytest.mark.server]
@@ -30,7 +31,8 @@ def load_tests(path):
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 message_tests = load_tests(os.path.join(test_data_path, 'enroll_messages.yaml'))
-
+configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
+configurations = load_wazuh_configurations(configurations_path, __name__, params=None, metadata=None)
 
 # Variables
 log_monitor_paths = []
@@ -42,8 +44,21 @@ monitored_sockets_params = [('wazuh-db', None, True), ('ossec-authd', None, True
 receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
 # Tests
 
-@pytest.mark.parametrize('test_case', [case['test_case'] for case in message_tests])
-def test_ossec_auth_messages( configure_mitm_environment, connect_to_sockets_module, wait_for_agentd_startup, test_case: list):
+@pytest.fixture(scope="function", params=message_tests)
+def set_up_groups(request):
+    groups = request.param.get('groups', [])
+    for group in groups:
+        subprocess.call(['/var/ossec/bin/agent_groups', '-a', '-g', f'{group}', '-q'])
+    yield request.param
+    for group in groups:
+        subprocess.call(['/var/ossec/bin/agent_groups', '-r', '-g', f'{group}', '-q'])
+
+@pytest.fixture(scope="module", params=configurations)
+def get_configuration(request):
+    """Get configurations from the module"""
+    yield request.param
+    
+def test_ossec_auth_messages( get_configuration, set_up_groups, configure_environment, configure_mitm_environment, connect_to_sockets_module, wait_for_agentd_startup):
     """Check that every input message in authd port generates the adequate output
 
     Parameters
@@ -51,6 +66,7 @@ def test_ossec_auth_messages( configure_mitm_environment, connect_to_sockets_mod
     test_case : list
         List of test_case stages (dicts with input, output and stage keys).
     """    
+    test_case = set_up_groups['test_case']
     for stage in test_case:
         # Reopen socket (socket is closed by maanger after sending message with client key)
         receiver_sockets[0].open()
@@ -58,5 +74,4 @@ def test_ossec_auth_messages( configure_mitm_environment, connect_to_sockets_mod
         message = stage['input']
         receiver_sockets[0].send(stage['input'], size=False)
         response = receiver_sockets[0].receive().decode()
-        assert response, 'Failed connection stage {}: {}'.format(test_case.index(stage) + 1, stage['stage'])
-        assert response[:len(expected)] == expected, 'Failed test case stage {}: {}'.format(test_case.index(stage) + 1, stage['stage'])
+        assert response[:len(expected)] == expected, 'Failed test case {}: Response was: {} instead of: {}'.format(test_case.index(stage) + 1, response, expected)
