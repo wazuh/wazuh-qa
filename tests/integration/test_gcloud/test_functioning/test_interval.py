464a1a1eee274d0a393ba769c5795c3a7d07b08c
Daniel Melgarejo <danimegar@gmail.com>
2020-04-29 16:34:24 +0200

    Force restart in interval test

--- tests/integration/test_gcloud/test_functioning/test_interval.py
+++ tests/integration/test_gcloud/test_functioning/test_interval.py
@@ -42,6 +42,7 @@ logging = "info"
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
+force_restart_after_restoring = True
 
 # configurations
 
