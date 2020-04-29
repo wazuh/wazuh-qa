d7859fe4f609304835d16a404f06391657bba44d
Pedro Nicolas Gomez Palacios <nicogp2@gmail.com>
2020-04-29 05:32:59 +0000

    Add integrated test to Authd for password verification

--- tests/integration/test_authd/test_authd_manager.py
+++ tests/integration/test_authd/test_authd_manager.py
@@ -8,6 +8,8 @@ import pytest
 import yaml
 import socket
 import ssl
+import time
+import subprocess
 
 from wazuh_testing import global_parameters
 from wazuh_testing.fim import generate_params
@@ -36,11 +38,13 @@ def load_tests(path):
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'manager_conf.yaml')
 client_keys_path = os.path.join(WAZUH_PATH, 'etc', 'client.keys')
+authd_default_password_path = os.path.join(WAZUH_PATH, 'etc', 'authd.pass')
 ip_name_configuration_tests = load_tests(os.path.join(test_data_path, 'ip_name_tests.yaml'))
 
-DEFAULT_FORCE_INSERT = "yes"
+DEFAULT_FORCE_INSERT = 'yes'
 DEFAULT_USE_USER_IP = 'no'
 DEFAULT_USE_PASSWORD = 'no'
+DEFAULT_TEST_PASSWORD = 'TopSecret'
 CLIENT_KEY_ENTRY_LEN = 4
 
 conf_params = {'USE_SOURCE_IP' : [], 'FORCE_INSERT' : [], 'USE_PASSWORD' : []}
@@ -56,11 +60,8 @@ configurations = load_wazuh_configurations(configurations_path, __name__, params
 
 # Variables
 log_monitor_paths = []
-
 receiver_sockets_params = [(("localhost", 1515), 'AF_INET', 'SSL_TLSv1_2')]
-
 monitored_sockets_params = [('wazuh-db', None, True), ('ossec-authd', None, True)]
-
 receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
 # fixtures
 
@@ -73,29 +74,9 @@ def get_configuration(request):
     yield request.param
     setattr(request.module, 'test_index', test_index + 1) 
 
-def override_wazuh_conf(configuration):
-    # Stop Wazuh
-    control_service('stop')
-     # Configuration for testing
-    test_config = set_section_wazuh_conf(configuration.get('sections'))
-    # Set new configuration
-    write_wazuh_conf(test_config)
-    # Start Wazuh
-    control_service('start')
 
-    """Wait until agentd has begun"""
-    def callback_agentd_startup(line):
-        if 'Accepting connections on port 1515' in line:
-            return line
-        return None
-
-    log_monitor = FileMonitor(LOG_FILE_PATH)
-    log_monitor.start(timeout=30, callback=callback_agentd_startup)
-
+   
 def clean_client_keys_file():
-    # Stop Wazuh
-    control_service('stop')
-
     # Clean client.keys
     try:
         with open(client_keys_path, 'w') as client_file:
@@ -103,6 +84,47 @@ def clean_client_keys_file():
     except IOError as exception:
         raise
 
+def read_random_pass():
+    grep = subprocess.check_output(["/bin/grep \"Random password\" /var/ossec/logs/ossec.log"], shell=True)
+    if grep:
+        parse_line = grep.split()
+        return parse_line[-1].decode("utf-8")
+    return grep
+
+def reset_password(set_password):
+    #in case of random pass, remove /etc/authd.pass
+    if set_password and set_password == 'random':
+        try:
+            os.remove(authd_default_password_path)
+        except FileNotFoundError:
+            pass
+        except IOError:
+            raise
+    #in case of defined pass, set predefined pass in  /etc/authd.pass
+    elif set_password and set_password == 'defined':
+        # Write authd.pass
+        try:
+            with open(authd_default_password_path, 'w') as pass_file:
+                pass_file.write(DEFAULT_TEST_PASSWORD)
+                pass_file.close()
+        except IOError as exception:
+            raise
+
+
+
+def override_wazuh_conf(configuration, set_password):
+    # Stop Wazuh
+    control_service('stop')
+     # Configuration for testing
+    test_config = set_section_wazuh_conf(configuration.get('sections'))
+    # Set new configuration
+    write_wazuh_conf(test_config)
+
+    #reset_client_keys
+    clean_client_keys_file()
+    #reset password
+    reset_password(set_password)
+
     # Start Wazuh
     control_service('start')
 
@@ -115,17 +137,21 @@ def clean_client_keys_file():
     log_monitor = FileMonitor(LOG_FILE_PATH)
     log_monitor.start(timeout=30, callback=callback_agentd_startup)
 
+
+
+
 def check_client_keys_file(response):
     try:
         with open(client_keys_path) as client_file:
             client_lines = client_file.readlines()
             for line in client_lines:
-                if line in response:
+                #discard endline because response finalize with "'\n\n" character and client.keys only with "\n"
+                if line[:-1] in response:
                     client_file.close()
-                    return line
+                    return True
     except IOError as exception:
         raise
-    return ""
+    return False
 
 
     
@@ -143,18 +169,24 @@ def test_ossec_auth_configurations(get_configuration, configure_environment, con
             - input: message that will be tried to send to the manager
             - output: expected response (if any)
     """   
+    #setup the password enviroment to password test
+    set_password = None
     test_case = ip_name_configuration_tests[test_index]['test_case']
-    override_wazuh_conf(get_configuration)
+    try:
+        if ip_name_configuration_tests[test_index]['USE_PASSWORD'] == 'yes':
+            set_password = 'defined'
+            if ip_name_configuration_tests[test_index]['random_pass'] == 'yes':
+                set_password = 'random'                
+    except KeyError:
+        pass
+    
+    override_wazuh_conf(get_configuration, set_password)
     for config in test_case:
         address, family, connection_protocol = receiver_sockets_params[0]
-        
         expect = config['expect']
-        insert_prev = config['insert_prev_agent_same_name']
-        if config['clean_client_keys'] and config['clean_client_keys'] == 'yes':
-            clean_client_keys_file()
-        
 
         #insert prev agent to force repeated case
+        insert_prev = config['insert_prev_agent_same_name']
         if insert_prev == "yes":
             SSL_socket_prev = SocketController(address, family=family, connection_protocol=connection_protocol)
             try:
@@ -167,17 +199,21 @@ def test_ossec_auth_configurations(get_configuration, configure_environment, con
                 else:
                     # We did not expect this error, fail test
                     raise
-            SSL_socket_prev.send(config['input'], size=False)
+            try:
+                SSL_socket_prev.send(config['insert_prev_agent'], size=False)
+            except KeyError:
+                SSL_socket_prev.send(config['input'], size=False)
+            
             if expect == 'output':
                 # Prev output is expected
                 expected = "OSSEC K:'"
                 if expected:
                     response = SSL_socket_prev.receive().decode()
-                    assert response, 'Failed connection in prev insert'
-                    #assert response == prev
-                    assert response[:len(expected)] == expected, 'Failed test case in prev insert'
+                    assert response, 'Failed connection prev insert for {}: {}'.format(ip_name_configuration_tests[test_index]['name'], config['input'])
+                    assert response[:len(expected)] == expected, 'Failed response prev {}: {}'.format(config['input'], config['output'])
                     if expected == "OSSEC K:'":
-                        assert check_client_keys_file(response) in response[:len(expected)]
+                        time.sleep(0.1)
+                        assert check_client_keys_file(response) == True, 'Failed test case checking client.keys in prev insert {}: {}'.format(config['input'], config['output'])
             SSL_socket_prev.close()
 
 
@@ -192,16 +228,28 @@ def test_ossec_auth_configurations(get_configuration, configure_environment, con
             else:
                 # We did not expect this error, fail test
                 raise
-        
+
+        #in case of test random and correct password register, read the random pass generated by os_authd and insert in query
+        #in case of test random and wrong password keep the password of the original query
+        if set_password and set_password == 'random':
+            try:
+                if config['insert_random_pass_in_query']:
+                    config['input'] = config['input'].format(read_random_pass())
+            except KeyError:
+                pass
+            except IndexError:
+                pass
+            
+
         SSL_socket.send(config['input'], size=False)
         if expect == 'output':
             # Output is expected
             expected = config['output']
             if expected:
                 response = SSL_socket.receive().decode()
-                assert response, 'Failed connection stage {}: {}'.format(test_case.index(config) + 1, config['stage'])
-                #assert response == prev
+                assert response, "Failed connection stage '{}'': '{}'".format(ip_name_configuration_tests[test_index]['name'], config['input'])
                 assert response[:len(expected)] == expected, 'Failed test case {}: {}'.format(config['input'], config['output'])
                 if expected == "OSSEC K:'":
-                    assert check_client_keys_file(response) in response[:len(expected)]
+                    time.sleep(0.1)
+                    assert check_client_keys_file(response) == True, 'Failed test case checking client.keys {}: {}'.format(config['input'], config['output'])
     return
