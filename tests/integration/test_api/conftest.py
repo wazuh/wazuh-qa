5a8cb32933d6a81ad496ddc9171fb1e570fa53ae
CarlosRS <carlos.ridao@wazuh.com>
2020-09-01 15:13:42 +0200

    Remove api v4 version

--- tests/integration/test_api/conftest.py
+++ tests/integration/test_api/conftest.py
@@ -9,7 +9,7 @@ import subprocess
 import pytest
 
 from wazuh_testing.api import callback_detect_api_start, get_base_url, get_token_login_api, API_HOST, \
-    API_LOGIN_ENDPOINT, API_PASS, API_PORT, API_USER, API_PROTOCOL, API_VERSION
+    API_LOGIN_ENDPOINT, API_PASS, API_PORT, API_USER, API_PROTOCOL
 from wazuh_testing.tools import API_LOG_FILE_PATH, WAZUH_PATH, WAZUH_API_CONF, WAZUH_SECURITY_CONF
 from wazuh_testing.tools.configuration import get_api_conf, write_api_conf, write_security_conf
 from wazuh_testing.tools.file import truncate_file
