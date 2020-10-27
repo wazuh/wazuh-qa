3ec000dac878f8d088259adf37720af3cfca1b69
Víctor Fernández <vicferpoy@gmail.com>
2020-10-27 13:12:09 +0100

    [API] Add security resources integration tests (#906)
    * Refactor api module
    
    * Add RBAC conftest
    
    * Add initial RBAC test
    
    * Add test_remove_relationship to RBAC tests
    
    * Add test_remove_resource to RBAC tests
    
    * Add test_add_old_resource to RBAC tests
    
    * Refactor RBAC conftest and tests
    
    * Add test_policy_position to RBAC tests
    
    * Add docstrings to RBAC tests
    
    * Add test_admin_resources to RBAC tests

--- tests/integration/test_api/conftest.py
+++ tests/integration/test_api/conftest.py
@@ -7,8 +7,7 @@ import shutil
 
 import pytest
 
-from wazuh_testing.api import callback_detect_api_start, get_base_url, get_token_login_api, API_HOST, \
-    API_LOGIN_ENDPOINT, API_PASS, API_PORT, API_USER, API_PROTOCOL
+from wazuh_testing.api import callback_detect_api_start, get_api_details_dict
 from wazuh_testing.tools import API_LOG_FILE_PATH, WAZUH_API_CONF, WAZUH_SECURITY_CONF
 from wazuh_testing.tools.configuration import get_api_conf, write_api_conf, write_security_conf
 from wazuh_testing.tools.file import truncate_file
@@ -100,14 +99,4 @@ def wait_for_start(get_configuration, request):
 
 @pytest.fixture(scope='module')
 def get_api_details():
-    def _get_api_details(protocol=API_PROTOCOL, host=API_HOST, port=API_PORT, user=API_USER,
-                         password=API_PASS, login_endpoint=API_LOGIN_ENDPOINT, timeout=10):
-        return {
-            'base_url': get_base_url(protocol, host, port),
-            'auth_headers': {
-                'Content-Type': 'application/json',
-                'Authorization': f'Bearer {get_token_login_api(protocol, host, port, user, password, login_endpoint, timeout)}'
-            }
-        }
-
-    return _get_api_details
+    return get_api_details_dict
---
+++ tests/integration/test_api/test_rbac/test_remove_relationship.py
@@ -0,0 +1,75 @@
+# Copyright (C) 2015-2020, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+import requests
+from wazuh_testing.api import get_security_resource_information
+
+# Variables
+user_id, role_id, policy_id, rule_id = None, None, None, None
+
+
+# Functions
+def remove_relationship(api_details, endpoint, resource, related_resource, relation):
+    """Remove a relationship between two security resources and check if the resources were deleted.
+
+    Parameters
+    ----------
+    api_details : dict
+        API details such as the headers.
+    endpoint : str
+        Request endpoint.
+    resource : int
+        Resource ID.
+    related_resource : dict
+        Dict with resource information (parameter and ID).
+    relation : str
+        Role entry of the related resource.
+    """
+    # Assert the relationship exists
+    assert get_security_resource_information(role_ids=resource)[relation], f'Resource {resource} does not belong to ' \
+                                                                           f'any {relation}'
+
+    # Remove relationship between
+    response = requests.delete(endpoint, headers=api_details['auth_headers'], verify=False)
+    assert response.status_code == 200, f'Status code was not 200. Response: {response.text}'
+    res = get_security_resource_information(role_ids=resource)
+
+    # Assert resources still exist but the relationship does not
+    assert res, 'Resource was removed as well'
+    assert not res[relation], f'Relationship still exists'
+    assert get_security_resource_information(**related_resource), 'Related user was removed as well'
+
+
+# Tests
+def test_remove_user_role_relationship(set_security_resources, get_api_details):
+    """Test if the user and role still exist after removing their relationship."""
+    api_details = get_api_details()
+    endpoint = api_details['base_url'] + f'/security/users/{user_id}/roles?role_ids={role_id}'
+    resource = role_id
+    related_resource = {'user_ids': user_id}
+    relation = 'users'
+
+    remove_relationship(api_details, endpoint, resource, related_resource, relation)
+
+
+def test_remove_role_policy_relationship(set_security_resources, get_api_details):
+    """Test if the role and policy still exist after removing their relationship."""
+    api_details = get_api_details()
+    endpoint = api_details['base_url'] + f'/security/roles/{role_id}/policies?policy_ids={policy_id}'
+    resource = role_id
+    related_resource = {'policy_ids': policy_id}
+    relation = 'policies'
+
+    remove_relationship(api_details, endpoint, resource, related_resource, relation)
+
+
+def test_remove_role_rule_relationship(set_security_resources, get_api_details):
+    """Test if the role and rule still exist after removing their relationship."""
+    api_details = get_api_details()
+    endpoint = api_details['base_url'] + f'/security/roles/{role_id}/rules?rule_ids={rule_id}'
+    resource = role_id
+    related_resource = {'rule_ids': rule_id}
+    relation = 'rules'
+
+    remove_relationship(api_details, endpoint, resource, related_resource, relation)
