d77fd9005bcdfbdf1235e7d02c84af932c717f75
Tomas Turina <tomas.turina@wazuh.com>
2021-01-27 13:08:08 -0300

    Add debug mode and log monitor to AR

--- tests/integration/test_active_response/test_analysisd/test_os_exec.py
+++ tests/integration/test_active_response/test_analysisd/test_os_exec.py
@@ -8,10 +8,12 @@ import pytest
 import socket
 import time
 
-from wazuh_testing.tools import WAZUH_PATH
+from wazuh_testing.tools import WAZUH_PATH, LOG_FILE_PATH
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.tools.agent_simulator import Sender, Injector, Agent, create_agents
 from wazuh_testing.tools.services import control_service
+from wazuh_testing.tools.file import truncate_file
+from wazuh_testing.tools.monitoring import FileMonitor
 
 pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0), pytest.mark.server]
 
@@ -25,7 +27,8 @@ cases = [
         'metadata': {
             'agents_number': 1,
             'agents_os': ['ubuntu20.04'],
-            'protocol': 'tcp'
+            'protocol': 'tcp',
+            'log_message': 'Jan 27 11:52:25 vm-test sshd[32046]: Accepted password for root from 172.16.5.15 port 62300 ssh2'
         }
     }
 ]
@@ -40,6 +43,19 @@ configurations = load_wazuh_configurations(configurations_path, __name__, metada
 agents = []
 
 
+@pytest.fixture(scope="session")
+def set_debug_mode():
+    local_int_conf_path = os.path.join(WAZUH_PATH, 'etc', 'local_internal_options.conf')
+    debug_line = 'remoted.debug=2\n'
+    with open(local_int_conf_path, 'r') as local_file_read:
+        lines = local_file_read.readlines()
+        for line in lines:
+            if line == debug_line:
+                return
+    with open(local_int_conf_path, 'a') as local_file_write:
+        local_file_write.write('\n'+debug_line)
+
+
 @pytest.fixture(scope="module", params=configurations)
 def get_configuration(request):
     """Get configurations from the module"""
@@ -48,6 +64,7 @@ def get_configuration(request):
 
 @pytest.fixture(scope="function")
 def restart_service():
+    clean_logs()
     control_service('restart')
     yield
 
@@ -68,16 +85,22 @@ def send_message(data_object, socket_path):
     sock.close()
 
 
-def wait_expected_line(line, expected):
-    if (expected in line):
+def clean_logs():
+    truncate_file(LOG_FILE_PATH)
+
+
+def wait_ar_line(line):
+    if ('Active response request received: ' in line):
         return line
     return None
 
 
-def test_os_exec(get_configuration, configure_environment, restart_service, configure_agents):
+def test_os_exec(set_debug_mode, get_configuration, configure_environment, restart_service, configure_agents):
     metadata = get_configuration.get('metadata')
     protocol = metadata['protocol']
+    log_message = metadata['log_message']
     sender = Sender(SERVER_ADDRESS, protocol=protocol)
+    log_monitor = FileMonitor(LOG_FILE_PATH)
     injectors = []
 
     # Agents
@@ -88,16 +111,22 @@ def test_os_exec(get_configuration, configure_environment, restart_service, conf
         if protocol == "tcp":
             sender = Sender(manager_address=SERVER_ADDRESS, protocol=protocol)
 
-    agents_id = [int(x.id) for x in agents]
-
     # Give time for registration key to be available and send a few heartbeats
-    time.sleep(10)
+    time.sleep(15)
 
-    for agent_id in agents_id:
-        # Send upgrade request
-        message = "8:[" + str(agent_id) + "] (vm-agent) 1.1.1.1->syscheck:{\"type\":\"event\",\"data\":{\"path\":\"/home/test/file\",\"mode\":\"realtime\",\"type\":\"added\",\"timestamp\":1575421292,\"attributes\":{\"type\":\"file\"}}}"
+    for agent in agents:
+        message = "1:[" + str(agent.id) + "] (" + agent.name + ") any->logcollector:" + log_message
         send_message(message, ANALYSISD_SOCKET)
-        # TODO
+
+        # Checking AR in logs
+        try:
+            log_monitor.start(timeout=10, callback=wait_ar_line)
+        except TimeoutError as err:
+            raise AssertionError("AR message tooks too much!")
+
+        last_log = log_monitor.result()
+        assert '' in last_log, \
+            'AR did not match expected!'
 
     for injector in injectors:
         injector.stop_receive()
