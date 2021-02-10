63e1a75ee65c9ef2ad2be43c1ef42696d557bdbd
Víctor Rebollo <victorrebollop@gmail.com>
2021-02-10 13:33:13 +0100

    Added vulnerability-detector tests indexes in documentation Added a mocked file for test-scan tests documentation

--- docs/run-documentation.sh
+++ docs/run-documentation.sh
@@ -1,4 +1,7 @@
 python /wazuh-qa/deps/wazuh_testing/setup.py install
 cd /wazuh-qa
 export PYTHONPATH=/wazuh-qa/deps/wazuh_testing
+
+mkdir -p /var/ossec/queue/vulnerabilities/dictionaries/
+echo "" >>  /var/ossec/queue/vulnerabilities/dictionaries/cpe_helper.json
 mkdocs serve -a 0.0.0.0:8080
