64bc0789c2250eaafd4392f808c7db8077303b41
BraulioV <brauliovl95@gmail.com>
2021-02-24 15:50:46 +0100

    Add the new tests for syslog

---
+++ tests/integration/test_remoted/test_socket_communication/test_syslog_message.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2015-2021, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+import os
+import pytest
+
+import wazuh_testing.remote as remote
+from wazuh_testing.tools.configuration import load_wazuh_configurations
+
+
+# Marks
+pytestmark = pytest.mark.tier(level=0)
+
+# Configuration
+test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+configurations_path = os.path.join(test_data_path, 'wazuh_syslog.yaml')
+
+parameters = [
+    {'PROTOCOL': 'UDP', 'PORT': 514},
+    {'PROTOCOL': 'UDP', 'PORT': 51000},
+    {'PROTOCOL': 'TCP', 'PORT': 514},
+    {'PROTOCOL': 'TCP', 'PORT': 51000}
+]
+
+metadata = [
+    {'protocol': 'UDP', 'port': 514},
+    {'protocol': 'UDP', 'port': 51000},
+    {'protocol': 'TCP', 'port': 514},
+    {'protocol': 'TCP', 'port': 51000}
+]
+
+configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
+configuration_ids = [f"syslog_{x['PROTOCOL']}_{x['PORT']}" for x in parameters]
+
+
+# fixtures
+@pytest.fixture(scope="module", params=configurations, ids=configuration_ids)
+def get_configuration(request):
+    """Get configurations from the module."""
+    return request.param
+
+
+def test_syslog_message(get_configuration, configure_environment, restart_wazuh):
+    """Test if remoted can receive syslog messages through the socket
+
+    Raises:
+        TimeoutError: if `wazuh-remoted` doesn't show the log message for syslog
+        AssertionError: if `wazuh-remoted` doesn't respond `#pong`
+    """
+    config = get_configuration['metadata']
+    port, protocol = config['port'], config['protocol']
+
+    # Monitor the archives.log
+    wazuh_archives_log_monitor = remote.create_archives_log_monitor()
+
+    # Check if remoted correctly started with the new conf
+    log_callback = remote.callback_detect_remoted_started(port=port, protocol=protocol, connection_type='syslog')
+    wazuh_log_monitor.start(timeout=5, callback=log_callback, error_message="Wazuh remoted didn't start as expected.")
+
+    # Check if wazuh-remoted receives syslog messages
+    syslog_message = f"Syslog message sent by wazuh-qa to test remoted syslog with {protocol} at {port}"
+    remote.check_syslog_event(wazuh_archives_log_monitor, syslog_message, port, protocol)
