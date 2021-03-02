d405db3d7fd516ec8e298d85b53ba4331650e95f
jmv74211 <jonathan.martin@wazuh.com>
2021-03-02 15:18:57 +0100

    Add new dummy folder and files for wazuh qa documentation

--- docs/run-documentation.sh
+++ docs/run-documentation.sh
@@ -3,10 +3,11 @@
 cd /wazuh-qa/deps/wazuh_testing/
 python setup.py install
 
-mkdir -p /var/ossec/queue/vulnerabilities/dictionaries/  /var/ossec/etc/
+mkdir -p /var/ossec/queue/vulnerabilities/dictionaries/  /var/ossec/etc/ /var/ossec/logs/archives
 
 echo "" >>  /var/ossec/queue/vulnerabilities/dictionaries/cpe_helper.json
 echo "" >>  /var/ossec/etc/local_internal_options.conf
+echo "" >>  /var/ossec/logs/archives/archives.log
 
 cd /wazuh-qa
 mkdocs serve -a 0.0.0.0:8080
