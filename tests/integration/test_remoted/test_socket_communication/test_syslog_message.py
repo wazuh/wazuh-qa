e562c59843c6a599de62c4e7a86fd40e2dbff739
BraulioV <brauliovl95@gmail.com>
2021-03-03 14:29:06 +0100

    Add messages with PRI headers to the test

--- tests/integration/test_remoted/test_socket_communication/test_syslog_message.py
+++ tests/integration/test_remoted/test_socket_communication/test_syslog_message.py
@@ -16,6 +16,14 @@ pytestmark = pytest.mark.tier(level=0)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_syslog.yaml')
 
+syslog_messages = {
+    'dummy': "Syslog message sent by wazuh-qa to test remoted syslog",
+    'sshd': remote.EXAMPLE_SYSLOG_EVENT,
+    'sshd_pri_header': f"<1>{remote.EXAMPLE_SYSLOG_EVENT}",
+    'multi_log': f"{remote.EXAMPLE_SYSLOG_EVENT}\n{remote.EXAMPLE_SYSLOG_EVENT}",
+    'multi_log_pri_header': f"<1>{remote.EXAMPLE_SYSLOG_EVENT}\n<2>{remote.EXAMPLE_SYSLOG_EVENT}"
+}
+
 parameters = [
     {'PROTOCOL': 'UDP', 'PORT': 514},
     {'PROTOCOL': 'UDP', 'PORT': 51000},
@@ -49,8 +57,9 @@ def get_configuration(request):
     return request.param
 
 
-def test_syslog_message(get_configuration, configure_environment, restart_wazuh):
-    """Test if remoted can receive syslog messages through the socket
+@pytest.mark.parametrize("syslog_message", syslog_messages.values())
+def test_syslog_message(syslog_message, get_configuration, configure_environment, restart_wazuh):
+    """Test if remoted can receive syslog messages with PRI header through the socket
 
     Raises:
         TimeoutError: if `wazuh-remoted` doesn't show the log message for syslog
@@ -63,8 +72,8 @@ def test_syslog_message(get_configuration, configure_environment, restart_wazuh)
 
     # Check if remoted correctly started with the new conf
     log_callback = remote.callback_detect_remoted_started(port=port, protocol=protocol, connection_type='syslog')
-    wazuh_log_monitor.start(timeout=5, callback=log_callback, error_message="Wazuh remoted didn't start as expected.")
+    wazuh_log_monitor.start(timeout=5, callback=log_callback, update_position=False,
+                            error_message="Wazuh remoted didn't start as expected.")
 
     # Check if wazuh-remoted receives syslog messages
-    syslog_message = f"Syslog message sent by wazuh-qa to test remoted syslog with {protocol} at {port}"
     remote.check_syslog_event(wazuh_archives_log_monitor, syslog_message, port, protocol)
