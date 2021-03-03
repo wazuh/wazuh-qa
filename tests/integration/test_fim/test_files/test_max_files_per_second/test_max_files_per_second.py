583df1aec896da0772f3f765a71a5d68f787bcd6
Antonio Fresneda <antonio.fresneda@wazuh.com>
2021-03-03 08:52:32 +0100

    Add documentation for test_max_files_per_second.
    - This commit also apply some style correction to the test.

--- tests/integration/test_fim/test_files/test_max_files_per_second/test_max_files_per_second.py
+++ tests/integration/test_fim/test_files/test_max_files_per_second/test_max_files_per_second.py
@@ -4,10 +4,9 @@
 
 import os
 import pytest
+import wazuh_testing.fim as fim
 
 from wazuh_testing import global_parameters
-from wazuh_testing.fim import LOG_FILE_PATH, generate_params, create_file, REGULAR, \
-    callback_detect_max_files_per_second, check_time_travel
 from wazuh_testing.tools import PREFIX
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.tools.monitoring import FileMonitor
@@ -20,7 +19,7 @@ pytestmark = pytest.mark.tier(level=1)
 
 test_directories = [os.path.join(PREFIX, 'testdir1')]
 max_files_per_second = 10
-wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+wazuh_log_monitor = FileMonitor(fim.LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
 # Values for max_files_per_second option
@@ -29,7 +28,7 @@ n_files_to_create = 50
 # Configurations
 
 conf_params = {'TEST_DIRECTORIES': test_directories[0]}
-p, m = generate_params(extra_params=conf_params, apply_to_all=({'MAX_FILES_PER_SEC': value} for value in values))
+p, m = fim.generate_params(extra_params=conf_params, apply_to_all=({'MAX_FILES_PER_SEC': value} for value in values))
 configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
 
 
@@ -45,20 +44,28 @@ def get_configuration(request):
 
 
 def test_max_files_per_second(get_configuration, configure_environment, restart_syscheckd, wait_for_fim_start):
-    """
-    Check that FIM sleeps for one second when the option max_files_per_second is enabled
+    """ Check that FIM sleeps for one second when the option max_files_per_second is enabled
+
+    Args
+        tags_to_apply (set): Run test if matches with a configuration identifier, skip otherwise.
+        get_configuration (fixture): Gets the current configuration of the test.
+        configure_environment (fixture): Configure the environment for the execution of the test.
+        restart_syscheckd (fixture): Restarts syscheck.
+        wait_for_fim_start (fixture): Waits until the first FIM scan is completed.
+    Raises:
+        TimeoutError: If an expected event couldn't be captured.
     """
     # Create the files in an empty folder to check realtime and whodata.
     for i in range(n_files_to_create):
-        create_file(REGULAR, test_directories[0], f'test_{i}', content='')
+        fim.create_file(fim.REGULAR, test_directories[0], f'test_{i}', content='')
 
     extra_timeout = n_files_to_create / max_files_per_second
 
     scheduled = get_configuration['metadata']['fim_mode'] == 'scheduled'
-    check_time_travel(scheduled)
+    fim.check_time_travel(scheduled)
     try:
         wazuh_log_monitor.start(timeout=global_parameters.default_timeout + extra_timeout,
-                                callback=callback_detect_max_files_per_second)
+                                callback=fim.callback_detect_max_files_per_second)
     except TimeoutError as e:
         if get_configuration['metadata']['max_files_per_sec'] == 0:
             pass
