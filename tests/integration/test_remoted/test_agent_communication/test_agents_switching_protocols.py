61657834e47b1a1781f2bfb8ce667de1fdab4238
BraulioV <brauliovl95@gmail.com>
2021-03-11 09:26:30 +0100

    Refactor the code of the test and add more test cases

--- tests/integration/test_remoted/test_agent_communication/test_agents_switching_protocols.py
+++ tests/integration/test_remoted/test_agent_communication/test_agents_switching_protocols.py
@@ -6,7 +6,7 @@ from time import sleep
 
 import pytest
 import wazuh_testing.tools.agent_simulator as ag
-from wazuh_testing import TCP, UDP, TCP_UDP
+from wazuh_testing import TCP, UDP
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 
 # Marks
@@ -15,16 +15,17 @@ pytestmark = pytest.mark.tier(level=2)
 
 # Configuration
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-configurations_path = os.path.join(test_data_path, 'wazuh_protocols_communication.yaml')
+configurations_path = os.path.join(test_data_path, 'wazuh_agents_reconnection.yaml')
+disconnect_time = 5
 
 parameters = [
-    {'PROTOCOL': TCP_UDP, 'PORT': 1514},
-    # {'PROTOCOL': TCP_UDP, 'PORT': 56000},
+    {'PORT': 1514, 'TIME': f"{disconnect_time}s"},
+    {'PORT': 56000, 'TIME': f"{disconnect_time}s"},
 ]
 
 metadata = [
-    {'protocol': TCP_UDP, 'port': 1514},
-    # {'protocol': TCP_UDP, 'port': 56000},
+    {'port': 1514, 'time': f"{disconnect_time}s"},
+    {'port': 56000, 'time': f"{disconnect_time}s"},
 ]
 
 agent_info = {
@@ -34,7 +35,7 @@ agent_info = {
 }
 
 configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
-config_ids = [f"{x['PROTOCOL']}_{x['PORT']}" for x in parameters]
+config_ids = [f"{x['PORT']}" for x in parameters]
 
 
 # fixtures
@@ -44,29 +45,50 @@ def get_configuration(request):
     return request.param
 
 
+def connect_and_check_agents_status(agents, agents_connections, port, use_tcp):
+    """Connect a list of agents changing the protocol used and checks its status
+
+    Args:
+        agents (list): list containing the agents.
+        agents_connections (dict): dictionary used to store the agents.
+        port (int): port used to connect the agent.
+        use_tcp (bool): variable used to alternate between protocols.
+    """
+    for agent in agents:
+        sender, injector = ag.connect(agent, protocol=TCP if use_tcp else UDP, port=port)
+        agents_connections[agent.id] = {'agent': agent, 'sender': sender, 'injector': injector}
+        use_tcp = not use_tcp
+        assert agent.get_connection_status() == 'active'
+
+
+def stop_all(connections):
+    """Stop all active agents
+
+    Args:
+        connections (dict): contains the agents, the injectors and the senders for each agent.
+    """
+    for agent in connections:
+        connections[agent]['injector'].stop_receive()
+
+
 def test_agents_switching_protocols(get_configuration, configure_environment, restart_remoted):
     """Checks if the agents can reconnect without issues to the manager after switching their protocol"""
 
     port = get_configuration['metadata']['port']
-    agents, use_tcp, num_agents = {}, False, 2
+    use_tcp, num_agents = False, 2
+    agents = ag.create_agents(agents_number=num_agents, manager_address='localhost',
+                              agents_os=['debian7']*num_agents)
+    agents_connections = {}
+    try:
+        connect_and_check_agents_status(agents, agents_connections, port, use_tcp)
 
-    for agent in ag.create_agents(agents_number=num_agents, manager_address='localhost',
-                                  agents_os=['debian7']*num_agents):
-        print(f"Agent {agent.id} will connect using {TCP if use_tcp else UDP}")
-        sender, injector = ag.connect(agent, protocol=TCP if use_tcp else UDP, port=port)
-        agents[agent.id] = {'agent': agent, 'sender': sender, 'injector': injector}
-        use_tcp = not use_tcp
-        assert agent.get_connection_status() == 'active'
+        stop_all(agents_connections)
 
-    for agent in agents:
-        agents[agent]['injector'].stop_receive()
+        # The test must wait until the manager considers the agents as disconnected. This time is
+        # set using the `agents_disconnection_time` option from the `global` section of the conf.
+        sleep(disconnect_time*2)
 
-    sleep(50)
+        connect_and_check_agents_status(agents, agents_connections, port, not use_tcp)
 
-    for agent_id in agents:
-        use_tcp = not use_tcp
-        agent = agents[agent_id]['agent']
-        print(f"Agent {agent_id} will connect using {TCP if use_tcp else UDP}")
-        sender, injector = ag.connect(agent, protocol=TCP if use_tcp else UDP, port=port)
-        agents[agent_id] = {'agent': agent, 'sender': sender, 'injector': injector}
-        assert agent.get_connection_status() == 'active'
+    finally:
+        stop_all(agents_connections)
