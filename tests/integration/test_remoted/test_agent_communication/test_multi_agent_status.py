c9e49150ceba07aea4a6896d3fe6ee140cca1bf6
jmv74211 <jonathan.martin@wazuh.com>
2021-03-11 11:00:42 +0100

    Update multi agent status test and add documentation #1114

--- tests/integration/test_remoted/test_agent_communication/test_multi_agent_status.py
+++ tests/integration/test_remoted/test_agent_communication/test_multi_agent_status.py
@@ -1,9 +1,16 @@
 import os
 import pytest
 
+import wazuh_testing.tools.agent_simulator as ag
+
+from time import sleep
+
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing import remote as rd
-from wazuh_testing import TCP_UDP
+from wazuh_testing.tools.thread_executor import ThreadExecutor
+from wazuh_testing.tools.monitoring import FileMonitor
+from wazuh_testing.tools import LOG_FILE_PATH
+from wazuh_testing import TCP, UDP, TCP_UDP
 
 
 # Marks
@@ -16,20 +23,94 @@ configurations_path = os.path.join(test_data_path, 'wazuh_multi_agent_status.yam
 
 # Set configuration
 parameters = [
-    {'PROTOCOL': TCP_UDP, 'PORT': 56000},
+    {'PROTOCOL': TCP, 'PORT': 1514},
+    {'PROTOCOL': TCP, 'PORT': 56000},
+    {'PROTOCOL': UDP, 'PORT': 1514},
+    {'PROTOCOL': UDP, 'PORT': 56000},
     {'PROTOCOL': TCP_UDP, 'PORT': 1514},
+    {'PROTOCOL': TCP_UDP, 'PORT': 56000}
 ]
 
 metadata = [
-    {'protocol': TCP_UDP, 'port': 56000},
+    {'protocol': TCP, 'port': 1514},
+    {'protocol': TCP, 'port': 56000},
+    {'protocol': UDP, 'port': 1514},
+    {'protocol': UDP, 'port': 56000},
     {'protocol': TCP_UDP, 'port': 1514},
+    {'protocol': TCP_UDP, 'port': 56000}
 ]
-
 
 configuration_ids = [f"{item['PROTOCOL'].upper()}_{item['PORT']}" for item in parameters]
 
 # Configuration data
 configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
+
+
+def check_active_agents(num_agents=1, manager_address='127.0.0.1', agent_version='4.2.0', agent_os='debian7',
+                        manager_port=1514, protocol=TCP):
+    """Check if the status of the agents is active after sending start-up and keep-alive events.
+
+    This can be done for n agents using any protocol.
+
+    Args:
+        num_agents (int): Number of agents to create and check their status.
+        manager_address (str): Manager IP address.
+        agent_version (str): Agent wazuh version.
+        agent_os (str): Agent operating system.
+        manager_port (int): Manager remote communication port.
+        protocol (str): It can be TCP, UDP or TCP_UDP (both).
+
+    Raises:
+        AttributeError: If the agent status is not active.
+    """
+    def send_initialization_events(agent, sender):
+        """Auxiliary function for sending start-up and keep-alive messages
+
+        Args:
+            agent (Agent): Agent that sends the events.
+            sender (Sender): Sender object to send the agent events.
+        """
+        try:
+            sender.send_event(agent.startup_msg)
+            # Wait 1 second between start-up message and keep_alive
+            sleep(1)
+            sender.send_event(agent.keep_alive_event)
+            # Wait 1 seconds to ensure that the message has ben sent before closing the socket.
+            sleep(1)
+        finally:
+            sender.socket.close()
+
+    wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+
+    # Create num_agents (parameter) agents
+    agents = ag.create_agents(agents_number=num_agents, manager_address=manager_address, disable_all_modules=True,
+                              agents_version=[agent_version]*num_agents, agents_os=[agent_os]*num_agents)
+    send_event_threads = []
+
+    # Wait until remoted has loaded the new agent key
+    rd.wait_to_remoted_key_update(wazuh_log_monitor)
+
+    # Create sender threads. One for each agent
+    for idx, agent in enumerate(agents):
+        if protocol == TCP_UDP:
+            # Round robin to select the protocol
+            protocol = TCP if idx % 2 == 0 else UDP
+
+        sender = ag.Sender(manager_address, manager_port, protocol)
+
+        send_event_threads.append(ThreadExecutor(send_initialization_events, {'agent': agent, 'sender': sender}))
+
+    # Run sender threads
+    for thread in send_event_threads:
+        thread.start()
+
+    # Wait until sender threads finish
+    for thread in send_event_threads:
+        thread.join()
+
+    # Check agent active status for earch agent
+    for agent in agents:
+        agent.wait_status_active()
 
 
 # Fixtures
@@ -40,7 +121,8 @@ def get_configuration(request):
 
 
 def test_protocols_communication(get_configuration, configure_environment, restart_remoted):
-    """Validate agent-manager communication using different protocols and ports"""
+    """Validate agent status after sending the start-up and keep-alive events"""
     manager_port = get_configuration['metadata']['port']
+    protocol = get_configuration['metadata']['protocol']
 
-    rd.check_active_agents(num_agents=2, manager_port=manager_port)
+    check_active_agents(num_agents=2, manager_port=manager_port, protocol=protocol)
