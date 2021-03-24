7ca509bbe5fd3aacc449ba69b94867e22d26fd92
Víctor Rebollo <victorrebollop@gmail.com>
2021-03-24 15:19:19 +0100

    Fixed documentation dockerfile, requirements now is copied before build the container Fixed pandas version

--- docs/provision_documentation.sh
+++ docs/provision_documentation.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+cp ../requirements.txt .
 docker build -t  wazuh-qa-documentation .
+rm ./requirements.txt
 docker run -it --rm -v "$(dirname $(pwd))":/wazuh-qa -p 8080:8080 wazuh-qa-documentation
