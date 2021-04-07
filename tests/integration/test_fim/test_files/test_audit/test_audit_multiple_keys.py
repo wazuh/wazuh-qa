16c95c7a9af3c1661e4a1db45899d6d59c7467b1
Antonio Fresneda <antonio.fresneda@wazuh.com>
2021-04-07 10:43:32 +0200

    Style improvements in test_audit_multiple_keys.

--- tests/integration/test_fim/test_files/test_audit/test_audit_multiple_keys.py
+++ tests/integration/test_fim/test_files/test_audit/test_audit_multiple_keys.py
@@ -22,11 +22,10 @@ pytestmark = [pytest.mark.linux, pytest.mark.tier(level=1)]
 
 monitored_test_dir = os.path.join(PREFIX, 'monitored_test_dir')
 non_monitored_test_dir = os.path.join(PREFIX, 'non_monitored_test_dir')
-custom_keys = ['a random key', 'key2']
+custom_keys = ['key1', 'key2']
 param_list = []
 
-for key in custom_keys:
-    param_list = param_list + ['-k', key]
+param_list = ' '.join([f'-k {key}' for key in custom_keys])
 
 test_directories = [monitored_test_dir, non_monitored_test_dir]
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -36,9 +35,36 @@ wazuh_log_monitor = FileMonitor(fim.LOG_FILE_PATH)
 
 # Configurations
 
-p, m = fim.generate_params(extra_params={'MONITORED_DIR': monitored_test_dir,
-                                         'AUDIT_KEYS': ','.join(key for key in custom_keys)}, modes=['whodata'])
-configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
+config_params, config_metadata = fim.generate_params(extra_params={'MONITORED_DIR': monitored_test_dir,
+                                                     'AUDIT_KEYS': ','.join(key for key in custom_keys)},
+                                                     modes=['whodata'])
+
+configurations = load_wazuh_configurations(configurations_path, __name__,
+                                           params=config_params, metadata=config_metadata)
+
+
+def run_audit_command(directory, params='', cmd_type='add'):
+    """Function to run auditctl commands.
+
+    Args:
+        directory (str): An audit rule will be added or deleted for this directory.
+        params (str): Parameters for auditctl.
+        cmd_type (str): type of command that will be used.
+                        - add to create a new audit rule.
+                        - delete to remove an audit rule.
+    Raises:
+        ValueError: If cmd_type is not add or delete.
+    """
+
+    if cmd_type == 'add':
+        audit_flag = '-w'
+    elif cmd_type == 'delete':
+        audit_flag = '-W'
+    else:
+        raise ValueError("Only add and delete values are allowed")
+
+    command = f"auditctl {audit_flag} {directory} -p wa {params}"
+    subprocess.run(command.split())
 
 
 # fixtures
@@ -53,30 +79,24 @@ def get_configuration(request):
 
 def extra_configuration_before_yield():
     # Create the custom audit rules for the non monitored directory
-    subprocess.run(['auditctl', '-w', f'{non_monitored_test_dir}', '-p', 'wa'] + param_list)
-
+    run_audit_command(directory=non_monitored_test_dir, params=param_list, cmd_type='add')
     # Remove audit rule that FIM configures for each monitored directory
-    subprocess.run(['auditctl', '-W', f'{monitored_test_dir}', '-p', 'wa', '-k', 'wazuh_fim'])
+    run_audit_command(directory=monitored_test_dir, params='-k wazuh_fim', cmd_type='delete')
     # Set the audit rule for the monitored directory with more than one key
-    subprocess.run(['auditctl', '-w', f'{monitored_test_dir}', '-p', 'wa', '-k', 'wazuh_fim', '-k', 'a_random_key'])
+    run_audit_command(directory=monitored_test_dir, params='-k wazuh_fim -k "a_random_key"', cmd_type='add')
 
 
 def extra_configuration_after_yield():
     # Remove the audit rules configured by the test
-    subprocess.run(['auditctl', '-W', f'{non_monitored_test_dir}', '-p', 'wa'] + param_list)
-    subprocess.run(['auditctl', '-W', f'{monitored_test_dir}', '-p', 'wa', '-k', 'wazuh_fim', '-k', 'a_random_key'])
+    run_audit_command(directory=non_monitored_test_dir, params=param_list, cmd_type='delete')
+    run_audit_command(directory=monitored_test_dir, params=param_list, cmd_type='delete')
 
 
-@pytest.mark.parametrize('tags_to_apply', [
-    {'audit_multiple_keys'}
-])
-@pytest.mark.parametrize('directory', [
-    monitored_test_dir,
-    non_monitored_test_dir
-])
+@pytest.mark.parametrize('tags_to_apply', [{'audit_multiple_keys'}])
+@pytest.mark.parametrize('directory', [monitored_test_dir, non_monitored_test_dir])
 def test_audit_multiple_keys(directory, tags_to_apply, get_configuration, configure_environment, restart_syscheckd,
                              wait_for_fim_start):
-    """ Checks that FIM correctly handles audit rules with multiple keys.
+    """Checks that FIM correctly handles audit rules with multiple keys.
 
     Args:
         directory (str): Directory where the changes will be done.
@@ -117,9 +137,9 @@ def test_audit_multiple_keys(directory, tags_to_apply, get_configuration, config
 
         assert get_configuration['metadata']['monitored_dir'] == directory, 'No events should be detected.'
         event_path = event['data']['path']
-        assert directory in event_path, f'Expected path = {directory}, event path = {event_path}'
+        assert directory in event_path, f"Expected path = {directory}, event path = {event_path}"
 
-    except TimeoutError:
+    except TimeoutError as e:
         # If the directory of the event is monitored, raise the TimeoutError exception
         if get_configuration['metadata']['monitored_dir'] == directory:
-            raise
+            raise e
