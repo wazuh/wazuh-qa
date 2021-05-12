ee2335c43fd84b156d0bf25804dba87139613402
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-05-12 09:50:29 +0200

    Moved wildcards testing to a new file and added a new .yml conf file for the test

--- tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_changes.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_changes.py
@@ -64,10 +64,7 @@ def get_configuration(request):
                                                                         pytest.mark.sunos5)),
     pytest.param('Ξ³ΞµΞΉΞ±', None, {CHECK_ALL}, {'ossec_conf'}, marks=(pytest.mark.win32,
                                                                        pytest.mark.xfail(reason='Xfail due to issue: \
-                                                                       https://github.com/wazuh/wazuh/issues/4612'))),
-    pytest.param('regular1', None, {CHECK_ALL}, {'ossec_conf_wildcards'}, marks=(pytest.mark.linux,
-                                                                                 pytest.mark.darwin,
-                                                                                 pytest.mark.sunos5))
+                                                                       https://github.com/wazuh/wazuh/issues/4612')))
 ])
 def test_regular_file_changes(folder, name, encoding, checkers, tags_to_apply,
                               get_configuration, configure_environment,
