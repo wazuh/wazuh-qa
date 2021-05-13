77f3ff76a3b14dfb1ed7d48fa43e93029e9e3dab
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-05-13 12:20:31 +0200

    Parameters and configuration options refactored and added more specific wildcards use cases

--- tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_wildcards.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_wildcards.py
@@ -5,8 +5,8 @@
 import os
 import sys
 import pytest
-from wazuh_testing import global_parameters, logger
-from wazuh_testing.fim import CHECK_ALL, LOG_FILE_PATH, regular_file_cud, generate_params
+from wazuh_testing import global_parameters
+from wazuh_testing.fim import LOG_FILE_PATH, regular_file_cud, generate_params
 from wazuh_testing.tools import PREFIX
 from wazuh_testing.tools.configuration import load_wazuh_configurations, check_apply_test
 from wazuh_testing.tools.monitoring import FileMonitor
@@ -15,92 +15,81 @@ from wazuh_testing.tools.monitoring import FileMonitor
 
 pytestmark = pytest.mark.tier(level=0)
 
-# variables
+# Variables
 
 test_folder = os.path.join(PREFIX, 'test_folder')
-test_directories = [test_folder]
-test_subdirectories = ['simple1', 'simple2', 'simple3']
+matched_directories = ['simple1', 'simple2', 'star12', 'stars123', 'multiple_1', 'multiplet']
+matched_subdirectories = ['sub_simple1', 'sub_simple2', 'substar', 'subdtar', '_submult', 'submult']
+matched_subdirectories = [os.path.join(matched_directories[index], matched_subdirectories[index])
+for index in range(len(matched_subdirectories))]
+test_subdirectories = matched_directories + matched_subdirectories + ['random_directory', 'not_monitored_directory']
+wildcards = [os.path.join(test_folder, 'simple?'),
+                  os.path.join(test_folder, 'star*'),
+                  os.path.join(test_folder, '*ple*'),
+                  os.path.join(test_folder, 'simple?/*'),
+                  os.path.join(test_folder, 'star*/sub?*'),
+                  os.path.join(test_folder, 'mul*/*?lt')]
 
+wildcards = ','.join(wildcards)
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_conf_wildcards.yml')
 
-# configurations
+# Configurations
 
-conf_params = {'TEST_WILDCARDS_START': [os.path.join(test_folder, 'simple?'),
-os.path.join(test_folder, 'simp*'), os.path.join(test_folder, '*ple?')], 'FIM_MODE': ['scheduled', 'realtime', "whodata"],
- 'MODULE_NAME': __name__}
+conf_params = {'TEST_WILDCARDS': wildcards}
 parameters, metadata = generate_params(extra_params=conf_params)
 configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
 
 
 def extra_configuration_before_yield():
 
+    if not os.path.exists(test_folder):
+        os.mkdir(test_folder)
+
     for sub_directory in test_subdirectories:
         if not os.path.exists(os.path.join(test_folder, sub_directory)):
             os.mkdir(os.path.join(test_folder, sub_directory))
 
 
-'''
-def extra_configuration_after_yield():
-    for sub_directory in test_subdirectories:
-        if os.path.exists(os.path.join(test_folder, sub_directory)):
-            os.rmdir(os.path.join(test_folder, sub_directory))
-'''
-
-# fixtures
-
-
+# Fixtures
 @pytest.fixture(scope='module', params=configurations)
 def get_configuration(request):
     """Get configurations from the module."""
     return request.param
 
 
-# tests
+# Test
+@pytest.mark.parametrize('parent_folder', [test_folder])
+@pytest.mark.parametrize('subfolder', test_subdirectories)
+@pytest.mark.parametrize('file_name', ['regular_1', 'regular2', '*.*', '*.log'])
+@pytest.mark.parametrize('tags_to_apply', [{'ossec_conf_wildcards'}])
+def test_basic_usage_wildcards(parent_folder, subfolder, file_name, tags_to_apply,
+                               get_configuration, configure_environment,
+                               restart_syscheckd, wait_for_fim_start):
+    """Test the correct expansion of wildcards for monitored directories in syscheck
 
-@pytest.mark.parametrize('parent_folder', [
-    test_folder
-])
-@pytest.mark.parametrize('tags_to_apply', [
-    {'ossec_conf_wildcards_start'}
-])
-@pytest.mark.parametrize('subfolder_name, file_name, encoding, checkers, triggers_event', [
-    pytest.param('simple1', 'regular1', None, {CHECK_ALL}, True),
-    pytest.param('simple2', '*.log', None, {CHECK_ALL}, True),
-    pytest.param('simple3', '*.*', None, {CHECK_ALL}, True),
-
-    pytest.param('imple', 'regular1', None, {CHECK_ALL}, False),
-    pytest.param('simble1', 'regular1', None, {CHECK_ALL}, False),
-    ])
-def test_basic_usage_wildcards(parent_folder, subfolder_name, file_name, encoding, checkers, tags_to_apply,
-                              get_configuration, configure_environment,
-                              restart_syscheckd, wait_for_fim_start, triggers_event):
+    Params:
+        parent_folder (str): Name of the root folder.
+        subfolder (str): Name of the subfolder under root folder.
+        file_name (str): Name of the file that will be created under subfolder.
+        tags_to_apply (str): Value holding the configuration used in the test.
+        get_configuration (fixture): Gets the current configuration of the test.
+        configure_environment (fixture): Configure the environment for the execution of the test.
+        restart_syscheckd (fixture): Restarts syscheck.
+        wait_for_fim_start (fixture): Waits until the first FIM scan is completed.
     """
-    Check if syscheckd detects regular file changes with wildcards (add, modify, delete)
+    folder = os.path.join(parent_folder, subfolder)
 
-    Parameters
-    ----------
-    folder : str
-        Directory where the files will be created.
-    checkers : dict
-        Syscheck checkers (check_all).
-    """
+    mult = 1 if sys.platform == 'win32' else 2
 
     if sys.platform == 'win32':
-        if "?" or "*" in file_name:
+        if "?" in file_name or "*" in file_name:
             pytest.skip("Windows can't create files with wildcards.")
 
     check_apply_test(tags_to_apply, get_configuration['tags'])
-    mult = 1 if sys.platform == 'win32' else 2
-
-    if encoding is not None:
-        file_name = file_name.encode(encoding)
-        parent_folder = parent_folder.encode(encoding)
-
-    folder = os.path.join(parent_folder, subfolder_name)
 
     regular_file_cud(folder, wazuh_log_monitor, file_list=[file_name],
                      time_travel=get_configuration['metadata']['fim_mode'] == 'scheduled',
-                     min_timeout=global_parameters.default_timeout * mult, options=checkers, encoding=encoding,
-                     triggers_event=triggers_event)
+                     min_timeout=global_parameters.default_timeout * mult,
+                     triggers_event=subfolder in matched_directories or subfolder in matched_subdirectories)
