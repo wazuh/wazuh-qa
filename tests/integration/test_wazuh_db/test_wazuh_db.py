b1ea961591edd11c52185a213e67a5471027bbb0
Cazajous Miguel A <migue7490@gmail.com>
2021-05-13 15:53:15 -0300

    Update tests/integration/test_wazuh_db/test_wazuh_db.py
    Co-authored-by: Braulio Vargas López <brauliovl95@gmail.com>

--- tests/integration/test_wazuh_db/test_wazuh_db.py
+++ tests/integration/test_wazuh_db/test_wazuh_db.py
@@ -332,7 +332,6 @@ def test_wazuh_db_timeout(configure_sockets_environment,
                           connect_to_sockets_module,
                           pre_insert_packages,
                           pre_set_sync_info):
-
     """Check that efectively the socket is closed after timeout is reached"""
 
     def send_row_by_row_command(command):
