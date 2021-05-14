b6fe450e9f4027984b447afdbc5c3cfe9a453c10
Víctor Fernández Poyatos <vicferpoy@gmail.com>
2021-05-14 14:00:32 +0200

    Improve key-polling scripts code and clean up

--- deps/wazuh_testing/wazuh_testing/tools/key_polling_cluster.py
+++ deps/wazuh_testing/wazuh_testing/tools/key_polling_cluster.py
@@ -1,22 +1,23 @@
-import base64
-import json
-import random
 import socket
-import string
-import struct
-import sys
+from base64 import b64encode
+from json import dumps
+from os.path import join
+from random import choices, randrange
+from secrets import token_hex
+from socket import socket, AF_UNIX, SOCK_STREAM
+from string import ascii_lowercase, digits
+from struct import pack
+from sys import argv
 
 from cryptography.fernet import Fernet
-
-CLIENT_KEYS = '/var/ossec/etc/lists/client.keys'
-AGENT_IDS = dict()
 
 CLUSTER_DATA_HEADER_SIZE = 20
 CLUSTER_CMD_HEADER_SIZE = 12
 CLUSTER_HEADER_FORMAT = '!2I{}s'.format(CLUSTER_CMD_HEADER_SIZE)
-FERNET_KEY = ''.join(random.choices(string.ascii_lowercase + string.digits, k=32))
-_my_fernet = Fernet(base64.b64encode(FERNET_KEY.encode()))
-COUNTER = random.randrange(100000)
+WAZUH_PATH = '/var/ossec'
+FERNET_KEY = ''.join(choices(ascii_lowercase + digits, k=32))
+_my_fernet = Fernet(b64encode(FERNET_KEY.encode()))
+COUNTER = randrange(100000)
 
 
 def cluster_msg_build(cmd: bytes = None, counter: int = None, payload: bytes = None, encrypt=True) -> bytes:
@@ -32,71 +33,61 @@ def cluster_msg_build(cmd: bytes = None, counter: int = None, payload: bytes = N
     # Add - to command until it reaches cmd length
     cmd = cmd + b' ' + b'-' * (CLUSTER_CMD_HEADER_SIZE - cmd_len - 1)
 
-    out_msg[:CLUSTER_DATA_HEADER_SIZE] = struct.pack(CLUSTER_HEADER_FORMAT, counter, len(encrypted_data), cmd)
+    out_msg[:CLUSTER_DATA_HEADER_SIZE] = pack(CLUSTER_HEADER_FORMAT, counter, len(encrypted_data), cmd)
     out_msg[CLUSTER_DATA_HEADER_SIZE:CLUSTER_DATA_HEADER_SIZE + len(encrypted_data)] = encrypted_data
 
     return bytes(out_msg[:CLUSTER_DATA_HEADER_SIZE + len(encrypted_data)])
 
 
-def load_client_keys():
-    global AGENT_IDS
-
-    for agent in open(CLIENT_KEYS).readlines():
-        try:
-            line = agent.strip().split(' ')
-            AGENT_IDS.update({line[0]: {'name': line[1], 'key': line[3]}})
-        except (IndexError, KeyError):
-            continue
+def get_agent_info(agent_id):
+    # Retrieve agent information here and return a dict
+    return {'agent_id': agent_id,
+            'agent_name': f'test_agent_{agent_id}',
+            'agent_ip': 'any',
+            'agent_key': token_hex(32)}
 
 
-def add_agent_authd_from_worker(agent_id):
-
-    global AGENT_IDS
-    agent = AGENT_IDS[agent_id]
-    name = agent['name']
-    key = agent['key']
-
+def add_agent_authd_from_worker(agent_id=None, agent_ip=None, agent_name=None, agent_key=None):
     data = {
         'daemon_name': 'authd',
         'message':
             {"function": "add",
              "arguments":
-                 {"name": name,
-                  "ip": "any",
+                 {"name": agent_name,
+                  "ip": agent_ip,
                   "id": agent_id,
-                  "key": key,
+                  "key": agent_key,
                   "force": -1
                   }
              }
     }
 
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    sock = socket(AF_UNIX, SOCK_STREAM)
     try:
         # Create connection with socket
-        sock.connect("/var/ossec/queue/cluster/c-internal.sock")
+        sock.connect(join(WAZUH_PATH, "queue/cluster/c-internal.sock"))
 
-        message = cluster_msg_build(cmd=b'sendsync', counter=COUNTER, payload=json.dumps(data).encode(), encrypt=False)
+        message = cluster_msg_build(cmd=b'sendsync', counter=COUNTER, payload=dumps(data).encode(), encrypt=False)
         sock.sendall(message)
         response = sock.recv(4096)
 
         if b'"error":0' in response:
-            print(json.dumps({"error": 0, "data": {"id": agent_id, "name": name, "ip": 'any', "key": key}}))
+            print(dumps({"error": 0, "data": {"id": agent_id, "name": agent_name, "ip": agent_ip, "key": agent_key}}))
         else:
             print(response)
-    except Exception as e:
-        print(json.dumps({"error": 4, "message": "Could not register agent"}))
+    except Exception:
+        print(dumps({"error": 4, "message": "Could not register agent"}))
     finally:
         sock.close()
 
 
 def main():
-    if len(sys.argv) < 3:
-        print(json.dumps({"error": 1, "message": "Too few arguments"}))
+    if len(argv) < 3:
+        print(dumps({"error": 1, "message": "Too few arguments"}))
         return
 
-    agent_id = sys.argv[2]
-    load_client_keys()
-    add_agent_authd_from_worker(agent_id)
+    agent_id = argv[2]
+    add_agent_authd_from_worker(**get_agent_info(agent_id))
 
 
 if __name__ == '__main__':
