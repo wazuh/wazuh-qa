a8e0647a33ef482e1b669d6da023425a229d7cb3
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-05-14 12:02:16 +0200

    Refactorized test to hold basic cases only

--- tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_wildcards.py
+++ tests/integration/test_fim/test_files/test_basic_usage/test_basic_usage_wildcards.py
@@ -11,73 +11,69 @@ from wazuh_testing.tools import PREFIX
 from wazuh_testing.tools.configuration import load_wazuh_configurations, check_apply_test
 from wazuh_testing.tools.monitoring import FileMonitor
 
-# Marks
+# marks
 
 pytestmark = pytest.mark.tier(level=0)
 
-# Variables
+# variables
 
 test_folder = os.path.join(PREFIX, 'test_folder')
 test_directories = [test_folder]
-matched_directories = ['simple1', 'simple2', 'star12', 'stars123', 'multiple_1', 'multiplet']
-matched_subdirs = ['sub_simple1', 'sub_simple2', 'substar', 'subdtar', '_submult', 'submult']
-matched_subdirs = [os.path.join(parent, child) for parent, child in zip(matched_directories, matched_subdirs)]
-test_subdirectories = matched_directories + matched_subdirs + ['random_directory', 'not_monitored_directory']
-wildcards = [os.path.join(test_folder, 'simple?'),
-             os.path.join(test_folder, 'star*'),
-             os.path.join(test_folder, '*ple*'),
-             os.path.join(test_folder, 'star*/sub*'),
-             os.path.join(test_folder, 'mul*/*lt')]
-
-wildcards = ','.join(wildcards)
+matched_dirs = ['simple1', 'stars123']
+test_subdirectories = matched_dirs + ['not_monitored_directory']
+expresions = [os.path.join(test_folder, 'simple?'),
+              os.path.join(test_folder, 'star*')]
+expresion_str = ','.join(expresions)
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_conf_wildcards.yml')
 
-# Configurations
+# configurations
 
-conf_params = {'TEST_WILDCARDS': wildcards}
+conf_params = {'TEST_WILDCARDS': expresion_str}
 parameters, metadata = generate_params(extra_params=conf_params)
 configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
 
 
 def extra_configuration_before_yield():
-
-    if not os.path.exists(test_folder):
-        os.mkdir(test_folder)
-
     for sub_directory in test_subdirectories:
+
         if not os.path.exists(os.path.join(test_folder, sub_directory)):
             os.mkdir(os.path.join(test_folder, sub_directory))
 
 
-def extra_configuration_after_yield():
+# fixtures
 
-    for sub_directory in test_subdirectories:
-        if os.path.exists(os.path.join(test_folder, sub_directory)):
-            os.rmdir(os.path.join(test_folder, sub_directory))
-
-
-# Fixtures
 @pytest.fixture(scope='module', params=configurations)
 def get_configuration(request):
     """Get configurations from the module."""
     return request.param
 
 
-# Test
+# tests
+
 @pytest.mark.parametrize('parent_folder', [test_folder])
-@pytest.mark.parametrize('subfolder', test_subdirectories)
-@pytest.mark.parametrize('file_name', ['regular_1', 'regular2', '*.*', '*.log'])
+@pytest.mark.parametrize('subfolder_name', test_subdirectories)
+@pytest.mark.parametrize('file_name', ['regular_1'])
 @pytest.mark.parametrize('tags_to_apply', [{'ossec_conf_wildcards'}])
-def test_basic_usage_wildcards(parent_folder, subfolder, file_name, tags_to_apply,
-                               get_configuration, configure_environment,
-                               restart_syscheckd, wait_for_fim_start):
+def test_basic_usage_wildcards(parent_folder, subfolder_name, file_name, tags_to_apply,
+                               get_configuration, configure_environment, restart_syscheckd,
+                               wait_for_fim_start):
     """Test the correct expansion of wildcards for monitored directories in syscheck
+
+    The following wildcards expansions will be tried against the directory list:
+        - test_folder/simple? will match simple?
+        - test_folder/star* will match stars123
+        - test_folder/*ple* will match simple1 and multiple_1
+        - not_monitored_directory won't match any of the previous expressions
+
+    For each subfolder there will be three different calls to regular_file_cud and
+    for every subfolder the variable triggers_event will be set properly depending on the
+    wildcards matching of the subfolder.
 
     Params:
         parent_folder (str): Name of the root folder.
-        subfolder (str): Name of the subfolder under root folder.
+        subfolder_name (str): Name of the subfolder under root folder.
         file_name (str): Name of the file that will be created under subfolder.
         tags_to_apply (str): Value holding the configuration used in the test.
         get_configuration (fixture): Gets the current configuration of the test.
@@ -85,17 +81,12 @@ def test_basic_usage_wildcards(parent_folder, subfolder, file_name, tags_to_appl
         restart_syscheckd (fixture): Restarts syscheck.
         wait_for_fim_start (fixture): Waits until the first FIM scan is completed.
     """
-    folder = os.path.join(parent_folder, subfolder)
-
-    mult = 1 if sys.platform == 'win32' else 2
-
     if sys.platform == 'win32':
-        if "?" in file_name or "*" in file_name:
+        if '?' in file_name or '*' in file_name:
             pytest.skip("Windows can't create files with wildcards.")
-
     check_apply_test(tags_to_apply, get_configuration['tags'])
 
+    folder = os.path.join(parent_folder, subfolder_name)
     regular_file_cud(folder, wazuh_log_monitor, file_list=[file_name],
                      time_travel=get_configuration['metadata']['fim_mode'] == 'scheduled',
-                     min_timeout=global_parameters.default_timeout * mult,
-                     triggers_event=subfolder in matched_directories or subfolder in matched_subdirs)
+                     min_timeout=global_parameters.default_timeout, triggers_event=subfolder_name in matched_dirs)
