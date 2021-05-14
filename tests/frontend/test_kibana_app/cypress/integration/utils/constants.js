278500786fbbd53c72c7bdd97b17ec90d72a21ea
MauGaP <mauro.pedano@gmail.com>
2021-05-14 18:15:09 -0300

    added cypress.env.json to run tests on more than one environment

--- tests/frontend/test_kibana_app/cypress/integration/utils/constants.js
+++ tests/frontend/test_kibana_app/cypress/integration/utils/constants.js
@@ -1,5 +1,7 @@
-export const ODFE_USERNAME = 'admin';
+export const BASIC = 'basic';
+export const ODFE = 'odfe';
 export const ODFE_PASSWORD = 'admin';
-export const WAZUH_URL = '/app/wazuh';
-export const XPACK_USERNAME = 'elastic';
+export const ODFE_USERNAME = 'admin';
+export const XPACK = 'xpack';
 export const XPACK_PASSWORD = 'elastic';
+export const XPACK_USERNAME = 'elastic';
