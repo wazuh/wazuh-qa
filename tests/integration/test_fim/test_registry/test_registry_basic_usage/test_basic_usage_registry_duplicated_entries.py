bb2d709f667ca29382f33d10846b78fa73db271d
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-05-17 12:06:47 +0200

    Refatorized test to detect added event instead of modified

--- tests/integration/test_fim/test_registry/test_registry_basic_usage/test_basic_usage_registry_duplicated_entries.py
+++ tests/integration/test_fim/test_registry/test_registry_basic_usage/test_basic_usage_registry_duplicated_entries.py
@@ -10,6 +10,24 @@ import wazuh_testing.fim as fim
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.tools.monitoring import FileMonitor
 
+
+# Helper functions
+
+def extra_configuration_after_yield():
+    fim.delete_registry(fim.registry_parser[key], sub_key_1, fim.KEY_WOW64_32KEY)
+
+
+def check_event_type_and_path(fim_event, monitorized_registry):
+    check_event = False
+
+    if fim_event['type'] == 'added':
+        registry_event_path = fim_event['path']
+        if monitorized_registry.lower() == registry_event_path.lower():
+            check_event = True
+
+    return check_event
+
+
 # Marks
 
 pytestmark = [pytest.mark.win32, pytest.mark.tier(level=0)]
@@ -39,14 +57,16 @@ registry_list = [(key, sub_key_1, fim.KEY_WOW64_32KEY), (key, sub_key_2,
                  fim.KEY_WOW64_32KEY)]
 
 
-# fixtures
+# Fixtures
+
 @pytest.fixture(scope='module', params=configurations)
 def get_configuration(request):
     """Get configurations from the module."""
     return request.param
 
 
-# test
+# Test
+
 @pytest.mark.parametrize('key, subkey1, subkey2, arch', [(key, sub_key_1, sub_key_2,
                          fim.KEY_WOW64_32KEY)])
 def test_registry_duplicated_entry(key, subkey1, subkey2, arch,
@@ -55,7 +75,7 @@ def test_registry_duplicated_entry(key, subkey1, subkey2, arch,
     """Two registries with capital differences must trigger just one modify event
 
     Test to check that two registries monitored with the same name but
-    capital differences only triggers one modified event when the registry is changed.
+    capital differences only triggers one added event when the registry is created.
 
 
     Params:
@@ -73,22 +93,23 @@ def test_registry_duplicated_entry(key, subkey1, subkey2, arch,
     """
     mode = get_configuration['metadata']['fim_mode']
     scheduled = mode == 'scheduled'
+    monitorized_registry = os.path.join(key, subkey2)
 
-    registry_1 = fim.create_registry(fim.registry_parser[key], subkey1, arch)
     fim.create_registry(fim.registry_parser[key], subkey2, arch)
-
-    fim.modify_registry_value(registry_1, "testkey", fim.REG_SZ, "some content x2")
 
     fim.check_time_travel(scheduled, monitor=wazuh_log_monitor)
 
-    wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
-                            callback=fim.callback_detect_modified_event,
-                            error_message='Did not receive expected '
-                            '"Sending Fim event: ..." event').result()
+    json = wazuh_log_monitor.start(timeout=global_parameters.default_timeout, callback=fim.callback_detect_event,
+                                   error_message='Did not receive expected "Sending Fim event: ..." event').result()
 
-    with pytest.raises(TimeoutError):
-        wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
-                                callback=fim.callback_detect_modified_event,
-                                error_message='Did not receive expected '
-                                '"Sending Fim event: ..." event').result()
-        raise AttributeError('Only one modification event was expected.')
+    if check_event_type_and_path(json['data'], monitorized_registry):
+        with pytest.raises(TimeoutError):
+            json = wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
+                                           callback=fim.callback_detect_event, error_message='Did not receive expected '
+                                           '"Sending Fim event: ..." event').result()
+
+            if check_event_type_and_path(json['data'], monitorized_registry):
+                raise pytest.fail("Only one added event for registry was expected.")
+
+    else:
+        raise pytest.fail("Unexpected fim event detected. Added event for " + str(subkey2) + " registry was expected.")
