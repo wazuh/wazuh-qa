6d6737eb4e3d0a14460db1accbce5fed32e9e982
JoseAntonioMHerrera <jose.mherrera@wazuh.com>
2021-05-19 13:10:18 +0200

    Changed number of tests cases to avoid excesive execution time and refactorized test variables creation

--- tests/integration/test_fim/test_files/test_wildcards_complex/test_wildcards_complex.py
+++ tests/integration/test_fim/test_files/test_wildcards_complex/test_wildcards_complex.py
@@ -20,20 +20,17 @@ pytestmark = pytest.mark.tier(level=1)
 test_folder = os.path.join(PREFIX, 'test_folder')
 test_directories = [test_folder]
 
-matched_dirs = ['simple1', 'simple2', 'star12', 'stars123', 'multiple_1', 'multiplet']
-matched_subdirs = ['sub_simple1', 'sub_simple2', 'substar', 'subdtar1', '_submult', 'submult']
-no_match_dirs = ['random_directory', 'not_monitored_directory', 'star12', 'stars123']
+matched_dirs = [os.path.join('stardir', 'sub_test'), os.path.join('multiple_wildcards', 'sub_test'),
+                os.path.join('directory_test', 'test_subdir1'), os.path.join('test_all', 'testdir'),
+                os.path.join('test_all', 'testdir', 'all')]
 
-matched_dirs = [os.path.join(test_folder, directory) for directory in matched_dirs]
-matched_subdirs = [os.path.join(parent, child) for parent, child in zip(matched_dirs, matched_subdirs)]
-no_match_dirs = [os.path.join(test_folder, directory) for directory in no_match_dirs]
+no_match_dirs = ['random_directory']
 
-test_subdirectories = matched_dirs + matched_subdirs + no_match_dirs
+wildcards = ','.join([os.path.join(test_folder, 'star*', 'sub*'), os.path.join(test_folder, 'mul*', '*test'),
+                      os.path.join(test_folder, '*test*', '*dir?'), os.path.join(test_folder, 'test_all', '*'),
+                      os.path.join(test_folder, 'test_all', '*', '*')])
 
-wildcards = [os.path.join(test_folder, 'star*/sub*'),
-             os.path.join(test_folder, 'mul*/*lt'),
-             os.path.join(test_folder, '*ple*')]
-wildcards = ','.join(wildcards)
+test_subdirectories = matched_dirs + no_match_dirs
 
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -46,19 +43,14 @@ parameters, metadata = generate_params(extra_params=conf_params)
 configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
 
 
-def recursive_directory_creation(splitted_path):
-    if splitted_path[0] != PREFIX:
-        if not os.path.exists(splitted_path[0]):
-            splitted_path = os.path.split(splitted_path[0])
-            recursive_directory_creation(splitted_path)
-        elif not os.path.exists(os.path.join(splitted_path[0], splitted_path[1])):
-            os.mkdir(os.path.join(splitted_path[0], splitted_path[1]))
-
-
 def extra_configuration_before_yield():
-    for subdir in test_subdirectories:
-        splitted_path = os.path.split(subdir)
-        recursive_directory_creation(splitted_path)
+    for dir in test_subdirectories:
+        split_path = os.path.split(dir)
+        parent_folder = os.path.join(test_folder, split_path[0])
+        if not os.path.exists(parent_folder):
+            os.mkdir(os.path.join(parent_folder))
+        if not os.path.exists(os.path.join(parent_folder, split_path[1])):
+            os.mkdir(os.path.join(parent_folder, split_path[1]))
 
 
 # Fixtures
@@ -80,7 +72,6 @@ def test_wildcards_complex(subfolder, file_name, tags_to_apply,
     """Test the correct expansion of complex wildcards for monitored directories in syscheck
 
     Params:
-        parent_folder (str): Name of the root folder.
         subfolder (str): Name of the subfolder under root folder.
         file_name (str): Name of the file that will be created under subfolder.
         tags_to_apply (str): Value holding the configuration used in the test.
@@ -98,7 +89,7 @@ def test_wildcards_complex(subfolder, file_name, tags_to_apply,
 
     check_apply_test(tags_to_apply, get_configuration['tags'])
 
-    regular_file_cud(subfolder, wazuh_log_monitor, file_list=[file_name],
+    regular_file_cud(os.path.join(test_folder, subfolder), wazuh_log_monitor, file_list=[file_name],
                      time_travel=get_configuration['metadata']['fim_mode'] == 'scheduled',
                      min_timeout=global_parameters.default_timeout * mult,
                      triggers_event=subfolder not in no_match_dirs)
