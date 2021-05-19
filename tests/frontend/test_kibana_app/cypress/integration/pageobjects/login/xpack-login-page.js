329fcc0c1112188fd74be38654bbbd001dee7be5
MauGaP <mauro.pedano@gmail.com>
2021-05-19 17:58:45 -0300

    refactored login with xpack method functions

--- tests/frontend/test_kibana_app/cypress/integration/pageobjects/login/xpack-login-page.js
+++ tests/frontend/test_kibana_app/cypress/integration/pageobjects/login/xpack-login-page.js
@@ -1,7 +1,3 @@
-class XpackLoginPage {
-  static inputUsernameSelector = 'input[data-test-subj="loginUsername"]';
-  static inputPasswordSelector = 'input[data-test-subj="loginPassword"]';
-  static buttonSubmitSelector = 'button[data-test-subj="loginSubmit"]';
-}
-
-export default XpackLoginPage;
+export const inputUsernameSelector = 'input[data-test-subj="loginUsername"]';
+export const inputPasswordSelector = 'input[data-test-subj="loginPassword"]';
+export const buttonSubmitSelector = 'button[data-test-subj="loginSubmit"]';
