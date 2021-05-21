71d5dee332dbb6dc0be4568dece328941a0205ba
gabiwassan <gabriel.wassan@wazuh.com>
2021-05-21 12:22:31 -0300

    feature(reports): Added reporting of all test and scripts to use it.

---
+++ tests/frontend/test_kibana_app/delete-reports.js
@@ -0,0 +1,25 @@
+const fs = require("fs")
+const path = require("path")
+
+const removeDir = function(path) {
+  if (fs.existsSync(path)) {
+    const files = fs.readdirSync(path)
+
+    if (files.length > 0) {
+      files.forEach(function(filename) {
+        if (fs.statSync(path + "/" + filename).isDirectory()) {
+          removeDir(path + "/" + filename)
+        } else {
+          fs.unlinkSync(path + "/" + filename)
+        }
+      })
+      fs.rmdirSync(path)
+    } else {
+      fs.rmdirSync(path)
+    }
+  } else {
+    console.log("Directory path not found.")
+  }
+}
+
+removeDir('./cypress/report')
