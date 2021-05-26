5bfe42780d0a020b783bb24c26dcc66af3406f0e
Matias Moreno <matiasmoreno876@gmail.com>
2021-05-26 11:55:30 -0300

    Switch structure is removed at login.given.

--- tests/frontend/test_kibana_app/cypress/integration/utils/constants.js
+++ tests/frontend/test_kibana_app/cypress/integration/utils/constants.js
@@ -1,7 +1,14 @@
-export const BASIC = 'basic';
-export const ODFE = 'odfe';
+import { loginXpack } from '../step-definitions/login/login-xpack';
+import { loginOdfe } from '../step-definitions/login/login-odfe';
+
 export const ODFE_PASSWORD = 'admin';
 export const ODFE_USERNAME = 'admin';
-export const XPACK = 'xpack';
 export const XPACK_PASSWORD = 'elastic';
 export const XPACK_USERNAME = 'elastic';
+
+export const LOGIN_TYPE = {
+  xpack: () => loginXpack,
+  odfe: () => loginOdfe,
+  basic: () => console.log(`Parameter loginMethod is: BASIC`),
+  default: loginMethod => console.error(`Parameter loginMethod is: ${loginMethod}`),
+};
