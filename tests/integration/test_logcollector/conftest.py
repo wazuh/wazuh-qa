32543f2f2b95887584d3cf74a2faf5f21d0951b4
Víctor Rebollo <victorrebollop@gmail.com>
2021-05-31 09:58:41 +0200

    Create basic macos oslog events test

--- tests/integration/test_logcollector/conftest.py
+++ tests/integration/test_logcollector/conftest.py
@@ -1,14 +1,16 @@
 # Copyright (C) 2015-2021, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
-import os
-import subprocess as sb
-
 import pytest
 from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.tools.file import truncate_file
 from wazuh_testing.tools.monitoring import FileMonitor
 from wazuh_testing.tools.services import control_service
+from wazuh_testing.tools.remoted_sim import RemotedSimulator
+from wazuh_testing.tools.authd_sim import AuthdSimulator
+from wazuh_testing.tools.manager_keys import sslmanager_key, sslmanager_cert
+
+from time import sleep
 
 DAEMON_NAME = "wazuh-logcollector"
 
@@ -21,3 +23,43 @@ def restart_logcollector(get_configuration, request):
     file_monitor = FileMonitor(LOG_FILE_PATH)
     setattr(request.module, 'wazuh_log_monitor', file_monitor)
     control_service('start', daemon=DAEMON_NAME)
+
+
+@pytest.fixture(scope='module')
+def init_authd_remote_simulator(get_connection_configuration, request):
+    authd_remoted_simulator_configuration = get_connection_configuration
+
+    # Write custom manager keys and certs in specified paths
+
+    with open(authd_remoted_simulator_configuration['server_keys'], "w") as key:
+        key.write(sslmanager_key)
+    with open(authd_remoted_simulator_configuration['server_cert'], "w") as cert:
+        cert.write(sslmanager_cert)
+
+    authd_simulator = AuthdSimulator(authd_remoted_simulator_configuration['ip_address'],
+                                     enrollment_port=authd_remoted_simulator_configuration['authd_port'],
+                                     key_path=authd_remoted_simulator_configuration['server_keys'],
+                                     cert_path=authd_remoted_simulator_configuration['server_cert'])
+    authd_simulator.start()
+
+    remoted_simulator = RemotedSimulator(server_address=authd_remoted_simulator_configuration['ip_address'],
+                                         remoted_port=authd_remoted_simulator_configuration['remoted_port'],
+                                         protocol=authd_remoted_simulator_configuration['protocol'],
+                                         mode=authd_remoted_simulator_configuration['remoted_mode'],
+                                         start_on_init=True,
+                                         client_keys=authd_remoted_simulator_configuration['client_keys'])
+
+    setattr(request.module, 'remoted_simulator', remoted_simulator)
+    setattr(request.module, 'authd_simulator', authd_simulator)
+
+    # We should copy before truncate
+    truncate_file(authd_remoted_simulator_configuration['client_keys'])
+
+    sleep(1)
+
+    control_service('restart')
+
+    yield
+
+    remoted_simulator.stop()
+    authd_simulator.shutdown()
---
+++ tests/integration/test_logcollector/test_macos/test_macos_format_basic.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2015-2021, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+import os
+import pytest
+
+from time import sleep
+from wazuh_testing.logcollector import macos_logger_message, macos_os_log_message, \
+    DEFAULT_AUTHD_REMOTED_SIMULATOR_CONFIGURATION, format_macos_message_pattern, TEMPLATE_OSLOG_MESSAGE
+from wazuh_testing.tools.configuration import load_wazuh_configurations
+from wazuh_testing.remote import check_agent_received_message
+
+# Marks
+pytestmark = pytest.mark.tier(level=1)
+
+# Configuration
+test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+configurations_path = os.path.join(test_data_path, 'wazuh_macos_format_basic.yaml')
+
+configurations = load_wazuh_configurations(configurations_path, __name__)
+
+macos_log_messages = [
+    {
+        'command': 'logger',
+        'message': "Logger testing message",
+    },
+    {
+        'command': 'os_log',
+        'type': 'error',
+        'subsystem': 'testing.wazuh-agent.macos',
+        'category': 'category',
+        'message': 1,
+    }
+]
+
+
+# fixtures
+@pytest.fixture(scope="module", params=configurations)
+def get_configuration(request):
+    """Get configurations from the module."""
+    return request.param
+
+
+@pytest.fixture(scope="module")
+def get_connection_configuration():
+    """Get configurations from the module."""
+    return DEFAULT_AUTHD_REMOTED_SIMULATOR_CONFIGURATION
+
+
+@pytest.mark.parametrize('macos_message', macos_log_messages)
+def test_macos_format_basic(get_configuration, configure_environment, get_connection_configuration,
+                            init_authd_remote_simulator, macos_message, restart_logcollector):
+    """
+    """
+    expected_macos_message = ""
+    log_command = macos_message['command']
+
+    if log_command == 'logger':
+        macos_logger_message(macos_message['message'])
+        expected_macos_message = format_macos_message_pattern(macos_message['command'], macos_message['message'])
+
+    elif log_command == 'os_log':
+        macos_os_log_message(macos_message['type'], macos_message['subsystem'], macos_message['category'])
+        expected_macos_message = format_macos_message_pattern(
+                                                        'custom_log',
+                                                        TEMPLATE_OSLOG_MESSAGE, macos_message['subsystem'],
+                                                        macos_message['category'])
+    sleep(20)
+
+    check_agent_received_message(remoted_simulator.rcv_msg_queue, expected_macos_message)
