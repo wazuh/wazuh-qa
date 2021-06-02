0c3d16a6da39059d15d240daeaa1af30cb0d6ecb
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2021-06-02 12:13:31 -0300

    prettier

--- tests/frontend/test_kibana_app/cypress/integration/step-definitions/management/decoders/navigate-to-decoders.when.js
+++ tests/frontend/test_kibana_app/cypress/integration/step-definitions/management/decoders/navigate-to-decoders.when.js
@@ -2,12 +2,12 @@ import { When } from 'cypress-cucumber-preprocessor/steps';
 import { clickElement } from '../../../utils/driver';
 import {
   decodersButton,
-  wazuhButton,
+  wazuhMenuButton,
   managementButton,
 } from '../../../pageobjects/wazuh-menu/wazuh-menu.page';
 
 When('The user navigates to decoders', () => {
-  clickElement(wazuhButton);
+  clickElement(wazuhMenuButton);
   clickElement(managementButton);
   clickElement(decodersButton);
   cy.wait(5000);
