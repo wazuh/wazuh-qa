a5b89113f7bf939272e3f6d0d979724beacb8637
Víctor Rebollo <victorrebollop@gmail.com>
2021-06-02 11:34:43 +0200

    Fix archlinux documentation and refactor test macos format only future events

--- tests/integration/test_logcollector/test_macos/test_macos_format_only_future_events.py
+++ tests/integration/test_logcollector/test_macos/test_macos_format_only_future_events.py
@@ -9,9 +9,6 @@ from wazuh_testing import logcollector
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.remote import check_agent_received_message
 from wazuh_testing.tools.services import control_service
-from wazuh_testing.tools.file import truncate_file
-from wazuh_testing.tools import LOG_FILE_PATH
-from wazuh_testing.tools.monitoring import FileMonitor
 
 # Marks
 
@@ -50,9 +47,6 @@ def test_macos_format_only_future_events(get_configuration, configure_environmen
     """
 
     macos_logcollector_monitored = logcollector.callback_monitoring_macos_logs
-    truncate_file(LOG_FILE_PATH)
-    wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
-
     wazuh_log_monitor.start(timeout=30, callback=macos_logcollector_monitored,
                             error_message=logcollector.GENERIC_CALLBACK_ERROR_TARGET_SOCKET)
 
@@ -69,11 +63,7 @@ def test_macos_format_only_future_events(get_configuration, configure_environmen
 
     control_service('restart')
 
-
     macos_logcollector_monitored = logcollector.callback_monitoring_macos_logs
-
-    truncate_file(LOG_FILE_PATH)
-    wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 
     wazuh_log_monitor.start(timeout=30, callback=macos_logcollector_monitored,
                             error_message=logcollector.GENERIC_CALLBACK_ERROR_TARGET_SOCKET)
