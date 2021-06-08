0e407a72accaa609c2194cbdf11fddd49458a603
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2021-06-08 09:36:40 -0300

    Update selectors

--- tests/frontend/test_kibana_app/cypress/integration/pageobjects/wazuh-menu/wazuh-menu.page.js
+++ tests/frontend/test_kibana_app/cypress/integration/pageobjects/wazuh-menu/wazuh-menu.page.js
@@ -1,14 +1,15 @@
-export const wazuhMenuButton = '[data-cy=menu-wazuh-button]';
+export const wazuhMenuButton = '[data-test-subj=menuWazuhButton]';
 //region Menu
-export const modulesButton = '[data-cy=menu-modules-button]';
-export const managementButton = '[data-cy=menu-management-button]';
-export const agentsButton = '[data-cy=menu-agents-button]';
-export const toolsButton = '[data-cy=menu-tools-button]';
-export const securityButton = '[data-cy=menu-security-button]';
-export const settingsButton = '[data-cy=menu-settings-button]';
+export const modulesButton = '[data-test-subj=menuModulesButton]';
+export const managementButton = '[data-test-subj=menuManagementButton]';
+export const agentsButton = '[data-test-subj=menuAgentsButton]';
+export const toolsButton = '[data-test-subj=menuToolsButton]';
+export const securityButton = '[data-test-subj=menuSecurityButton]';
+export const settingsButton = '[data-test-subj=menuSettingsButton]';
 //endregion Menu
 //region SubMenu
 //region Modules
+<<<<<<< HEAD
 <<<<<<< HEAD
 export const modulesDirectory = 'div.wz-menu-right-side > div > div > div.euiFlexGroup.euiFlexGroup--gutterLarge.euiFlexGroup--directionRow.euiFlexGroup--responsive > div > button > span > span';
 export const securityEventsButton = ':nth-child(1) > .euiSideNav > .euiSideNav__content > .euiSideNavItem--root > .euiSideNavItem__items > :nth-child(1) > .euiSideNavItemButton > .euiSideNavItemButton__content > .euiSideNavItemButton__label';
@@ -51,36 +52,57 @@ export const hipaaLink = '[data-cy=menu-modules-hipaa-link]';
 export const nistLink = '[data-cy=menu-modules-nist-link]';
 export const tscLink = '[data-cy=menu-modules-tsc-link]';
 >>>>>>> Update selectors
+=======
+export const securityEventsLink = '[data-test-subj=menuModulesSecurityEventsLink]';
+export const integrityMonitoringLink = '[data-test-subj=menuModulesFimLink]';
+export const amazonAwsLink = '[data-test-subj=menuModulesAwsLink]';
+export const googleCloudPlatformLink = '[data-test-subj=menuModulesGcpLink]';
+export const policyMonitoringLink = '[data-test-subj=menuModulesPolicyMonitoringLink]';
+export const systemAuditingLink = '[data-test-subj=menuModulesAuditLink]';
+export const openScapLink = '[data-test-subj=menuModulesOpenScapLink]';
+export const cisCatLink = '[data-test-subj=menuModulesCiscatLink]';
+export const securityConfigurationAssessmentLink = '[data-test-subj=menuModulesScaLink]';
+export const vulnerabilitiesLink = '[data-test-subj=menuModulesVulsLink]';
+export const virusTotalLink = '[data-test-subj=menuModulesVirustotalLink]';
+export const osqueryLink ='[data-test-subj=menuModulesOsqueryLink]';
+export const dockerListenerLink = '[data-test-subj=menuModulesDockerLink]';
+export const mitreAttackLink = '[data-test-subj=menuModulesMitreLink]';
+export const pciDssLink = '[data-test-subj=menuModulesPciLink]';
+export const gdprLink = '[data-test-subj=menuModulesGdprLink]';
+export const hipaaLink = '[data-test-subj=menuModulesHipaaLink]';
+export const nistLink = '[data-test-subj=menuModulesNistLink]';
+export const tscLink = '[data-test-subj=menuModulesTscLink]';
+>>>>>>> Update selectors
 //endregion
 //region Management
-export const rulesLink ='[data-cy=menu-management-rules-link]';
-export const decodersLink = '[data-cy=menu-management-decoders-link]';
-export const cdbListLink = '[data-cy=menu-management-lists-link]';
-export const groupsLink = '[data-cy=menu-management-groups-link]';
-export const configurationLink = '[data-cy=menu-management-configuration-link]';
-export const statusLink = '[data-cy=menu-management-status-link]';
-export const clusterLink = '[data-cy=menu-management-monitoring-link]';
-export const statisticsLink = '[data-cy=menu-management-statistics-link]';
-export const logsLink = '[data-cy=menu-management-logs-link]';
-export const reportingLink = '[data-cy=menu-management-reporting-link]';
+export const rulesLink ='[data-test-subj=menuManagementRulesLink]';
+export const decodersLink = '[data-test-subj=menuManagementDecodersLink]';
+export const cdbListLink = '[data-test-subj=menuManagementCdbListsLink]';
+export const groupsLink = '[data-test-subj=menuManagementGroupsLink]';
+export const configurationLink = '[data-test-subj=menuManagementConfigurationLink]';
+export const statusLink = '[data-test-subj=menuManagementStatusLink]';
+export const clusterLink = '[data-test-subj=menuManagementMonitoringLink]';
+export const statisticsLink = '[data-test-subj=menuManagementStatisticsLink]';
+export const logsLink = '[data-test-subj=menuManagementLogsLink]';
+export const reportingLink = '[data-test-subj=menuManagementReportingLink]';
 //endregion
 //region Tools
-export const apiConsoleLink = '[data-cy=menu-tools-devTools-link]';
-export const rulesetTestLink = '[data-cy=menu-tools-logtest-link]';
+export const apiConsoleLink = '[data-test-subj=menuToolsDevToolsLink]';
+export const rulesetTestLink = '[data-test-subj=menuToolsLogtestLink]';
 //endregion
 //region Security
-export const usersLink = '[data-cy=menu-security-users-link]';
-export const rolesLink = '[data-cy=menu-security-roles-link]';
-export const policiesLink = '[data-cy=menu-security-policies-link]';
-export const rolesMappingLink = '[data-cy=menu-security-roleMapping-link]';
+export const usersLink = '[data-test-subj=menuSecurityUsersLink]';
+export const rolesLink = '[data-test-subj=menuSecurityRolesLink]';
+export const policiesLink = '[data-test-subj=menuSecurityPoliciesLink]';
+export const rolesMappingLink = '[data-test-subj=menuSecurityRoleMappingLink]';
 //endregion
 //region Settings
-export const settingsApiConfigurationLink = '[data-cy=menu-settings-api-link]';
-export const settingsModulesLink = '[data-cy=menu-settings-modules-link]';
-export const settingsSampleDataLink = '[data-cy=menu-settings-sample_data-link]';
-export const settingsConfigurationLink = '[data-cy=menu-settings-configuration-link]';
-export const settingsLogsLink = '[data-cy=menu-settings-logs-link]';
-export const settingsMiscellaneousLink = '[data-cy=menu-settings-miscellaneous-link]';
-export const settingsAboutLink = '[data-cy=menu-settings-about-link]';
+export const settingsApiConfigurationLink = '[data-test-subj=menuSettingsApiLink]';
+export const settingsModulesLink = '[data-test-subj=menuSettingsModulesLink]';
+export const settingsSampleDataLink = '[data-test-subj=menuSettingsSampleDataLink]';
+export const settingsConfigurationLink = '[data-test-subj=menuSettingsConfigurationLink]';
+export const settingsLogsLink = '[data-test-subj=menuSettingsLogsLink]';
+export const settingsMiscellaneousLink = '[data-test-subj=menuSettingsMiscellaneousLink]';
+export const settingsAboutLink = '[data-test-subj=menuSettingsAboutLink]';
 //endregion
 //endregion
