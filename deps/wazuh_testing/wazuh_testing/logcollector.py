92432acf37225d1b2f7ad4cfd96c167bbb76372a
Víctor Rebollo <victorrebollop@gmail.com>
2021-06-10 12:42:29 +0200

    Add more callbacks and fix some errors in test_options_state_interval_no_file

--- deps/wazuh_testing/wazuh_testing/logcollector.py
+++ deps/wazuh_testing/wazuh_testing/logcollector.py
@@ -82,6 +82,17 @@ def callback_removed_file(file):
     return monitoring.make_callback(pattern=msg, prefix=prefix, escape=True)
 
 
+def callback_ignored_removed_file(file):
+    """Create a callback to detect if logcollector is ignoring specified deleted file.
+    Args:
+        file (str): Name with absolute path of the deleted file.
+    Returns:
+        callable: callback to detect this event.
+    """
+    msg = fr"File not available, ignoring it: '{file}'."
+    return monitoring.make_callback(pattern=msg, prefix=prefix, escape=True)
+
+
 def callback_monitoring_command(log_format, command):
     """Create a callback to detect if logcollector is monitoring a command.
     Args:
@@ -258,6 +269,18 @@ def callback_read_line_from_file(n_lines, filename):
         callable: callback to detect this event.
     """
     msg = fr"DEBUG: Read {n_lines} lines from {filename}"
+    return monitoring.make_callback(pattern=msg, prefix=prefix, escape=True)
+
+
+def callback_unable_to_open(file_path, n_attempt):
+    """Create a callback to detect if `wazuh-logcollector` fails to open the specified file.
+    Args:
+        file_path (str): Path of the file that `wazuh-logcollector` is trying to open.
+        n_attempt (str): Number of attempts remains to ignore the file.
+    Returns:
+        callable: callback to detect this event.
+    """
+    msg = fr"Unable to open file '{file_path}'. Remaining attempts: {n_attempt}"
     return monitoring.make_callback(pattern=msg, prefix=prefix, escape=True)
 
 
--- tests/integration/test_logcollector/test_options/test_options_state_interval_no_file.py
+++ tests/integration/test_logcollector/test_options/test_options_state_interval_no_file.py
@@ -14,7 +14,11 @@ from wazuh_testing.tools import LOGCOLLECTOR_STATISTICS_FILE
 from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.tools.monitoring import FileMonitor
 from wazuh_testing.tools.services import control_service
+from wazuh_testing.tools.file import truncate_file
+from wazuh_testing.tools.monitoring import FileMonitor
+from wazuh_testing.tools.services import control_service
 
+from time import sleep
 
 # Marks
 pytestmark = pytest.mark.tier(level=1)
@@ -46,8 +50,10 @@ metadata = [
 # Configuration data
 configurations = load_wazuh_configurations(configurations_path, __name__, params=parameters, metadata=metadata)
 configuration_ids = [f"{x['LOCATION']}_{x['LOG_FORMAT']}" for x in parameters]
-state_interval = [1, 2, 3, 4, 5]
-wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+local_options = [{'state_interval': '1', 'open_attempts': '1'},
+                 {'state_interval': '4', 'open_attempts': '4'},
+                 {'state_interval': '5', 'open_attempts': '10'},
+                 ]
 
 
 # Fixtures
@@ -63,48 +69,53 @@ def get_files_list():
     return file_structure
 
 
-@pytest.fixture(scope="module", params=state_interval)
-def get_local_internal_options(request):
+@pytest.fixture(scope="function", params=local_options)
+def get_local_internal_options_function(request):
     """Get configurations from the module."""
     backup_options_lines = conf.get_wazuh_local_internal_options()
 
     if sys.platform == 'win32':
         conf.add_wazuh_local_internal_options({'\n windows.debug': '2'})
     else:
-        conf.add_wazuh_local_internal_options({'\n logcollector.debug': '2'})
+        conf.add_wazuh_local_internal_options(
+            {'\n logcollector.debug': '2', 'logcollector.open_attempts': request.param['state_interval'],
+             'logcollector.vcheck_files': '3'})
 
-    conf.add_wazuh_local_internal_options({'logcollector.state_interval': request.param})
-
-    control_service('restart')
+    conf.add_wazuh_local_internal_options({'logcollector.state_interval': request.param['open_attempts']})
 
     yield request.param
+
     conf.set_wazuh_local_internal_options(backup_options_lines)
-    control_service('restart')
 
 
-def test_options_state_interval_no_file(get_local_internal_options, get_files_list, create_file_structure_module,
-                                        get_configuration, configure_environment, restart_logcollector):
+def test_options_state_interval_no_file(get_local_internal_options_function, get_files_list,
+                                        create_file_structure_function,
+                                        get_configuration, configure_environment):
     """Check if the monitorized file does not appear in logcollector.state when it is removed.
 
     Raises:
         AssertionError: If the elapsed time is different from the interval.
         TimeoutError: If the expected callback is not generated.
     """
+    truncate_file(LOG_FILE_PATH)
+    wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+    control_service("restart")
 
-    interval = get_local_internal_options
+    interval = int(get_local_internal_options_function['state_interval'])
+    open_attempts = int(get_local_internal_options_function['open_attempts'])
 
     for file in get_files_list:
         for name in file['filename']:
             # Ensure file is analyzed
             log_path = os.path.join(file['folder_path'], name)
             with open(log_path, 'w') as log_file:
-                log_file.write('Modifying the file')
+                log_file.write('Modifying the file\n')
 
             log_callback = logcollector.callback_analyzing_file(log_path)
             wazuh_log_monitor.start(timeout=5, callback=log_callback,
                                     error_message="ERROR GENERIC CHANGE")
 
-            # Ensure LOGCOLLECTOR STATISTICS FILE FILE is created
+            # Ensure wazuh-logcollector.state is created
             logcollector.wait_statistics_file(timeout=interval + 5)
 
             with open(LOGCOLLECTOR_STATISTICS_FILE, 'r') as json_file:
@@ -118,17 +129,27 @@ def test_options_state_interval_no_file(get_local_internal_options, get_files_li
 
             os.remove(log_path)
 
-            # Ensure logcollector has detected deletion of the file
             log_callback = logcollector.callback_removed_file(log_path)
-            wazuh_log_monitor.start(timeout=30, callback=log_callback,
-                                    error_message="ERROR GENERIC CHANGE")
+            wazuh_log_monitor.start(timeout=5, callback=log_callback,
+                                    error_message="File not available, ignoring it:")
+
+            for n_attempts in open_attempts:
+                log_callback = logcollector.callback_unable_to_open(log_path, open_attempts - n_attempts)
+                wazuh_log_monitor.start(timeout=5, callback=log_callback,
+                                        error_message="Unable to open file callback has not been generated")
+
+            log_callback = logcollector.callback_ignored_removed_file(log_path)
+            wazuh_log_monitor.start(timeout=5, callback=log_callback,
+                                    error_message="File not available, ignoring it:")
+
+            sleep(10)
 
             with open(LOGCOLLECTOR_STATISTICS_FILE, 'r') as next_json_file:
                 data = load(next_json_file)
 
+            print("Check it is not in state")
             global_files = data['global']['files']
             interval_files = data['interval']['files']
 
             assert not list(filter(lambda global_file: global_file['location'] == log_path, global_files))
             assert not list(filter(lambda interval_file: interval_file['location'] == log_path, interval_files))
-
