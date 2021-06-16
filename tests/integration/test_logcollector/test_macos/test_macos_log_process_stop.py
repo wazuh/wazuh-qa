0f4c100ec4cc363cdbb4d3e5e656fc988ae6c563
Víctor Rebollo <victorrebollop@gmail.com>
2021-06-16 10:14:47 +0200

    Modificate macos log process tests to add new checks for macOS Sierra

--- tests/integration/test_logcollector/test_macos/test_macos_log_process_stop.py
+++ tests/integration/test_logcollector/test_macos/test_macos_log_process_stop.py
@@ -4,6 +4,7 @@
 
 import os
 import pytest
+import platform
 
 import wazuh_testing.logcollector as logcollector
 from wazuh_testing.tools.configuration import load_wazuh_configurations
@@ -35,20 +36,42 @@ def test_macos_log_process_stop(get_configuration, configure_environment, restar
         TimeoutError: If the expected callback is not generated.
     """
 
+    macos_sierra = True if str(platform.mac_ver()[0]).startswith('10.12') else False
+
     macos_logcollector_monitored = logcollector.callback_monitoring_macos_logs
     wazuh_log_monitor.start(timeout=30, callback=macos_logcollector_monitored,
                             error_message=logcollector.GENERIC_CALLBACK_ERROR_TARGET_SOCKET)
 
-    log_processes = search_process('log')
+    process_to_stop = ['log']
+    if macos_sierra:
+        process_to_stop += ['script']
 
-    assert len(log_processes) == 1
+    for process in process_to_stop:
+        log_processes = search_process(process)
+        assert len(log_processes) == 1
+
+    control_service('stop', daemon='wazuh-logcollector')
+
+    sleep(5)
+
+    for process in process_to_stop:
+        log_processes = search_process(process)
+        assert len(log_processes) == 0
+
+    control_service('start', daemon='wazuh-logcollector')
+
+    macos_logcollector_monitored = logcollector.callback_monitoring_macos_logs
+    wazuh_log_monitor.start(timeout=30, callback=macos_logcollector_monitored,
+                            error_message=logcollector.GENERIC_CALLBACK_ERROR_TARGET_SOCKET)
+
+    for process in process_to_stop:
+        log_processes = search_process(process)
+        assert len(log_processes) == 1
 
     control_service('stop')
 
     sleep(5)
 
-    log_processes = search_process('log')
-
-    assert len(log_processes) == 0
-
-    control_service('start')
+    for process in process_to_stop:
+        log_processes = search_process(process)
+        assert len(log_processes) == 0
