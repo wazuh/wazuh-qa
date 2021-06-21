70c966e2befced430cadce8c8ff3108085b1f439
Julian Morales <jmorales@unc.edu.ar>
2021-06-21 09:31:18 -0700

    Adds base macos bad predicate IT

--- tests/integration/test_logcollector/test_macos/test_macos_file_status_predicate.py
+++ tests/integration/test_logcollector/test_macos/test_macos_file_status_predicate.py
@@ -8,18 +8,20 @@ import json
 import re
 import time
 import wazuh_testing.logcollector as logcollector
-import macos_utils
 from wazuh_testing.fim import change_internal_options
 from wazuh_testing.tools.configuration import load_wazuh_configurations
-from wazuh_testing.remote import check_agent_received_message
-from wazuh_testing.tools import WAZUH_PATH
+from wazuh_testing.tools import LOG_FILE_PATH, WAZUH_PATH
+from wazuh_testing import global_parameters
+from wazuh_testing.tools.monitoring import FileMonitor
+
+
 
 # Marks
 pytestmark = [pytest.mark.darwin, pytest.mark.tier(level=0)]
 
 # Configuration
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-configurations_path = os.path.join(test_data_path, 'wazuh_macos_file_status_basic.yaml')
+configurations_path = os.path.join(test_data_path, 'wazuh_macos_file_status_predicate.yaml')
 parameters = [{'ONLY_FUTURE_EVENTS': 'yes'}, {'ONLY_FUTURE_EVENTS': 'no'}]
 metadata = [{'only-future-events': 'yes'}, {'only-future-events': 'no'}]
 
@@ -38,6 +40,8 @@ macos_log_messages = [
 
 file_status_update = 4
 
+wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+
 
 def extra_configuration_before_yield():
     """Delete old OFE data and change file status update interval."""
@@ -46,6 +50,17 @@ def extra_configuration_before_yield():
     # Set default values
     change_internal_options('logcollector.vcheck_files', str(file_status_update))
 
+def callback_log_bad_predicate(line):
+    match = re.match(r'.*Execution error \'log:', line)
+    if match:
+        return True
+    return None
+
+def callback_log_exit_log(line):
+    match = re.match(r'.*macOS \'log stream\' process exited, pid:', line)
+    if match:
+        return True
+    return None
 
 # Fixtures
 @pytest.fixture(scope="module", params=configurations, ids=configuration_ids)
@@ -61,7 +76,7 @@ def get_connection_configuration():
 
 
 @pytest.mark.parametrize('macos_message', macos_log_messages)
-def test_macos_file_status_basic(get_configuration, configure_environment, get_connection_configuration,
+def test_macos_file_status_predicate(get_configuration, configure_environment, get_connection_configuration,
                                  init_authd_remote_simulator, macos_message, restart_logcollector):
 
     """Checks if logcollector stores correctly "macos"-formatted localfile data.
@@ -73,22 +88,18 @@ def test_macos_file_status_basic(get_configuration, configure_environment, get_c
         TimeoutError: If the expected callback is not generated.
     """
 
-    # Remove status file to check if agent behavior is as expected
-    os.remove(file_status_path) if os.path.exists(file_status_path) else None
+    wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
+                            callback=callback_log_bad_predicate,
+                            error_message="log 1: ")
 
-    macos_logcollector_monitored = logcollector.callback_monitoring_macos_logs
-    wazuh_log_monitor.start(timeout=30, callback=macos_logcollector_monitored,
-                            error_message=logcollector.GENERIC_CALLBACK_ERROR_TARGET_SOCKET)
-
-    logcollector.generate_macos_logger_log(macos_message['message'])
-    expected_message = logcollector.format_macos_message_pattern(macos_message['command'], macos_message['message'])
-
-    check_agent_received_message(remoted_simulator.rcv_msg_queue, expected_message, timeout=40)
-
+    wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
+                            callback=callback_log_exit_log,
+                            error_message="log 2: ")
+    
     file_status_json = ""
 
     # Waiting for file_status.json to be updated with macOS data by logcollector
-    time.sleep(file_status_update + 1)
+    time.sleep(file_status_update + 5)
 
     try:
         with open(file_status_path) as json_status:
@@ -98,11 +109,4 @@ def test_macos_file_status_basic(get_configuration, configure_environment, get_c
 
     # Check if json has a structure
     assert file_status_json["macos"], "Error finding 'macos' key"
-    assert file_status_json["macos"]["timestamp"], "Error finding 'timestamp' key inside 'macos'"
-    assert re.match(r'^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}-\d{4}$', 
-                    file_status_json["macos"]["timestamp"]), "Error of timestamp format"
-    assert file_status_json["macos"]["settings"], "Error finding 'settings' key inside 'macos'"
-    conf_predicate = get_configuration['sections'][1]['elements'][2]['query']['value']
-    conf_level = get_configuration['sections'][1]['elements'][2]['query']['attributes'][0]['level']
-    conf_type = get_configuration['sections'][1]['elements'][2]['query']['attributes'][1]['type']
-    assert file_status_json["macos"]["settings"] == macos_utils.compose_settings(conf_type, conf_level, conf_predicate)
+
