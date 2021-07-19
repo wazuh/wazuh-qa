385be80e0b3e8be78929f403f9b77bc8a0914e24
jmv74211 <jonathan.martin@wazuh.com>
2021-07-19 16:25:41 +0200

    refac: Improve ansible classes #1596

--- deps/wazuh_testing/wazuh_testing/provisioning/ansible/AnsiblePlaybook.py
+++ deps/wazuh_testing/wazuh_testing/provisioning/ansible/AnsiblePlaybook.py
@@ -1,63 +1,43 @@
 import os
-import textwrap
 import yaml
-from AnsibleRole import AnsibleRole
-from AnsibleTask import AnsibleTask
 
 
 class AnsiblePlaybook():
 
-    def __init__(self, name, hosts, gather_facts, tasks_list, ignore_errors, become, list_vars, playbook_file_path):
+    def __init__(self, name, tasks_list=None, playbook_file_path=None, hosts='all', gather_facts=False,
+                 ignore_errors=False, become=False, playbook_vars=None):
         self.name = name
         self.hosts = hosts
         self.gather_facts = gather_facts
         self.tasks_list = tasks_list
         self.ignore_errors = ignore_errors
         self.become = become
-        self.list_vars = list_vars
+        self.playbook_vars = playbook_vars
         self.playbook_file_path = playbook_file_path
 
-    def generate_playbook(self):
-        playbook_string = ""
-        host_string = f"hosts: {self.hosts}\n"
-        host_string += f"gather_facts: {self.gather_facts}\n"
-        host_string += f"become: {self.become}\n"
-        if self.list_vars:
-            host_string += "vars:\n"
-            host_var_string = yaml.dump(self.list_vars, None, allow_unicode=True)
-            host_string += f"{textwrap.indent(host_var_string, '  ')}"
+    def __str__(self):
+        playbook = {'hosts': self.hosts, 'gather_facts': self.gather_facts, 'become': self.become,
+                    'ignore_errors': self.ignore_errors}
 
-        playbook_string += f"{textwrap.indent(host_string, '  ')}".replace(' ', '-', 1)
+        if self.playbook_vars is not None:
+            playbook['vars'] = self.playbook_vars
 
-        if self.tasks_list:
-            my_role = AnsibleRole(self.tasks_list)
-            role_string = my_role.generate_role()
-            playbook_string += f"{textwrap.indent(role_string, '  ')}"
+        if self.tasks_list is not None:
+            playbook['tasks'] = []
+            for ansible_task in self.tasks_list:
+                playbook['tasks'].append(ansible_task.items)
+
+        playbook_string = yaml.dump([playbook], default_flow_style=False, sort_keys=False)
 
         return playbook_string
 
-    def write_playbook_to_file(self, playbook):
+    def write_playbook_to_file(self):
+        if not os.path.exists(os.path.dirname(self.playbook_file_path)):
+            os.makedirs(os.path.dirname(self.playbook_file_path))
+
         with open(self.playbook_file_path, 'w+') as file:
-            file.write(playbook)
+            file.write(self.__str__())
 
     def delete_playbook_file(self):
         if os.path.exists(self.playbook_file_path):
             os.remove(self.playbook_file_path)
-
-
-task1 = {"become": "true", "service": {"name": "wazuh-agent", "state": "restarted"}, "when": "os == 'macos' or os == 'solaris-11' or os == 'solaris-10'"}
-my_task1 = AnsibleTask("task_name1", task1)
-
-task2 = {"asdf": "true", "service": {"name": "wazuh-agent", "state": "restarted"}, "when": "os == 'macos' or os == 'solaris-11' or os == 'solaris-10'"}
-my_task2 = AnsibleTask("task_name2", task2)
-
-task3 = {"fdsa": "true", "service": {"name": "wazuh-agent", "state": "restarted"}, "when": "os == 'macos' or os == 'solaris-11' or os == 'solaris-10'"}
-my_task3 = AnsibleTask("task_name3", task3)
-
-vars = {"var1": "value1", "var2": "value2"}
-
-tasks_list = [my_task1, my_task2, my_task3]
-
-my_playbook = AnsiblePlaybook("playbook_test", "group_test", "yes", tasks_list, None, "true", vars, "/home/vhalgarv/drawers/git/wazuh-qa/deps/wazuh_testing/qactl/qa_provisioning/playbook.yaml")
-playbook_string = my_playbook.generate_playbook()
-my_playbook.write_playbook_to_file(playbook_string)
--- deps/wazuh_testing/wazuh_testing/provisioning/ansible/AnsibleTask.py
+++ deps/wazuh_testing/wazuh_testing/provisioning/ansible/AnsibleTask.py
@@ -3,22 +3,10 @@ import yaml
 
 class AnsibleTask():
 
-    def __init__(self, name, items):
-        self.name = name
+    def __init__(self, items):
         self.items = items
 
-    def print_data(self):
-        task_string = ""
-        task_string += f"name: {self.name}\n"
-        task_string += yaml.dump(self.items, None, allow_unicode=True)
+    def __str__(self):
+        task_string = yaml.dump(self.items, allow_unicode=True, sort_keys=False)
 
         return task_string
-
-
-"""
-item_dict = {"become": "true", "service": {"name": "wazuh-agent", "state": "restarted"}, "when": "os == 'macos' or os == 'solaris-11' or os == 'solaris-10'"}
-my_task = AnsibleTask("task_name", item_dict)
-task_string = my_task.print_data()
-
-print(task_string)
-"""
