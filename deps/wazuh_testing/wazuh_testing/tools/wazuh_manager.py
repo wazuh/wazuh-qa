623284d38ec4c251bd8a24535322e86bac9ca89c
Víctor Rebollo <victorrebollop@gmail.com>
2021-07-27 16:37:07 +0200

    Fix wazuh manager module

--- deps/wazuh_testing/wazuh_testing/tools/wazuh_manager.py
+++ deps/wazuh_testing/wazuh_testing/tools/wazuh_manager.py
@@ -3,36 +3,29 @@ import subprocess
 import subprocess as sb
 from wazuh_testing.tools import WAZUH_PATH
 from wazuh_testing.wazuh_db import query_wdb
-import sqlite3
 
 
-def list_agents(fields=['id']):
-    global_db_conn = sqlite3.connect(f"{WAZUH_PATH}/queue/db/global.db")
-    selected_fields = ''.join(fields)
-    global_db_conn.row_factory = lambda cursor, row: row[0]
-    cursor = global_db_conn.cursor()
 
-    list_agents = []
-
-    try:
-        sqlite_response = cursor.execute(f"SELECT id FROM agent").fetchall()
-        sqlite_response.remove(0)
-        list_agents = sqlite_response 
-    except Exception:
-        list_agents = []
+def list_agents_ids():
+    wazuhdb_result = query_wdb("global get-all-agents last_id -1")
+    list_agents = [agent['id'] for agent in wazuhdb_result if not (0 == agent.get('id'))]
 
     return list_agents
 
 
-def remove_agents(agents_id, wazuh_script=True, wazuh_db_query=False):
+def remove_agents(agents_id, remove_option='wazuhdb'):
+    if remove_option not in ['wazuhdb','manage_agents']:
+        raise ValueError("Invalid type of agent removal: %s" % remove_option)
+
     if agents_id:
         for agent_id in agents_id:
-            if wazuh_script:
+            if remove_option == 'manage_agents':
                 subprocess.call([f"{WAZUH_PATH}/bin/manage_agents", "-r", f"{agent_id}"], stdout=open(os.devnull, "w"),
                                 stderr=subprocess.STDOUT)
-            if wazuh_db_query:
+            elif remove_option == 'wazuhdb':
                 result = query_wdb(f"global delete-agent {str(agent_id)}")
 
+
 def remove_all_agents():
-    agent_ids_list = list_agents(['id'])
+    agent_ids_list = list_agents_ids()
     remove_agents(agent_ids_list)
