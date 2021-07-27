7a438a6f5117795ba3dc794f978710b5d33841a1
Víctor Rebollo <victorrebollop@gmail.com>
2021-07-27 16:40:13 +0200

    Rename remove_option with remove_type

--- deps/wazuh_testing/wazuh_testing/tools/wazuh_manager.py
+++ deps/wazuh_testing/wazuh_testing/tools/wazuh_manager.py
@@ -5,7 +5,6 @@ from wazuh_testing.tools import WAZUH_PATH
 from wazuh_testing.wazuh_db import query_wdb
 
 
-
 def list_agents_ids():
     wazuhdb_result = query_wdb("global get-all-agents last_id -1")
     list_agents = [agent['id'] for agent in wazuhdb_result if not (0 == agent.get('id'))]
@@ -13,19 +12,19 @@ def list_agents_ids():
     return list_agents
 
 
-def remove_agents(agents_id, remove_option='wazuhdb'):
-    if remove_option not in ['wazuhdb','manage_agents']:
-        raise ValueError("Invalid type of agent removal: %s" % remove_option)
+def remove_agents(agents_id, remove_type='wazuhdb'):
+    if remove_type not in ['wazuhdb','manage_agents']:
+        raise ValueError("Invalid type of agent removal: %s" % remove_type)
 
     if agents_id:
         for agent_id in agents_id:
-            if remove_option == 'manage_agents':
+            if remove_type == 'manage_agents':
                 subprocess.call([f"{WAZUH_PATH}/bin/manage_agents", "-r", f"{agent_id}"], stdout=open(os.devnull, "w"),
                                 stderr=subprocess.STDOUT)
-            elif remove_option == 'wazuhdb':
+            elif remove_type == 'wazuhdb':
                 result = query_wdb(f"global delete-agent {str(agent_id)}")
 
 
-def remove_all_agents():
+def remove_all_agents(remove_type):
     agent_ids_list = list_agents_ids()
-    remove_agents(agent_ids_list)
+    remove_agents(agent_ids_list, remove_type)
