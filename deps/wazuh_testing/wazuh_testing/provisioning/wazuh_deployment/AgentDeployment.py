8373fa28b9fb0208c3ba4d0fed0378412b3b4852
jmv74211 <jonathan.martin@wazuh.com>
2021-07-27 18:10:32 +0200

    refac: Refactor of deployment classes #1594

--- deps/wazuh_testing/wazuh_testing/provisioning/wazuh_deployment/AgentDeployment.py
+++ deps/wazuh_testing/wazuh_testing/provisioning/wazuh_deployment/AgentDeployment.py
@@ -1,13 +1,24 @@
 
-from wazuh_testing.provisioning.wazuh_install.WazuhDeployment import WazuhDeployment
+from tempfile import gettempdir
+
+from wazuh_testing.provisioning.wazuh_deployment.WazuhDeployment import WazuhDeployment
 from wazuh_testing.provisioning.ansible.AnsibleTask import AnsibleTask
 from wazuh_testing.provisioning.ansible.AnsibleRunner import AnsibleRunner
 
 
 class AgentDeployment(WazuhDeployment):
 
+    def __init__(self, installation_files_path, system, inventory_file_path, install_mode='package',
+                install_dir='/var/ossec', server_ip=None, hosts='all', preloaded_vars_file=gettempdir()):
+        super().__init__(installation_files_path=installation_files_path, system=system,
+                         inventory_file_path=inventory_file_path, server_ip=server_ip,
+                         install_mode=install_mode, install_dir=install_dir,
+                         hosts=hosts, preloaded_vars_file=preloaded_vars_file)
+
     def install(self):
         super().install('agent')
+        self.register_agent()
+        self.start_service()
 
     def start_service(self):
         super().start_service('agent')
@@ -24,19 +35,19 @@ class AgentDeployment(WazuhDeployment):
         tasks_list.append(AnsibleTask({'name': 'Configuring server ip to autoenrollment agent',
                                        'lineinfile': {'path': f'{self.install_dir}/etc/ossec.conf',
                                                       'regexp': '<address>(.*)</address>',
-                                                      'line': f'<address>{self.ip_server}</address>',
+                                                      'line': f'<address>{self.server_ip}</address>',
                                                       'backrefs': 'yes'},
                                        'when': 'ansible_system != "Windows"'}))
 
         tasks_list.append(AnsibleTask({'name': 'Configuring server ip to autoenrollment agent',
                                        'lineinfile': {'path': f'{self.install_dir}\\ossec.conf',
                                                       'regexp': '<address>(.*)</address>',
-                                                      'line': f'<address>{self.ip_server}</address>',
+                                                      'line': f'<address>{self.server_ip}</address>',
                                                       'backrefs': 'yes'},
                                        'when': 'ansible_system == "Windows"'}))\
 
         playbook_parameters = {'tasks_list': tasks_list, 'hosts': self.hosts, 'gather_facts': True, 'become': True}
 
         self.stop_service()
-        AnsibleRunner.run_ephemeral_tasks(self.inventory, playbook_parameters)
+        AnsibleRunner.run_ephemeral_tasks(self.inventory_file_path, playbook_parameters)
         self.start_service()
--- deps/wazuh_testing/wazuh_testing/provisioning/wazuh_deployment/ManagerDeployment.py
+++ deps/wazuh_testing/wazuh_testing/provisioning/wazuh_deployment/ManagerDeployment.py
@@ -1,11 +1,20 @@
 
-from wazuh_testing.provisioning.wazuh_install.WazuhDeployment import WazuhDeployment
+from tempfile import gettempdir
+
+from wazuh_testing.provisioning.wazuh_deployment.WazuhDeployment import WazuhDeployment
 
 
 class ManagerDeployment(WazuhDeployment):
 
+    def __init__(self, installation_files_path, system, inventory_file_path, server_ip=None, install_mode='package',
+                install_dir='/var/ossec', hosts='all', preloaded_vars_file=gettempdir()):
+        super().__init__(installation_files_path=installation_files_path, system=system, server_ip=server_ip,
+                         inventory_file_path=inventory_file_path, install_mode=install_mode, install_dir=install_dir,
+                         hosts=hosts, preloaded_vars_file=preloaded_vars_file)
+
     def install(self):
         super().install('server')
+        self.start_service()
 
     def start_service(self):
         super().start_service('manager')
