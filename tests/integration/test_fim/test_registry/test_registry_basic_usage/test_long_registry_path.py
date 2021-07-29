09002ad334e1e506b77de301e5599b50a8272b5d
Julia Magan <juliamgnr@gmail.com>
2021-07-29 15:36:18 +0200

    fix: Change how the registry is created
    When the key is in `test_regs`, the registry is created in the `conftest.py`
    instead of with the functions.

--- tests/integration/test_fim/test_registry/test_registry_basic_usage/test_long_registry_path.py
+++ tests/integration/test_fim/test_registry/test_registry_basic_usage/test_long_registry_path.py
@@ -7,7 +7,7 @@ import os
 import pytest
 from wazuh_testing import global_parameters
 from wazuh_testing.fim import LOG_FILE_PATH, generate_params, \
-    create_registry, registry_parser, registry_value_cud, KEY_WOW64_64KEY, delete_registry
+    registry_value_cud, KEY_WOW64_64KEY
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.tools.monitoring import FileMonitor
 
@@ -26,13 +26,13 @@ for i in range(30):
 
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-reg1 = os.path.join(key, sub_key_1)
+test_regs = [os.path.join(key, sub_key_1)]
 
 monitoring_modes = ['scheduled']
 
 # Configurations
 
-conf_params = {'WINDOWS_REGISTRY_1': reg1}
+conf_params = {'WINDOWS_REGISTRY_1': test_regs}
 configurations_path = os.path.join(test_data_path, 'wazuh_conf_reg_attr.yaml')
 p, m = generate_params(extra_params=conf_params, modes=monitoring_modes)
 configurations = load_wazuh_configurations(configurations_path, __name__, params=p, metadata=m)
@@ -44,15 +44,6 @@ configurations = load_wazuh_configurations(configurations_path, __name__, params
 def get_configuration(request):
     """Get configurations from the module."""
     return request.param
-
-
-# tests
-
-def extra_configuration_before_yield():
-    create_registry(registry_parser[key], sub_key_1, arch)
-
-def extra_configuration_after_yield():
-    delete_registry(registry_parser[key], sub_key_1, arch)
 
 
 def test_long_registry_path(get_configuration, configure_environment, restart_syscheckd, wait_for_fim_start):
