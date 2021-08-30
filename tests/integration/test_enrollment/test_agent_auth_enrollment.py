490edfabfce0f7e84747e14ceb10ea800a83d8dd
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-08-30 08:33:52 -0700

    Fix empte message hanlding

--- tests/integration/test_enrollment/test_agent_auth_enrollment.py
+++ tests/integration/test_enrollment/test_agent_auth_enrollment.py
@@ -39,6 +39,7 @@ params = [{'SERVER_ADDRESS': SERVER_ADDRESS, }]
 metadata = [{}]
 configurations = load_wazuh_configurations(configurations_path, __name__, params=params, metadata=metadata)
 
+receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
 LAST_MESSAGE = None
 CURRENT_TEST_CASE = {}
 
@@ -51,15 +52,26 @@ def check_log_error_conf(msg):
     return None
 
 def receiver_callback(received):
+    if len(received) == 0:
+        return b""
+
     global LAST_MESSAGE
     LAST_MESSAGE = received.decode()
     socket_listener.event.set()
-    return 'ERROR'
+    response = CURRENT_TEST_CASE['message']['response'].format(**DEFAULT_VALUES).encode()
+    return response
 
-socket_listener = ManInTheMiddle(address=(SERVER_ADDRESS, 1515), family='AF_INET',
-                                              connection_protocol='SSL', func=receiver_callback)
+def get_last_message():
+    global LAST_MESSAGE
+    import time
+    timeout = time.time() + 20 # 20 seconds timeout
+    while not LAST_MESSAGE and time.time() <= timeout:
+        pass
+    return LAST_MESSAGE
 
-receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
+def clear_last_message():
+    global LAST_MESSAGE
+    LAST_MESSAGE = None
 
 def launch_agent_auth(configuration):
     parse_configuration_string(configuration)
@@ -90,6 +102,9 @@ def launch_agent_auth(configuration):
     out = subprocess.Popen(parser.get_command(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     out.communicate()
 
+socket_listener = ManInTheMiddle(address=(SERVER_ADDRESS, 1515), family='AF_INET',
+                                              connection_protocol='SSL', func=receiver_callback)
+
 # fixtures
 @pytest.fixture(scope="module", params=configurations)
 def get_configuration(request):
@@ -97,21 +112,24 @@ def get_configuration(request):
     return request.param
 
 @pytest.fixture(scope="function")
-def configure_socket_listener(request):
+def configure_socket_listener():
     socket_listener.start()
     socket_listener.listener.set_ssl_configuration(connection_protocol=ssl.PROTOCOL_TLSv1_2,
                                                    certificate='/var/ossec/etc/manager.cert',
                                                    keyfile='/var/ossec/etc/manager.key',
+                                                   options=None,
                                                    cert_reqs=ssl.CERT_OPTIONAL)
+
     while not socket_listener.queue.empty():
         socket_listener.queue.get_nowait()
     socket_listener.event.clear()
+
     yield
     socket_listener.shutdown()
 
 @pytest.mark.parametrize('test_case', [case for case in tests])
 @pytest.fixture(scope="function")
-def set_keys(request, test_case):
+def set_keys(test_case):
     keys = test_case.get('pre_existent_keys', [])
     if not keys:
         return
@@ -119,34 +137,33 @@ def set_keys(request, test_case):
     try:
         with open(CLIENT_KEYS_PATH, "w") as keys_file:
             for key in keys:
-                keys_file.write(key + '\n')
+                keys_file.writelines(key)
             keys_file.close()
     except IOError as exception:
         raise
 
-
-
 @pytest.mark.parametrize('test_case', [case for case in tests])
-def test_agent_auth_enrollment(configure_socket_listener, configure_environment, set_keys, test_case: list):
+@pytest.fixture(scope="function")
+def set_test_case(test_case):
     global CURRENT_TEST_CASE
-    global LAST_MESSAGE
-    LAST_MESSAGE = None
     CURRENT_TEST_CASE = test_case
 
+@pytest.mark.parametrize('test_case', [case for case in tests])
+def test_agent_auth_enrollment(set_test_case, configure_socket_listener, configure_environment, set_keys):
 
-    if 'agent-auth' in test_case.get("skips", []):
+    if 'agent-auth' in CURRENT_TEST_CASE.get("skips", []):
         pytest.skip("This test does not apply to agent-auth")
 
-    launch_agent_auth(test_case.get('configuration', {}))
+    clear_last_message()
+    launch_agent_auth(CURRENT_TEST_CASE.get('configuration', {}))
+
     if 'expected_error' in CURRENT_TEST_CASE:
-        assert check_log_error_conf(test_case.get('expected_error')) != None, \
+        assert check_log_error_conf(CURRENT_TEST_CASE.get('expected_error')) != None, \
             'Expected error log doesn´t occurred'
     else:
-        while not LAST_MESSAGE:
-            time.sleep(1)
-        result = LAST_MESSAGE
-        assert result == test_case['message']['expected'].format(**DEFAULT_VALUES),  \
-            'Expected enrollment request message does not match'
-
+        result = get_last_message()
+        assert result != None, "Enrollment request message never arraived"
+        assert result == CURRENT_TEST_CASE['message']['expected'].format(**DEFAULT_VALUES),  \
+               'Expected enrollment request message does not match'
 
     return
