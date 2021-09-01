7e7249444d456f92e0c1e19376eab5b5cf83a0c8
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-09-01 13:15:15 +0200

    refac: change dockerfile in order to use this branch and DocGenerator index variable name when launching arg

--- docs/DocGenerator/DocGenerator.py
+++ docs/DocGenerator/DocGenerator.py
@@ -212,7 +212,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', help="Test configuration", action='store_true', dest='test_config')
     parser.add_argument('-d', help="Enable debug messages.", action='count', dest='debug_level')
     parser.add_argument('-i', help="Indexes the data to elasticsearch.", dest='index_name')
-    parser.add_argument('-l', help="Indexes the data and launch the application.", dest='launch_app')
+    parser.add_argument('-l', help="Indexes the data and launch the application.", dest='index_name')
     args = parser.parse_args()
 
     if args.debug_level:
@@ -231,7 +231,7 @@ if __name__ == '__main__':
         indexData = IndexData(args.index_name, Config(FIRST_PROPOSAL))
         indexData.run()
     elif args.launch_app:
-        indexData = IndexData(args.launch_app, Config(FIRST_PROPOSAL))
+        indexData = IndexData(args.index_name, Config(FIRST_PROPOSAL))
         indexData.run()
         os.chdir("Search-UI")
         os.system("ELASTICSEARCH_HOST=http://localhost:9200 npm start")
