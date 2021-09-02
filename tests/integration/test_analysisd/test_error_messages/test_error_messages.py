0a9d0e245bcb19dfb392ee1006977f4d9f060232
mdengra <miguel.dengra@wazuh.com>
2021-09-02 13:44:18 +0200

    doc: Create/migrate test_analysisd documentation in QA Docs style
    The following tests have been documented:
      * test_error_messages/test_error_messages.py
      * test_event_messages/test_event_messages.py
      * test_integrity_messages/test_integrity_messages.py
      * test_analysisd/test_scan_messages/test_scan_messages.py
    Migrated test_mitre/test_mitre_check_alert.py
    The scheme of proposal 3 has been used until the definitive one is available.
    PEP-8 fixes
    
    Closes: #1804

--- tests/integration/test_analysisd/test_error_messages/test_error_messages.py
+++ tests/integration/test_analysisd/test_error_messages/test_error_messages.py
@@ -1,7 +1,53 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+'''
+copyright:
+    Copyright (C) 2015-2021, Wazuh Inc.
 
+    Created by Wazuh, Inc. <info@wazuh.com>.
+
+    This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type:
+    integration
+
+description:
+    These tests will check if the `analysisd` daemon handles correctly the invalid events it receives.
+
+tiers:
+    - 0
+
+component:
+    manager
+
+path:
+    tests/integration/test_analysisd/test_error_messages/
+
+daemons:
+    - analysisd
+    - syscheckd
+    - wazuh-db
+
+os_support:
+    - linux, rhel5
+    - linux, rhel6
+    - linux, rhel7
+    - linux, rhel8
+    - linux, amazon linux 1
+    - linux, amazon linux 2
+    - linux, debian buster
+    - linux, debian stretch
+    - linux, debian wheezy
+    - linux, ubuntu bionic
+    - linux, ubuntu xenial
+    - linux, ubuntu trusty
+    - linux, arch linux
+
+coverage:
+
+pytest_args:
+
+tags:
+
+'''
 import os
 
 import pytest
@@ -48,13 +94,45 @@ receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in t
                          ids=[test_case['name'] for test_case in test_cases])
 def test_error_messages(configure_sockets_environment, connect_to_sockets_module, wait_for_analysisd_startup,
                         test_case: list):
-    """Check that every input message in analysisd socket generates the adequate output to wazuh-db socket
+    '''
+    description:
+        Check if when the `analysisd` socket receives a message with an invalid event,
+        it generates the corresponding error that sends to the `wazuh-db` socket.
 
-    Parameters
-    ----------
-    test_case : list
-        List of test_case stages (dicts with input, output and stage keys)
-    """
+    wazuh_min_version:
+        3.12
+
+    parameters:
+        - configure_sockets_environment:
+            type: fixture
+            brief: Configure environment for sockets and MITM.
+
+        - connect_to_sockets_module:
+            type: fixture
+            brief: Module scope version of `connect_to_sockets` fixture.
+
+        - wait_for_analysisd_startup:
+            type: fixture
+            brief: Wait until analysisd has begun and alerts.json is created.
+
+        - test_case:
+            type: list
+            brief: List of tests to be performed.
+
+    assertions:
+        - Check that the error messages generated are consistent with the events received.
+
+    test_input:
+        Different test cases that are contained in an external `YAML` file (error_messages.yaml)
+        that includes `syscheck` events data and the expected output.
+
+    logging:
+        - ossec.log:
+            - "Multiple values located in the `error_messages.yaml` file."
+
+    tags:
+
+    '''
     for stage in test_case:
         receiver_sockets[0].send(stage['input'])
         result = log_monitors[0].start(timeout=4 * global_parameters.default_timeout,
