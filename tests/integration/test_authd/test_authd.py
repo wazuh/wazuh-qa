763c52dcf13f7b9689d222508c8dbf526c3fa49c
pereyra-m <matias.pereyra@wazuh.com>
2021-09-03 15:43:17 -0300

    Updating configuration file name for authd tests

--- tests/integration/test_authd/test_authd.py
+++ tests/integration/test_authd/test_authd.py
@@ -33,7 +33,7 @@ def load_tests(path):
 
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 message_tests = load_tests(os.path.join(test_data_path, 'enroll_messages.yaml'))
-configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
+configurations_path = os.path.join(test_data_path, 'wazuh_authd_configuration.yaml')
 configurations = load_wazuh_configurations(configurations_path, __name__, params=None, metadata=None)
 
 # Variables
