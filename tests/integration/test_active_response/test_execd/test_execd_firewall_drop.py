caacc383d08a8a13b5f0d5829aa9ec0e1a3ad12c
mdengra <miguel.dengra@wazuh.com>
2021-09-06 14:17:21 +0200

    doc: Migrate test documentation for QACTL 0.1
    The following tests have been documented:
      * test_execd_firewall_drop.py
      * test_execd_restart.py
      * test_cache.py
      * test_cache.py
    The tests have been documented following the schema proposed by the qactl team.
    Update DocGenerator/config.yaml
    PEP-8 fixes
    
    Closes: #1840

--- tests/integration/test_active_response/test_execd/test_execd_firewall_drop.py
+++ tests/integration/test_active_response/test_execd/test_execd_firewall_drop.py
@@ -1,7 +1,57 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
-
+'''
+brief:
+    These tests will check if the active responses, which are executed by
+    the `wazuh-execd` program via scripts, run correctly.
+copyright:
+    Copyright (C) 2015-2021, Wazuh Inc.
+    Created by Wazuh, Inc. <info@wazuh.com>.
+    This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+modules:
+    - active response
+daemons:
+    - wazuh-execd
+category:
+    integration
+os_platform:
+    - linux
+    - windows
+os_vendor:
+    - redhat
+    - debian
+    - ubuntu
+    - alas
+    - arch-linux
+    - centos
+    - windows
+os_version:
+    - centos6
+    - centos7
+    - centos8
+    - rhel6
+    - rhel7
+    - rhel8
+    - buster
+    - stretch
+    - wheezy
+    - bionic
+    - xenial
+    - trusty
+    - amazon-linux-1
+    - amazon-linux-2
+    - 7
+    - 8
+    - 10
+    - server-2003
+    - server-2012
+    - server-2016
+tiers:
+    - 0
+tags:
+    - log_monitor
+    - active_response
+component:
+    - agent
+'''
 import json
 import os
 import platform
@@ -189,15 +239,37 @@ def build_message(metadata, expected):
 
 def test_execd_firewall_drop(set_debug_mode, get_configuration, test_version, configure_environment,
                              remove_ip_from_iptables, start_agent, set_ar_conf_mode):
-    """Check if firewall-drop Active Response is executed correctly.
-
-    Args:
-        set_debug_mode (fixture): Set execd daemon in debug mode.
-        test_version (fixture): Validate Wazuh version.
-        set_ar_conf_mode (fixture): Configure Active Responses used in tests.
-        start_agent (fixture): Create Remoted and Authd simulators, register agent and start it.
-        remove_ip_from_iptables (fixture): Remove the test IP from iptables if it exist
-    """
+    '''
+    description:
+        Check if `firewall-drop` command of Active Response is executed correctly.
+    parameters:
+        - set_debug_mode:
+            type: fixture
+            brief: Set execd daemon in debug mode.
+        - get_configuration:
+            type: fixture
+            brief: Get configurations from the module.
+        - test_version:
+            type: fixture
+            brief: Validate Wazuh version.
+        - configure_environment:
+            type: fixture
+            brief: Configure a custom environment for testing.
+        - start_agent:
+            type: fixture
+            brief: Create Remoted and Authd simulators, register agent and start it.
+        - set_ar_conf_mode:
+            type: fixture
+            brief: Configure Active Responses used in tests.
+    wazuh_min_version:
+        4.2
+    behaviour:
+        - Verify that Active Response is enabled by looking in the `ossec.log` and `active-responses.log` files.
+        - Check that `firewall-drop` works by verifying that it adds/deletes the IP sent in iptables.
+    expected_behaviour:
+        - The sent IP is added to iptables.
+        - the sent IP is removed from iptables.
+    '''
     metadata = get_configuration['metadata']
     expected = metadata['results']
     ossec_log_monitor = FileMonitor(LOG_FILE_PATH)
