b644ff1c9f993e850c9544bacb475f33314ad8cb
Víctor Rebollo <victorrebollop@gmail.com>
2021-09-06 17:45:11 +0200

    Remove redundant logging messages

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -615,17 +615,17 @@ def configure_local_internal_options_module(request):
     try:
         local_internal_options = getattr(request.module, 'local_internal_options')
     except AttributeError as local_internal_configuration_not_set:
-        logger.debug('Configure Local Internal Options: Error - local_internal_options is not set')
+        logger.debug('local_internal_options is not set')
         raise local_internal_configuration_not_set
 
     backup_local_internal_options = conf.get_local_internal_options_dict()
 
-    logger.debug('Configure Local Internal Options: DEBUG - Set local_internal_option to ' +
+    logger.debug('Set local_internal_option to ' +
                  f"{str(local_internal_options)}")
     conf.set_local_internal_options_dict(local_internal_options)
 
     yield
 
-    logger.debug('Configure Local Internal Options: DEBUG - Restore local_internal_option to ' +
+    logger.debug('Restore local_internal_option to ' +
                  f"{str(backup_local_internal_options)}")
     conf.set_local_internal_options_dict(backup_local_internal_options)
