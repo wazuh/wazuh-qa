ab3fcf56a283d95cad79c1f9f06b2a449025f87e
pereyra-m <matias.pereyra@wazuh.com>
2021-09-07 23:24:48 -0300

    Creating test for force_insert option

---
+++ tests/integration/test_authd/test_authd_force_insert_yes.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2015-2021, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+import os
+import socket
+import ssl
+import time
+import pytest
+import yaml
+from wazuh_testing.fim import generate_params
+from wazuh_testing.tools import WAZUH_PATH, LOG_FILE_PATH
+from wazuh_testing.tools.configuration import load_wazuh_configurations, set_section_wazuh_conf, write_wazuh_conf
+from wazuh_testing.tools.file import truncate_file, load_tests
+from wazuh_testing.tools.monitoring import SocketController, FileMonitor
+from wazuh_testing.tools.services import control_service, check_daemon_status
+
+# Marks
+
+pytestmark = [pytest.mark.linux, pytest.mark.tier(level=0), pytest.mark.server]
+
+# Configurations
+
+test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+configurations_path = os.path.join(test_data_path, 'wazuh_authd_configuration.yaml')
+client_keys_path = os.path.join(WAZUH_PATH, 'etc', 'client.keys')
+test_authd_force_insert_yes_tests = load_tests(os.path.join(test_data_path, 'test_authd_force_insert_yes.yaml'))
+configurations = load_wazuh_configurations(configurations_path, __name__)
+
+# Variables
+
+log_monitor_paths = []
+receiver_sockets_params = [(("localhost", 1515), 'AF_INET', 'SSL_TLSv1_2')]
+monitored_sockets_params = [('wazuh-modulesd', None, True), ('wazuh-db', None, True), ('wazuh-authd', None, True)]
+receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
+hostname = socket.gethostname()
+
+# Functions
+
+def send_message(message):
+    address, family, connection_protocol = receiver_sockets_params[0]
+    SSL_socket = SocketController(address, family=family, connection_protocol=connection_protocol)
+    try:
+        SSL_socket.open()
+    except ssl.SSLError as exception:
+        # We did not expect this error, fail test
+        raise
+    SSL_socket.send(message, size=False)
+    response = SSL_socket.receive().decode()
+    SSL_socket.close()
+    return response
+
+# Fixtures
+
+@pytest.fixture(scope='module', params=configurations)
+def get_configuration(request):
+    """Get configurations from the module"""
+    return request.param
+
+
+@pytest.fixture(scope='module')
+def clean_client_keys_file():
+    # Stop Wazuh
+    control_service('stop')
+
+    # Clean client.keys
+    try:
+        with open(client_keys_path, 'w') as client_file:
+            client_file.close()
+    except IOError as exception:
+        raise
+
+    # Start Wazuh
+    control_service('start')
+
+
+# Test
+
+@pytest.mark.parametrize('test_case', [case['test_case'] for case in test_authd_force_insert_yes_tests],
+                         ids=[test_case['name'] for test_case in test_authd_force_insert_yes_tests])
+def test_authd_force_options(clean_client_keys_file, get_configuration, configure_environment,
+                             configure_sockets_environment, connect_to_sockets_module, test_case):
+    """Check that every input message in authd port generates the adequate output
+
+    Every test case is defined the following way:
+        - input: message that will be tried to send to the manager
+        - output: expected response
+    """
+
+    for stage in test_case:
+        # Reopen socket (socket is closed by manager after sending message with client key)
+        receiver_sockets[0].open()
+        expected = stage['output']
+        message = stage['input']
+        receiver_sockets[0].send(message, size=False)
+        timeout = time.time() + 10
+        response = ''
+        while response == '':
+            response = receiver_sockets[0].receive().decode()
+            if time.time() > timeout:
+                raise ConnectionResetError('Manager did not respond to sent message!')
+        assert response[:len(expected)] == expected, 'Failed: Response is different from expected'
