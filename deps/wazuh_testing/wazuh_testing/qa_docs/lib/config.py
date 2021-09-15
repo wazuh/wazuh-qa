762e83e81f94477a9891c637ba055835f7195d90
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-09-15 09:35:51 +0200

    add: add debug logging to `config.py` #1879

--- deps/wazuh_testing/wazuh_testing/qa_docs/lib/config.py
+++ deps/wazuh_testing/wazuh_testing/qa_docs/lib/config.py
@@ -37,23 +37,22 @@ class Config():
         self.test_cases_field = None
 
         try:
+            Config.LOGGER.debug('Loading config file')
             with open(args[0]) as fd:
                 self._config_data = yaml.safe_load(fd)
         except:
-            Config.LOGGER.error('Cannot load config file')
             raise QAValueError('Cannot load config file', Config.LOGGER.error)
 
         self._read_function_regex()
         self._read_output_fields()
         self._read_test_cases_field()
-        self._read_documentation_path()
         self._read_include_paths()
         self._read_include_regex()
         self._read_group_files()
         self._read_ignore_paths()
 
         if len(args) >= 3:
-            self.documentation_path = args[2]
+            self._set_documentation_path(args[2])
         if len(args) == 4:
             # It is called with a single test to parse
             self.mode = mode.SINGLE_TEST
@@ -63,41 +62,66 @@ class Config():
 
 
     def _read_test_info(self):
-        '''
-        brief: Reads from the config file the keys to be printed from test info
-        '''
+        """Reads from the config file the keys to be printed from module info.
+
+        This functionality is used to print any custom field(s) you want.
+        You can use it if you only need a few fields to parse when a single test is run.
+        
+            For example you have this in your config.yaml:
+
+            Test info:
+                - test_wazuh_min_version: wazuh_min_version
+        """
+        Config.LOGGER.debug('Reading test info from config file')
         if 'Test info' in self._config_data:
             self.test_info = self._config_data['Test info']
+        else:
+            Config.LOGGER.warning('Cannot read test info fields')
     
     def _read_module_info(self):
-        '''
-        brief: Reads from the config file the keys to be printed from module info
-        '''
+        """Reads from the config file the fields to be printed from test info.
+
+        This functionality is used to print any custom field(s) you want.
+        You can use it if you only need a few fields to parse when a single test is run.
+        
+            For example you have this in your config.yaml:
+
+            Module info:
+                - test_system: os_platform
+                - test_vendor: os_vendor
+                - test_version: os_version
+                - test_target: component
+        """
+        Config.LOGGER.debug('Reading module info from config file')
+
         if 'Module info' in self._config_data:
             self.module_info = self._config_data['Module info']
+        else:
+            Config.LOGGER.warning('Cannot read module info fields')
 
-    def _read_project_path(self):
+    def _set_documentation_path(self, path):
         """
-        brief: Reads from the config file the path of the project.
+        brief: Sets the path of the documentation output.
         """
-        if 'Project path' in self._config_data:
-            self.project_path = self._config_data['Project path']
+        Config.LOGGER.debug('Setting the path documentation')
 
-    def _read_documentation_path(self):
-        """
-        brief: Reads from the config file the path of the documentation output.
-        """
-        if 'Output path' in self._config_data:
-            self.documentation_path = self._config_data['Output path']
+        if path:
+            self.documentation_path = path
+        else:
+            Config.LOGGER.warning('You have not passed a path where the documentation data is dumped')
 
     def _read_include_paths(self):
         """
         brief: Reads from the config file all the paths to be included in the parsing.
         """
+        Config.LOGGER.debug('Reading include paths from config file')
+
+        # Will be replaced by --type --module and --test , so you can run what you need
         if not 'Include paths' in self._config_data:
-            Config.LOGGER.error('Config include paths are empty')
             raise QAValueError('Config include paths are empty', Config.LOGGER.error)
+
         include_paths = self._config_data['Include paths']
+
         for path in include_paths:
             self.include_paths.append(os.path.join(self.project_path, path))
 
@@ -105,35 +129,44 @@ class Config():
         """
         brief: Reads from the config file the regexes used to identify test files.
         """
+        Config.LOGGER.debug('Reading the regular expressions to include files from config file')
+
         if not 'Include regex' in self._config_data:
-            Config.LOGGER.error('Config include regex is empty')
             raise QAValueError('Config include regex is empty', Config.LOGGER.error)
+
         self.include_regex = self._config_data['Include regex']
 
     def _read_group_files(self):
         """
         brief: Reads from the config file the file name to be identified with a group.
         """
+        Config.LOGGER.debug('Reading group files from config file')
+
         if not 'Group files' in self._config_data:
-            Config.LOGGER.error("Config group files is empty")
             raise QAValueError('Config include paths are empty', Config.LOGGER.error)
+
         self.group_files = self._config_data['Group files']
 
     def _read_function_regex(self):
         """
         brief: Reads from the config file the regexes used to identify a test method.
         """
+        Config.LOGGER.debug('Reading the regular expressions to include test methods from config file')
+
         if not 'Function regex' in self._config_data:
-            Config.LOGGER.error('Config function regex is empty')
             raise QAValueError('Config function regex is empty', Config.LOGGER.error)
+
         self.function_regex = self._config_data['Function regex']
 
     def _read_ignore_paths(self):
         """
         brief: Reads from the config file all the paths to be excluded from the parsing.
         """
+        Config.LOGGER.debug('Reading the paths to be ignored from config file')
+
         if 'Ignore paths' in self._config_data:
             ignore_paths = self._config_data['Ignore paths']
+
             for path in ignore_paths:
                 self.ignore_paths.append(os.path.join(self.project_path, path))
 
@@ -141,15 +174,19 @@ class Config():
         """
         brief: Reads from the config file the optional and mandatory fields for the test module.
         """
+        Config.LOGGER.debug('Reading mandatory and optional module fields from config file')
+
         if not 'Module' in self._config_data['Output fields']:
-            Config.LOGGER.error('Config output module fields is missing')
             raise QAValueError('Config output module fields is missing', Config.LOGGER.error)
+
         module_fields = self._config_data['Output fields']['Module']
+
         if not 'Mandatory' in module_fields and not 'Optional' in module_fields:
-            Config.LOGGER.error('Config output module fields are empty')
             raise QAValueError('Config output module fields are empty', Config.LOGGER.error)
+
         if 'Mandatory' in module_fields:
             self.module_fields.mandatory = module_fields['Mandatory']
+
         if 'Optional' in module_fields:
             self.module_fields.optional = module_fields['Optional']
 
@@ -157,15 +194,19 @@ class Config():
         """
         brief: Reads from the config file the optional and mandatory fields for the test functions.
         """
+        Config.LOGGER.debug('Reading mandatory and optional test fields from config file')
+
         if not 'Test' in self._config_data['Output fields']:
-            Config.LOGGER.error('Config output test fields is missing')
             raise QAValueError('Config output test fields is missing', Config.LOGGER.error)
+    
         test_fields = self._config_data['Output fields']['Test']
+
         if not 'Mandatory' in test_fields and not 'Optional' in test_fields:
-            Config.LOGGER.error('Config output test fields are empty')
             raise QAValueError('Config output test fields are empty', Config.LOGGER.error)
+
         if 'Mandatory' in test_fields:
             self.test_fields.mandatory = test_fields['Mandatory']
+
         if 'Optional' in test_fields:
             self.test_fields.optional = test_fields['Optional']
 
@@ -174,8 +215,8 @@ class Config():
         brief: Reads all the mandatory and optional fields.
         """
         if not 'Output fields' in self._config_data:
-            Config.LOGGER.error('Config output fields is missing')
             raise QAValueError('Config output fields is missing', Config.LOGGER.error)
+
         self._read_module_fields()
         self._read_test_fields()
 
@@ -183,6 +224,8 @@ class Config():
         """
         brief: Reads from the configuration file the key to identify a Test Case list.
         """
+        Config.LOGGER.debug('Reading Test Case key from config file')
+
         if 'Test cases field' in self._config_data:
             self.test_cases_field = self._config_data['Test cases field']
 
