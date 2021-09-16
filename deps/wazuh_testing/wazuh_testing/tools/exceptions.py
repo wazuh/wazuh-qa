fda1004a52e8b80a837151300e5c769f2e445e6e
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-09-16 10:35:46 +0200

    add: add QAExceptions and custom loggin class.

---
+++ deps/wazuh_testing/wazuh_testing/tools/exceptions.py
@@ -0,0 +1,5 @@
+class QAValueError(Exception):
+    def __init__(self, message, logger=None):
+        self.message = f"\033[91m{message}\033[0m"
+        logger(message)
+        super().__init__(self.message)
