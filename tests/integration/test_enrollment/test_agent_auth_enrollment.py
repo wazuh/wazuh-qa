cc9fd611484bb08928e38aa6f9c80d31d928aa08
palaciosjeremias <jeremias.palacios@wazuh.com>
2021-09-16 03:41:34 -0700

    Migrate log_monitor to fixture implementation

--- tests/integration/test_enrollment/test_agent_auth_enrollment.py
+++ tests/integration/test_enrollment/test_agent_auth_enrollment.py
@@ -83,7 +83,7 @@ def get_configuration(request):
 
 
 @pytest.mark.parametrize('test_case', [case for case in tests])
-def test_agent_auth_enrollment(configure_environment, create_certificates, set_keys, set_pass, test_case: list):
+def test_agent_auth_enrollment(configure_environment, create_certificates, set_keys, set_pass, file_monitoring, request, test_case: list):
     """
         test_logic:
             "Check that different configuration generates the adequate enrollment message or the corresponding
@@ -99,15 +99,15 @@ def test_agent_auth_enrollment(configure_environment, create_certificates, set_k
 
     control_service('stop', daemon='wazuh-agentd')
 
-    if 'expected_error' in test_case:
-        receiver_callback = lambda received_event: ""
-        socket_listener = configure_socket_listener(receiver_callback)
-        # Monitor ossec.log file
-        truncate_file(LOG_FILE_PATH)
-        log_monitor = FileMonitor(LOG_FILE_PATH)
 
+
+    receiver_callback = lambda received_event: test_response if test_expected == received_event else "".encode()
+    socket_listener = configure_socket_listener(receiver_callback)
+
+    if 'expected_error' in test_case:
         launch_agent_auth(test_case.get('configuration', {}))
 
+        log_monitor = request.module.log_monitor
         if test_case.get('expected_fail'):
             with pytest.raises(TimeoutError):
                 log_monitor.start(timeout=AGENT_AUTH_TIMEOUT,
@@ -116,16 +116,13 @@ def test_agent_auth_enrollment(configure_environment, create_certificates, set_k
             log_monitor.start(timeout=AGENT_AUTH_TIMEOUT,
                             callback=make_callback(test_case.get('expected_error'), prefix='.*', escape=True),
                             error_message='Expected error log does not occured')
-        socket_listener.shutdown()
 
     else:
-        test_expected = test_case['message']['expected'].format(host_name=host_name)
-        test_response = test_case['message']['response'].format(host_name=host_name)
-        receiver_callback = lambda received_event: test_response if test_expected.encode() == received_event else ""
-        socket_listener = configure_socket_listener(receiver_callback)
+        test_expected = test_case['message']['expected'].format(host_name=host_name).encode()
+        test_response = test_case['message']['response'].format(host_name=host_name).encode()
         # Monitor MITM queue
         socket_monitor = QueueMonitor(socket_listener.queue)
-        event = (test_expected.encode(), test_response)
+        event = (test_expected, test_response)
 
         launch_agent_auth(test_case.get('configuration', {}))
 
@@ -133,5 +130,7 @@ def test_agent_auth_enrollment(configure_environment, create_certificates, set_k
             # Start socket monitoring
             socket_monitor.start(timeout=AGENT_AUTH_TIMEOUT, callback=lambda received_event: event == received_event,
                                  error_message='Enrollment request message never arrived', update_position=False)
-        finally:
-            socket_listener.shutdown()
+        except:
+            pass
+
+    socket_listener.shutdown()
