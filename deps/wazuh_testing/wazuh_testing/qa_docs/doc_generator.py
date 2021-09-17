a311e06e5a944050a60fa4fca5126415d5643229
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-09-17 11:53:59 +0200

    doc: Add `doc_generator.py` documentation. #1899
    Also, few incorrect sentences have been corrected because they may create doubts when a user would be read them.

--- deps/wazuh_testing/wazuh_testing/qa_docs/doc_generator.py
+++ deps/wazuh_testing/wazuh_testing/qa_docs/doc_generator.py
@@ -6,6 +6,7 @@ import os
 import re
 import json
 import yaml
+
 from wazuh_testing.qa_docs.lib.config import Config, Mode
 from wazuh_testing.qa_docs.lib.code_parser import CodeParser
 from wazuh_testing.qa_docs.lib.utils import clean_folder
@@ -14,13 +15,29 @@ from wazuh_testing.tools.logging import Logging
 from wazuh_testing.tools.exceptions import QAValueError
 
 class DocGenerator:
-    """
-    brief: Main class of DocGenerator tool.
-    It´s in charge of walk every test file, and every group file to dump the parsed documentation.
+    """Main class of DocGenerator tool.
+
+    It is in charge of walk every test file, and every group file to dump the parsed documentation.
+    Every folder is checked so they are ignored when the path matches. Then, every test from folders not ignored
+    that matches a include regex, is parsed.
+
+    Attributes:
+        conf: A `Config` instance with data loaded from config file.
+        parser: A `CodeParser` instance with parsing utilities.
+        __id_counter: An integer that counts the test/group ID when it is created.
+        ignore_regex: A list with compiled paths to be ignored.
+        include_regex: A list with regular expressions used to parse a file or not.
     """
     LOGGER = Logging.get_logger(QADOCS_LOGGER)
 
     def __init__(self, config):
+        """Class constructor
+
+        Initialize every attribute.
+
+        Args:
+            config: A `Config` instance with data loaded from config file.
+        """
         self.conf = config
         self.parser = CodeParser(self.conf)
         self.__id_counter = 0
@@ -33,48 +50,56 @@ class DocGenerator:
                 self.include_regex.append(re.compile(include_regex))
 
     def is_valid_folder(self, path):
-        """
-        brief: Checks if a path should be ignored because it is in the ignore list.
-        args:
-            - "path (str): Folder location to be controlled"
-        returns: "boolean: False if the path should be ignored. True otherwise."
+        """Check if a folder is included so it would be parsed.
+
+        That happens when is not ignored using the ignore list.
+        
+        Args:
+            path: A string that contains the folder location to be controlled
+
+        Return: 
+            A boolean with False if the path should be ignored. True otherwise.
         """
         for regex in self.ignore_regex:
             if regex.match(path):
-                DocGenerator.LOGGER.warning(f"Folder validation: {regex} not matching with {path}")
+                DocGenerator.LOGGER.debug(f"Ignoring path: {regex} matching with {path}")
                 return False
 
         return True
 
     def is_valid_file(self, file):
-        """
-        brief: Checks if a file name should be ignored because it's in the ignore list
-        or doesn´t match with the regexes.
-        args:
-            - "file (str): File name to be controlled"
-        returns: "boolean: False if the file should be ignored. True otherwise."
-        """
+        """Check if a path file is included.
 
+        Also, that file could be ignored(because it is in the ignore list or does not match with the regexes).
+
+        Args:
+            file: A string that contains the file name to be checked.
+
+        Returns:
+            A boolean with True when matches with an include regex. False if the file should be ignored
+            (because it matches with an ignore path or does not match with any include regular expression).
+        """
         for regex in self.ignore_regex:
             if regex.match(file):
-                DocGenerator.LOGGER.warning(f"File validation: {regex} not matching with {file}")
+                DocGenerator.LOGGER.debug(f"Ignoring file: {regex} matching with {file}")
                 return False
 
         for regex in self.include_regex:
             if regex.match(file):
-                DocGenerator.LOGGER.warning(f"File validation: {regex} not matching with {file}")
+                DocGenerator.LOGGER.debug(f"Including file: {regex} matching with {file}")
                 return True
 
         return False
 
     def is_group_file(self, path):
-        """
-        brief: Checks if a file path should be considered as a file containing group information.
-        args:
-            - "path (str): File location to be controlled"
-        returns: "boolean: True if the file is a group file. False otherwise."
-        """
+        """Check if a file path should be considered as a file containing group information.
 
+        Args:
+            path: A string that contains the file name to be checked
+
+        Returns:
+            A boolean with True if the file is a group file. False otherwise."
+        """
         for group_file in self.conf.group_files:
             if path == group_file:
                 return True
@@ -82,9 +107,9 @@ class DocGenerator:
         return False
 
     def get_group_doc_path(self, group):
-        """
-        brief: Returns the name of the group file in the documentation output based on the original file name.
-        returns: "string: The name of the documentation group file"
+        """Get the name of the group file in the documentation output based on the original file name.
+
+        Returns: A string that contains the name of the documentation group file.
         """
         base_path = os.path.join(self.conf.documentation_path, os.path.basename(self.scan_path))
         doc_path = os.path.join(base_path, group['name']+".group")
@@ -92,12 +117,15 @@ class DocGenerator:
         return doc_path
 
     def get_test_doc_path(self, path):
+        """Get the name of the test file in the documentation output based on the original file name.
+
+        Args:
+            path: A string that contains the original file name.
+
+        Returns:
+            A string with the name of the documentation test file.
         """
-        brief: Returns the name of the test file in the documentation output based on the original file name.
-        args:
-            - "path (str): The original file name"
-        returns: "string: The name of the documentation test file"
-        """
+
         base_path = os.path.join(self.conf.documentation_path, os.path.basename(self.scan_path))
         relative_path = path.replace(self.scan_path, "")
         doc_path = os.path.splitext(base_path + relative_path)[0]
@@ -105,12 +133,17 @@ class DocGenerator:
         return doc_path
 
     def dump_output(self, content, doc_path):
-        """
-        brief: Creates a JSON and a YAML file with the parsed content of a test module.
-        It also creates the containing folder if it doesn´t exists.
-        args:
-            - "content (dict): The parsed content of a test file."
-            - "doc_path (string): The path where the information should be dumped."
+        """Create a JSON and a YAML file with the parsed content of a test module.
+        
+        Also, create the containing folder if it does not exist.
+
+        Args:
+            content: A dict that contains the parsed content of a test file.
+            doc_path: A string with the path where the information should be dumped.
+
+        Raises:
+            QAValueError: Cannot write in {doc_path}.json
+            QAValueError: Cannot write in {doc_path}.yaml
         """
         if not os.path.exists(os.path.dirname(doc_path)):
             DocGenerator.LOGGER.debug('Creating documentation folder')
@@ -131,12 +164,15 @@ class DocGenerator:
             raise QAValueError(f"Cannot write in {doc_path}.yaml", DocGenerator.LOGGER.error)
 
     def create_group(self, path, group_id):
-        """
-        brief: Parses the content of a group file and dumps the content into a file.
-        args:
-            - "path (string): The path of the group file to be parsed."
-            - "group_id (string): The id of the group where the new group belongs."
-        return "integer: The ID of the new generated group document.
+        """Parse the content of a group file and dump the content into a file.
+
+        Args:
+            path: A string with the path of the group file to be parsed.
+            group_id: A string with the id of the group where the new group belongs.
+
+        Returns:
+            An integer with the ID of the newly generated group document.
+            None if the test does not have documentation.
         """
         self.__id_counter = self.__id_counter + 1
         group = self.parser.parse_group(path, self.__id_counter, group_id)
@@ -151,12 +187,15 @@ class DocGenerator:
             return None
 
     def create_test(self, path, group_id):
-        """
-        brief: Parses the content of a test file and dumps the content into a file.
-        args:
-            - "path (string): The path of the test file to be parsed."
-            - "group_id (string): The id of the group where the new test belongs."
-        return "integer: The ID of the new generated test document.
+        """Parse the content of a test file and dumps the content into a file.
+
+        Args:
+            path: A string with the path of the test file to be parsed.
+            group_id: A string with the id of the group where the new test belongs.
+
+        Returns:
+            An integer with the ID of the new generated test document.
+            None if the test does not have documentation.
         """
         self.__id_counter = self.__id_counter + 1
         test = self.parser.parse_test(path, self.__id_counter, group_id)
@@ -167,8 +206,9 @@ class DocGenerator:
             elif self.conf.mode == Mode.SINGLE_TEST:
                 doc_path = self.conf.documentation_path
                 if self.print_test_info(test) is None:
-                    # 
+                    # When only a test is parsed, exit
                     return
+
             self.dump_output(test, doc_path)
             DocGenerator.LOGGER.debug(f"New documentation file '{doc_path}' was created with ID:{self.__id_counter}")
             return self.__id_counter
@@ -177,18 +217,17 @@ class DocGenerator:
             return None
 
     def parse_folder(self, path, group_id):
-        """
-        brief: Search in a specific folder to parse possible group files and each test file.
-        args:
-            - "path (string): The path of the folder to be parsed."
-            - "group_id (string): The id of the group where the new elements belong."
+        """Search in a specific folder to parse possible group files and each test file.
+
+        Args:
+            path: A string with the path of the folder to be parsed.
+            group_id: A string with the id of the group where the new elements belong.
         """
         if not os.path.exists(path):
             DocGenerator.LOGGER.warning(f"Include path '{path}' doesn´t exist")
             return
 
         if not self.is_valid_folder(path):
-            DocGenerator.LOGGER.debug(f"Ignoring files on '{path}'")
             return
 
         (root, folders, files) = next(os.walk(path))
@@ -207,8 +246,10 @@ class DocGenerator:
             self.parse_folder(os.path.join(root, folder), group_id)
 
     def locate_test(self):
-        """
-        brief: try to get the test path
+        """Get the test path when a test is specified by the user.
+
+        Returns:
+            A string with the test path.
         """
         complete_test_name = f"{self.conf.test_name}.py"
         DocGenerator.LOGGER.info(f"Looking for {complete_test_name}")
@@ -222,9 +263,14 @@ class DocGenerator:
         return None
 
     def print_test_info(self, test):
-        """
-        brief: Print the test info to standard output. If an output path is specified,
-               the output is redirected to `output_path/test_info.json`.
+        """Print the test info to standard output. If an output path is specified by the user,
+           the output is redirected to `output_path/{test_name}.json`.
+
+        Return None to avoid the default parsing behaviour.
+        This method will change, will only print the test data but the JSON will be generated using `dump_output`.
+
+        Args:
+            test: A dict with the parsed test data
         """
         # dump into file
         if self.conf.documentation_path:
@@ -258,12 +304,22 @@ class DocGenerator:
                 for name, schema_field in field.items():
                     print(str(name)+": "+str(test['tests'][0][schema_field]))
                     
-            return None
+        return None
 
     def run(self):
-        """
-        brief: Run a complete scan of each include path to parse every test and group found.
-               Normal mode: expected behaviour, Single test mode: found the test required and par it
+        """Run a complete scan of each included path to parse every test and group found.
+            
+        Default mode: parse the files within the included paths.
+        Single test mode: found the test required and parse it.
+
+            For example:
+            qa-docs -I ../../tests/ -> It would be running as `default mode`.
+
+            qa-docs -I ../../tests/ -T test_cache -> It would be running as `single test mode`
+            using the standard output
+
+            qa-docs -I ../../tests/ -T test_cache -o /tmp -> It would be running as `single test mode`
+            creating `/tmp/test_cache.json`
         """
         if self.conf.mode == Mode.DEFAULT:
             DocGenerator.LOGGER.info("Starting documentation parsing")
