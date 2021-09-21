f813123cfe06cb0d5dff2ca7ed5eed290de9f89d
jmv74211 <jonathan.martin@wazuh.com>
2021-09-21 17:22:17 +0200

    refac: Improve QA exceptions according to the logging level #1900

--- deps/wazuh_testing/wazuh_testing/tools/exceptions.py
+++ deps/wazuh_testing/wazuh_testing/tools/exceptions.py
@@ -1,10 +1,23 @@
+import sys
 
-class AnsibleException(Exception):
-    def __init__(self, message):
-        super().__init__(message)
 
-class QAValueError(Exception):
+class QABaseException(Exception):
     def __init__(self, message, logger=None):
         self.message = f"\033[91m{message}\033[0m"
-        logger(message)
-        super().__init__(self.message)
+        if logger:
+            logger(self.message)
+            if logger.__name__ == 'debug':
+                super().__init__(self.message)
+            else:
+                sys.exit(1)
+        else:
+            super().__init__(self.message)
+
+class AnsibleException(QABaseException):
+    def __init__(self, message, logger=None):
+        super().__init__(message, logger)
+
+
+class QAValueError(QABaseException):
+    def __init__(self, message, logger=None):
+        super().__init__(message, logger)
