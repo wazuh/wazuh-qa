0473dda4a7ecdeed19459b2f61485f0747563d5b
mdengra <miguel.dengra@wazuh.com>
2021-09-29 16:57:00 +0200

    doc: Add test_env_variables and test_file_limit of test_fim/test_files documentation in QA Docs style
    The following tests have been documentated:
      * test_dir.py
      * test_dir_win32.py
      * test_ignore.py
      * test_nodiff.py
    The current scheme of the issue #1694 has been used.
    PEP-8 fixes.
    
    Closes: #1945

--- tests/integration/test_fim/test_files/test_env_variables/test_dir.py
+++ tests/integration/test_fim/test_files/test_env_variables/test_dir.py
@@ -1,7 +1,76 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+'''
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
 
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: File Integrity Monitoring (FIM) system watches selected files and triggering alerts
+       when these files are modified. Specifically, these tests will check if FIM events are
+       generated when multiple environment variables are used to monitor directories.
+       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks  configured
+       files for changes to the checksums, permissions, and ownership.
+
+tier: 2
+
+modules:
+    - fim
+
+components:
+    - agent
+    - manager
+
+daemons:
+    - wazuh-syscheckd
+
+os_platform:
+    - linux
+    - windows
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+    - Windows 10
+    - Windows 8
+    - Windows 7
+    - Windows Server 2016
+    - Windows server 2012
+    - Windows server 2003
+    - Windows XP
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html#directories
+
+pytest_args:
+    - fim_mode:
+        realtime: Enable real-time monitoring on Linux (using the 'inotify' system calls) and Windows systems.
+        whodata: Implies real-time monitoring but adding the 'who-data' information.
+    - tier:
+        0: Only level 0 tests are performed, they check basic functionalities and are quick to perform.
+        1: Only level 1 tests are performed, they check functionalities of medium complexity.
+        2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
+
+tags:
+    - fim_env_variables
+'''
 import os
 import sys
 
@@ -61,9 +130,49 @@ def get_configuration(request):
 ])
 def test_tag_directories(directory, get_configuration, put_env_variables, configure_environment,
                          restart_syscheckd, wait_for_fim_start):
-    """
-    Test alerts are generated when monitor environment variables
-    """
+    '''
+    description: Check if the 'wazuh-syscheckd' daemon detects CUD events ('added', 'modified', and 'deleted')
+                 when environment variables are used to monitor directories. For this purpose, the test
+                 will monitor a directory that is defined in an environment variable. Then, different
+                 operations will be performed on testing files, and finally, the test will verify
+                 that the proper FIM events have been generated.
+
+    wazuh_min_version: 4.2.0
+
+    parameters:
+        - directory:
+            type: str
+            brief: Path to the directory to be monitored.
+        - get_configuration:
+            type: fixture
+            brief: Get configurations from the module.
+        - put_env_variables:
+            type: fixture
+            brief: Create the environment variables.
+        - configure_environment:
+            type: fixture
+            brief: Configure a custom environment for testing.
+        - restart_syscheckd:
+            type: fixture
+            brief: Clear the 'ossec.log' file and start a new monitor.
+        - wait_for_fim_start:
+            type: fixture
+            brief: Wait for realtime start, whodata start, or end of initial FIM scan.
+
+    assertions:
+        - Verify that FIM events are generated when environment variables are used to monitor directories.
+
+    input_description: A test case (ossec_conf) is contained in external YAML file (wazuh_conf_dir.yaml) which
+                       includes configuration settings for the 'wazuh-syscheckd' daemon and, it is combined
+                       with the directories to be monitored defined as environment variables in this module.
+
+    expected_output:
+        - r'.*Sending FIM event: (.+)$' ('added', 'modified', and 'deleted' events)
+
+    tags:
+        - scheduled
+        - time_travel
+    '''
     regular_file_cud(directory, wazuh_log_monitor, file_list=["testing_env_variables"],
                      min_timeout=global_parameters.default_timeout,
                      time_travel=get_configuration['metadata']['fim_mode'] == 'scheduled')
