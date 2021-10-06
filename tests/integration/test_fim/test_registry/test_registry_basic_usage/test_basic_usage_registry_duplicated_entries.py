bc50b32431685068b59c3d361da358bec409daf7
Antonio Zambrano Fernández <antonio.zambrano@wazuh.com>
2021-10-06 20:25:03 +0200

    Skiping test if it is not being executed in v4.2.3

--- tests/integration/test_fim/test_registry/test_registry_basic_usage/test_basic_usage_registry_duplicated_entries.py
+++ tests/integration/test_fim/test_registry/test_registry_basic_usage/test_basic_usage_registry_duplicated_entries.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+import json
 from wazuh_testing import global_parameters
 import wazuh_testing.fim as fim
 from wazuh_testing.tools.configuration import load_wazuh_configurations
@@ -39,6 +40,10 @@ test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data
 
 monitoring_modes = ['scheduled']
 
+f = open('../../version.json')
+data = json.load(f)
+version = data['version']
+
 # Configurations
 
 conf_params = {'WINDOWS_DUPLICATED_REGISTRY_1': registry_1,
@@ -62,6 +67,7 @@ def get_configuration(request):
 
 # Test
 
+@pytest.mark.skipif(version != 'v4.2.3', reason="This test fails unless it is executed in v4.2.3")
 @pytest.mark.parametrize('key, subkey1, subkey2, arch', [(key, sub_key_1, sub_key_2, fim.KEY_WOW64_32KEY)])
 def test_registry_duplicated_entry(key, subkey1, subkey2, arch, get_configuration, configure_environment,
                                    file_monitoring, configure_local_internal_options_module, daemons_handler,
