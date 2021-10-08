1c58369f4012b118a1fe8ee29958552f08d7168d
damarisg <seyla.gomez@wazuh.com>
2021-10-07 22:32:40 -0300

    Disable tests affected by #1602

--- tests/integration/test_fim/test_files/test_checks/test_check_others.py
+++ tests/integration/test_fim/test_files/test_checks/test_check_others.py
@@ -114,6 +114,7 @@ else:
     ])
 
 
+@pytest.mark.skip(reason="It will be blocked by #1602, when it was solve we can enable again this test")
 @pytest.mark.parametrize('path, checkers', parametrize_list)
 def test_check_others(path, checkers, get_configuration, configure_environment,
                       restart_syscheckd, wait_for_fim_start):
