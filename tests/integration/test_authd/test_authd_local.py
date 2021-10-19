f31c7a7f50f0a8999055c0d1780a2ea34472ae0c
pereyra-m <matias.pereyra@wazuh.com>
2021-10-19 02:58:12 +0000

    Updating tests with new force configuration block

--- tests/integration/test_authd/test_authd_local.py
+++ tests/integration/test_authd/test_authd_local.py
@@ -51,11 +51,14 @@ import subprocess
 
 import pytest
 import yaml
-from wazuh_testing.tools import WAZUH_PATH, CLIENT_KEYS_PATH
+import time
+from wazuh_testing.tools import WAZUH_PATH, CLIENT_KEYS_PATH, WAZUH_DB_SOCKET_PATH
 from wazuh_testing.tools.configuration import load_wazuh_configurations
+from wazuh_testing.wazuh_db import query_wdb
 # TODO Move to utils
 from wazuh_testing.tools.services import control_service
 from wazuh_testing.tools.file import read_yaml
+from wazuh_testing.tools.file import truncate_file
 
 # Marks
 
@@ -72,98 +75,201 @@ configurations = load_wazuh_configurations(configurations_path, __name__, params
 # Variables
 log_monitor_paths = []
 ls_sock_path = os.path.join(os.path.join(WAZUH_PATH, 'queue', 'sockets', 'auth'))
-receiver_sockets_params = [(ls_sock_path, 'AF_UNIX', 'TCP')]
+receiver_sockets_params = [(ls_sock_path, 'AF_UNIX', 'TCP'), (WAZUH_DB_SOCKET_PATH, 'AF_UNIX', 'TCP')]
+test_case_ids = [f"{test_case['name']}" for test_case in message_tests]
 
 # TODO Replace or delete
 monitored_sockets_params = [('wazuh-modulesd', None, True), ('wazuh-db', None, True), ('wazuh-authd', None, True)]
-
 receiver_sockets, monitored_sockets, log_monitors = None, None, None  # Set in the fixtures
 
+# Fixtures
 
-# Tests
-
-@pytest.fixture(scope="module", params=configurations)
+@pytest.fixture(scope="module", params=configurations, ids=['authd_local_config'])
 def get_configuration(request):
     """Get configurations from the module"""
     yield request.param
 
 
-@pytest.fixture(scope="function", params=message_tests)
-def set_up_groups_keys(request):
+@pytest.fixture(scope='function', params=message_tests, ids=test_case_ids)
+def get_current_test_case(request):
     """
-    Set pre-existent groups and keys.
+    Get current test case from the module
     """
-    keys = request.param.get('pre_existent_keys', [])
+    return request.param
 
-    # Stop Wazuh
-    control_service('stop')
-    # Write keys
-    try:
-        # The client.keys file is cleaned always
-        # but the keys are added only if pre_existent_keys has values
-        with open(CLIENT_KEYS_PATH, "w") as keys_file:
-            if(keys is not None):
-                for key in keys:
-                    keys_file.write(key + '\n')
-            keys_file.close()
-    except IOError as exception:
-        raise
 
-    # Starting wazuh in another fixture
-    # control_service('start')
+@pytest.fixture(scope="function")
+def set_up_groups(get_current_test_case, request):
+    """
+    Set pre-existent groups.
+    """
 
-    groups = request.param.get('groups', [])
+    groups = get_current_test_case.get('groups', [])
+
     for group in groups:
         subprocess.call(['/var/ossec/bin/agent_groups', '-a', '-g', f'{group}', '-q'])
 
-    yield request.param
+    yield
 
     for group in groups:
         subprocess.call(['/var/ossec/bin/agent_groups', '-r', '-g', f'{group}', '-q'])
 
+@pytest.fixture(scope='function')
+def insert_pre_existent_agents_client_keys(get_current_test_case, request):
 
-def test_ossec_auth_messages(set_up_groups_keys, get_configuration, configure_environment,
-                             configure_sockets_environment_function, connect_to_sockets_function,
-                             wait_for_authd_startup_module):
-    '''
-    description: Check that every input message in trough local 'authd' port generates the adequate response to worker.
+    control_service("stop", daemon='wazuh-authd')
 
-    wazuh_min_version: 4.2.0
+    agents = get_current_test_case.get('pre_existent_agents', [])
 
-    parameters:
-        - set_up_groups_keys:
-            type: fixture
-            brief: Set pre-existent groups and keys.
-        - get_configuration:
-            type: fixture
-            brief: Get the configuration of the test.
-        - configure_environment:
-            type: fixture
-            brief: Configure a custom environment for testing.
-        - configure_sockets_environment_function:
-            type: fixture
-            brief: Configure the socket listener to receive and send messages on the sockets at function scope.
-        - connect_to_sockets_function:
-            type: fixture
-            brief: Bind to the configured sockets at function scope.
-        - wait_for_authd_startup_module:
-            type: fixture
-            brief: Waits until Authd is accepting connections.
+    with open(CLIENT_KEYS_PATH, "w") as keys_file:
 
-    assertions:
-        - The received output must match with expected.
-        - The enrollment messages are parsed as expected.
-        - The agent keys are denied if the hash is the same than the manager's.
+        for agent in agents:
 
-    input_description:
-        Different test cases are contained in an external YAML file (local_enroll_messages.yaml) which
-        includes the different possible registration requests and the expected responses.
+            if 'id' in agent:
+                id = agent['id']
+            else:
+                id = '001'
 
-    expected_output:
-        - Registration request responses on 'authd' socket.
-    '''
-    test_case = set_up_groups_keys['test_case']
-    for stage in test_case:
+            if 'name' in agent:
+                name = agent['name']
+            else:
+                name = f'TestAgent{id}'
+
+            if 'ip' in agent:
+                ip = agent['ip']
+            else:
+                ip = 'any'
+
+            if 'key' in agent:
+                key = agent['key']
+            else:
+                key = 'TopSecret'
+
+            # Write agent in client.keys
+            keys_file.write(f'{id} {name} {ip} {key}\n')
+
+    keys_file.close()
+
+    control_service("start", daemon='wazuh-authd')
+
+
+@pytest.fixture(scope='function')
+def insert_pre_existent_agents_db(get_current_test_case, request):
+    agents = get_current_test_case.get('pre_existent_agents', [])
+    time_now = int(time.time())
+
+    # Clean agents from DB
+    command = f'global sql DELETE FROM agent WHERE id != 0'
+    query_wdb(command)
+
+    for agent in agents:
+        if 'id' in agent:
+            id = agent['id']
+        else:
+            id = '001'
+
+        if 'name' in agent:
+            name = agent['name']
+        else:
+            name = f'TestAgent{id}'
+
+        if 'ip' in agent:
+            ip = agent['ip']
+        else:
+            ip = 'any'
+
+        if 'key' in agent:
+            key = agent['key']
+        else:
+            key = 'TopSecret'
+
+        if 'connection_status' in agent:
+            connection_status = agent['connection_status']
+        else:
+            connection_status = 'never_connected'
+
+        if 'disconnection_time' in agent and 'delta' in agent['disconnection_time']:
+            disconnection_time = time_now + agent['disconnection_time']['delta']
+        elif 'disconnection_time' in agent and 'value' in agent['disconnection_time']:
+            disconnection_time = agent['disconnection_time']['value']
+        else:
+            disconnection_time = 0
+
+        if 'registration_time' in agent and 'delta' in agent['registration_time']:
+            registration_time = time_now + agent['registration_time']['delta']
+        elif 'registration_time' in agent and 'value' in agent['registration_time']:
+            registration_time = agent['registration_time']['value']
+        else:
+            registration_time = time_now
+
+        # Write agent in global.db
+        command = f'global insert-agent {{"id":{id},"name":"{name}","ip":"{ip}","date_add":{registration_time},\
+                  "connection_status":"{connection_status}", "disconnection_time":"{disconnection_time}"}}'
+
+        query_wdb(command)
+
+    yield
+
+    command = f'global sql DELETE FROM agent WHERE id != 0'
+    query_wdb(command)
+
+@pytest.fixture(scope='function')
+def clean_authd_function():
+    """
+    Restart Authd.
+    """
+    control_service("stop", daemon='wazuh-authd')
+    truncate_file(CLIENT_KEYS_PATH)
+    control_service("start", daemon='wazuh-authd')
+
+    yield
+
+    control_service("stop", daemon='wazuh-authd')
+    truncate_file(CLIENT_KEYS_PATH)
+    control_service("start", daemon='wazuh-authd')
+
+
+# Tests
+
+def test_authd_local_messages(clean_authd_function, set_up_groups, insert_pre_existent_agents_db, insert_pre_existent_agents_client_keys, get_configuration,
+                              configure_environment, configure_sockets_environment_function, connect_to_sockets_function,
+                              wait_for_authd_startup_module, get_current_test_case):
+    """
+        description:
+            "Check that every input message in trough local authd port generates the adequate response to worker"
+        wazuh_min_version:
+            4.2
+        parameters:
+            - set_up_groups_keys:
+                type: fixture
+                brief: Set pre-existent groups and keys.
+            - get_configuration:
+                type: fixture
+                brief: Get the configuration of the test.
+            - configure_environment:
+                type: fixture
+                brief: Configure a custom environment for testing.
+            - configure_sockets_environment_function:
+                type: fixture
+                brief: Configure the socket listener to receive and send messages on the sockets at function scope.
+            - connect_to_sockets_function:
+                type: fixture
+                brief: Bind to the configured sockets at function scope.
+            - wait_for_authd_startup_module:
+                type: fixture
+                brief: Waits until Authd is accepting connections.
+        assertions:
+            - The received output must match with expected
+            - The enrollment messages are parsed as expected
+            - The agent keys are denied if the hash is the same than the manager's
+        input_description:
+            Different test cases are contained in an external YAML file (local_enroll_messages.yaml) which includes
+            the different possible registration requests and the expected responses.
+        expected_output:
+            - Registration request responses on Authd socket
+    """
+    case = get_current_test_case['test_case']
+    for index, stage in enumerate(case):
         # Reopen socket (socket is closed by manager after sending message with client key)
         receiver_sockets[0].open()
         expected = stage['output']
@@ -171,5 +277,5 @@ def test_ossec_auth_messages(set_up_groups_keys, get_configuration, configure_en
         receiver_sockets[0].send(stage['input'], size=True)
         response = receiver_sockets[0].receive(size=True).decode()
         assert response[:len(expected)] == expected, \
-            'Failed test case "{}". Response was: {} instead of: {}' \
-            .format(set_up_groups_keys['name'], response, expected)
+            'Failed stage "{}". Response was: {} instead of: {}' \
+            .format(index+1, response, expected)
