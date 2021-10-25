8d3dc50e578a3a08426fb1f9e15a44d9dfdefed5
mdengra <miguel.dengra@wazuh.com>
2021-10-25 16:48:51 +0200

    doc: Add test_api documentation in QA Docs style
    The current scheme of the issue #1694 has been used.
    PEP-8 fixes.
    
    Related: #1796

--- tests/integration/test_api/test_config/test_request_timeout/test_request_timeout.py
+++ tests/integration/test_api/test_config/test_request_timeout/test_request_timeout.py
@@ -1,7 +1,61 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+'''
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
 
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: These tests will check if the 'request_timeout' setting of the API is working properly.
+       This setting allows specifying the time limit for the API to process a request.
+       The Wazuh API is an open source 'RESTful' API that allows for interaction with
+       the Wazuh manager from a web browser, command line tool like 'cURL' or any script
+       or program that can make web requests.
+
+tier: 0
+
+modules:
+    - api
+
+components:
+    - manager
+
+daemons:
+    - wazuh-apid
+    - wazuh-analysisd
+    - wazuh-syscheckd
+    - wazuh-db
+
+os_platform:
+    - linux
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/api/getting-started.html
+    - https://documentation.wazuh.com/current/user-manual/api/configuration.html
+
+tags:
+    - api
+'''
 import os
 from json import loads
 
@@ -37,15 +91,43 @@ def get_configuration(request):
 ])
 def test_request_timeout(tags_to_apply, get_configuration, configure_api_environment, restart_api,
                          wait_for_start, get_api_details):
-    """Check that the maximum request time for an API request works.
+    '''
+    description: Check if the maximum request time for an API request works.
+                 For this purpose, a value of '0' seconds is set for the 'request_timeout'
+                 setting, and a request is made to the API, expecting an error in the response.
 
-    Make a request to check the timeout error.
+    wazuh_min_version: 4.3.0
 
-    Parameters
-    ----------
-    tags_to_apply : set
-        Run test if match with a configuration identifier, skip otherwise.
-    """
+    parameters:
+        - tags_to_apply:
+            type: set
+            brief: Run test if match with a configuration identifier, skip otherwise.
+        - get_configuration:
+            type: fixture
+            brief: Get configurations from the module.
+        - configure_api_environment:
+            type: fixture
+            brief: Configure a custom environment for API testing.
+        - restart_api:
+            type: fixture
+            brief: Reset 'api.log' and start a new monitor.
+        - wait_for_start:
+            type: fixture
+            brief: Wait until the API starts.
+        - get_api_details:
+            type: fixture
+            brief: Get API information.
+
+    assertions:
+        - Verify that the request cannot finish successfully, resulting in a timeout error.
+
+    input_description: A test case is contained in an external YAML file (conf.yaml) which includes
+                       API configuration parameters ('request_timeout' set to '0' seconds).
+
+    expected_output:
+        - r'500' ('Internal server error' HTTP status code)
+        - r'3021' ('timeout error' in the response body)
+    '''
     check_apply_test(tags_to_apply, get_configuration['tags'])
     get_response = requests.get(f'{api.API_PROTOCOL}://{api.API_HOST}:{api.API_PORT}{api.API_LOGIN_ENDPOINT}',
                                 headers=api.get_login_headers(api.API_USER, api.API_PASS), verify=False)
