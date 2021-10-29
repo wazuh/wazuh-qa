3f01bab9d41fb2811c6a426920348f7f14b9cb68
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-10-29 13:05:29 +0200

    refac: Remove a useless commented line.

--- deps/wazuh_testing/setup.py
+++ deps/wazuh_testing/setup.py
@@ -51,7 +51,6 @@ def get_files_from_directory(directory):
 
 
 package_data_list.extend(get_files_from_directory('wazuh_testing/qa_docs/search_ui'))
-#package_data_list.extend(get_files_from_directory('wazuh_testing/qa_docs/dockerfiles'))
 
 setup(name='wazuh_testing',
       version='4.3.0',
