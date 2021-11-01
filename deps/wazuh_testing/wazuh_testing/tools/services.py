10b9b1a4b2a46dfc44faa939c3d010348a9ffc0f
pereyra-m <matias.pereyra@wazuh.com>
2021-11-01 22:14:23 +0000

    Using wazuh_control in check_daemon_status for authd tests

--- deps/wazuh_testing/wazuh_testing/tools/services.py
+++ deps/wazuh_testing/wazuh_testing/tools/services.py
@@ -183,7 +183,7 @@ def get_process_cmd(search_cmd):
             return proc
 
 
-def check_daemon_status(target_daemon=None, running_condition=True, timeout=10, extra_sockets=[]):
+def check_daemon_status(target_daemon=None, running_condition=True, timeout=10, extra_sockets=[], use_wazuh_control=False):
     """Wait until Wazuh daemon's status matches the expected one. If timeout is reached and the status didn't match,
        it raises a TimeoutError.
 
@@ -205,8 +205,13 @@ def check_daemon_status(target_daemon=None, running_condition=True, timeout=10, 
         elapsed_time = 0
 
         while elapsed_time < timeout and not condition_met:
-            control_status_output = subprocess.run([f'{WAZUH_PATH}/bin/wazuh-control', 'status'],
-                                                   stdout=subprocess.PIPE).stdout.decode()
+            if use_wazuh_control == True:
+                control_status_output = subprocess.run([f"{WAZUH_PATH}/bin/wazuh-control", 'status'],
+                                                    stdout=subprocess.PIPE).stdout.decode()
+            else:
+                control_status_output = subprocess.run([f'{WAZUH_PATH}/bin/wazuh-control', 'status'],
+                                                    stdout=subprocess.PIPE).stdout.decode()
+
             condition_met = True
             for lines in control_status_output.splitlines():
                 daemon_status_tokens = lines.split()
