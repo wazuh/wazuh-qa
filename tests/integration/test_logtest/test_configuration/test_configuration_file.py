192e2c5379ffce040d6619dde01e51d96e0fa09c
Luis Gonzalez <luis.gonzalez@wazuh.com>
2021-11-05 10:19:33 +0100

    doc: Add the `test_logtest/test_configuration` documentation using the `qa-docs` schema. #1814
    The following tests have been documented:
    - `test_configuration_file.py`
    - `test_get_configuration_sock.py`

--- tests/integration/test_logtest/test_configuration/test_configuration_file.py
+++ tests/integration/test_logtest/test_configuration/test_configuration_file.py
@@ -1,14 +1,63 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+'''
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
 
+           Created by Wazuh, Inc. <info@wazuh.com>.
 
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: The 'wazuh-logtest' tool allows the testing and verification of rules and decoders against provided log examples
+        remotely inside a sandbox in 'wazuh-analysisd'. This functionality is provided by the manager, whose work
+        parameters are configured in the ossec.conf file in the XML rule_test section. Test logs can be evaluate through
+        'wazuh-logtest' tool or making requests via RESTful API.
+
+tier: 0
+
+modules:
+    - logtest
+
+components:
+    - manager
+
+daemons:
+    - wazuh-analysisd
+
+os_platform:
+    - linux
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/reference/tools/wazuh-logtest.html
+    - https://documentation.wazuh.com/current/user-manual/capabilities/wazuh-logtest/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/daemons/wazuh-analysisd.html
+
+tags:
+    - logtest_configuration
+'''
 import os
-
 import pytest
+
 from wazuh_testing import global_parameters
-from wazuh_testing.logtest import (callback_logtest_started, callback_logtest_disabled,
-                                   callback_configuration_error)
+from wazuh_testing.logtest import (callback_logtest_started, callback_logtest_disabled, callback_configuration_error)
 from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.tools.monitoring import FileMonitor
@@ -34,9 +83,40 @@ def get_configuration(request):
 
 # Test
 def test_configuration_file(get_configuration, configure_environment, restart_wazuh):
-    """Check multiples logtest configuration
-    """
+    '''
+    description: Checks if `wazuh-logtest` works as expected under different predefined configurations that cause
+                 `wazuh-logtest` to start correctly, to be disabled, or to register an error. To do this, it checks
+                 some values in these configurations from 'wazuh_conf.yaml' file.
 
+    wazuh_min_version: 4.2.0
+
+    parameters:
+        - get_configuration:
+            type: fixture
+            brief: Get configuration from the module.
+        - configure_environment:
+            type: fixture
+            brief: Configure a custom environment for testing
+        - restart_wazuh:
+            type: fixture
+            brief: Restart wazuh, ossec.log and start a new monitor.
+
+    assertions:
+        - Verify that a valid configuration is loaded.
+        - Verify that wrong loaded configurations lead to an error.
+
+    input_description: Five test cases are defined in the module. These include some configurations stored in
+                       the 'wazuh_conf.yaml'.
+
+    expected_output:
+        - r'.* Logtest started'
+        - r'.* Logtest disabled'
+        - r'.* Invalid value for element'
+        - 'Event not found'
+
+    tags:
+        - logtest_configuration_file
+    '''
     callback = None
     if 'valid_conf' in get_configuration['tags']:
         callback = callback_logtest_started
--- tests/integration/test_logtest/test_configuration/test_get_configuration_sock.py
+++ tests/integration/test_logtest/test_configuration/test_get_configuration_sock.py
@@ -1,7 +1,58 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+'''
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
 
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: The 'wazuh-logtest' tool allows the testing and verification of rules and decoders against provided log examples
+        remotely inside a sandbox in 'wazuh-analysisd'. This functionality is provided by the manager, whose work
+        parameters are configured in the ossec.conf file in the XML rule_test section. Test logs can be evaluate through
+        'wazuh-logtest' tool or making requests via RESTful API.
+
+tier: 0
+
+modules:
+    - logtest
+
+components:
+    - manager
+
+daemons:
+    - wazuh-analysisd
+
+os_platform:
+    - linux
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/reference/tools/wazuh-logtest.html
+    - https://documentation.wazuh.com/current/user-manual/capabilities/wazuh-logtest/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/daemons/wazuh-analysisd.html
+
+tags:
+    - logtest_configuration
+'''
 import os
 import re
 
@@ -33,9 +84,46 @@ def get_configuration(request):
 
 # Test
 def test_get_configuration_sock(get_configuration, configure_environment, restart_wazuh, connect_to_sockets_function):
-    """Check analisis Unix socket returns the correct Logtest configuration
-    """
+    '''
+    description: Check analysis Unix socket returns the correct Logtest configuration under different sets of
+                 configurations, `wazuh-analisysd` returns the right information from the `rule_test` configuration
+                 block. To do this, it overwrites wrong field values and checks that the values within the received
+                 message are the same that the loaded fields from the 'wazuh_conf.yaml' file.
 
+    wazuh_min_version: 4.2.0
+
+    parameters:
+        - get_configuration:
+            type: fixture
+            brief: Get configuration from the module.
+        - configure_environment:
+            type: fixture
+            brief: Configure a custom environment for testing
+        - restart_wazuh:
+            type: fixture
+            brief: Restart wazuh, ossec.log and start a new monitor.
+        - connect_to_sockets_function:
+            type: fixture
+            brief: Function scope version of 'connect_to_sockets' which connects to the specified sockets for the test.
+
+    assertions:
+        - Verify that a valid configuration is loaded.
+        - Verify that wrong loaded configurations are fixed with limit values.
+        - Verify that each message field received matches the loaded configuration fields.
+
+    input_description: Five test cases are defined in the module. These include some configurations stored in
+                       the 'wazuh_conf.yaml'.
+
+    expected_output:
+        - 'Real message was: .*'
+        - 'Expected value in enabled tag: .*. Value received: .*'
+        - 'Expected value in threads tag: .*.  Value received: .*'
+        - 'Expected value in max_sessions tag: .*.  Value received: .*'
+        - 'Expected value in session_timeout tag: .*.  Value received: .*'
+
+    tags:
+        - logtest_configuration_sock
+    '''
     configuration = get_configuration['sections'][0]['elements']
 
     if 'invalid_threads_conf' in get_configuration['tags']:
