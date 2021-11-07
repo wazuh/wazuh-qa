90294117a63fd3e0c58fa76e89de870edf40e802
camila <camiromero100@gmail.com>
2021-11-06 22:56:24 -0300

    Added local internal options

--- tests/integration/test_logcollector/test_configuration/test_basic_configuration_alias.py
+++ tests/integration/test_logcollector/test_configuration/test_basic_configuration_alias.py
@@ -21,7 +21,7 @@ pytestmark = pytest.mark.tier(level=0)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_basic_configuration.yaml')
 
-local_internal_options = {'logcollector.remote_commands': '1'}
+local_internal_options = {'logcollector.remote_commands': '1', 'logcollector.debug': '2'}
 
 wazuh_component = get_service()
 
@@ -56,13 +56,7 @@ def get_configuration(request):
     return request.param
 
 
-@pytest.fixture(scope="module")
-def get_local_internal_options():
-    """Get local internal options from the module."""
-    return local_internal_options
-
-
-def test_configuration_alias(get_local_internal_options, configure_local_internal_options,
+def test_configuration_alias(configure_local_internal_options_module,
                              get_configuration, configure_environment, restart_logcollector):
     """Check if the module runs correctly with the specified command monitoring configuration and that it uses an alias value.
     Ensure command monitoring uses specified alias value. Also, in the case of the manager instance, check if the API
