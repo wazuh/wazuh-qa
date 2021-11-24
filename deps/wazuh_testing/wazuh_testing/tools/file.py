347c5cd2480e16b030537db9a1ecfe7cede409c4
jmv74211 <jonathan.martin@wazuh.com>
2021-11-24 12:18:35 +0100

    fix: Add related checks to avoid using incompatible functions in Windows #2240
    Done in file tools module

--- deps/wazuh_testing/wazuh_testing/tools/file.py
+++ deps/wazuh_testing/wazuh_testing/tools/file.py
@@ -10,6 +10,7 @@ import random
 import shutil
 import socket
 import stat
+import sys
 import string
 import xml.etree.ElementTree as ET
 import zipfile
@@ -252,7 +253,9 @@ def copy(source, destination):
     """
     shutil.copy2(source, destination)
     source_stats = os.stat(source)
-    os.chown(destination, source_stats[stat.ST_UID], source_stats[stat.ST_GID])
+
+    if sys.platform != 'win32':
+        os.chown(destination, source_stats[stat.ST_UID], source_stats[stat.ST_GID])
 
 
 def bind_unix_socket(socket_path, protocol='TCP'):
@@ -264,7 +267,7 @@ def bind_unix_socket(socket_path, protocol='TCP'):
         socket_path (str): Path where create the unix socket.
         protocol (str): It can be TCP or UDP.
     """
-    if not os.path.exists(socket_path):
+    if not os.path.exists(socket_path) and sys.platform != 'win32':
         sock_type = socket.SOCK_STREAM if protocol.upper() == 'TCP' else socket.SOCK_DGRAM
         new_socket = socket.socket(socket.AF_UNIX, sock_type)
         new_socket.bind(socket_path)
@@ -298,14 +301,15 @@ def set_file_owner_and_group(file_path, owner, group):
     Raises:
         KeyError: If owner or group does not exist.
     """
-    from pwd import getpwnam
-    from grp import getgrnam
+    if sys.platform != 'win32':
+        from pwd import getpwnam
+        from grp import getgrnam
 
-    if os.path.exists(file_path):
-        uid = getpwnam(owner).pw_uid
-        gid = getgrnam(group).gr_gid
+        if os.path.exists(file_path):
+            uid = getpwnam(owner).pw_uid
+            gid = getgrnam(group).gr_gid
 
-        os.chown(file_path, uid, gid)
+            os.chown(file_path, uid, gid)
 
 
 def recursive_directory_creation(path):
