013947b431e0ba26cfd62c7f9946b9f0122dca9c
jmv74211 <jonathan.martin@wazuh.com>
2021-11-24 12:20:14 +0100

    add: Add Windows support for qa-ctl tasks launcher #2240

--- deps/wazuh_testing/wazuh_testing/qa_ctl/run_tasks/qa_tasks_launcher.py
+++ deps/wazuh_testing/wazuh_testing/qa_ctl/run_tasks/qa_tasks_launcher.py
@@ -1,4 +1,6 @@
 import os
+import sys
+from copy import deepcopy
 from tempfile import gettempdir
 
 from wazuh_testing.tools.file import download_text_file, remove_file
@@ -8,7 +10,9 @@ from wazuh_testing.qa_ctl.provisioning.ansible.ansible_inventory import AnsibleI
 from wazuh_testing.qa_ctl.provisioning.ansible.ansible_runner import AnsibleRunner
 from wazuh_testing.qa_ctl import QACTL_LOGGER
 from wazuh_testing.tools.logging import Logging
+from wazuh_testing.tools import file
 from wazuh_testing.tools.time import get_current_timestamp
+from wazuh_testing.qa_ctl.provisioning.local_actions import qa_ctl_docker_run
 
 
 class QATasksLauncher:
@@ -27,8 +31,9 @@ class QATasksLauncher:
     def __init__(self, tasks_data, qa_ctl_configuration):
         self.qa_ctl_configuration = qa_ctl_configuration
         self.ansible_runners = []
+        self.tasks_data = tasks_data
 
-        self.__process_tasks_data(tasks_data)
+        self.__process_tasks_data()
 
     def __read_ansible_instance(self, host_info):
         """Read every host info and generate the AnsibleInstance object.
@@ -67,7 +72,7 @@ class QATasksLauncher:
 
         return instance
 
-    def __process_tasks_data(self, tasks_data):
+    def __process_tasks_data(self):
         """Process tasks module info from the qa-ctl configuration file.
 
         Args:
@@ -75,7 +80,7 @@ class QATasksLauncher:
         """
         QATasksLauncher.LOGGER.debug('Processing tasks module data')
 
-        for task_id, task_data in tasks_data.items():
+        for task_id, task_data in self.tasks_data.items():
             playbooks_path = []
             inventory_path = ''
 
@@ -111,10 +116,39 @@ class QATasksLauncher:
     def run(self):
         """Run the ansible tasks specified in the tasks module."""
         try:
-            for task_runner in self.ansible_runners:
-                QATasksLauncher.LOGGER.info(f"Running {task_runner.task_id}")
-                task_runner.run()
-                QATasksLauncher.LOGGER.info(f"The {task_runner.task_id} has been finished successfully")
+            if sys.platform == 'win32':
+                # If Windows, run the qa-ctl tasks in a linux container due to ansible is not compatible with Windows
+                tmp_config_file_name = f"config_{get_current_timestamp()}.yaml"
+                tmp_path = os.path.join(gettempdir(), 'wazuh_qa_ctl')
+                tmp_config_file = os.path.join(tmp_path, tmp_config_file_name)
+
+                # Copy the tasks data to modify
+                container_tasks = deepcopy(self.tasks_data)
+
+                # Copy the local playbooks to the wazuh qa-ctl path, that is shared with the container through a volume
+                for _, task_data in container_tasks.items():
+                    if 'playbooks' in task_data:
+                        for playbook_data in task_data['playbooks']:
+                            if 'local_path' in playbook_data:
+                                file.copy(playbook_data['local_path'], tmp_path)
+                                playbook_file = os.path.split(playbook_data['local_path'])[1]
+
+                                # Update local path to specify the playbooks path in the container
+                                playbook_data['local_path'] = f"/wazuh_qa_ctl/{playbook_file}"
+
+                # Write a custom configuration file with tasks section modified (local paths)
+                file.write_yaml_file(tmp_config_file, {'tasks': container_tasks})
+
+                try:
+                    qa_ctl_docker_run(tmp_config_file_name, self.qa_ctl_configuration.qa_ctl_launcher_branch,
+                                      self.qa_ctl_configuration.debug_level, topic='launching the custom tasks')
+                finally:
+                    file.remove_file(tmp_config_file)
+            else:
+                for task_runner in self.ansible_runners:
+                    QATasksLauncher.LOGGER.info(f"Running {task_runner.task_id}")
+                    task_runner.run()
+                    QATasksLauncher.LOGGER.info(f"The {task_runner.task_id} has been finished successfully")
         finally:
             self.destroy()
 
