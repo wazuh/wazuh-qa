0e57a68699a6501d00ab192d3625caa626e9a225
jmv74211 <jonathan.martin@wazuh.com>
2021-12-01 11:12:22 +0100

    fix: Fix FileNotFoundException in check-files tool #2244

--- deps/wazuh_testing/wazuh_testing/scripts/check_files.py
+++ deps/wazuh_testing/wazuh_testing/scripts/check_files.py
@@ -96,15 +96,15 @@ def get_check_files_data(path='/', ignored_paths=[]):
 
         for ignore_path in ignored_paths:
             if ignore_path in dirpath:
-                script_logger.debug(f"Skipping '{dirpath}' path")
                 skip_path_checking = True
 
         if not skip_path_checking:
             for filename in filenames:
                 file_path = os.path.join(dirpath, filename)
-                files_items_dict[dirpath] = get_data_information(dirpath)
+                if os.path.exists(dirpath):
+                    files_items_dict[dirpath] = get_data_information(dirpath)
 
-                if file_path not in ignored_paths:
+                if file_path not in ignored_paths and os.path.exists(file_path):
                     files_items_dict[file_path] = get_data_information(file_path)
 
     return files_items_dict
