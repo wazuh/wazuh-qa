3f7ded54a3a7c5084d3f438cd76236ffe5c9b17f
Deblintrake09 <amicalizzi2005@gmail.com>
2021-12-14 13:05:57 -0300

    Modify message insertion method

--- tests/system/test_active_response_log_format/test_active_response_log_format.py
+++ tests/system/test_active_response_log_format/test_active_response_log_format.py
@@ -20,8 +20,8 @@ local_path = os.path.dirname(os.path.abspath(__file__))
 messages_files = ['data/messages_415_or_lower.yml', 'data/messages_420_to_424.yml', 'data/messages_425_or_greater.yml']
 tmp_path = os.path.join(local_path, 'tmp')
 log_path = "/var/log/secure"
-log_cases=["Dec  9 22:15:40 localhost sshd[5332]: Failed password for invalid user BALROG from 192.168.123.321 port 52620 $token: `132`! ssh2"]
-sleep_time = 10
+log_cases=["Dec  9 22:15:40 localhost sshd[5332]: Failed password for invalid user BALROGs from 192.168.11.21 port 52620 '$token': `132`! ssh2\n"]
+sleep_time = 15
 
 
 def clean_environment(wazuh_agent):
@@ -31,7 +31,7 @@ def clean_environment(wazuh_agent):
     host_manager.clear_file(host=wazuh_agent, file_path=os.path.join(WAZUH_LOGS_PATH, 'active-responses.log'))
 
 
-@pytest.mark.parametrize('wazuh_agent, message_file', [('wazuh-agent1', messages_files[0]),
+@pytest.mark.parametrize('wazuh_agent, message_file', [#('wazuh-agent1', messages_files[0])])
                         ('wazuh-agent2', messages_files[1]),('wazuh-agent3', messages_files[2])])
 def test_active_response_log_format(wazuh_agent, message_file):
     """Check that when an Active Response is activated, the manager sends back the information to the agent
@@ -39,14 +39,15 @@ def test_active_response_log_format(wazuh_agent, message_file):
 
     clean_environment(wazuh_agent)
     
-    # Insert log
-    command = f"echo {log_cases[0]} >> {log_path}"
-
-    # wait for active responses messages to be genrated
-    time.sleep(sleep_time)
+    # generate log command
+    command = f"echo '{log_cases[0]}' > {log_path}"
     
     # Add log message to agent monitored source
-    host_manager.run_shell(host=wazuh_agent, cmd=command)
+    #host_manager.run_shell(host=wazuh_agent, cmd=command)
+    host_manager.modify_file_content(host=wazuh_agent, path=log_path, content=log_cases[0])
+    
+    # wait for active responses messages to be genrated
+    time.sleep(sleep_time)
 
     # Run the callback checks for the ossec.log and the actibe-responses.log
     HostMonitor(inventory_path=inventory_path,
