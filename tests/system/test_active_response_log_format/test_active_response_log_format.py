b8eeb77f5db5b8885aad875420f8f54319470c9d
Deblintrake09 <amicalizzi2005@gmail.com>
2021-12-14 20:26:40 -0300

    added Doc Schema 2.0 documentation

--- tests/system/test_active_response_log_format/test_active_response_log_format.py
+++ tests/system/test_active_response_log_format/test_active_response_log_format.py
@@ -1,6 +1,46 @@
-# Copyright (C) 2015-2021, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+"""
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
+           Created by Wazuh, Inc. <info@wazuh.com>.
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+type: system
+brief: Check that when an Active Response is activated, the manager sends back the information to the agent
+    and that it appears in active-response.log and ossec.log with the expected format.
+tier: 0
+modules:
+    - active_response
+components:
+    - manager
+    - agent
+path: tests/system/test_active_response/test_active_response_log_format/test_active_response_log_format.py
+daemons:
+    - wazuh-execd
+os_platform:
+    - linux
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/vulnerability-detection/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/vuln-detector.html#enabled
+tags:
+    - active_response
+    - 
+"""
 
 import os
 import time
@@ -26,16 +66,33 @@ sleep_time = 15
 
 def clean_environment(wazuh_agent):
     # Clean ossec.log and active-responses.log
-    host_manager.clear_file(host='wazuh-master', file_path=os.path.join(WAZUH_LOGS_PATH, 'ossec.log'))
     host_manager.clear_file(host=wazuh_agent, file_path=os.path.join(WAZUH_LOGS_PATH, 'ossec.log'))
     host_manager.clear_file(host=wazuh_agent, file_path=os.path.join(WAZUH_LOGS_PATH, 'active-responses.log'))
 
 
-@pytest.mark.parametrize('wazuh_agent, message_file', [('wazuh-agent1', messages_files[0])])
-                        #('wazuh-agent2', messages_files[1]),('wazuh-agent3', messages_files[2])])
+@pytest.mark.parametrize('wazuh_agent, message_file', [('wazuh-agent1', messages_files[0]),
+                        ('wazuh-agent2', messages_files[1]),('wazuh-agent3', messages_files[2])])
 def test_active_response_log_format(wazuh_agent, message_file):
-    """Check that when an Active Response is activated, the manager sends back the information to the agent
-    and that it appears in active-response.log and ossec.log with the expected format"""
+    """
+    description: Check that when an Active Response is activated, the manager sends back the information to the agent
+    and that it appears in active-response.log and ossec.log with the expected format
+
+    wazuh_min_version: 4.2.0
+
+    parameters:
+        - wazuh_agent:
+            type: string
+            brief: tells which agent to insert the log message.
+        - message_file:
+            type: string
+            brief: tells HostManager the location of the yml file with the expected messages and where to look for them
+        
+    input_description: Each agent has a different version with a specific yml file containing the expected messages 
+                       and their location in the agent's logs
+    
+    expected_output: In case the test passes it will show "Received from {host} the expected message: {message}"
+                     in case it failes it will show: "Did not found the expected callback in {host}: {message}"
+    """
 
     clean_environment(wazuh_agent)
     
