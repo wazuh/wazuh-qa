4bcf9b7e622f0df3ca0f5ab791b1910db1044eb3
camila <camiromero100@gmail.com>
2021-12-13 22:24:56 -0300

    rm: delete unused function

--- tests/integration/test_fim/test_synchronization/test_registry_responses_win32.py
+++ tests/integration/test_fim/test_synchronization/test_registry_responses_win32.py
@@ -123,20 +123,6 @@ def get_sync_msgs(tout, new_data=True):
     return events
 
 
-def find_path_in_event_list(path, event_list):
-    """Function that looks for a key path in a list of json events.
-    Params:
-        path (str): Path of the registry key.
-        event_list (list): List containing the events in JSON format.
-    Returns:
-        The event that matches the specified path. None if no event was found.
-    """
-    for event in event_list:
-        if event['path'] == path:
-            return event
-    return None
-
-
 def find_value_in_event_list(key_path, value_name, event_list):
     """Function that looks for a key path and value_name in a list of json events.
     Params:
