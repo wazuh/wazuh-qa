1d8b7c2519b82197915fe5dfe2528677b2832aa7
Deblintrake09 <amicalizzi2005@gmail.com>
2021-12-16 17:35:28 -0300

    extracted clean_enviroment method and modified log variable.

--- tests/system/test_active_response_log_format/test_active_response_log_format.py
+++ tests/system/test_active_response_log_format/test_active_response_log_format.py
@@ -46,7 +46,7 @@ import time
 import pytest
 from wazuh_testing.tools import WAZUH_LOGS_PATH
 from wazuh_testing.tools.monitoring import HostMonitor
-from wazuh_testing.tools.system import HostManager
+from wazuh_testing.tools.system import HostManager, clean_environment
 
 # Hosts and variables
 # In order to run this test, first you need to launch the manager_agent enviroment
@@ -54,19 +54,12 @@ testinfra_hosts = ["wazuh-manager", "wazuh-agent1", "wazuh-agent2", "wazuh-agent
 inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
                               'system','provisioning', 'manager_agent', 'inventory.yml')
 host_manager = HostManager(inventory_path)
-
 local_path = os.path.dirname(os.path.abspath(__file__))
 messages_files = ['data/messages_415_or_lower.yml', 'data/messages_420_to_424.yml', 'data/messages_425_or_greater.yml']
 tmp_path = os.path.join(local_path, 'tmp')
 log_path = "/var/log/secure"
-log_cases=["Dec  9 22:15:40 localhost sshd[5332]: Failed password for invalid user BALROG from 192.168.222.11 port 52620 '$token': `132`! ssh2\n\n"]
+log_sample= "Dec  9 22:15:40 localhost sshd[5332]: Failed password for invalid user BALROG from 192.168.222.11 port 52620 '$token': `132`! ssh2\n\n"
 sleep_time = 5
-
-
-def clean_environment(wazuh_agent):
-    # Clean ossec.log and active-responses.log
-    host_manager.clear_file(host=wazuh_agent, file_path=os.path.join(WAZUH_LOGS_PATH, 'ossec.log'))
-    host_manager.clear_file(host=wazuh_agent, file_path=os.path.join(WAZUH_LOGS_PATH, 'active-responses.log'))
 
 
 @pytest.mark.parametrize('wazuh_agent, message_file', [('wazuh-agent1', messages_files[0]),
@@ -92,11 +85,14 @@ def test_active_response_log_format(wazuh_agent, message_file):
     expected_output: In case the test passes it will show "Received from {host} the expected message: {message}"
                      in case it failes it will show: "Did not found the expected callback in {host}: {message}"
     """
-
-    clean_environment(wazuh_agent)
+    
+    #clear the agents files that will be monitored.
+    enviroment_files = [(wazuh_agent, os.path.join(WAZUH_LOGS_PATH, 'ossec.log')),
+                        (wazuh_agent, os.path.join(WAZUH_LOGS_PATH, 'active-responses.log'))]
+    clean_environment(host_manager, enviroment_files)
     
     # Add log message to agent monitored source
-    host_manager.modify_file_content(host=wazuh_agent, path=log_path, content=log_cases[0])
+    host_manager.modify_file_content(host=wazuh_agent, path=log_path, content=log_sample)
     
     # wait for active responses messages to be generated
     time.sleep(sleep_time)
