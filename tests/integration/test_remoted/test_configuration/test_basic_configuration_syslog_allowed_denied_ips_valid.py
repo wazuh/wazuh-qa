58e499446a0b7d4a403d8cde5755b9cb8ad428d1
Antonio Zambrano Fernández <antonio.zambrano@wazuh.com>
2021-12-22 11:58:28 +0100

    Add test cases for IPv6 to Remoted syslog allowed denied ips valid

--- tests/integration/test_remoted/test_configuration/test_basic_configuration_syslog_allowed_denied_ips_valid.py
+++ tests/integration/test_remoted/test_configuration/test_basic_configuration_syslog_allowed_denied_ips_valid.py
@@ -4,6 +4,7 @@
 
 import os
 import pytest
+import ipaddress
 
 import wazuh_testing.remote as remote
 from wazuh_testing.api import compare_config_api_response
@@ -24,7 +25,17 @@ parameters = [
     {'ALLOWED': '127.0.0.0/25', 'DENIED': '192.168.1.1/25'},
     {'ALLOWED': '127.0.0.0/26', 'DENIED': '192.168.1.1/26'},
     {'ALLOWED': '127.0.0.0/27', 'DENIED': '192.168.1.1/27'},
-    {'ALLOWED': '127.0.0.0/30', 'DENIED': '192.168.1.1/30'}
+    {'ALLOWED': '127.0.0.0/30', 'DENIED': '192.168.1.1/30'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'DENIED': 'fe80::1003:889f:a584:0101/64'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/68', 'DENIED': 'fe80::1003:889f:a584:0101/68'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/72', 'DENIED': 'fe80::1003:889f:a584:0101/72'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/76', 'DENIED': 'fe80::1003:889f:a584:0101/76'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/80', 'DENIED': 'fe80::1003:889f:a584:0101/80'},
+    {'ALLOWED': '::1/64', 'DENIED': 'fe80::1003:889f:a584:0101/64'},
+    {'ALLOWED': '::1/68', 'DENIED': 'fe80::1003:889f:a584:0101/68'},
+    {'ALLOWED': '::1/72', 'DENIED': 'fe80::1003:889f:a584:0101/72'},
+    {'ALLOWED': '::1/76', 'DENIED': 'fe80::1003:889f:a584:0101/76'},
+    {'ALLOWED': '::1/80', 'DENIED': 'fe80::1003:889f:a584:0101/80'}
 ]
 
 metadata = [
@@ -32,7 +43,17 @@ metadata = [
     {'allowed-ips': '127.0.0.0/25', 'denied-ips': '192.168.1.1/25'},
     {'allowed-ips': '127.0.0.0/26', 'denied-ips': '192.168.1.1/26'},
     {'allowed-ips': '127.0.0.0/27', 'denied-ips': '192.168.1.1/27'},
-    {'allowed-ips': '127.0.0.0/30', 'denied-ips': '192.168.1.1/30'}
+    {'allowed-ips': '127.0.0.0/30', 'denied-ips': '192.168.1.1/30'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/68', 'denied-ips': 'fe80::1003:889f:a584:0101/68'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/72', 'denied-ips': 'fe80::1003:889f:a584:0101/72'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/76', 'denied-ips': 'fe80::1003:889f:a584:0101/76'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/80', 'denied-ips': 'fe80::1003:889f:a584:0101/80'},
+    {'allowed-ips': '::1/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64'},
+    {'allowed-ips': '::1/68', 'denied-ips': 'fe80::1003:889f:a584:0101/68'},
+    {'allowed-ips': '::1/72', 'denied-ips': 'fe80::1003:889f:a584:0101/72'},
+    {'allowed-ips': '::1/76', 'denied-ips': 'fe80::1003:889f:a584:0101/76'},
+    {'allowed-ips': '::1/80', 'denied-ips': 'fe80::1003:889f:a584:0101/80'}
 ]
 
 configurations = load_wazuh_configurations(configurations_path, "test_basic_configuration_allowed_denied_ips",
@@ -56,7 +77,10 @@ def test_allowed_denied_ips_syslog(get_configuration, configure_environment, res
     requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
     cfg = get_configuration['metadata']
 
-    log_callback = remote.callback_detect_syslog_allowed_ips(cfg['allowed-ips'])
+    address = cfg['allowed-ips'][:-3]
+    netmask = cfg['allowed-ips'][-3:]
+    allowed_ips = ipaddress.ip_address(address).exploded + netmask
+    log_callback = remote.callback_detect_syslog_allowed_ips(allowed_ips)
     wazuh_log_monitor.start(timeout=5, callback=log_callback, error_message="Wazuh remoted didn't start as expected.")
 
     compare_config_api_response([cfg], 'remote')
