49289e5d1e1eb729f22b4f9cfd35d0f5135092d3
camila <camiromero100@gmail.com>
2021-12-23 12:44:48 -0300

    add: Add variables for FIM module

--- deps/wazuh_testing/wazuh_testing/fim_module/fim_variables.py
+++ deps/wazuh_testing/wazuh_testing/fim_module/fim_variables.py
@@ -19,10 +19,24 @@ WINDOWS_REGISTRY = 'WINDOWS_REGISTRY'
 SYNC_INTERVAL = 'SYNC_INTERVAL'
 SYNC_INTERVAL_VALUE = MAX_EVENTS_VALUE = 20
 
+
 # Folders variables
 TEST_DIR_1 = 'testdir1'
 TEST_DIRECTORIES = 'TEST_DIRECTORIES'
 TEST_REGISTRIES = 'TEST_REGISTRIES'
+
+
+# Syscheck attributes
+REPORT_CHANGES = 'report_changes'
+DIFF_SIZE_LIMIT = 'diff_size_limit'
+FILE_SIZE_ENABLED = 'FILE_SIZE_ENABLED'
+FILE_SIZE_LIMIT = 'FILE_SIZE_LIMIT'
+DISK_QUOTA_ENABLED = 'DISK_QUOTA_ENABLED'
+DISK_QUOTA_LIMIT = 'DISK_QUOTA_LIMIT'
+
+# Syscheck values
+DIFF_LIMIT_VALUE = 2
+
 
 # FIM modules
 SCHEDULE_MODE = 'scheduled'
@@ -30,6 +44,7 @@ SCHEDULE_MODE = 'scheduled'
 # Yaml Configuration
 YAML_CONF_REGISTRY_RESPONSE = 'wazuh_conf_registry_responses_win32.yaml'
 YAML_CONF_SYNC_WIN32 = 'wazuh_sync_conf_win32.yaml'
+YAML_CONF_DIFF = 'wazuh_conf_diff.yaml'
 
 # Synchronization options
 SYNCHRONIZATION_ENABLED = 'SYNCHRONIZATION_ENABLED'
@@ -38,3 +53,4 @@ SYNCHRONIZATION_REGISTRY_ENABLED = 'SYNCHRONIZATION_REGISTRY_ENABLED'
 # Callbacks message
 INTEGRITY_CONTROL_MESSAGE = r'.*Sending integrity control message: (.+)$'
 REGISTRY_DBSYNC_NO_DATA = r'.*#!-fim_registry dbsync no_data (.+)'
+MAXIMUM_FILE_SIZE = r'.*Maximum file size limit to generate diff information configured to \'(\d+) KB\'.*'
