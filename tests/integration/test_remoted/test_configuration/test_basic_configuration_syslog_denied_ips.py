7b90d7b6e8846b0bbdc89fbc7bf5c023fccf4535
Antonio Zambrano Fernández <antonio.zambrano@wazuh.com>
2021-12-23 11:05:50 +0100

    Add test cases for IPv6 to Remoted syslog denied IPs

--- tests/integration/test_remoted/test_configuration/test_basic_configuration_syslog_denied_ips.py
+++ tests/integration/test_remoted/test_configuration/test_basic_configuration_syslog_denied_ips.py
@@ -20,11 +20,13 @@ test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '')
 configurations_path = os.path.join(test_data_path, 'data', 'wazuh_basic_configuration.yaml')
 
 parameters = [
-    {'ALLOWED': '127.0.0.0/24', 'DENIED': '127.0.0.1'}
+    {'ALLOWED': '127.0.0.0/24', 'DENIED': '127.0.0.1', 'IPV6': 'no'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'DENIED': '::1', 'IPV6': 'yes'}
 ]
 
 metadata = [
-    {'allowed-ips': '127.0.0.0/24', 'denied-ips': '127.0.0.1'}
+    {'allowed-ips': '127.0.0.0/24', 'denied-ips': '127.0.0.1', 'ipv6': 'no'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'denied-ips': '::1', 'ipv6': 'yes'}
 ]
 
 configurations = load_wazuh_configurations(configurations_path, 'test_basic_configuration_allowed_denied_ips' , params=parameters, metadata=metadata)
@@ -54,7 +56,8 @@ def test_denied_ips_syslog(get_configuration, configure_environment, restart_rem
 
     wazuh_log_monitor.start(timeout=5, callback=log_callback, error_message="Wazuh remoted didn't start as expected.")
 
-    remote.send_syslog_message(message='Feb 22 13:08:48 Remoted Syslog Denied testing', port=514, protocol=remote.UDP)
+    remote.send_syslog_message(message='Feb 22 13:08:48 Remoted Syslog Denied testing', port=514, protocol=remote.UDP,
+        manager_address=cfg['denied-ips'])
 
     log_callback = remote.callback_detect_syslog_denied_ips(cfg['denied-ips'])
 
