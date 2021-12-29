a7491100af5d8d5ab29513424f1fdce74e57024e
Antonio Zambrano Fernández <antonio.zambrano@wazuh.com>
2021-12-29 12:56:59 +0100

    Add test cases for multiple allowed-ips

--- tests/integration/test_remoted/test_configuration/test_basic_configuration_syslog_allowed_denied_ips_valid.py
+++ tests/integration/test_remoted/test_configuration/test_basic_configuration_syslog_allowed_denied_ips_valid.py
@@ -20,46 +20,49 @@ pytestmark = pytest.mark.tier(level=0)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 configurations_path = os.path.join(test_data_path, 'wazuh_basic_configuration.yaml')
 
-parameters = [
-    {'ALLOWED': '127.0.0.0/24', 'DENIED': '192.168.1.1/24'},
-    {'ALLOWED': '127.0.0.0/25', 'DENIED': '192.168.1.1/25'},
-    {'ALLOWED': '127.0.0.0/26', 'DENIED': '192.168.1.1/26'},
-    {'ALLOWED': '127.0.0.0/27', 'DENIED': '192.168.1.1/27'},
-    {'ALLOWED': '127.0.0.0/30', 'DENIED': '192.168.1.1/30'},
-    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'DENIED': 'fe80::1003:889f:a584:0101/64'},
-    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/68', 'DENIED': 'fe80::1003:889f:a584:0101/68'},
-    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/72', 'DENIED': 'fe80::1003:889f:a584:0101/72'},
-    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/76', 'DENIED': 'fe80::1003:889f:a584:0101/76'},
-    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/80', 'DENIED': 'fe80::1003:889f:a584:0101/80'},
-    {'ALLOWED': '::1/64', 'DENIED': 'fe80::1003:889f:a584:0101/64'},
-    {'ALLOWED': '::1/68', 'DENIED': 'fe80::1003:889f:a584:0101/68'},
-    {'ALLOWED': '::1/72', 'DENIED': 'fe80::1003:889f:a584:0101/72'},
-    {'ALLOWED': '::1/76', 'DENIED': 'fe80::1003:889f:a584:0101/76'},
-    {'ALLOWED': '::1/80', 'DENIED': 'fe80::1003:889f:a584:0101/80'}
+parameters_single = [
+    {'ALLOWED': '127.0.0.0/24', 'DENIED': '192.168.1.1/24', 'IPV6': 'no'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'DENIED': 'fe80::1003:889f:a584:0101/64', 'IPV6': 'yes'},
+    {'ALLOWED': '::1/64', 'DENIED': 'fe80::1003:889f:a584:0101/64', 'IPV6': 'yes'}
 ]
 
-metadata = [
-    {'allowed-ips': '127.0.0.0/24', 'denied-ips': '192.168.1.1/24'},
-    {'allowed-ips': '127.0.0.0/25', 'denied-ips': '192.168.1.1/25'},
-    {'allowed-ips': '127.0.0.0/26', 'denied-ips': '192.168.1.1/26'},
-    {'allowed-ips': '127.0.0.0/27', 'denied-ips': '192.168.1.1/27'},
-    {'allowed-ips': '127.0.0.0/30', 'denied-ips': '192.168.1.1/30'},
-    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64'},
-    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/68', 'denied-ips': 'fe80::1003:889f:a584:0101/68'},
-    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/72', 'denied-ips': 'fe80::1003:889f:a584:0101/72'},
-    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/76', 'denied-ips': 'fe80::1003:889f:a584:0101/76'},
-    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/80', 'denied-ips': 'fe80::1003:889f:a584:0101/80'},
-    {'allowed-ips': '::1/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64'},
-    {'allowed-ips': '::1/68', 'denied-ips': 'fe80::1003:889f:a584:0101/68'},
-    {'allowed-ips': '::1/72', 'denied-ips': 'fe80::1003:889f:a584:0101/72'},
-    {'allowed-ips': '::1/76', 'denied-ips': 'fe80::1003:889f:a584:0101/76'},
-    {'allowed-ips': '::1/80', 'denied-ips': 'fe80::1003:889f:a584:0101/80'}
+metadata_single = [
+    {'allowed-ips': '127.0.0.0/24', 'denied-ips': '192.168.1.1/24', 'ipv6': 'no'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64', 'ipv6': 'yes'},
+    {'allowed-ips': '::1/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64', 'ipv6': 'yes'}
 ]
 
-configurations = load_wazuh_configurations(configurations_path, "test_basic_configuration_allowed_denied_ips",
-                                           params=parameters, metadata=metadata)
+parameters_multiple = [
+    {'ALLOWED': '127.0.0.0/24', 'ALLOWED2': '192.168.0.0/24', 'DENIED': '192.168.1.1/24', 'IPV6': 'no'},
+    {'ALLOWED': '0000:0000:0000:0000:0000:0000:0000:0001/64', 
+        'ALLOWED2': '0000:0000:0000:0000:0000:0000:0000:0002/64', 'DENIED': 'fe80::1003:889f:a584:0101/64', 'IPV6': 'yes'},
+    {'ALLOWED': '::1/64', 'ALLOWED2': '::2/64', 'DENIED': 'fe80::1003:889f:a584:0101/64', 'IPV6': 'yes'},
+    {'ALLOWED': '127.0.0.0/24', 'ALLOWED2': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'DENIED': '192.168.1.1/24', 'IPV6': 'yes'},
+    {'ALLOWED': '127.0.0.0/24', 'ALLOWED2': '::1/64', 'DENIED': '192.168.1.1/24', 'IPV6': 'yes'},
+    {'ALLOWED': '::1/64', 'ALLOWED2': '0000:0000:0000:0000:0000:0000:0000:0002/64', 'DENIED': '192.168.1.1/24', 'IPV6': 'yes'}
+]
+
+metadata_multiple = [
+    {'allowed-ips': '127.0.0.0/24', 'allowed-ips2': '192.168.0.0/24', 'denied-ips': '192.168.1.1/24', 'ipv6': 'no'},
+    {'allowed-ips': '0000:0000:0000:0000:0000:0000:0000:0001/64', 
+        'allowed-ips2': '0000:0000:0000:0000:0000:0000:0000:0002/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64', 'ipv6': 'yes'},
+    {'allowed-ips': '::1/64', 'allowed-ips2': '::2/64', 'denied-ips': 'fe80::1003:889f:a584:0101/64', 'ipv6': 'yes'},
+    {'allowed-ips': '127.0.0.0/24',
+        'allowed-ips2': '0000:0000:0000:0000:0000:0000:0000:0001/64', 'denied-ips': '192.168.1.1/24', 'ipv6': 'yes'},
+    {'allowed-ips': '127.0.0.0/24', 'allowed-ips2': '::1/64', 'denied-ips': '192.168.1.1/24', 'ipv6': 'yes'},
+    {'allowed-ips': '::1/64',
+        'allowed-ips2': '0000:0000:0000:0000:0000:0000:0000:0002/64', 'denied-ips': '192.168.1.1/24', 'ipv6': 'yes'}
+]
+
+parameters = parameters_single + parameters_multiple
+metadata = metadata_single + metadata_multiple
+
+configurations_single = load_wazuh_configurations(configurations_path, "test_basic_configuration_allowed_denied_ips",
+                                           params=parameters_single, metadata=metadata_single)
+configurations_multiple = load_wazuh_configurations(configurations_path, "test_basic_configuration_multiple_allowed_denied_ips",
+                                           params=parameters_multiple, metadata=metadata_multiple)
+configurations = configurations_single + configurations_multiple
 configuration_ids = [f"{x['ALLOWED']}_{x['DENIED']}" for x in parameters]
-
 
 # fixtures
 @pytest.fixture(scope="module", params=configurations, ids=configuration_ids)
@@ -83,4 +86,11 @@ def test_allowed_denied_ips_syslog(get_configuration, configure_environment, res
     log_callback = remote.callback_detect_syslog_allowed_ips(allowed_ips)
     wazuh_log_monitor.start(timeout=5, callback=log_callback, error_message="Wazuh remoted didn't start as expected.")
 
-    compare_config_api_response([cfg], 'remote')
+    if 'allowed-ips2' in cfg:
+        address2 = cfg['allowed-ips2'][:-3]
+        netmask2 = cfg['allowed-ips2'][-3:]
+        allowed_ips2 = ipaddress.ip_address(address2).exploded + netmask2
+        log_callback = remote.callback_detect_syslog_allowed_ips(allowed_ips2)
+        wazuh_log_monitor.start(timeout=5, callback=log_callback, error_message="Wazuh remoted didn't start as expected.")
+
+    #compare_config_api_response([cfg], 'remote')
