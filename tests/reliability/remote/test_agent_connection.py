52348d32ed3b301115b5caf05fbaa2eb07205ff4
Víctor Rebollo <victorrebollop@gmail.com>
2022-01-28 13:59:34 +0100

    feat: agent connection reliability test

--- tests/reliability/remote/test_agent_connection.py
+++ tests/reliability/remote/test_agent_connection.py
@@ -2,7 +2,7 @@ import pytest
 from wazuh_testing import global_parameters
 
 
-def test_remoted_error(get_report):
+def test_agent_connection(get_report):
     """Check that:
         - Agents are connected everytime 
         - N tcp connections is equal to n of active agents
@@ -10,5 +10,6 @@ def test_remoted_error(get_report):
     report = global_parameters.report
     wazuh_agentd_report = report['agents']['wazuh-agentd']
 
-    assert wazuh_agentd_report['ever_disconnected'] == wazuh_agentd_report['ever_pending'] == 0
-    assert wazuh_agentd_report['begin_status'] == wazuh_agentd_report['end_status'] == report['metadata']['n_agents']
+    assert wazuh_agentd_report['ever_disconnected'] == 0, "Some agents has disconnected"
+    assert wazuh_agentd_report['ever_pending'] == 0, "Some agents has change to pending status"
+    assert wazuh_agentd_report['begin_status'] == wazuh_agentd_report['end_status'] == report['metadata']['n_agents'], "Some agents statuses have not been gathered correctly"
