5240ace1507a358fb4b5174796afbf22939a6b68
jmv74211 <jonathan.martin@wazuh.com>
2022-01-31 11:53:40 +0100

    add: Add wdb socket error management in db_interface module #2469

--- deps/wazuh_testing/wazuh_testing/db_interface/__init__.py
+++ deps/wazuh_testing/wazuh_testing/db_interface/__init__.py
@@ -2,15 +2,11 @@ import json
 import socket
 import os
 import sqlite3
+from time import sleep
 
+from wazuh_testing import WAZUH_DB_SOCKET_PATH
 from wazuh_testing.tools.monitoring import wazuh_pack, wazuh_unpack
-import wazuh_testing
-
-
-QUEUE_DB_PATH = os.path.join(wazuh_testing.WAZUH_PATH, 'queue', 'db')
-WAZUH_DB_SOCKET_PATH = os.path.join(QUEUE_DB_PATH, 'wdb')
-
-CVE_DB_PATH = os.path.join(wazuh_testing.WAZUH_PATH, 'queue', 'vulnerabilities', 'cve.db')
+from wazuh_testing.tools.services import control_service
 
 
 def query_wdb(command):
@@ -22,12 +18,33 @@ def query_wdb(command):
     Returns:
         list: Query response data.
     """
+    # If the wdb socket is not yet up, then wait or restart wazuh-db
+    if not os.path.exists(WAZUH_DB_SOCKET_PATH):
+        retry = 0
+        max_retries = 6
+        for _ in range(2):
+            # Wait if the wdb socket is not still alive (due to wazuh-db restarts). Max 3 seconds
+            while not os.path.exists(WAZUH_DB_SOCKET_PATH) and retry < max_retries:
+                print("Retrying ...")
+                sleep(0.5)
+                retry += 1
+
+            # Restart wazuh-db in case of wdb socket is not yet up.
+            if not os.path.exists(WAZUH_DB_SOCKET_PATH):
+                print("Restarting wazuh-db ...")
+                control_service('restart', daemon='wazuh-db')
+
+        # Raise custom exception if the socket is not up in the expected time, even restarting wazuh-db
+        if not os.path.exists(WAZUH_DB_SOCKET_PATH):
+            raise Exception('The wdb socket is not up. wazuh-db was restarted but the socket was not found')
+
+    # Create and open the socket connection with wazuh-db socket
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect(WAZUH_DB_SOCKET_PATH)
-
     data = []
 
     try:
+        # Send the query request
         sock.send(wazuh_pack(len(command)) + command.encode())
 
         rcv = sock.recv(4)
@@ -69,6 +86,7 @@ def make_sqlite_query(db_path, query_list):
         db_path (string): Path where is located the DB.
         query_list (list): List with queries to run.
     """
+    control_service('stop', daemon='wazuh-db')
     connect = sqlite3.connect(db_path)
 
     try:
@@ -77,6 +95,7 @@ def make_sqlite_query(db_path, query_list):
                 connect.execute(item)
     finally:
         connect.close()
+        control_service('start', daemon='wazuh-db')
 
 
 def get_sqlite_query_result(db_path, query):
@@ -90,6 +109,7 @@ def get_sqlite_query_result(db_path, query):
         result (List[list]): Each row is the query result row and each column is the query field value.
     """
     try:
+        control_service('stop', daemon='wazuh-db')
         db, cursor = load_sqlite_db(db_path)
         cursor.execute(query)
         records = cursor.fetchall()
@@ -103,3 +123,4 @@ def get_sqlite_query_result(db_path, query):
     finally:
         cursor.close()
         db.close()
+        control_service('start', daemon='wazuh-db')
