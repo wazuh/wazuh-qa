8ee732a93ec3e61586e22799ba0c3bc67cab267f
Deblintrake09 <amicalizzi2005@gmail.com>
2022-02-02 15:38:01 +0000

    rem - remove unnecesary variable asignment

--- tests/integration/test_fim/test_registry/test_registry_report_changes/test_registry_file_size_values.py
+++ tests/integration/test_fim/test_registry/test_registry_report_changes/test_registry_file_size_values.py
@@ -77,7 +77,6 @@ test_regs = [os.path.join(WINDOWS_HKEY_LOCAL_MACHINE, MONITORED_KEY),
              os.path.join(WINDOWS_HKEY_LOCAL_MACHINE, MONITORED_KEY_2)]
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
-size_limit_configured = SIZE_LIMIT_CONFIGURED_VALUE
 scan_delay = 2
 
 # Configurations
@@ -180,7 +179,7 @@ def test_file_size_values(key, subkey, arch, value_name, size, get_configuration
         assert os.path.exists(diff_file), '{diff_file} does not exist'
         assert event['data'].get('content_changes') is not None, ERR_MSG_CONTENT_CHANGES_EMPTY
 
-    if size > size_limit_configured:
+    if size > SIZE_LIMIT_CONFIGURED_VALUE:
         callback_test = report_changes_validator_no_diff
     else:
         callback_test = report_changes_validator_diff
