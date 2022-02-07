66b9707bd08be1ec4e90081f1fa10da6970462ba
mauromalara <mauro.malara@wazuh.com>
2022-02-06 22:01:39 -0300

    doc: comments deleted.

--- tests/system/test_auditing_user_commands/launcher.py
+++ tests/system/test_auditing_user_commands/launcher.py
@@ -76,11 +76,9 @@ def set_environment(parameters):
     """
     set_logger(parameters)
 
-    # Download wazuh-qa repository to launch the check-files test files.
     local_actions.download_local_wazuh_qa_repository(branch=parameters.qa_branch, path=TMP_FILES)
     test_build_files.append(WAZUH_QA_FILES)
 
-    # Create output file if it has been specified and it does not exist
     if parameters.output_file_path:
         file.recursive_directory_creation(parameters.output_file_path)
 
@@ -94,7 +92,6 @@ def set_logger(parameters):
     level = 'DEBUG' if parameters.debug >= 1 else 'INFO'
     logger.set_level(level)
 
-    # Disable traceback if it is not run in DEBUG mode
     if level != 'DEBUG':
         sys.tracebacklimit = 0
 
@@ -113,7 +110,6 @@ def validate_parameters(parameters):
             if key_data not in data.keys():
                 return False
 
-        # Check for password data
         if 'ansible_password' not in data.keys() and 'ansible_ssh_private_key_file' not in data.keys():
             return False
 
@@ -121,34 +117,27 @@ def validate_parameters(parameters):
 
     logger.info('Validating input parameters')
 
-    # Check if QA branch exists
     if not github_checks.branch_exists(parameters.qa_branch, repository=WAZUH_QA_REPO):
         raise QAValueError(f"{parameters.qa_branch} branch does not exist in Wazuh QA repository.",
                            logger.error, QACTL_LOGGER)
 
-    # Check version parameter
     if parameters.wazuh_version and len((parameters.wazuh_version).split('.')) != 3:
         raise QAValueError(f"Version parameter has to be in format x.y.z. You entered {parameters.wazuh_version}",
                            logger.error, QACTL_LOGGER)
 
-    # Check if Wazuh has the specified version
     if parameters.wazuh_version and not github_checks.version_is_released(parameters.wazuh_version):
         raise QAValueError(f"The wazuh {parameters.wazuh_version} version has not been released. Enter a right "
                            'version.', logger.error, QACTL_LOGGER)
 
-    # Check the deployment-info parameter
     if parameters.deployment_info:
-        # Validate the file parameter
         if not os.path.isfile(parameters.deployment_info) or not os.path.exists(parameters.deployment_info):
             raise QAValueError('The specified deployment-info file does not exist.', logger.error, QACTL_LOGGER)
 
-        # Read parameter file format
         if not file.validate_yaml_file(parameters.deployment_info):
             raise QAValueError(f"The deployment-info {parameters.deployment_info} is not in YAML format, or it has "
                                'wrong syntax', logger.error, QACTL_LOGGER)
         deployment_data = file.read_yaml(parameters.deployment_info)
 
-        # Validate the data content
         for item in deployment_data:
             if not validate_deployment_info_data(item):
                 raise QAValueError('Some necessary field is missing in the deployment-info file. The necessary one '
@@ -158,7 +147,7 @@ def validate_parameters(parameters):
 
     logger.info('Input parameters validation has passed successfully')
 
-#Se corre manualmente con python3 launcher.py con los parametros definidos en el script
+
 def main():
     """Run the check-files test according to the script parameters."""
     parameters = get_parameters()
@@ -167,14 +156,11 @@ def main():
     test_output_path = parameters.output_file_path if parameters.output_file_path else \
         os.path.join(TMP_FILES, f"test_auditing_commands_result_{current_timestamp}")
 
-    # Set logging and Download QA files
     set_environment(parameters)
 
-    # Validate script parameters
     if not parameters.no_validation:
         validate_parameters(parameters)
 
-    # Clean test build files
     if parameters and not parameters.persistent:
         logger.info('Deleting all test artifacts files of this build (config files, playbooks, data results ...)')
         for file_to_remove in test_build_files:
