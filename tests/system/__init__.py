7dcbc79417113994c9c3660f4e37fafe4d26d5e8
Deblintrake09 <amicalizzi2005@gmail.com>
2022-02-09 13:25:09 -0300

    rd: fixed remove_cluster_agents

--- tests/system/__init__.py
+++ tests/system/__init__.py
@@ -32,7 +32,7 @@ def remove_cluster_agents(wazuh_master, agents_list, host_manager):
     while (agent_id != ''):
         host_manager.get_host(wazuh_master).ansible("command", f'{WAZUH_PATH}/bin/manage_agents -r {agent_id}',
                                                   check=False)
-    agent_id = get_agent_id(host_manager)
+        agent_id = get_agent_id(host_manager)
     for agent in agents_list:
         host_manager.control_service(host=agent, service='wazuh', state="stopped")
         host_manager.clear_file(agent, file_path=os.path.join(WAZUH_PATH, 'etc', 'client.keys'))
