33580d2b01c7924c8998198e6ed125af9b807217
Deblintrake09 <amicalizzi2005@gmail.com>
2022-02-10 16:26:39 -0300

    add: new test agents connected to workers

--- tests/system/test_cluster/test_agent_groups/test_agent_groups_new_cluster_node.py
+++ tests/system/test_cluster/test_agent_groups/test_agent_groups_new_cluster_node.py
@@ -4,7 +4,8 @@ copyright: Copyright (C) 2015-2022, Wazuh Inc.
            This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 type: system
-brief: 
+brief: This tests check that when a cluster has a series of agents with groups assigned, when a new node is
+       added to the cluster, that new node the agent's status and groups are synchronized.
 tier: 0
 modules:
     - cluster
@@ -124,3 +125,50 @@ def test_agent_groups_new_cluster_node(clean_cluster_environment):
     check_agent_groups(agent1_data[1], agent_groups[0], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_new_nodes
     check_agent_groups(agent2_data[1], agent_groups[1], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_new_nodes
     check_agent_groups(agent3_data[1], agent_groups[2], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_new_nodes
+
+
+def test_agent_groups_sync_from_worker_new_node(clean_cluster_environment):
+    '''
+    description: Having two agents assined to different workers and different groups, check that when an new node
+    is added to the cluster, the group data is synchronized to the new node.
+    wazuh_min_version: 4.4.0
+    parameters:
+        - clean_enviroment:
+            type: fixture
+            brief: Reset the wazuh log files at the start of the test. Remove all registered agents from master.
+    assertions:
+        - Verify that after registering the agents appear as active in all nodes.
+        - Verify that after registering and after starting the agent, the indicated group is synchronized.
+        - Verify that after after adding a new node to the cluster, the agent's group data is synchronized.
+    expected_output:
+        - The 'Agent_name' with ID 'Agent_id' belongs to groups: 'group_name'.
+    '''
+    for group in ["Group1", "Group2"]:
+        create_new_agent_group(test_infra_managers[0], group, host_manager)
+
+    agent1_data = register_agent(test_infra_agents[0], test_infra_managers[1], host_manager, agent_groups[0])
+    agent2_data = register_agent(test_infra_agents[1], test_infra_managers[2], host_manager, agent_groups[1])
+    
+    restart_cluster(test_infra_agents[0:2], host_manager)
+    time.sleep(10)
+    # Check that agent status is active in cluster
+    check_agent_status(agent1_data[1], agent1_data[2], agent1_data[0], "active", host_manager, test_infra_managers)
+    check_agent_status(agent2_data[1], agent2_data[2], agent2_data[0], "active", host_manager, test_infra_managers)
+
+
+    # Check that agent has the expected group assigned in all nodes
+    check_agent_groups(agent1_data[1], agent_groups[0], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_managers
+    check_agent_groups(agent2_data[1], agent_groups[1], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_managers
+    
+
+    restart_cluster(test_infra_new_nodes, host_manager)
+    time.sleep(10)
+
+    # Check that agent status is active in new node
+    check_agent_status(agent1_data[1], agent1_data[2], agent1_data[0], "active", host_manager, test_infra_new_nodes)
+    check_agent_status(agent2_data[1], agent2_data[2], agent2_data[0], "active", host_manager, test_infra_new_nodes)
+    
+    
+    # Check that agent has group set to default in new node
+    check_agent_groups(agent1_data[1], agent_groups[0], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_new_nodes
+    check_agent_groups(agent2_data[1], agent_groups[1], ["wazuh-master"], host_manager) # replace wazuh-master for test_infra_new_nodes
