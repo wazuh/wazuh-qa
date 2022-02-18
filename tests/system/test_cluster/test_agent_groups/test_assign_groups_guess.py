52af353f6505928e9e6c3e6548d63bfb109929bf
camila <camiromero100@gmail.com>
2022-02-18 11:43:54 -0300

    Add: add test to check guessing

---
+++ tests/system/test_cluster/test_agent_groups/test_assign_groups_guess.py
@@ -0,0 +1,153 @@
+"""
+copyright: Copyright (C) 2015-2022, Wazuh Inc.
+           Created by Wazuh, Inc. <info@wazuh.com>.
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+type: system
+brief: Check that when an agent registered inthe manager and assigned to group is removed, performs a
+       guessing operation and determinates the groups to with the agent was assigned.
+tier: 1
+modules:
+    - cluster
+components:
+    - manager
+    - agent
+path: /tests/system/test_cluster/test_agent_groups/test_assign_groups_guess.py
+daemons:
+    - wazuh-db
+    - wazuh-clusterd
+os_platform:
+    - linux
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+references:
+    - https://github.com/wazuh/wazuh-qa/issues/2513
+tags:
+    - cluster
+"""
+
+
+import os
+
+import pytest
+from wazuh_testing.tools.system import HostManager
+from system import (check_agent_groups, check_agent_status, clean_cluster_logs,
+                    check_keys_file, delete_group_of_agents,
+                    remove_cluster_agents, assign_agent_to_new_group, restart_cluster)
+from common import register_agent
+from wazuh_testing.tools import WAZUH_PATH
+import time
+
+
+# Hosts
+test_infra_managers = ["wazuh-master", "wazuh-worker1", "wazuh-worker2"]
+test_infra_agents = ["wazuh-agent1"]
+
+inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
+                              'provisioning', 'enrollment_cluster', 'inventory.yml')
+host_manager = HostManager(inventory_path)
+local_path = os.path.dirname(os.path.abspath(__file__))
+tmp_path = os.path.join(local_path, 'tmp')
+remoted_guess_agent_groups = 'remoted.guess_agent_group='
+
+
+@pytest.fixture(scope='function')
+def clean_environment():
+
+    clean_cluster_logs(test_infra_agents + test_infra_managers, host_manager)
+
+    yield
+    # Remove the agent once the test has finished
+    remove_cluster_agents(test_infra_managers[0], test_infra_agents, host_manager)
+
+
+@pytest.mark.parametrize("status_guess_agent_group", ['0', '1'])
+@pytest.mark.parametrize("agent_target", ['wazuh-worker1'])
+def test_assign_agent_to_a_group(agent_target, status_guess_agent_group, clean_environment):
+    '''
+    description: Check that when an agent registered inthe manager and assigned to group is removed, performs a
+                 guessing operation and determinates the groups to with the agent was assigned.
+    wazuh_min_version: 4.4.0
+    parameters:
+        - agent_target:
+            type: string
+            brief: name of the host where the agent will register
+        - clean_enviroment:
+            type: fixture
+            brief: Reset the wazuh log files at the start of the test. Remove all registered agents from master.
+    assertions:
+        - Verify that after registering the agent key file exists in all nodes.
+        - Verify that after registering the agent appears as never_connected in all nodes.
+        - Verify that after registering it has the 'Null' group assigned.
+        - Verify that after assign group with agent-groups the change is sync with the cluster.
+    expected_output:
+        - The agent 'Agent_name' with ID 'Agent_id' belongs to groups: group_test."
+    '''
+    group_id = 'group_test'
+
+    # Modify local_internal_options
+    replace = '\n' + remoted_guess_agent_groups + f'{status_guess_agent_group} \n'
+
+    for host in test_infra_managers:
+        host_manager.add_block_to_file(host, path=f"{WAZUH_PATH}/etc/local_internal_options.conf",
+                                       after="upgrades.", before="authd.debug=2", replace=replace)
+
+    # Register agent with agent-auth
+    agent_ip, agent_id, agent_name, manager_ip = register_agent(test_infra_agents[0], agent_target,
+                                                                host_manager)
+
+    # Restart agent
+    restart_cluster(test_infra_agents, host_manager)
+
+    # Check that agent has client key file
+    assert check_keys_file(test_infra_agents[0], host_manager)
+
+    try:
+        # Create new group and assing agent
+        assign_agent_to_new_group(test_infra_managers[0], group_id, agent_id, host_manager)
+
+        time.sleep(10)
+
+        # Check that agent has group set to group_test on Managers
+        # check_agent_groups(agent_id, group_id, test_infra_managers, host_manager)
+
+        check_agent_status(agent_id, agent_name, agent_ip, 'active', host_manager, [test_infra_managers[0]])
+
+        # Remove the agent
+        remove_cluster_agents(test_infra_managers[0], test_infra_agents, host_manager)
+
+        # Register agent with agent-auth
+        agent_ip, agent_id, agent_name, manager_ip = register_agent(test_infra_agents[0], agent_target,
+                                                                    host_manager)
+
+        # Check that agent has client key file
+        assert check_keys_file(test_infra_agents[0], host_manager)
+
+        # Restart agent
+        restart_cluster(test_infra_agents, host_manager)
+        check_agent_status(agent_id, agent_name, agent_ip, 'active', host_manager, test_infra_managers)
+
+        # Check if remoted.guess_agent_group is disabled
+        if(status_guess_agent_group == 0):
+
+            group_id = 'default'
+        check_agent_groups(agent_id, group_id, test_infra_managers, host_manager)
+
+    finally:
+        # Delete group of agent
+        delete_group_of_agents(test_infra_managers[0], 'group_test', host_manager)
