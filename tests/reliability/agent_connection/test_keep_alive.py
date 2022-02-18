bc08759557ae2f9dd35eb59604eb68d3fd9a1d71
Víctor Rebollo <victorrebollop@gmail.com>
2022-02-18 10:06:25 +0000

    style: improve keep_alive reliability test output

--- tests/reliability/agent_connection/test_keep_alive.py
+++ tests/reliability/agent_connection/test_keep_alive.py
@@ -6,11 +6,13 @@ MAX_DIFFERENCE_ACK_KEEP_ALIVE = 20
 
 def test_keep_alives(get_report):
     # Agent
-    assert get_report['agents']['wazuh-agentd']['max_diff_ack_keep_alive'] < MAX_DIFFERENCE_ACK_KEEP_ALIVE
+    assert get_report['agents']['wazuh-agentd']['max_diff_ack_keep_alive'] < MAX_DIFFERENCE_ACK_KEEP_ALIVE, \
+        f"Some agents keep alive interval surpassed {MAX_DIFFERENCE_ACK_KEEP_ALIVE} seconds maximun"
 
     # Manager
     keep_alives = get_report['managers']['wazuh-remoted']['keep_alives']
 
     max_differences = [keep_alives[agent]['max_difference'] for agent in keep_alives.keys()]
-    assert max(max_differences) < MAX_DIFFERENCE_ACK_KEEP_ALIVE, "Some agents keep alive interval surpassed " + \
-                                                                 f"{MAX_DIFFERENCE_ACK_KEEP_ALIVE} maximun"
+    assert max(max_differences) < MAX_DIFFERENCE_ACK_KEEP_ALIVE, \
+        f"Some managers received keep-alives from agents at an interval that exceeded {MAX_DIFFERENCE_ACK_KEEP_ALIVE}" \
+        + 'seconds maximun'
