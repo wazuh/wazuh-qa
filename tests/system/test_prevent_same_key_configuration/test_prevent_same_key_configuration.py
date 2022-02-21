af9ff4415eee2411b40468c3a4c5e49e8440aa2a
mauromalara <mauro.malara@wazuh.com>
2022-02-21 12:32:02 -0300

    fix: asserts and control output fixed. #2456

--- tests/system/test_prevent_same_key_configuration/test_prevent_same_key_configuration.py
+++ tests/system/test_prevent_same_key_configuration/test_prevent_same_key_configuration.py
@@ -56,6 +56,8 @@ import os
 from wazuh_testing.tools.file import read_json_file
 
 expected_message_regex = r"(\d+\/)+\d+ (\d+:)+\d+ wazuh-remoted: WARNING.*\'001\'"
+no_enrollment_expected_status = ['Active', 'Disconnected', 'Active']
+enrollment_expected_status = ['Active', 'Active']
 
 
 @pytest.fixture
@@ -76,6 +78,16 @@ def get_control_output(request):
         request (fixture): Provide information on the executing test function.
     """
     return request.config.getoption('--control-output')
+
+
+def get_only_agents_info(file_path):
+    control_output = read_json_file(file_path)
+    agents_info = []
+    for _, output in enumerate(control_output):
+        output['data'].pop(0)
+        control_output[_] = output['data']
+        agents_info.extend([agent_info for agent_info in control_output[_]])
+    return agents_info
 
 
 def test_prevent_same_key_config(get_log_output, get_control_output):
@@ -111,11 +123,10 @@ def test_prevent_same_key_config(get_log_output, get_control_output):
         - analysisd
     '''
 
-    control_output = read_json_file(get_control_output)['data']
-    control_output.pop(0)
+    agents_info = get_only_agents_info(get_control_output)
     agents_status = []
     agents_ids = []
-    for agent in control_output:
+    for agent in agents_info:
         agents_status.append(agent['status'])
         agents_ids.append(agent['id'])
 
@@ -126,19 +137,22 @@ def test_prevent_same_key_config(get_log_output, get_control_output):
             for _ in re.finditer(pattern, line):
                 line_found = True
                 break
-        assert line_found, 'The message was not present in the ossec.log file.'
+        assert line_found, 'The expected message was not present in the ossec.log file.\n'
 
-        assert ['Disconnected', 'Active'] == agents_status, 'The actual behaviour was not the expected.\n' \
-                                                            f'Actual result: 1st agent status was {agents_status[0]}' \
-                                                            f' and 2nd agent status was {agents_status[1]}.\n' \
-                                                            'Expected result: When enrollment is disabled, the second' \
-                                                            ' agent will only connect to the manager when the' \
-                                                            ' first agent disconnects.\n'
+        assert no_enrollment_expected_status == agents_status, 'The actual behavior was not the expected.\n' \
+                                                               f'Actual result: {agents_status}' \
+                                                               'Expected result: When enrollment is disabled,' \
+                                                               ' the second agent will only connect to the ' \
+                                                               'manager when the first agent disconnects.\n'
 
-        assert ['001', '001'] == agents_ids, 'The actual behaviour was not the expected.\n' \
-                                             f'Actual result: The agents IDs are {agents_ids}\n' \
-                                             f'Expected result: Both connections must have the same agent ID.\n'
+        assert agents_ids[0] == agents_ids[1] and agents_ids[0] == agents_ids[2], 'The actual behavior was not the ' \
+                                                                                  'expected.\n' \
+                                                                                  'Actual result: The agents IDs are' \
+                                                                                  f' {agents_ids}\n' \
+                                                                                  f'Expected result: Connections' \
+                                                                                  f' must have the same agent ID.\n'
     else:
-        assert ['001', '002'] in agents_ids, 'MAL, tienen que ser difrentes IDs compaaa.\n' \
-                                             'esperado: 001 y 002.\n' \
-                                             f'obtenido: {agents_ids}\n'
+        assert enrollment_expected_status == agents_status, 'Both agents must be active.\n' \
+                                                            f'Actual result: {agents_status}\n'
+        assert agents_ids[0] != agents_ids[1], 'Agents ID`s must be different.\n' \
+                                               f'Actual result: {agents_ids}\n'
