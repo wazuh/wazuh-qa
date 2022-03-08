4ff79abaf196362c9a81734ed9f764ba5db7ff0a
mauromalara <mauro.malara@wazuh.com>
2022-03-08 01:40:54 +0000

    add: documentatin added to the test. #2580

--- tests/integration/test_api/test_config/test_logs/test_api_logs_formats.py
+++ tests/integration/test_api/test_config/test_logs/test_api_logs_formats.py
@@ -1,3 +1,45 @@
+'''
+copyright: Copyright (C) 2015-2021, Wazuh Inc.
+           Created by Wazuh, Inc. <info@wazuh.com>.
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+type: integration
+brief: There is an API configuration option, called 'logs', which allows to log in 4 formats ("json", "plain",
+       "json,plain", and "plain,json") through the 'format' field. The test checks if the logs of the API are in the
+       specified formats and the content of the log files are the expected.
+tier: 2
+modules:
+    - api
+components:
+    - manager
+daemons:
+    - wazuh-apid
+os_platform:
+    - linux
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+references:
+    - https://documentation.wazuh.com/current/user-manual/api/configuration.html#logs
+tags:
+    - api
+    - logs
+    - logging
+'''
 import os
 import re
 
@@ -29,6 +71,7 @@ tcase_ids = [f"level_{configuration['configuration']['logs']['level']}" +  \
 @pytest.fixture(scope='module', params=configurations, ids=tcase_ids)
 def get_configuration(request):
     """Get configurations from the module."""
+
     return request.param
 
 
@@ -44,6 +87,8 @@ def send_request(login_attempts=5):
 
 
 def callback_json_log_login_info(line):
+    """Match a given line with a regular expression."""
+
     payload = r'"user": "wazuh", "ip": "127.0.0.1", "http_method": "GET", "uri": "GET ' + f'{API_LOGIN_ENDPOINT}"' + \
               r', "parameters": {}' + r', "body": {}' + \
               r', "time": "(\d*\.*\d+)s", "status_code": (\d+)'
@@ -54,6 +99,8 @@ def callback_json_log_login_info(line):
 
 
 def callback_plain_log_login_info(line):
+    """Match a given line with a regular expression."""
+
     msg = fr'(\d+/\d+/\d+ \d+:\d+:\d+) INFO: wazuh 127.0.0.1 "GET {API_LOGIN_ENDPOINT}" with parameters' \
           r' {} and body {} done in (\d*\.*\d+)s: (\d+)'
     match = re.match(msg, line)
@@ -61,6 +108,8 @@ def callback_plain_log_login_info(line):
 
 
 def callback_json_log_error(line):
+    """Match a given line with a regular expression."""
+
     msg = r'{"timestamp": "(\d+/\d+/\d+ \d+:\d+:\d+)", "levelname": "ERROR", "data": {"type": "informative", ' \
           fr'"payload": "{error_log_payload}"}}'
     match = re.match(msg, line)
@@ -68,6 +117,8 @@ def callback_json_log_error(line):
 
 
 def callback_plain_error(line):
+    """Match a given line with a regular expression."""
+
     msg = fr"(\d+/\d+/\d+ \d+:\d+:\d+) ERROR: {error_log_payload}"
     match = re.match(msg, line)
     return match
@@ -77,6 +128,39 @@ def callback_plain_error(line):
 @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
 def test_api_logs_formats(get_configuration, configure_api_environment, clean_log_files, daemons_handler,
                           wait_for_start):
+    '''
+    description: The test checks if the logs of the API are in the specified formats and the content of the log files
+                 are the expected.
+    wazuh_min_version: 4.4.0
+    parameters:
+        - get_configuration:
+            type: fixture
+            brief: Get configurations from the module.
+        - configure_api_environment:
+            type: fixture
+            brief: Configure a custom environment for API testing.
+        - clean_log_files:
+            type: fixture
+            brief: Reset the log files of the API.
+        - daemons_handler:
+            type: fixture
+            brief: Handle the Wazuh daemons.
+        - wait_for_start:
+            type: fixture
+            brief: Wait until the API starts.
+    assertions:
+        - Verify that the expected log exists in the log file.
+        - Verify that the logs values are the same in both log formats.
+    input_description: The test gets the configuration from the YAML file, which contains the API configuration.
+    expected_output:
+        - The log was not the expected.
+        - The length of the subgroups of the match is not equal.
+        - The values of the logs doesn't match.
+    tags:
+        - api
+        - logs
+        - logging
+    '''
     wazuh_log_monitor = FileMonitor(API_LOG_FILE_PATH)
     json_log_monitor = FileMonitor(API_JSON_LOG_FILE_PATH)
 
