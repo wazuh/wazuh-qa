01c66ffe7409c50ebdf9068045156751f39aacd2
Deblintrake09 <amicalizzi2005@gmail.com>
2022-03-11 16:09:43 -0300

    Merge branch 'system_test_wazuh_db' into 2513_system_test_never_connected_assign_group

--- tests/system/test_cluster/test_agent_groups/test_agent_never_connected_change_groups.py
+++ tests/system/test_cluster/test_agent_groups/test_agent_never_connected_change_groups.py
@@ -47,8 +47,9 @@ import time
 import pytest
 
 from common import register_agent
+
 from system import (check_agent_groups, check_agent_status, check_keys_file, delete_group_of_agents,
-                    assign_agent_to_new_group)
+                    assign_agent_to_new_group, AGENT_NO_GROUPS, AGENT_STATUS_NEVER_CONNECTED)
 from wazuh_testing.tools.system import HostManager
 
 
@@ -62,6 +63,7 @@ host_manager = HostManager(inventory_path)
 local_path = os.path.dirname(os.path.abspath(__file__))
 tmp_path = os.path.join(local_path, 'tmp')
 group_id = 'group_test'
+wait_time = 10
 
 
 # Tests
@@ -108,16 +110,16 @@ def test_assign_agent_to_a_group(agent_target, clean_environment, test_infra_man
     assert check_keys_file(test_infra_agents[0], host_manager)
 
     # Check that agent status is never_connected in cluster
-    check_agent_status(agent_id, agent_name, agent_ip, 'never_connected', host_manager, test_infra_managers)
+    check_agent_status(agent_id, agent_name, agent_ip, AGENT_STATUS_NEVER_CONNECTED, host_manager, test_infra_managers)
 
     # Check that agent has group set to Null on Managers
-    check_agent_groups(agent_id, 'Null', test_infra_managers, host_manager)
+    check_agent_groups(agent_id, AGENT_NO_GROUPS, test_infra_managers, host_manager)
 
     try:
         # Add group to agent1
         assign_agent_to_new_group(test_infra_managers[0], group_id, agent_id, host_manager)
 
-        time.sleep(10)
+        time.sleep(wait_time)
         # Check that agent has group set to group_test on Managers
         check_agent_groups(agent_id, group_id, test_infra_managers, host_manager)
 
