2627bb42e4fff8e22e86745dd555406e4da1c7c4
damarisg <seyla.gomez@wazuh.com>
2022-03-23 11:26:45 -0300

    fix: Add file created to check events.

--- tests/integration/test_fim/test_synchronization/test_sync_enabled_win32.py
+++ tests/integration/test_fim/test_synchronization/test_sync_enabled_win32.py
@@ -59,7 +59,7 @@ import os
 
 import pytest
 from wazuh_testing import global_parameters
-from wazuh_testing.fim import LOG_FILE_PATH, generate_params, callback_detect_integrity_event
+from wazuh_testing.fim import LOG_FILE_PATH, generate_params, callback_detect_integrity_event, REGULAR, create_file
 from wazuh_testing.tools import PREFIX
 from wazuh_testing.tools.configuration import load_wazuh_configurations
 from wazuh_testing.tools.monitoring import FileMonitor
@@ -101,10 +101,14 @@ def get_configuration(request):
     """Get configurations from the module."""
     return request.param
 
+@pytest.fixture(scope='module')
+def create_a_file(get_configuration):
+    """Create a file previous to restart syscheckd"""
+    create_file(REGULAR, TEST_DIRECTORIES, 'testfile')
 
 # Tests
 
-def test_sync_disabled(get_configuration, configure_environment, restart_syscheckd, wait_for_fim_start_sync_disabled):
+def test_sync_disabled(get_configuration, configure_environment, create_a_file, restart_syscheckd, wait_for_fim_start_sync_disabled):
     '''
     description: Check if the 'wazuh-syscheckd' daemon uses the value of the 'enabled' tag to start/stop
                  the file/registry synchronization. For this purpose, the test will monitor a directory/key.
@@ -122,6 +126,9 @@ def test_sync_disabled(get_configuration, configure_environment, restart_syschec
         - configure_environment:
             type: fixture
             brief: Configure a custom environment for testing.
+        - create_a_file:
+            type: fixture
+            brief: It creates a file. It verifies that also appear the files created when is enabled.
         - restart_syscheckd:
             type: fixture
             brief: Clear the 'ossec.log' file and start a new monitor.
