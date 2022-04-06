f94a9bcd7fe23d40128ddb06805200c287e1a4ea
Deblintrake09 <amicalizzi2005@gmail.com>
2022-04-06 22:59:38 +0000

    add:  new fixtures for restarting wazuh

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -92,6 +92,24 @@ def restart_wazuh(get_configuration, request):
 
 
 @pytest.fixture(scope='module')
+def restart_wazuh_daemon(daemon=None):
+    """
+    Restart a Wazuh daemon
+    """
+    truncate_file(LOG_FILE_PATH)
+    control_service("restart", daemon=daemon)
+
+
+@pytest.fixture(scope='function')
+def restart_wazuh_daemon_function(daemon=None):
+    """
+    Restart a Wazuh daemon
+    """
+    truncate_file(LOG_FILE_PATH)
+    control_service("restart", daemon=daemon)
+
+
+@pytest.fixture(scope='module')
 def reset_ossec_log(get_configuration, request):
     # Reset ossec.log and start a new monitor
     truncate_file(LOG_FILE_PATH)
@@ -903,7 +921,7 @@ def configure_local_internal_options_function(request):
 
     logger.debug(f"Restore local_internal_option to {str(backup_local_internal_options)}")
     conf.set_local_internal_options_dict(backup_local_internal_options)
-    
+
 @pytest.fixture(scope='function')
 def truncate_log_files():
     """Truncate all the log files before and after the test execution"""
