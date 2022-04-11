d6a1bdca8c6d2efd3d49a3a043d207a926d1c437
Cami Romero <37776796+CamiRomero@users.noreply.github.com>
2022-04-11 12:45:36 -0300

    Fx: Apply changes in order to follow Pycostyle

--- tests/system/test_cluster/test_agent_groups/test_groups_sync_default.py
+++ tests/system/test_cluster/test_agent_groups/test_groups_sync_default.py
@@ -3,8 +3,8 @@ copyright: Copyright (C) 2015-2022, Wazuh Inc.
            Created by Wazuh, Inc. <info@wazuh.com>.
            This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 type: system
-brief: This tests check that when a cluster has a series of agents registered and connecting one after the other,
-        and it is unable to do a Sync, that after a expected time, a Sync is forced and the DB is Synched.
+brief: These tests check that when a cluster has a series of agents registered and connecting one after the other,
+       and it is unable to do a Sync, that after a expected time, a Sync is forced and the DB is Synched.
 tier: 2
 modules:
     - cluster
@@ -52,8 +52,8 @@ from system.test_cluster.test_agent_groups.common import register_agent
 # Hosts
 test_infra_managers = ["wazuh-master", "wazuh-worker1", "wazuh-worker2"]
 agents_in_cluster = 40
-test_infra_agents=[]
-for x in range(agents_in_cluster): 
+test_infra_agents = []
+for x in range(agents_in_cluster):
     test_infra_agents.append("wazuh-agent" + str(x+1))
 
 inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
@@ -65,35 +65,35 @@ sync_delay = 40
 
 
 # Tests
-@pytest.mark.parametrize("test_infra_managers",[test_infra_managers])
-@pytest.mark.parametrize("test_infra_agents",[test_infra_agents])
-@pytest.mark.parametrize("host_manager",[host_manager])
+@pytest.mark.parametrize("test_infra_managers", [test_infra_managers])
+@pytest.mark.parametrize("test_infra_agents", [test_infra_agents])
+@pytest.mark.parametrize("host_manager", [host_manager])
 @pytest.mark.parametrize("agent_host", test_infra_managers[0:2])
 def test_agent_groups_sync_default(agent_host, clean_environment, test_infra_managers, test_infra_agents,
                                    host_manager):
     '''
     description: Check that after a long time when the manager has been unable to synchronize de databases, because
     new agents are being continually added, database synchronization is forced and the expected information is in
-    all nodes after the expected sync time. For this, an agent is restarted and connects to the agent every roughly 10 
+    all nodes after the expected sync time. For this, an agent is restarted and connects to the agent every roughly 10
     seconds, during 400 seconds. After all agents have been registered, it is checked that the wazuhDB has been synched
     in all the cluster nodes.
     wazuh_min_version: 4.4.0
     parameters:
         - agent_host:
             type: List
-            brief: name of the host where the agent will register en each case
+            brief: Name of the host where the agent will register in each case.
         - clean_enviroment:
-            type: fixture
+            type: Fixture
             brief: Reset the wazuh log files at the start of the test. Remove all registered agents from master.
         - test_infra_managers
             type: List
-            brief: list of manager hosts in enviroment
+            brief: List of manager hosts in enviroment.
         - test_infra_agents
             type: List
-            brief: list of agent hosts in enviroment
+            brief: List of agent hosts in enviroment.
         - host_manager
             type: HostManager object
-            brief: handles connection the enviroment's hosts.
+            brief: Handles connection the enviroment's hosts.
     assertions:
         - Verify that after registering and after starting the agent, the agent has the default group is assigned.
         - Assert that all Agents have been restarted
@@ -102,7 +102,7 @@ def test_agent_groups_sync_default(agent_host, clean_environment, test_infra_man
     '''
 
     # Register agents in manager
-    agent_data=[]
+    agent_data = []
     for index, agent in enumerate(test_infra_agents):
         data = register_agent(agent, agent_host, host_manager)
         agent_data.append(data)
@@ -113,7 +113,7 @@ def test_agent_groups_sync_default(agent_host, clean_environment, test_infra_man
     while time.time() < end_time:
         if active_agent < agents_in_cluster:
             host_manager.run_command(test_infra_agents[active_agent], f'{WAZUH_PATH}/bin/wazuh-control start')
-            active_agent = active_agent +1
+            active_agent = active_agent + 1
 
     assert active_agent == agents_in_cluster, f"Unable to restart all agents in the expected time. \
                                                 Agents restarted: {active_agent}"
