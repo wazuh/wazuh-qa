3622c46ef843599f5a49ac336d44e1424c8e4d4a
Facundo Mayon <facundomayon@gmail.com>
2022-04-22 13:49:13 -0300

    Refactor xpath functions

--- tests/frontend/test_kibana_app/cypress/integration/step-definitions/filters/navigate-to-agent-module.when.js
+++ tests/frontend/test_kibana_app/cypress/integration/step-definitions/filters/navigate-to-agent-module.when.js
@@ -1,10 +1,10 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
-import { clickElement, elementIsVisible, xpathElementIsVisible, clickXpathElement} from '../../utils/driver';
+import { clickElement, elementIsVisible, xpathElementIsVisible, clickElementByXpath} from '../../utils/driver';
 import { moreLink} from '../../pageobjects/agents/agents.page';
 import { AGENT_MODULES } from '../../utils/mappers/agent-modules-mapper'; 
 When('The user navigates to agentModule {}', (moduleName) => {
   xpathElementIsVisible(moreLink);
-  clickXpathElement(moreLink);
+  clickElementByXpath(moreLink);
   elementIsVisible(AGENT_MODULES[moduleName]);
   clickElement(AGENT_MODULES[moduleName]);
   });
--- tests/frontend/test_kibana_app/cypress/integration/step-definitions/filters/navigate-to-modules.when.js
+++ tests/frontend/test_kibana_app/cypress/integration/step-definitions/filters/navigate-to-modules.when.js
@@ -1,7 +1,7 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
-import { xpathElementIsVisible, forceXpathClickElement} from '../../utils/driver';
+import { xpathElementIsVisible, forceClickElementByXpath} from '../../utils/driver';
 import { BASIC_MODULES } from '../../utils/mappers/basic-modules-mapper';
 When('The user goes to {}', (moduleName) => {
   xpathElementIsVisible(BASIC_MODULES[moduleName]);
-  forceXpathClickElement(BASIC_MODULES[moduleName]);
+  forceClickElementByXpath(BASIC_MODULES[moduleName]);
 });
