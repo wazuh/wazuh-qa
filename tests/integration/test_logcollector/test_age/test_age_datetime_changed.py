f197aa4eb07ff0d7e03c2c8e40fecb033f409e98
camila <camiromero100@gmail.com>
2022-05-20 14:18:25 -0300

    add: Add skip for windows

--- tests/integration/test_logcollector/test_age/test_age_datetime_changed.py
+++ tests/integration/test_logcollector/test_age/test_age_datetime_changed.py
@@ -58,6 +58,7 @@ tags:
     - logcollector_age
 '''
 import os
+import sys
 import time
 import tempfile
 from datetime import datetime
@@ -135,6 +136,7 @@ def restart_logcollector_function():
 
 
 @pytest.mark.parametrize('new_datetime', new_host_datetime)
+@pytest.mark.skipif(sys.platform == 'win32', reason='It will be blocked by https://github.com/wazuh/wazuh-qa/issues/2174.')
 def test_configuration_age_datetime(get_configuration, configure_environment, configure_local_internal_options_module,
                                     restart_monitord, restart_logcollector_function, file_monitoring,
                                     new_datetime, get_files_list, create_file_structure_function):
