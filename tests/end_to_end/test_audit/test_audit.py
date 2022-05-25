0abbf465519d2ae2cd0c1197f85b7323055d6e1f
Julia <juliamgnr@gmail.com>
2022-05-25 16:14:55 +0200

    add: Add audit test

--- tests/end_to_end/test_audit/test_audit.py
+++ tests/end_to_end/test_audit/test_audit.py
@@ -1,6 +1,16 @@
 import pytest
+import os
+
+from wazuh_testing.event_monitor import check_event
+
+alerts_json = os.path.join('/tmp', 'alerts.json')
 
 
-@pytest.mark.ansible_playbook_setup('configuration.yml', 'generate_events.yml', 'get_alerts.yml')
-def test_bar(ansible_playbook):
-    assert 1 == 1
+@pytest.mark.ansible_playbook_setup('credentials.yml', 'configuration.yml', 'generate_events.yml')
+def test_audit(ansible_playbook, get_dashboard_credentials, clean_environment):
+
+    expected_alert = r'\{"timestamp":"(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)","rule"\:{"level"\:3,"description"\:"Audit\: '\
+                        r'Command\: \/usr\/bin\/ping\.","id"\:"80792","firedtimes"\:(\d+).*euid=1000.*' \
+                        r'a3=\\"www\.google\.com\\".*\}'
+
+    check_event(callback=expected_alert, file_to_monitor=alerts_json)
