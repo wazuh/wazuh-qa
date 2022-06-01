579c3bd34ed96c461a823a33f128a9e06aaade49
Belen Valdivia <belen.valdivia@wazuh.com>
2022-06-01 14:36:31 -0300

    added test for netcat

--- tests/end_to_end/test_netcat/test_netcat.py
+++ tests/end_to_end/test_netcat/test_netcat.py
@@ -9,49 +9,32 @@ brief: Wazuh includes a registration process that provides the user with an auto
        is empty, it automatically requests the key from the configured manager the agent is reporting to.
 '''
 
-import paramiko
+import os
 import pytest
 from opensearchpy import OpenSearch
 import yaml
-import datetime
-import time
 
 
 @pytest.fixture
 def configurations():
-    yaml_file_path = 'data/config.yml'
+    yaml_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/config.yml')
     with open(yaml_file_path) as stream:
         configurations = yaml.safe_load(stream)
     return configurations
 
-
-def test_netcat(configurations):
+@pytest.mark.ansible_playbook_setup('generate_events.yml')
+def test_netcat(configurations, ansible_playbook):
     """
     Test to detect Netcat
     """
-    ip_agent = configurations['wazuh-agent-linux'][0]['ip']
     agent_hostname = configurations['wazuh-agent-linux'][1]['hostname']
     rule_id = configurations['wazuh-agent-linux'][2]['rule_id']
     indexer_ip = configurations['wazuh-manager-indexer'][0]['ip']
     indexer_user = configurations['wazuh-manager-indexer'][2]['username_indexer']
     indexer_password = configurations['wazuh-manager-indexer'][3]['password_ indexer']
 
-    current_time = datetime.datetime.utcnow()
-    timestamp = current_time.timestamp()
-    timestamp_formatted = time.strftime('%Y-%m-%dT%H:%M:%S.%3M+0000', time.localtime(timestamp))
-
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-    ssh.connect(hostname= ip_agent, username='vagrant', password="vagrant")
-    command = 'nc -l 8000'
-    ssh.exec_command(command)
-    time.sleep(30)
-    kill_nc = "sudo pkill -fx 'nc -l 8000'"
-    ssh.exec_command(kill_nc)
-    print(kill_nc)
-    ssh.close
     open_search_alerts = _get_opensearch_alert(indexer_ip, rule_id, indexer_user, indexer_password)
-    _asserts(open_search_alerts, agent_hostname, timestamp_formatted)
+    _asserts(open_search_alerts, agent_hostname)
 
 
 def _get_opensearch_alert(indexer_ip, rule_id, username, password):
@@ -62,8 +45,7 @@ def _get_opensearch_alert(indexer_ip, rule_id, username, password):
     host = indexer_ip
     port = '9200'
     index_name = 'wazuh-alerts-4.x-*'
-    rule_id = rule_id
-    query = '{"query": {"bool": {"must": [{"term": {"rule.id": "' + rule_id + '"}}]}}, "size": 1, "sort": [{"timestamp": {"order": "desc"}}]}'
+    query = '{"query": {"bool": {"must": []}}, "size": 1, "sort": [{"timestamp": {"order": "desc"}}]}'
 
     client = OpenSearch(
         hosts= [{'host': host, 'port': port}],
@@ -85,12 +67,11 @@ def _get_opensearch_alert(indexer_ip, rule_id, username, password):
     return response
 
 
-def _asserts(response, agent_hostname, timestamp_test):
+def _asserts(response, agent_hostname):
     agent = response['hits']['hits'][0]['_source']['agent']['name']
     description = response['hits']['hits'][0]['_source']['rule']['description']
     rule_id = response['hits']['hits'][0]['_source']['rule']['id']
-    timestamp = response['hits']['hits'][0]['_source']['timestamp']
-    assert timestamp >= timestamp_test , 'Alert not generated'
+    print(response['hits']['hits'][0]['_source']['timestamp'])
     assert description == 'Netcat listening for incoming connections.'
     assert rule_id == '100051', 'Invalid rule id'
     assert agent == agent_hostname, 'Invalid agent'
