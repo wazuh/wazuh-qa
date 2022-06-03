db49b50a7254d22970a990e2840240bf258e256e
Mauro Malara <mauro.malara@wazuh.com>
2022-06-03 15:17:43 -0300

    fix: run_ansible_playbooks fixed, debug line deleted. #2935

--- tests/end_to_end/conftest.py
+++ tests/end_to_end/conftest.py
@@ -5,7 +5,6 @@ from pytest_ansible_playbook import runner
 @pytest.fixture(scope="module")
 def run_ansible_playbooks(request):
     # Check if the required attributes are defined.
-    import pdb; pdb.set_trace()
     try:
         params = request.module.playbooks
     except AttributeError as e:
