0242710672680aa242f0b427e239bd181d67721f
Julia <juliamgnr@gmail.com>
2022-06-03 12:52:13 +0200

    fix: Move `conftest.py` and fixtures to run playbooks without marker and run extra playbooks after setup. (#2935)
    Author:    Julia <juliamgnr@gmail.com>
    Date:      Fri Jun 3 12:52:13 2022 +0200
    Co-authored-by: mauromalara <mauro.malara@wazuh.com>

--- tests/end_to_end/conftest.py
+++ tests/end_to_end/conftest.py
@@ -1,8 +1,45 @@
 # Copyright (C) 2015-2022, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+import os
 import pytest
 from pytest_ansible_playbook import runner
+
+from wazuh_testing.tools.file import remove_file, get_file_lines
+
+alerts_json = os.path.join('/tmp', 'alerts.json')
+credentials_file = os.path.join('/tmp', 'passwords.wazuh')
+
+
+@pytest.fixture(scope='function')
+def clean_environment():
+
+    yield
+
+    remove_file(alerts_json)
+    remove_file(credentials_file)
+
+
+@pytest.fixture(scope='function')
+def get_dashboard_credentials():
+
+    password = ''
+    user = ''
+
+    for line in get_file_lines(credentials_file):
+        if 'username: admin' in line:
+            user = 'admin'
+
+        if 'password: ' in line and user == 'admin':
+            password_line = line
+            password = password_line.split()[1]
+
+        if user != '' and password != '':
+            break
+
+    dashboard_credentials = {'user': user, 'password': password}
+
+    yield dashboard_credentials
 
 
 @pytest.fixture(scope="module")
