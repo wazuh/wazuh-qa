493de86ea34b17911e51acf49f0ce5c346872403
Mauro Malara <mauro.malara@wazuh.com>
2022-06-07 16:36:58 -0300

    doc: documentation added and gettempdir function added. #2935

--- tests/end_to_end/test_docker_monitoring/conftest.py
+++ tests/end_to_end/test_docker_monitoring/conftest.py
@@ -1,10 +1,14 @@
+# Copyright (C) 2015-2022, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 import os
 import pytest
+from tempfile import gettempdir
 
 from wazuh_testing.tools import file
 
 
-credentials_file = os.path.join('/tmp', 'passwords.wazuh')
+credentials_file = os.path.join(gettempdir(), 'passwords.wazuh')
 
 
 @pytest.fixture(scope="function")
