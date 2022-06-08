1e2fb2535cf0bb939d775b98ea5e716abfb8dfdb
camila <camiromero100@gmail.com>
2022-06-08 11:29:42 -0300

    add: Add custom ar script

--- tests/integration/test_active_response/test_analysisd/test_overwritten_rules_ar.py
+++ tests/integration/test_active_response/test_analysisd/test_overwritten_rules_ar.py
@@ -13,14 +13,16 @@ TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data
 CONFIGURATIONS_PATH = os.path.join(TEST_DATA_PATH, 'configuration_template')
 TEST_CASES_PATH = os.path.join(TEST_DATA_PATH, 'test_cases')
 RULES_PATH = os.path.join(TEST_DATA_PATH, 'rules')
+CUSTOM_AR_SCRIPT_PATH = os.path.join(TEST_DATA_PATH, 'scripts')
 
 # Configuration and cases data
 configurations_path = os.path.join(CONFIGURATIONS_PATH, 'configuration_overwritten_rules.yaml')
 cases_path = os.path.join(TEST_CASES_PATH, 'cases_overwritten_rules.yaml')
 custom_rule = os.path.join(RULES_PATH, '0270-web_appsec_rules_edit.xml')
 local_rules = os.path.join(RULES_PATH, 'local_rules.xml')
+custom_ar_script = os.path.join(CUSTOM_AR_SCRIPT_PATH, 'custom-ar.py')
 output_custom_ar_script = '/tmp/file-ar.txt'
-source_path = [custom_rule, local_rules]
+source_path = [custom_rule, local_rules, custom_ar_script]
 destination_path = [f"{CUSTOM_RULES_PATH}/0270-web_appsec_rules_edit.xml", LOCAL_RULES_PATH, f"{AR_SCRIPTS_PATH}/custom-ar.py"]
 
 AR_timeout = 10
