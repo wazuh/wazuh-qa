c5b9d5ea7398b50d5cd1cc4b8f0a8d5eeae34119
Belen Valdivia <belen.valdivia@wazuh.com>
2022-06-10 11:32:09 -0300

    refact: changes requested in the review

--- tests/end_to_end/test_brute_force/test_brute_force.py
+++ tests/end_to_end/test_brute_force/test_brute_force.py
@@ -1,30 +1,21 @@
-'''
-copyright: Copyright (C) 2015-2022, Wazuh Inc.
-           Created by Wazuh, Inc. <info@wazuh.com>.
-           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
-type: e2e
-brief: Wazuh includes a registration process that provides the user with an automated mechanism to enroll agents with
-       minimal configuration steps. To register an agent using the enrollment method, a manager with a valid IP needs
-       to be configured first. The agent then checks for the registration key in the client.keys file, and when the file
-       is empty, it automatically requests the key from the configured manager the agent is reporting to.
-
-'''
-
 import os
 import pytest
 from tempfile import gettempdir
 
 from wazuh_testing.tools import configuration as config
-from wazuh_testing.end_to_end import get_alert_indexer_api, make_query
-from wazuh_testing.event_monitor import check_event
+from wazuh_testing import end_to_end as e2e
+from wazuh_testing import event_monitor as evm
 
-
+# Test cases data
 alerts_json = os.path.join(gettempdir(), 'alerts.json')
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 test_cases_file_path = os.path.join(test_data_path, 'test_cases', 'cases_brute_force.yaml')
+
+# Playbooks
 configuration_playbooks = ['credentials.yaml']
 events_playbooks = ['generate_events.yaml']
 
+# Configuration
 configurations, configuration_metadata, cases_ids = config.get_test_cases_data(test_cases_file_path)
 
 
@@ -37,25 +28,29 @@ def test_brute_force(configure_environment, metadata, get_dashboard_credentials,
     rule_id = metadata['rule']['id']
     rule_level = metadata['rule']['level']
     rule_description = metadata['rule']['description']
-    rule_mitre_technique = metadata['rule']['mitre_technique']
+    rule_mitre_technique = metadata['extra']['mitre_technique']
 
     expected_alert = r'\{{"timestamp":"(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)","rule"\:{{"level"\:{},"description"\:"{}",'\
                      r'"id"\:"{}".*\}}'.format(rule_level, rule_description, rule_id)
 
-    query = make_query([
+    query = e2e.make_query([
          {
             "term": {
                "rule.id": f"{rule_id}"
             }
          }
      ])
-    indexed_alert = get_alert_indexer_api(query=query, credentials=get_dashboard_credentials).json()
+    indexed_alert = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials).json()
 
     try:
+        # Check that indexed alert has the correct rule id
         assert indexed_alert['hits']['hits'][0]['_source']['rule']['id'] == str(rule_id), 'Invalid rule id'
+        # Check that indexed alert has the correct rule level
         assert indexed_alert['hits']['hits'][0]['_source']['rule']['level'] == rule_level, 'Invalid rule level'
+        # Check that indexed alert has the correct rule description
         assert indexed_alert['hits']['hits'][0]['_source']['rule']['description'] == rule_description, 'Invalid description'
+        # Check that indexed alert has the correct mitre technique
         assert indexed_alert['hits']['hits'][0]['_source']['rule']['mitre']['technique'][0] == rule_mitre_technique, 'Invalid mitre technique'
     except AssertionError:
-        check_event(callback=expected_alert, file_to_monitor=alerts_json, error_message='The alert has not occurred')
+        evm.check_event(callback=expected_alert, file_to_monitor=alerts_json, error_message='The alert has not occurred')
         raise AssertionError('The alert has occurred, but has not been indexed.')
