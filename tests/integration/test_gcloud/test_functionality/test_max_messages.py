801eebfad0fe9aae44990b4241584b4654f33791
camila <camiromero100@gmail.com>
2022-06-15 10:54:05 -0300

    mf: Modify variable - count_message

--- tests/integration/test_gcloud/test_functionality/test_max_messages.py
+++ tests/integration/test_gcloud/test_functionality/test_max_messages.py
@@ -75,7 +75,6 @@ interval = '25s'
 pull_messages_timeout = global_parameters.default_timeout + 60
 pull_on_start = 'no'
 max_messages = 100
-count_message = 0
 logging = 'info'
 wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -169,6 +168,7 @@ def test_max_messages(get_configuration, configure_environment, reset_ossec_log,
         - logs
         - scheduled
     '''
+    count_message = 0
     str_interval = get_configuration['sections'][0]['elements'][4]['interval']['value']
     time_interval = int(''.join(filter(str.isdigit, str_interval)))
 
