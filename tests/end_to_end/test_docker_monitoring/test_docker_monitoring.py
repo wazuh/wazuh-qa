36b2041ccde824b50f63d79895dc0ba2dfbf12c4
Mauro Malara <mauro.malara@wazuh.com>
2022-06-16 21:03:02 -0300

    refac: test refactored to follow the e2e tests standard. #2935

--- tests/end_to_end/test_docker_monitoring/test_docker_monitoring.py
+++ tests/end_to_end/test_docker_monitoring/test_docker_monitoring.py
@@ -1,9 +1,13 @@
-import json
 import os
+import json
 import re
 import pytest
+from datetime import datetime
 from tempfile import gettempdir
+from time import sleep
 
+import wazuh_testing as fw
+from wazuh_testing.tools.time import parse_date_time_format
 from wazuh_testing import end_to_end as e2e
 from wazuh_testing import event_monitor as evm
 from wazuh_testing.tools import configuration as config
@@ -30,10 +34,12 @@ def test_docker_monitoring(configure_environment, metadata, get_dashboard_creden
     rule_id = metadata['rule.id']
     rule_level = metadata['rule.level']
     docker_action = metadata['extra']['data.docker.Action']
-    expected_api_alert = f".+Action.+{docker_action}.+level.+{rule_level}.+description.+{rule_description}.+" \
-                         f"id.+{rule_id}.+"
-    expected_log_alert = f".+level.+{rule_level}.+description.+{rule_description}.+id.+{rule_id}.+" \
-                         f"Action.+{docker_action}.+"
+
+    expected_alert_json = f".+timestamp\":\"(.+)\",.+level.+{rule_level}.+description.+{rule_description}.+" \
+                          f"id.+{rule_id}.+Action.+{docker_action}.+"
+
+    expected_indexed_alert = f".+Action.+{docker_action}.+level.+{rule_level}.+description.+{rule_description}.+" \
+                             f"id.+{rule_id}.+timestamp\": \"(.+)\",.+"
 
     query = e2e.make_query([
         {
@@ -53,15 +59,27 @@ def test_docker_monitoring(configure_environment, metadata, get_dashboard_creden
         }
     ])
 
-    response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
-    assert response.status_code == 200, f"The response is not the expected. Actual response {response.text}"
+    # Check that alert has been raised and save timestamp
+    raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
+                                   error_message='The alert has not occurred').result()
+    raised_alert_timestamp = raised_alert.group(1)
+    raised_alert_timestamp = datetime.strptime(parse_date_time_format(raised_alert_timestamp), '%Y-%m-%d %H:%M:%S')
 
+    # Wait a few seconds for the alert to be indexed (alert.json -> filebeat -> wazuh-indexer)
+    sleep(fw.T_10)
+
+    # Get indexed alert
+    response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
     indexed_alert = json.dumps(response.json())
 
-    try:
-        match = re.search(expected_api_alert, indexed_alert)
-        assert match is not None, 'The alert was triggered but not indexed.'
-    except AssertionError as exc:
-        err_msg = 'The alert was not triggered.'
-        evm.check_event(callback=expected_log_alert, file_to_monitor=alerts_json, error_message=err_msg)
-        raise AssertionError(exc.args[0])
+
+    # Check that the alert data is the expected one
+    alert_data = re.search(expected_indexed_alert, indexed_alert)
+    assert alert_data is not None, 'Alert triggered, but not indexed'
+
+    # Get indexed alert timestamp
+    indexed_alert_timestamp = alert_data.group(1)
+    indexed_alert_timestamp = datetime.strptime(parse_date_time_format(indexed_alert_timestamp), '%Y-%m-%d %H:%M:%S')
+
+    # Check that alert has been indexed (checking that the timestamp is the expected one)
+    assert indexed_alert_timestamp == raised_alert_timestamp, 'Alert triggered, but not indexed'
