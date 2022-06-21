9b77489c411072ee891e1697cafceb3d1ea43137
Belen Valdivia <belen.valdivia@wazuh.com>
2022-06-21 12:20:26 -0300

    add: test sql injection

--- tests/end_to_end/test_sql_injection/test_sql_injection.py
+++ tests/end_to_end/test_sql_injection/test_sql_injection.py
@@ -1,46 +1,47 @@
-from cmath import exp
 import os
 import json
 import re
 import pytest
 from datetime import datetime
 from tempfile import gettempdir
+from time import sleep
 
-from wazuh_testing.tools import configuration as config
 from wazuh_testing import end_to_end as e2e
 from wazuh_testing import event_monitor as evm
+from wazuh_testing.tools import configuration as config
 from wazuh_testing.tools.time import parse_date_time_format
 
-# Test cases data
+
 alerts_json = os.path.join(gettempdir(), 'alerts.json')
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-test_cases_file_path = os.path.join(test_data_path, 'test_cases', 'cases_brute_force.yaml')
-
-# Playbooks
+test_cases_file_path = os.path.join(test_data_path, 'test_cases', 'cases_sql_injection.yaml')
+configuration_playbooks = ['configuration.yaml']
 events_playbooks = ['generate_events.yaml']
+wait_indexed_alert = 5
 
-# Configuration
 configurations, configuration_metadata, cases_ids = config.get_test_cases_data(test_cases_file_path)
 
 
 @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
 @pytest.mark.parametrize('metadata', configuration_metadata, ids=cases_ids)
-def test_brute_force(metadata, get_dashboard_credentials, generate_events, clean_environment):
+def test_audit(configure_environment, metadata, get_dashboard_credentials, generate_events, clean_environment):
     """
-    Test to detect a SSH Brute Force attack
+    Test to detect a SQL injection attack
     """
     rule_id = metadata['rule.id']
     rule_level = metadata['rule.level']
     rule_description = metadata['rule.description']
     rule_mitre_technique = metadata['extra']['mitre_technique']
 
-    expected_alert_json = fr'.*"rule":.*"level":{rule_level},"description:"{rule_description}".*"id":"{rule_id}'
+    expected_alert_json = fr'\{{"timestamp":"(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)","rule"\:{{"level"\:{rule_level},' \
+                          fr'"description"\:"{rule_description}","id"\:"{rule_id}".*\}}'
 
     expected_indexed_alert = fr'.*"rule":.*"level": {rule_level},.*"description": "{rule_description}"' \
                              fr'.*"mitre":.*"{rule_mitre_technique}".*"id": "{rule_id}".*'\
                              r'"timestamp": "(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)".*'
 
     query = e2e.make_query([
+
          {
             "term": {
                "rule.id": f"{rule_id}"
@@ -49,11 +50,13 @@ def test_brute_force(metadata, get_dashboard_credentials, generate_events, clean
      ])
 
     # Check that alert has been raised and save timestamp
-    print(expected_alert_json)
     raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
                                    error_message='The alert has not occurred').result()
     raised_alert_timestamp = raised_alert.group(1)
     raised_alert_timestamp = datetime.strptime(parse_date_time_format(raised_alert_timestamp), '%Y-%m-%d %H:%M:%S')
+
+    # Wait a few seconds for the alert to be indexed (alert.json -> filebeat -> wazuh-indexer)
+    sleep(wait_indexed_alert)
 
     # Get indexed alert
     response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
