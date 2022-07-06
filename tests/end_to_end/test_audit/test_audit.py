4fa8fa6ae47571f217885a22471316938c2ce4d3
Julia <juliamgnr@gmail.com>
2022-07-06 15:13:10 +0200

    fix: Sort imports

--- tests/end_to_end/test_audit/test_audit.py
+++ tests/end_to_end/test_audit/test_audit.py
@@ -6,10 +6,10 @@ from datetime import datetime
 from tempfile import gettempdir
 from time import sleep
 
+from wazuh_testing.tools.time import parse_date_time_format
 from wazuh_testing import end_to_end as e2e
 from wazuh_testing import event_monitor as evm
 from wazuh_testing.tools import configuration as config
-from wazuh_testing.tools.time import parse_date_time_format
 
 
 alerts_json = os.path.join(gettempdir(), 'alerts.json')
