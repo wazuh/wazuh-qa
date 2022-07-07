4f72a1c552a8e436f1a749069407b2d956cf3a23
camila <camiromero100@gmail.com>
2022-07-07 16:42:06 -0300

    add: Add fixture to set wazuh configuration

---
+++ tests/integration/test_active_response/test_analysisd/conftest.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2015-2022, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+import pytest
+
+@pytest.fixture(scope='function')
+def set_wazuh_configuration_analysisd(configuration, set_wazuh_configuration):
+    """Set wazuh configuration
+    Args:
+        configuration (dict): Configuration template data to write in the ossec.conf.
+        set_wazuh_configuration (fixture): Set the wazuh configuration according to the configuration data.
+    """
+    yield
