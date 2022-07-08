40e19faf999451982cd1062431ef281e04ac9e70
Julia <juliamgnr@gmail.com>
2022-07-08 13:18:46 +0200

    fix: Get osquery alert by timestamp

--- tests/end_to_end/test_osquery_integration/test_osquery_integration.py
+++ tests/end_to_end/test_osquery_integration/test_osquery_integration.py
@@ -2,12 +2,8 @@ import os
 import json
 import re
 import pytest
-from datetime import datetime
 from tempfile import gettempdir
-from time import sleep
 
-import wazuh_testing as fw
-from wazuh_testing.tools.time import parse_date_time_format
 from wazuh_testing import end_to_end as e2e
 from wazuh_testing import event_monitor as evm
 from wazuh_testing.tools import configuration as config
@@ -40,6 +36,11 @@ def test_osquery_integration(configure_environment, metadata, get_dashboard_cred
                              fr"\"description\": \"{rule_description}\".+\"id\": \"{rule_id}\"" \
                              r'.+timestamp\": \"(.+)\"},.+'
 
+    # Check that alert has been raised and save timestamp
+    raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
+                                   error_message='The alert has not occurred').result()
+    raised_alert_timestamp = raised_alert.group(1)
+
     query = e2e.make_query([
         {
             "term": {
@@ -50,17 +51,13 @@ def test_osquery_integration(configure_environment, metadata, get_dashboard_cred
             "term": {
                 "data.osquery.name": f"{osquery_name}"
             }
+        },
+        {
+            "term": {
+                "timestamp": f"{raised_alert_timestamp }"
+            }
         }
     ])
-
-    # Check that alert has been raised and save timestamp
-    raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
-                                   error_message='The alert has not occurred').result()
-    raised_alert_timestamp = raised_alert.group(1)
-    raised_alert_timestamp = datetime.strptime(parse_date_time_format(raised_alert_timestamp), '%Y-%m-%d %H:%M:%S')
-
-    # Wait a few seconds for the alert to be indexed (alert.json -> filebeat -> wazuh-indexer)
-    sleep(fw.T_10)
 
     # Get indexed alert
     response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
@@ -69,10 +66,3 @@ def test_osquery_integration(configure_environment, metadata, get_dashboard_cred
     # Check that the alert data is the expected one
     alert_data = re.search(expected_indexed_alert, indexed_alert)
     assert alert_data is not None, 'Alert triggered, but not indexed'
-
-    # Get indexed alert timestamp
-    indexed_alert_timestamp = alert_data.group(1)
-    indexed_alert_timestamp = datetime.strptime(parse_date_time_format(indexed_alert_timestamp), '%Y-%m-%d %H:%M:%S')
-
-    # Check that alert has been indexed (checking that the timestamp is the expected one)
-    assert indexed_alert_timestamp == raised_alert_timestamp, 'Alert triggered, but not indexed'
