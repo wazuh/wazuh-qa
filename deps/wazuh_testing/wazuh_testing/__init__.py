4a0e933aabf86bd7b3519266fa3c2f8af7a44289
Mauro Malara <mauro.malara@wazuh.com>
2022-07-08 18:10:08 -0300

    style: imports ordered and rewritten correctly, pycodestyle suggestions applied, and variables added to the framework. #2580

--- deps/wazuh_testing/wazuh_testing/__init__.py
+++ deps/wazuh_testing/wazuh_testing/__init__.py
@@ -8,6 +8,13 @@ import os
 import yaml
 from collections import defaultdict
 
+# Timeouts
+T_5 = 5
+T_10 = 10
+T_20 = 20
+T_30 = 30
+
+# Protocols
 UDP = 'UDP'
 TCP = 'TCP'
 TCP_UDP = 'TCP,UDP'
@@ -22,7 +29,7 @@ def is_tcp(protocol):
 
 
 def is_tcp_udp(protocol):
-    _protocol = protocol.replace(' ','').upper().split(',')
+    _protocol = protocol.replace(' ', '').upper().split(',')
     _protocol.sort()
     return ','.join(_protocol) == TCP_UDP
 
