7cfa97e390f32a907084bf78a858fa6076a9b881
Julia <juliamgnr@gmail.com>
2022-07-08 13:19:48 +0200

    fix: Get SQL injection alert by timestamp

--- tests/end_to_end/test_sql_injection/test_sql_injection.py
+++ tests/end_to_end/test_sql_injection/test_sql_injection.py
@@ -2,11 +2,8 @@ import os
 import json
 import re
 import pytest
-from datetime import datetime
 from tempfile import gettempdir
-from time import sleep
 
-from wazuh_testing.tools.time import parse_date_time_format
 from wazuh_testing import end_to_end as e2e
 from wazuh_testing import event_monitor as evm
 from wazuh_testing.tools import configuration as config
@@ -17,7 +14,6 @@ test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data
 test_cases_file_path = os.path.join(test_data_path, 'test_cases', 'cases_sql_injection.yaml')
 configuration_playbooks = ['configuration.yaml']
 events_playbooks = ['generate_events.yaml']
-wait_indexed_alert = 5
 
 configurations, configuration_metadata, cases_ids = config.get_test_cases_data(test_cases_file_path)
 
@@ -40,23 +36,24 @@ def test_audit(configure_environment, metadata, get_dashboard_credentials, gener
                              fr'.*"mitre":.*"{rule_mitre_technique}".*"id": "{rule_id}".*'\
                              r'"timestamp": "(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)".*'
 
+    # Check that alert has been raised and save timestamp
+    raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
+                                   error_message='The alert has not occurred').result()
+    raised_alert_timestamp = raised_alert.group(1)
+
     query = e2e.make_query([
 
          {
             "term": {
                "rule.id": f"{rule_id}"
             }
+         },
+         {
+            "term": {
+               "timestamp": f"{raised_alert_timestamp}"
+            }
          }
      ])
-
-    # Check that alert has been raised and save timestamp
-    raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
-                                   error_message='The alert has not occurred').result()
-    raised_alert_timestamp = raised_alert.group(1)
-    raised_alert_timestamp = datetime.strptime(parse_date_time_format(raised_alert_timestamp), '%Y-%m-%d %H:%M:%S')
-
-    # Wait a few seconds for the alert to be indexed (alert.json -> filebeat -> wazuh-indexer)
-    sleep(wait_indexed_alert)
 
     # Get indexed alert
     response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
@@ -65,10 +62,3 @@ def test_audit(configure_environment, metadata, get_dashboard_credentials, gener
     # Check that the alert data is the expected one
     alert_data = re.search(expected_indexed_alert, indexed_alert)
     assert alert_data is not None, 'Alert triggered, but not indexed'
-
-    # Get indexed alert timestamp
-    indexed_alert_timestamp = alert_data.group(1)
-    indexed_alert_timestamp = datetime.strptime(parse_date_time_format(indexed_alert_timestamp), '%Y-%m-%d %H:%M:%S')
-
-    # Check that alert has been indexed (checking that the timestamp is the expected one)
-    assert indexed_alert_timestamp == raised_alert_timestamp, 'Alert triggered, but not indexed'
