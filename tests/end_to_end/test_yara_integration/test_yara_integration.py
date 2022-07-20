0abc9788956f81a57e334d350532d0deb5ee034b
Mauro Malara <mauro.malara@wazuh.com>
2022-07-20 15:51:24 -0300

    cherry-pick 2d8c31b and E2E test Yara integration updated. #3085

--- tests/end_to_end/test_yara_integration/test_yara_integration.py
+++ tests/end_to_end/test_yara_integration/test_yara_integration.py
@@ -2,10 +2,8 @@ import os
 import json
 import re
 import pytest
-from datetime import datetime
 from tempfile import gettempdir
 
-from wazuh_testing.tools.time import parse_date_time_format
 from wazuh_testing import end_to_end as e2e
 from wazuh_testing import event_monitor as evm
 from wazuh_testing.tools import configuration as config
@@ -14,23 +12,30 @@ from wazuh_testing.tools import configuration as config
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 test_cases_path = os.path.join(test_data_path, 'test_cases')
 test_cases_file_path = os.path.join(test_cases_path, 'cases_yara_integration.yaml')
-yara_script = os.path.join(test_data_path, 'configuration', 'yara.sh')
-malware_downloader_script = os.path.join(test_data_path, 'configuration', 'malware_downloader.sh')
 alerts_json = os.path.join(gettempdir(), 'alerts.json')
 
 # Playbooks
 configuration_playbooks = ['configuration.yaml']
 events_playbooks = ['generate_events.yaml']
 teardown_playbooks = ['teardown.yaml']
-configuration_extra_vars = {'yara_script': yara_script, 'malware_downloader_script': malware_downloader_script}
 
 # Configuration
 configurations, configuration_metadata, cases_ids = config.get_test_cases_data(test_cases_file_path)
 
 
-@pytest.mark.parametrize('metadata', configuration_metadata, ids=cases_ids)
+# Custom paths
+yara_script = os.path.join(test_data_path, 'configuration', 'yara.sh')
+malware_downloader_script = os.path.join(test_data_path, 'configuration', 'malware_downloader.sh')
+
+# Update configuration with custom paths
+configurations = config.update_configuration_template(configurations,
+                                                      ['CUSTOM_YARA_SCRIPT_PATH', 'CUSTOM_MALWARE_SCRIPT_PATH'],
+                                                      [yara_script, malware_downloader_script])
+
+
+@pytest.mark.parametrize('configuration,metadata', zip(configurations, configuration_metadata), ids=cases_ids)
 @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
-def test_yara_integration(configure_environment, metadata, get_dashboard_credentials, generate_events,
+def test_yara_integration(configure_environment, configuration, metadata, get_dashboard_credentials, generate_events,
                           clean_alerts_index):
     rule_description = metadata['rule.description']
     rule_id = metadata['rule.id']
