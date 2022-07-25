8673408a96efe3063c522f319218ca24b9ccdf07
Deblintrake09 <amicalizzi2005@gmail.com>
2022-07-25 10:25:53 -0300

    Fix: extract variables and fix format

--- tests/integration/test_integratord/conftest.py
+++ tests/integration/test_integratord/conftest.py
@@ -4,17 +4,19 @@ copyright: Copyright (C) 2015-2022, Wazuh Inc.
            This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 '''
 
+
 import pytest
 
 from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.tools.monitoring import FileMonitor, callback_generator
-from wazuh_testing.modules.integratord import (CB_VIRUSTOTAL_ENABLED,ERR_MSG_VIRUST_TOTAL_ENABLED_NOT_FOUND)
+from wazuh_testing.modules import integratord as integrator
 
 
+# Fixtures
 
 @pytest.fixture(scope='function')
 def wait_for_start_module(request):
     # Wait for Virustotal Integration to start
     file_monitor = FileMonitor(LOG_FILE_PATH)
-    file_monitor.start(timeout=20, callback=callback_generator(CB_VIRUSTOTAL_ENABLED),
-                       error_message=ERR_MSG_VIRUST_TOTAL_ENABLED_NOT_FOUND)
+    file_monitor.start(timeout=20, callback=callback_generator(integrator.CB_INTEGRATORD_THREAD_READY),
+                       error_message=integrator.ERR_MSG_VIRUST_TOTAL_ENABLED_NOT_FOUND)
