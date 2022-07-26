74f50fb97e13658c433ade112e0a90af982fb19b
Mauro Malara <mauro.malara@wazuh.com>
2022-07-26 18:19:57 -0300

    fix: corrections applied. #3079

--- tests/end_to_end/test_aws_infrastructure_monitoring/data/configuration/aws_cloudtrail_event.py
+++ tests/end_to_end/test_aws_infrastructure_monitoring/data/configuration/aws_cloudtrail_event.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
+from time import sleep
 import boto3
 
+
+# Hide deprecation warning for python<=3.6
+boto3.compat.filter_python_deprecation_warnings()
 
 formats = ['%a, %d %b %Y %H:%M:%S %Z', '%Y-%m-%dT%H:%M:%SZ']
 
@@ -24,28 +28,41 @@ def get_parameters():
     return arguments
 
 
-def create_bucket(access_key_id, secret_access_key):
-    """ Create an S3 bucket in the default region (us-east-1)
+def create_bucket(client):
+    """Create an S3 bucket in the default region (us-east-1)
 
     Args:
         access_key_id (str): AWS access key ID
         secret_access_key (str): AWS secret access key
     """
-    client = boto3.client('s3', aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key)
     response = client.create_bucket(Bucket='delete-this-dummy-bucket')
     response_date = response['ResponseMetadata']['HTTPHeaders']['date']
     # The format of the request datetieme is changed here to match the timestamp of the AWS event in the alerts.json log
-    request_datetime = datetime.strptime(response_date, formats[0])
-    # The last 3 characters are removed due to the difference with the server in seconds.
-    # e.g: 2022-07-20T15:41:05Z --> 2022-07-20T15:41:
-    datetime_str = request_datetime.strftime(formats[1])[:-3]
+    # 1 second is substracted to avoid the difference with the server time
+    request_datetime = datetime.strptime(response_date, formats[0]) - timedelta(seconds=1)
+    datetime_str = request_datetime.strftime(formats[1])
     # Print the formatted time from the request because Ansible will pick it up from the standard output
     print(datetime_str)
 
 
+def delete_bucket(client):
+    """Delete an S3 bucket in the default region (us-east-1)
+
+    Args:
+        access_key_id (str): AWS access key ID
+        secret_access_key (str): AWS secret access key
+    """
+    client.delete_bucket(Bucket='delete-this-dummy-bucket')
+
+
 def main():
     parameters = get_parameters()
-    create_bucket(parameters.aws_access_key_id, parameters.aws_secret_access_key)
+    client = boto3.client('s3', aws_access_key_id=parameters.aws_access_key_id,
+                          aws_secret_access_key=parameters.aws_secret_access_key)
+    create_bucket(client)
+    # Wait for the event to be generated in AWS
+    sleep(10)
+    delete_bucket(client)
 
 
 if __name__ == '__main__':
