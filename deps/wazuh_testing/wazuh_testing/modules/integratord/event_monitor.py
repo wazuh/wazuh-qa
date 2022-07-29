352bfb54225573943c2eba56b002433a05433839
Deblintrake09 <amicalizzi2005@gmail.com>
2022-07-29 14:53:58 -0300

    Add: integratord event_monitor

---
+++ deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
@@ -0,0 +1,25 @@
+
+
+from wazuh_testing.tools import LOG_FILE_PATH
+from wazuh_testing.tools.monitoring import FileMonitor
+
+
+
+def check_integratord_event(file_monitor=None, callback='', error_message=None, update_position=True,
+                              timeout=30, accum_results=1, file_to_monitor=LOG_FILE_PATH):
+    """Check if an event occurs
+    Args:
+        file_monitor (FileMonitor): FileMonitor object to monitor the file content.
+        callback (str): log regex to check in Wazuh log
+        error_message (str): error message to show in case of expected event does not occur
+        update_position (boolean): filter configuration parameter to search in Wazuh log
+        timeout (str): timeout to check the event in Wazuh log
+        accum_results (int): Accumulation of matches.
+    """
+    file_monitor = FileMonitor(file_to_monitor) if file_monitor is None else file_monitor
+    error_message = f"Could not find this event in {file_to_monitor}: {callback}" if error_message is None else \
+        error_message
+
+    file_monitor.start(timeout=timeout, update_position=update_position, accum_results=accum_results,
+                       callback=callback, error_message=error_message)
+
