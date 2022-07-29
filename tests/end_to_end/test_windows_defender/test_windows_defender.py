c6033305286b2780776d03f11459c66af0d098d2
Belen Valdivia <belen.valdivia@wazuh.com>
2022-07-29 14:25:06 -0300

    fix: rename case

--- tests/end_to_end/test_windows_defender/test_windows_defender.py
+++ tests/end_to_end/test_windows_defender/test_windows_defender.py
@@ -11,7 +11,7 @@ from wazuh_testing import event_monitor as evm
 # Test cases data
 alerts_json = os.path.join(gettempdir(), 'alerts.json')
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-test_cases_file_path = os.path.join(test_data_path, 'test_cases', 'cases_vulnerability_detector.yaml')
+test_cases_file_path = os.path.join(test_data_path, 'test_cases', 'cases_windows_defender.yaml')
 
 # Playbooks
 configuration_playbooks = ['configuration.yaml']
@@ -19,50 +19,50 @@ events_playbooks = ['generate_events.yaml']
 teardown_playbooks = ['teardown.yaml']
 
 # Configuration
-#configurations, configuration_metadata, cases_ids = config.get_test_cases_data(test_cases_file_path)
+configurations, configuration_metadata, cases_ids = config.get_test_cases_data(test_cases_file_path)
 
 
 @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
-#@pytest.mark.parametrize('metadata', configuration_metadata, ids=cases_ids)
-def test_vulnerability_detector(configure_environment, generate_events):
+@pytest.mark.parametrize('metadata', configuration_metadata, ids=cases_ids)
+def test_windows_defender(configure_environment, metadata, get_dashboard_credentials, generate_events,
+                          clean_alerts_index):
     """
     Test to detect a vulnerability
     """
-    print('HOLAAA')
-    # rule_level = metadata['rule.level']
-    # rule_id = metadata['rule.id']
-    # rule_description = metadata['rule.description']
+    rule_level = metadata['rule.level']
+    rule_id = metadata['rule.id']
+    rule_description = metadata['rule.description']
 
-    # expected_alert_json = fr'\{{"timestamp":"(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)",' \
-    #                       fr'"rule"\:{{"level"\:{rule_level},' \
-    #                       fr'"description"\:"{rule_description}","id"\:"{rule_id}".*\}}'
+    expected_alert_json = fr'\{{"timestamp":"(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)",' \
+                          fr'"rule"\:{{"level"\:{rule_level},' \
+                          fr'"description"\:"{rule_description}","id"\:"{rule_id}".*\}}'
 
-    # expected_indexed_alert = fr'.*"rule":.*"level": {rule_level},.*"description": "{rule_description}"' \
-    #                          fr'.*"id": "{rule_id}".*' \
-    #                          r'"timestamp": "(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)".*'
+    expected_indexed_alert = fr'.*"rule":.*"level": {rule_level},.*"description": "{rule_description}"' \
+                             fr'.*"id": "{rule_id}".*' \
+                             r'"timestamp": "(\d+\-\d+\-\w+\:\d+\:\d+\.\d+\+\d+)".*'
 
-    # # Check that alert has been raised and save timestamp
-    # raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
-    #                                error_message='The alert has not occurred').result()
-    # raised_alert_timestamp = raised_alert.group(1)
+    # Check that alert has been raised and save timestamp
+    raised_alert = evm.check_event(callback=expected_alert_json, file_to_monitor=alerts_json,
+                                   error_message='The alert has not occurred').result()
+    raised_alert_timestamp = raised_alert.group(1)
 
-    # query = e2e.make_query([
-    #     {
-    #         "term": {
-    #             "rule.id": f"{rule_id}"
-    #         }
-    #     },
-    #     {
-    #         "term": {
-    #             "timestamp": f"{raised_alert_timestamp}"
-    #         }
-    #     }
-    # ])
+    query = e2e.make_query([
+        {
+            "term": {
+                "rule.id": f"{rule_id}"
+            }
+        },
+        {
+            "term": {
+                "timestamp": f"{raised_alert_timestamp}"
+            }
+        }
+    ])
 
-    # # Check if the alert has been indexed and get its data
-    # response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
-    # indexed_alert = json.dumps(response.json())
+    # Check if the alert has been indexed and get its data
+    response = e2e.get_alert_indexer_api(query=query, credentials=get_dashboard_credentials)
+    indexed_alert = json.dumps(response.json())
 
-    # # Check that the alert data is the expected one
-    # alert_data = re.search(expected_indexed_alert, indexed_alert)
-    # assert alert_data is not None, 'Alert triggered, but not indexed'
+    # Check that the alert data is the expected one
+    alert_data = re.search(expected_indexed_alert, indexed_alert)
+    assert alert_data is not None, 'Alert triggered, but not indexed'
