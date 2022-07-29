f4b14e071ab823a107f2f16691d168b7e4c662fe
camila <camiromero100@gmail.com>
2022-07-29 00:29:29 -0300

    add: Add test documentation

--- tests/integration/test_active_response/test_analysisd/test_overwritten_rules_ar.py
+++ tests/integration/test_active_response/test_analysisd/test_overwritten_rules_ar.py
@@ -1,3 +1,54 @@
+'''
+copyright: Copyright (C) 2015-2022, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: Active responses perform various countermeasures to address active threats, such as blocking access
+       to an agent from the threat source when certain criteria are met.
+
+tier: 2
+
+modules:
+    - active_response
+
+components:
+    - manager
+
+daemons:
+    - wazuh-analysisd
+
+os_platform:
+    - linux
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - CentOS 6
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Ubuntu Xenial
+    - Ubuntu Trusty
+    - Debian Buster
+    - Debian Stretch
+    - Debian Jessie
+    - Debian Wheezy
+    - Red Hat 8
+    - Red Hat 7
+    - Red Hat 6
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/active-response/#active-response
+
+tags:
+    - ar_analysisd
+'''
 import os
 import pytest
 import time
@@ -8,7 +59,7 @@ from wazuh_testing.tools.configuration import load_configuration_template, get_t
 from wazuh_testing.tools.file import remove_file
 from wazuh_testing.tools import CUSTOM_RULES_PATH, LOCAL_RULES_PATH, AR_SCRIPTS_PATH
 
-pytestmark = [pytest.mark.tier(level=0), pytest.mark.server]
+pytestmark = [pytest.mark.tier(level=2), pytest.mark.server]
 
 # Reference paths
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -41,6 +92,51 @@ configurations = load_configuration_template(configurations_path, configuration_
 def test_overwritten_rules_ar(configuration, metadata, create_file_to_monitor, file_to_monitor,
                               set_wazuh_configuration_analysisd, copy_file, source_path, destination_path,
                               restart_wazuh_daemon_function):
+    '''
+    description: Check if 'active response' works correctly when binding an active response for a rule that will be overwritten.
+
+    wazuh_min_version: 4.3.5
+
+    parameters:
+        - configuration:
+            type: fixture
+            brief: Get configurations from the module.
+        - metadata:
+            type: fixture
+            brief: Get metadata from the module.
+        - create_file_to_monitor:
+            type: fixture
+            brief: Create a file to will monitored by Wazuh.
+        - file_to_monitor:
+            type: parametrize
+            brief:  Path of file that will monitored by Wazuh.
+        - set_wazuh_configuration_analysisd:
+            type: fixture
+            brief:  Set configurations from the module.
+        - copy_file:
+            type: fixture
+            brief:  Copy file from source to destination.
+        - source_path:
+            type: parametrize
+            brief: list that contains sources path of files.
+        - destination_path:
+            type: parametrize
+            brief: list that contains destinations path of files.
+        - restart_wazuh_daemon_function:
+            type: fixture
+            brief: restart Wazuh.
+        
+    assertions:
+        - verify that the custom AR created a file.
+
+    input_description: Different use cases are found in the test module.
+
+    expected_output:
+        - A file was successfully created by AR.
+
+    tags:
+        - ar_analysisd
+    '''
     os.chmod(wazuh_ar_script, 0o777)
     cmd = "echo '{}' >> '{}'".format(metadata['log_sample'], file_to_monitor)
     os.system(cmd)
