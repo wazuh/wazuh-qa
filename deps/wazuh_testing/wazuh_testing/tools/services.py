3171e5f1a3d8939f727540d365c9bb835a23985c
Deblintrake09 <amicalizzi2005@gmail.com>
2022-08-03 10:43:50 -0300

    Style: fix overindentation on line 118

--- deps/wazuh_testing/wazuh_testing/tools/services.py
+++ deps/wazuh_testing/wazuh_testing/tools/services.py
@@ -115,12 +115,12 @@ def control_service(action, daemon=None, debug_mode=False):
                 for proc in psutil.process_iter():
                     try:
                         if daemon in ['wazuh-clusterd', 'wazuh-apid']:
-                                for file in os.listdir(f'{WAZUH_PATH}/var/run'):
-                                    if daemon in file:
-                                        pid = file.split("-")
-                                        pid = pid[2][0:-4]
-                                        if pid == str(proc.pid):
-                                            processes.append(proc)
+                            for file in os.listdir(f'{WAZUH_PATH}/var/run'):
+                                if daemon in file:
+                                    pid = file.split("-")
+                                    pid = pid[2][0:-4]
+                                    if pid == str(proc.pid):
+                                        processes.append(proc)
                         elif daemon in proc.name() or daemon in ' '.join(proc.cmdline()):
                             processes.append(proc)
                     except (psutil.NoSuchProcess, psutil.AccessDenied):
