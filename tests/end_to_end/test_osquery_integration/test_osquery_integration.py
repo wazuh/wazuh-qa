5df088a2e5e1bdf9ef57937c90e27d504f6f9c5b
Julia <juliamgnr@gmail.com>
2022-08-11 14:33:00 +0200

    add: Add Osquery documentation

--- tests/end_to_end/test_osquery_integration/test_osquery_integration.py
+++ tests/end_to_end/test_osquery_integration/test_osquery_integration.py
@@ -1,3 +1,41 @@
+'''
+copyright: Copyright (C) 2015-2022, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: end_to_end
+
+brief: This test will verify that the Osquery integration is working correctly. This integration can be helpful for
+       telemetry and threat hinging. Osquery can be used to expose an operating system as a high-performance relational
+       database. This allows you to write SQL-based queries to explore operating system data.
+
+components:
+    - osquery
+
+targets:
+    - manager
+
+daemons:
+    - wazuh-modulesd
+    - wazuh-analysisd
+
+os_platform:
+    - linux
+
+os_version:
+    - CentOS 8
+
+references:
+    - https://github.com/wazuh/wazuh-automation/wiki/Wazuh-demo:-Execution-guide#osquery
+    - https://documentation.wazuh.com/current/proof-of-concept-guide/integrate-osquery.html
+    - https://documentation.wazuh.com/current/user-manual/capabilities/osquery.html#osquery
+
+tags:
+    - demo
+    - osquery
+'''
 import os
 import json
 import re
@@ -25,6 +63,44 @@ configurations, configuration_metadata, cases_ids = config.get_test_cases_data(t
 @pytest.mark.parametrize('metadata', configuration_metadata, ids=cases_ids)
 def test_osquery_integration(configure_environment, metadata, get_dashboard_credentials, generate_events,
                              clean_alerts_index):
+    '''
+    description: Check that an alert is generated when the host is stressed.
+
+    test_phases:
+        - Set a custom Wazuh configuration.
+        - Run stress command to generate the event.
+        - Check in the alerts.json log that the expected alert has been triggered and get its timestamp.
+        - Check that the obtained alert from alerts.json has been indexed.
+
+    wazuh_min_version: 4.4.0
+
+    tier: 0
+
+    parameters:
+        - configurate_environment:
+            type: fixture
+            brief: Set the wazuh configuration according to the configuration playbook.
+        - metadata:
+            type: dict
+            brief: Wazuh configuration metadata.
+        - get_dashboard_credentials:
+            type: fixture
+            brief: Get the wazuh dashboard credentials.
+        - generate_events:
+            type: fixture
+            brief: Generate events that will trigger the alert according to the generate_events playbook.
+        - clean_alerts_index:
+            type: fixture
+            brief: Delete obtained alerts.json and alerts index.
+
+    assertions:
+        - Verify that the alert has been triggered.
+        - Verify that the same alert has been indexed.
+
+    input_description:
+        - The `configuration.yaml` file provides the module configuration for this test.
+        - The `generate_events.yaml`file provides the function configuration for this test.
+    '''
     rule_level = metadata['rule.level']
     rule_description = metadata['rule.description']
     rule_id = metadata['rule.id']
