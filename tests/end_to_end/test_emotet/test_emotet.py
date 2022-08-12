8bedc526e387e84859eae481f1aee1e459c2c668
Julia <juliamgnr@gmail.com>
2022-08-12 11:37:44 +0200

    add: Add Emotet documentation

--- tests/end_to_end/test_emotet/test_emotet.py
+++ tests/end_to_end/test_emotet/test_emotet.py
@@ -1,3 +1,42 @@
+'''
+copyright: Copyright (C) 2015-2022, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: end_to_end
+
+brief: This test will verify that a Sysmon events are detected correctly. Sysmon monitors system activity as
+       process creation and termination, network connection and file changes.
+
+components:
+    - logcollector
+
+targets:
+    - manager
+    - agent
+
+daemons:
+    - wazuh-logcollector
+    - wazuh-analysisd
+
+os_platform:
+    - linux
+    - windows
+
+os_version:
+    - CentOS 8
+    - Windows Server 2019
+
+references:
+    - https://github.com/wazuh/wazuh-automation/wiki/Wazuh-demo:-Execution-guide#emotet
+    - https://wazuh.com/blog/learn-to-detect-threats-on-windows-by-monitoring-sysmon-events/
+tags:
+    - demo
+    - sysmon
+    - emotet
+'''
 import json
 import os
 import re
@@ -23,9 +62,44 @@ configurations, configuration_metadata, cases_ids = config.get_test_cases_data(t
 @pytest.mark.filterwarnings('ignore::urllib3.exceptions.InsecureRequestWarning')
 @pytest.mark.parametrize('metadata', configuration_metadata, ids=cases_ids)
 def test_emotet(configure_environment, metadata, get_dashboard_credentials, generate_events, clean_alerts_index):
-    """
-    Test to detect an emotet attack
-    """
+    '''
+    description: Check that an alert is generated when Emotet malware is executed.
+
+    test_phases:
+        - Set a custom Wazuh configuration.
+        - Execute Emotet malware to generate the event.
+        - Check in the alerts.json log that the expected alert has been triggered and get its timestamp.
+        - Check that the obtained alert from alerts.json has been indexed.
+
+    wazuh_min_version: 4.4.0
+
+    tier: 0
+
+    parameters:
+        - configurate_environment:
+            type: fixture
+            brief: Set the wazuh configuration according to the configuration playbook.
+        - metadata:
+            type: dict
+            brief: Wazuh configuration metadata.
+        - get_dashboard_credentials:
+            type: fixture
+            brief: Get the wazuh dashboard credentials.
+        - generate_events:
+            type: fixture
+            brief: Generate events that will trigger the alert according to the generate_events playbook.
+        - clean_alerts_index:
+            type: fixture
+            brief: Delete obtained alerts.json and alerts index.
+
+    assertions:
+        - Verify that the alert has been triggered.
+        - Verify that the same alert has been indexed.
+
+    input_description:
+        - The `configuration.yaml` file provides the module configuration for this test.
+        - The `generate_events.yaml`file provides the function configuration for this test.
+    '''
     regsvr32_alert = metadata['regsvr32']
     word_executing_script_alert = metadata['word_executing_script']
     expected_alerts = [regsvr32_alert, word_executing_script_alert]
