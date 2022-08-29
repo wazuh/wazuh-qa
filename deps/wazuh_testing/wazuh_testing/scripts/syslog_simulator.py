c6f92b59572c80c9a62fe8cc86c142de08eb0b68
fedepacher <fede_pacher@hotmail.com>
2022-08-29 12:34:44 -0300

    refactor(#2947): refactor check variables

--- deps/wazuh_testing/wazuh_testing/scripts/syslog_simulator.py
+++ deps/wazuh_testing/wazuh_testing/scripts/syslog_simulator.py
@@ -4,8 +4,10 @@ import logging.handlers
 from logging.handlers import SysLogHandler
 from time import sleep
 from sys import getsizeof
+import threading
 
-class CreateSyslogger:
+
+class CreateSyslogger(threading.Thread):
     """Class that allows sending syslog messages.
 
     Args:
@@ -32,16 +34,16 @@ class CreateSyslogger:
         """Send syslog messages according to the input parameters"""
         sent_messages = 0
         sent_messages_burst = 0
-        while self.total_msg is not None and sent_messages < self.total_msg:
+        while self.total_msg != 0 and sent_messages < self.total_msg:
             message = f"{self.message} {sent_messages}"
             # Add dummy chars if the message size is not reachead
             if self.fixed_message_size is not None:
-                    event_msg_size = getsizeof(message)
-                    dummy_message_size = self.fixed_message_size - event_msg_size
-                    char_size = getsizeof(message[0]) - getsizeof('')
-                    message += 'A' * (dummy_message_size//char_size)
+                event_msg_size = getsizeof(message)
+                dummy_message_size = self.fixed_message_size - event_msg_size
+                char_size = getsizeof(message[0]) - getsizeof('')
+                message += 'A' * (dummy_message_size//char_size)
             # Create interval between messages to simulate burst of messages
-            if self.interval_burst_time is not None and self.messages_per_burst is not None:
+            if self.interval_burst_time != 0 and self.messages_per_burst != 0:
                 if sent_messages_burst < self.messages_per_burst:
                     sent_messages_burst += 1
                 else:
@@ -50,6 +52,13 @@ class CreateSyslogger:
             # Send message
             self.send_syslog_messages(message)
             sent_messages += 1
+
+    def start(self):
+        thread = threading.Thread(target=self.run_module)
+        # Starting threads
+        thread.start()
+        # Wait until all threads finish
+        thread.join()
 
 
 def main():
@@ -61,23 +70,24 @@ def main():
 
     arg_parser.add_argument('-e', '--total-msg', metavar='<total_msg>', type=int,
                             help='Limit the amount of message to be sent.',
-                            required=False, default=None, dest='total_msg')
+                            required=False, default=0, dest='total_msg')
 
     arg_parser.add_argument('-f', '--fixed-message-size', metavar='<fixed_message_size>', type=int, required=False,
                             default=None, help='Size of all the agent modules messages (KB)', dest='fixed_message_size')
 
     arg_parser.add_argument('-t', '--interval-burst-time', metavar='<interval_burst_time>', dest='interval_burst_time',
-                            type=int, required=False, default=None,
+                            type=int, required=False, default=0,
                             help='Interval time in seconds for the messages burst')
 
     arg_parser.add_argument('-b', '--messages-per-burst', metavar='<messages_per_burst>', dest='messages_per_burst',
-                            type=int, required=False, default=None,
+                            type=int, required=False, default=0,
                             help='Total messages per burst burst')
 
     args = arg_parser.parse_args()
 
     syslogger = CreateSyslogger(args)
-    syslogger.run_module()
+
+    syslogger.start()
 
 
 if __name__ == "__main__":
