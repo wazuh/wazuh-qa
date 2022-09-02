690e6ba7a21d0d33b2e0c3024b75ef1019f7653b
jmv74211 <jonathan.martin@wazuh.com>
2022-09-02 15:42:32 +0200

    refactor(#2918): improve test overwritten AR rules

--- tests/integration/test_active_response/test_analysisd/test_overwritten_rules_ar.py
+++ tests/integration/test_active_response/test_analysisd/test_overwritten_rules_ar.py
@@ -53,13 +53,12 @@ import os
 import pytest
 import time
 
-from wazuh_testing import global_parameters
-from wazuh_testing.processes import check_if_daemons_are_running, execute_shell_command
+from wazuh_testing.processes import check_if_daemons_are_running, run_local_command_printing_output
 from wazuh_testing.tools.configuration import load_configuration_template, get_test_cases_data
 from wazuh_testing.tools.file import remove_file
 from wazuh_testing.tools import CUSTOM_RULES_PATH, LOCAL_RULES_PATH, AR_SCRIPTS_PATH
 
-pytestmark = [pytest.mark.tier(level=2), pytest.mark.server]
+pytestmark = [pytest.mark.tier(level=1), pytest.mark.server]
 
 # Reference paths
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
@@ -78,23 +77,48 @@ wazuh_ar_script = os.path.join(AR_SCRIPTS_PATH, 'custom-ar.sh')
 output_custom_ar_script = '/tmp/file-ar.txt'
 source_path = [custom_rule, local_rules, custom_ar_script]
 destination_path = [f"{CUSTOM_RULES_PATH}/0270-web_appsec_rules_edit.xml", LOCAL_RULES_PATH, wazuh_ar_script]
-
+file_to_monitor = '/tmp/file_to_monitor.log'
 
 # Test configurations
 configuration_parameters, configuration_metadata, case_ids = get_test_cases_data(cases_path)
 configurations = load_configuration_template(configurations_path, configuration_parameters, configuration_metadata)
 
 
-@pytest.mark.parametrize("source_path", [source_path])
-@pytest.mark.parametrize("destination_path", [destination_path])
-@pytest.mark.parametrize("file_to_monitor", ['/tmp/file_to_monitor.log'])
+@pytest.fixture(scope='function')
+def clean_AR_file():
+    "Remove in teardown the file created by the active response"
+    yield
+    remove_file(output_custom_ar_script)
+
+
+@pytest.mark.parametrize("source_path", [source_path], ids=[''])
+@pytest.mark.parametrize("destination_path", [destination_path], ids=[''])
+@pytest.mark.parametrize("new_file_path", [file_to_monitor], ids=[''])
 @pytest.mark.parametrize('configuration, metadata', zip(configurations, configuration_metadata), ids=case_ids)
-def test_overwritten_rules_ar(configuration, metadata, create_file_to_monitor, file_to_monitor,
-                              set_wazuh_configuration_analysisd, copy_file, source_path, destination_path,
-                              restart_wazuh_daemon_function):
+def test_overwritten_rules_ar(configuration, metadata, source_path, destination_path, new_file_path,
+                              create_file, set_wazuh_configuration, copy_file, truncate_monitored_files,
+                              restart_wazuh_function, clean_AR_file):
     '''
-    description: Check if 'active response' works correctly when binding an active response for a rule
-                 that will be overwritten.
+    description: Check if active response works correctly for the following cases:
+        - An active response is triggered when an event matches with a rule.
+        - An active response is triggered when an event matches with an overwritten rule.
+        - An active response is triggered when an event matches with a rule of depth > 0.
+        - An active response is triggered when an event matches with an overwritten rule of depth > 0.
+
+    test_phases:
+        - Setup:
+            - Copy custom rule and active response files to Wazuh paths.
+            - Create a new file which will be monitored with logcollector.
+            - Set wazuh configuration. Add active_response, command and localfile blocks.
+            - Clean logs files and restart wazuh to apply the configuration.
+        - Test:
+            - Add a custom log in a monitored file to generate an event that triggers a rule that trigger an AR.
+            - Check if the active response has been triggered (checking if a file has been created).
+            - Check that wazuh-analysisd is running (it has not been crashed).
+        - Tierdown:
+            - Remove generated file when triggering the active response.
+            - Restart initial wazuh configuration.
+            - Remove generated and custom copied files.
 
     wazuh_min_version: 4.3.5
 
@@ -105,49 +129,55 @@ def test_overwritten_rules_ar(configuration, metadata, create_file_to_monitor, f
         - metadata:
             type: fixture
             brief: Get metadata from the module.
-        - create_file_to_monitor:
-            type: fixture
-            brief: Create a file to will monitored by Wazuh.
-        - file_to_monitor:
-            type: parametrize
-            brief:  Path of file that will monitored by Wazuh.
-        - set_wazuh_configuration_analysisd:
-            type: fixture
-            brief:  Set configurations from the module.
-        - copy_file:
-            type: fixture
-            brief:  Copy file from source to destination.
         - source_path:
             type: parametrize
             brief: list that contains sources path of files.
         - destination_path:
             type: parametrize
             brief: list that contains destinations path of files.
-        - restart_wazuh_daemon_function:
+        - new_file_path:
+            type: parametrize
+            brief: File path to create.
+        - create_file:
+            type: fixture
+            brief: Create a file to be monitored by Wazuh.
+        - set_wazuh_configuration:
+            type: fixture
+            brief:  Set Wazuh custom configuration.
+        - copy_file:
+            type: fixture
+            brief:  Copy file from source to destination.
+        - truncate_monitored_files:
+            type: fixture
+            brief:  Truncate wazuh log files.
+        - restart_wazuh_function:
             type: fixture
             brief: restart Wazuh.
+        - clean_AR_file:
+            type: fixture
+            brief: Clean generated AR file.
 
     assertions:
-        - verify that the custom AR created a file.
+        - Check that the AR is triggered.
+        - Check that wazuh-analysisd daemon does not crash.
 
-    input_description: Different use cases are found in the test module.
-
-    expected_output:
-        - A file was successfully created by AR.
-
-    tags:
-        - ar_analysisd
+    input_description:
+        - The `configuration_overwritten_rules` file provides the module configuration for this test.
+        - The `cases_overwritten_rules` file provides the test cases.
     '''
+    # Set all permissions for wazuh to be able to use the custom active response
     os.chmod(wazuh_ar_script, 0o777)
+
+    # Write a line in a monitored file with logcollector for generating an event that matches with a custom rule
     cmd = "echo '{}' >> '{}'".format(metadata['log_sample'], file_to_monitor)
-    execute_shell_command(cmd)
+    run_local_command_printing_output(cmd)
+
+    # Waiting time for triggering the rule and AR.
+    time.sleep(5)
 
     # Checking if AR works properly
-    time.sleep(global_parameters.default_timeout)
-    assert os.path.exists(output_custom_ar_script)
+    assert os.path.exists(output_custom_ar_script), "The active response has not been triggered"
 
-    # Check that wazuh-analysisd is running and has not crashed when trying to parse files with unexpected file types
-    check_if_daemons_are_running(['wazuh-analysisd'])
-
-    # Remove file created by active response
-    remove_file(output_custom_ar_script)
+    # Check that wazuh-analysisd is running and has not crashed
+    assert check_if_daemons_are_running(['wazuh-analysisd'])[0], 'wazuh-analysisd daemon is not running. ' \
+                                                                 'Maybe it has crashed'
