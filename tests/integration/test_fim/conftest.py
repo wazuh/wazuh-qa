cceaea972d7b97517ad7caae4cd406f0b8dcb916
Deblintrake09 <amicalizzi2005@gmail.com>
2022-09-14 11:12:12 -0300

    feat(#3053): add fixtures to fim conftest

--- tests/integration/test_fim/conftest.py
+++ tests/integration/test_fim/conftest.py
@@ -7,11 +7,16 @@ import pytest
 
 from distro import id
 from wazuh_testing import global_parameters
+from wazuh_testing.tools.configuration import (get_wazuh_local_internal_options, set_wazuh_local_internal_options,
+                                               create_local_internal_options)
 from wazuh_testing.tools.services import control_service
 from wazuh_testing.fim import (create_registry, registry_parser, KEY_WOW64_64KEY, delete_registry,
                                LOG_FILE_PATH, callback_detect_registry_integrity_clear_event)
+
 from wazuh_testing.tools.file import truncate_file
-from wazuh_testing.fim_module.fim_variables import WINDOWS_HKEY_LOCAL_MACHINE, MONITORED_KEY, SYNC_INTERVAL_VALUE
+from wazuh_testing.modules.fim import (WINDOWS_HKEY_LOCAL_MACHINE, MONITORED_KEY, SYNC_INTERVAL_VALUE,
+                                       FIM_DEFAULT_LOCAL_INTERNAL_OPTIONS)
+from wazuh_testing.modules.fim.event_monitor import detect_whodata_start, detect_realtime_start, detect_initial_scan
 from wazuh_testing.wazuh_variables import WAZUH_SERVICES_START, WAZUH_SERVICES_STOP, WAZUH_LOG_MONITOR
 from wazuh_testing.tools.monitoring import FileMonitor
 
@@ -54,9 +59,64 @@ def install_audit(get_configuration):
         package_management = "apt-get"
         audit = "auditd"
         option = "--yes"
-    else:
-        raise ValueError(f"Linux distro ({linux_distro}) not supported for uninstall/install audit")
-
+    else:s
     # Install audit and start the service
     process = subprocess.run([package_management, "install", audit, option], check=True)
     process = subprocess.run(["service", "auditd", "start"], check=True)
+
+
+@pytest.fixture(scope='session')
+def configure_local_internal_options_fim():
+    """Fixture to configure the local internal options file."""
+    
+    # Backup the old local internal options
+    backup_local_internal_options = get_wazuh_local_internal_options()
+
+    # Set the new local internal options configuration
+    set_wazuh_local_internal_options(create_local_internal_options(FIM_DEFAULT_LOCAL_INTERNAL_OPTIONS))
+
+    yield
+
+    # Backup the old local internal options cofiguration
+    set_wazuh_local_internal_options(backup_local_internal_options)
+
+
+@pytest.fixture(scope='function')
+def set_wazuh_configuration_fim(configuration, set_wazuh_configuration, configure_local_internal_options_fim):
+    """Set wazuh configuration
+
+    Args:
+        configuration (dict): Configuration template data to write in the ossec.conf.
+        set_wazuh_configuration (fixture): Set the wazuh configuration according to the configuration data.
+        configure_local_internal_options_fim (fixture): Set the local_internal_options.conf file.
+    """
+    yield
+
+
+@pytest.fixture(scope='function')
+def wait_for_fim_start_function(configuration):
+    """
+    Wait for realtime start, whodata start or end of initial FIM scan.
+    """
+    file_monitor = FileMonitor(LOG_FILE_PATH)
+    mode_key = 'fim_mode' if 'fim_mode2' not in configuration else 'fim_mode2'
+
+    try:
+        if configuration[mode_key] == 'realtime':
+            detect_realtime_start(file_monitor)
+        elif configuration[mode_key] == 'whodata':
+            detect_whodata_start(file_monitor)
+        else:  # scheduled
+            detect_initial_scan(file_monitor)
+    except KeyError:
+        detect_initial_scan(file_monitor)
+
+
+@pytest.fixture(scope="function")
+def restart_syscheck_function():
+    """
+    Restart syscheckd daemon.
+    """
+    control_service("stop", daemon="wazuh-syscheckd")
+    truncate_file(LOG_FILE_PATH)
+    control_service("start", daemon="wazuh-syscheckd")
