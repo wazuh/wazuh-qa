0a2b99b36bcee31dc01e927ddae1f05a8242513d
fedepacher <fede_pacher@hotmail.com>
2022-10-14 11:53:17 -0300

    feat(#3218): add logcollector module to group related funtions

---
+++ deps/wazuh_testing/wazuh_testing/modules/logcollector/__init__.py
@@ -0,0 +1,9 @@
+
+LOG_COLLECTOR_PREFIX = r'.*wazuh-logcollector.*'
+WINDOWS_AGENT_PREFIX = r'.*wazuh-agent.*'
+MAILD_PREFIX = r'.*wazuh-maild.*'
+MACOS_LOG_COMMAND_PATH = '/usr/bin/log'
+GENERIC_CALLBACK_ERROR_ANALYZING_FILE = 'The expected analyzing file log has not been produced'
+GENERIC_CALLBACK_ERROR_ANALYZING_EVENTCHANNEL = "The expected analyzing eventchannel log has not been produced"
+GENERIC_CALLBACK_ERROR_ANALYZING_MACOS = "The expected analyzing macos log has not been produced"
+GENERIC_CALLBACK_ERROR_COMMAND_MONITORING = 'The expected command monitoring log has not been produced'
