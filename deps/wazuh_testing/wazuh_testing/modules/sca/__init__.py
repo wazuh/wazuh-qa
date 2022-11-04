9540bb534c9be483b24837aeb0fbe9bd3831e2d8
Deblintrake09 <amicalizzi2005@gmail.com>
2022-11-04 16:23:51 -0300

    feat(#3406): add sca module callbacks and evm

--- deps/wazuh_testing/wazuh_testing/modules/sca/__init__.py
+++ deps/wazuh_testing/wazuh_testing/modules/sca/__init__.py
@@ -1,29 +1,32 @@
+# Copyright (C) 2015-2022, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-
-# Timeouts
-T_5 = 5
-T_10 = 10
-T_15 = 15
-T_20 = 20
-T_60 = 60
-T_40 = 40
+import sys
+import os
+from wazuh_testing import WAZUH_PATH
 
 
 # Variables
-SCA_PREFIX = '.*sca.*'
+TEMP_FILE_PATH = '/tmp'
 
 
 # Callback Messages
-CB_SCA_STARTED = f"{SCA_PREFIX}INFO: Module started."
-CB_SCA_DISABLED = f"{SCA_PREFIX}INFO: Module disabled. Exiting."
-CB_SCA_SCAN_STARTED = f"{SCA_PREFIX}INFO: Starting Security Configuration Assessment scan."
-CB_SCA_SCAN_ENDED = f"{SCA_PREFIX}INFO: Security Configuration Assessment scan finished. Duration: (\d+) seconds."
-CB_POLICY_EVALUATION_FINISHED = f"{SCA_PREFIX}INFO: Evaluation finished for policy '(.*)'."
-CB_SCAN_DB_DUMP_FINISHED = f".*sca.*DEBUG: Finished dumping scan results to SCA DB for policy '(.*)'.*"
+CB_SCA_ENABLED = r".*sca.*INFO: (Module started.)"
+CB_SCA_DISABLED = r".*sca.*INFO: (Module disabled.) Exiting."
+CB_SCA_SCAN_STARTED = r".*sca.*INFO: (Starting Security Configuration Assessment scan)."
+CB_SCA_SCAN_ENDED = r".*sca.*INFO: Security Configuration Assessment scan finished. Duration: (\d+) seconds."
+CB_SCA_OSREGEX_ENGINE = r".*sca.*DEBUG: SCA will use '(.*)' engine to check the rules."
+CB_POLICY_EVALUATION_FINISHED = r".*sca.*INFO: Evaluation finished for policy '(.*)'."
+CB_SCAN_DB_DUMP_FINISHED = r".*sca.*DEBUG: Finished dumping scan results to SCA DB for policy '(.*)'.*"
+CB_SCAN_RULE_RESULT = r".*sca.*wm_sca_hash_integrity.*DEBUG: ID: (\d+); Result: '(.*)'"
+CB_SCA_SCAN_EVENT = r".*sca_send_alert.*Sending event: (.*)"
 
 
 # Error Messages
-
+ERR_MSG_REGEX_ENGINE = "Did not receive the expected 'SCA will use '.*' engine to check the rules' event"
+ERR_MSG_ID_RESULTS = 'Expected sca_has_integrity Result events not found'
+ERR_MSG_SCA_SUMMARY = 'Expected SCA Scan Summary type event not found.'
 
 # Setting Local_internal_option file
 SCA_DEFAULT_LOCAL_INTERNAL_OPTIONS = {'wazuh_modules.debug': '2'}
--- deps/wazuh_testing/wazuh_testing/modules/sca/event_monitor.py
+++ deps/wazuh_testing/wazuh_testing/modules/sca/event_monitor.py
@@ -1,12 +1,34 @@
-import re
+# Copyright (C) 2015-2022, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-from wazuh_testing.tools import LOG_FILE_PATH, ALERT_FILE_PATH
+import re
+import json
+
+from wazuh_testing import T_60, T_10, T_20
+from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.modules import sca
 from wazuh_testing.tools.monitoring import FileMonitor, generate_monitoring_callback
 
 
-def check_sca_event(file_monitor=None, callback='', error_message=None, update_position=True,
-                              timeout=sca.T_60, accum_results=1,file_to_monitor=LOG_FILE_PATH):
+# Callbacks
+def callback_scan_id_result(line):
+    match = re.match(sca.CB_SCAN_RULE_RESULT, line)
+    if match:
+        return [match.group(1), match.group(2)]
+
+
+def callback_detect_sca_scan_summary(line):
+    match = re.match(sca.CB_SCA_SCAN_EVENT, line)
+    if match:
+        if json.loads(match.group(1))['type']=='summary':
+            return json.loads(match.group(1))
+
+
+
+# Event checkers
+def check_sca_event(file_monitor=None, callback='', error_message=None, update_position=False,
+                              timeout=T_60, accum_results=1,file_to_monitor=LOG_FILE_PATH):
     """Check if a sca event occurs
 
     Args:
@@ -25,11 +47,44 @@ def check_sca_event(file_monitor=None, callback='', error_message=None, update_p
                        callback=generate_monitoring_callback(callback), error_message=error_message)
 
 
-def check_sca_enabled():
+def check_sca_enabled(file_monitor=None):
     """Check if the sca module is enabled"""
-    check_sca_event(callback=sca.CB_SCA_ENABLED, timeout=sca.T_10)
+    check_sca_event(callback=sca.CB_SCA_ENABLED, timeout=T_10, file_monitor=file_monitor)
 
 
-def check_sca_disabled():
+def check_sca_disabled(file_monitor=None):
     """Check if the sca module is disabled"""
-    check_sca_event(callback=sca.CB_SCA_DISABLED, timeout=sca.T_10)
+    check_sca_event(callback=sca.CB_SCA_DISABLED, timeout=T_10, file_monitor=file_monitor)
+
+
+def check_sca_scan_started(file_monitor=None):
+    """Check if the sca scan has started"""
+    check_sca_event(callback=sca.CB_SCA_SCAN_STARTED, timeout=T_10, file_monitor=file_monitor)
+
+
+def check_sca_scan_ended(file_monitor=None):
+    """Check if the sca scan has ended"""
+    check_sca_event(callback=sca.CB_SCA_SCAN_ENDED, timeout=T_10, file_monitor=file_monitor)
+
+
+def check_scan_regex_engine(file_monitor=None):
+    """Check the expected ammount of check results have been recieved"""
+    file_monitor = FileMonitor(LOG_FILE_PATH) if file_monitor is None else file_monitor
+    engine = file_monitor.start(callback=generate_monitoring_callback(sca.CB_SCA_OSREGEX_ENGINE), timeout=T_10,
+                                 error_message=sca.ERR_MSG_REGEX_ENGINE, update_position=False).result()
+    return engine
+
+
+def get_sca_scan_rule_id_results(file_monitor=None, results_num=1):
+    """Check the expected ammount of check results have been recieved"""
+    file_monitor = FileMonitor(LOG_FILE_PATH) if file_monitor is None else file_monitor
+    results = file_monitor.start(callback=callback_scan_id_result, timeout=T_20, accum_results=results_num,
+                                 error_message=sca.ERR_MSG_ID_RESULTS).result()
+    return results
+
+def get_sca_scan_summary(file_monitor=None):
+    """Get the scan summary event"""
+    file_monitor = FileMonitor(LOG_FILE_PATH) if file_monitor is None else file_monitor
+    results = file_monitor.start(callback=callback_detect_sca_scan_summary, timeout=T_20,
+                                 error_message=sca.ERR_MSG_SCA_SUMMARY).result()
+    return results
