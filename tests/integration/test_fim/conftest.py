406dd3358eaa53efcc1d4639372359d94a4e8a2f
Deblintrake09 <amicalizzi2005@gmail.com>
2022-11-18 16:25:42 -0300

    refactor(#2971): delete unnecesary fixtures

--- tests/integration/test_fim/conftest.py
+++ tests/integration/test_fim/conftest.py
@@ -41,37 +41,6 @@ def create_key(request):
                        error_message='Did not receive expected "integrity clear" event')
 
 
-@pytest.fixture(scope='session')
-def configure_local_internal_options_fim():
-    """
-    Fixture to configure the local internal options file.
-    """
-
-    # Backup the old local internal options
-    backup_local_internal_options = get_wazuh_local_internal_options()
-
-    # Set the new local internal options configuration
-    set_wazuh_local_internal_options(create_local_internal_options(FIM_DEFAULT_LOCAL_INTERNAL_OPTIONS))
-
-    yield
-
-    # Backup the old local internal options cofiguration
-    set_wazuh_local_internal_options(backup_local_internal_options)
-
-
-@pytest.fixture()
-def set_wazuh_configuration_fim(configuration, set_wazuh_configuration, configure_local_internal_options_fim):
-    """
-    Set wazuh configuration with local_internal_options for FIM
-
-    Args:
-        configuration (dict): Configuration template data to write in the ossec.conf.
-        set_wazuh_configuration (fixture): Set the wazuh configuration according to the configuration data.
-        configure_local_internal_options_fim (fixture): Set the local_internal_options.conf file.
-    """
-    yield
-
-
 @pytest.fixture()
 def wait_fim_start_function(configuration):
     """ Wait for realtime start, whodata start or end of initial FIM scan.
@@ -104,7 +73,7 @@ def restart_syscheck_function():
 
 
 @pytest.fixture()
-def create_monitored_folders_function(test_folders):
+def create_monitored_folders(test_folders):
     """
     Create the folders that will be monitored and delete them at the end.
     Args:
