7b5a7dd86be0c202a27550cfe2c591926fbd8235
Deblintrake09 <amicalizzi2005@gmail.com>
2022-11-23 11:00:09 -0300

    refactor(#3406): move variables to event_monitor

--- deps/wazuh_testing/wazuh_testing/modules/sca/__init__.py
+++ deps/wazuh_testing/wazuh_testing/modules/sca/__init__.py
@@ -7,23 +7,5 @@ from wazuh_testing import MODULESD_DEBUG, VERBOSE_DEBUG_OUTPUT
 # Variables
 TEMP_FILE_PATH = '/tmp'
 
-
-# Callback Messages
-CB_SCA_ENABLED = r".*sca.*INFO: (Module started.)"
-CB_SCA_DISABLED = r".*sca.*INFO: (Module disabled). Exiting."
-CB_SCA_SCAN_STARTED = r".*sca.*INFO: (Starting Security Configuration Assessment scan)."
-CB_SCA_SCAN_ENDED = r".*sca.*INFO: Security Configuration Assessment scan finished. Duration: (\d+) seconds."
-CB_SCA_OSREGEX_ENGINE = r".*sca.*DEBUG: SCA will use '(.*)' engine to check the rules."
-CB_POLICY_EVALUATION_FINISHED = r".*sca.*INFO: Evaluation finished for policy '(.*)'."
-CB_SCAN_DB_DUMP_FINISHED = r".*sca.*DEBUG: Finished dumping scan results to SCA DB for policy '(.*)'.*"
-CB_SCAN_RULE_RESULT = r".*sca.*wm_sca_hash_integrity.*DEBUG: ID: (\d+); Result: '(.*)'"
-CB_SCA_SCAN_EVENT = r".*sca_send_alert.*Sending event: (.*)"
-
-
-# Error Messages
-ERR_MSG_REGEX_ENGINE = "Did not receive the expected 'SCA will use '.*' engine to check the rules' event"
-ERR_MSG_ID_RESULTS = 'Expected sca_has_integrity result events not found'
-ERR_MSG_SCA_SUMMARY = 'Expected SCA Scan Summary type event not found.'
-
 # Setting Local_internal_option file
 SCA_DEFAULT_LOCAL_INTERNAL_OPTIONS = {MODULESD_DEBUG: VERBOSE_DEBUG_OUTPUT}
--- deps/wazuh_testing/wazuh_testing/modules/sca/event_monitor.py
+++ deps/wazuh_testing/wazuh_testing/modules/sca/event_monitor.py
@@ -10,14 +10,31 @@ from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.modules import sca
 from wazuh_testing.tools.monitoring import FileMonitor, generate_monitoring_callback
 
+# Callback Messages
+CB_SCA_ENABLED = r".*sca.*INFO: (Module started.)"
+CB_SCA_DISABLED = r".*sca.*INFO: (Module disabled). Exiting."
+CB_SCA_SCAN_STARTED = r".*sca.*INFO: (Starting Security Configuration Assessment scan)."
+CB_SCA_SCAN_ENDED = r".*sca.*INFO: Security Configuration Assessment scan finished. Duration: (\d+) seconds."
+CB_SCA_OSREGEX_ENGINE = r".*sca.*DEBUG: SCA will use '(.*)' engine to check the rules."
+CB_POLICY_EVALUATION_FINISHED = r".*sca.*INFO: Evaluation finished for policy '(.*)'."
+CB_SCAN_DB_DUMP_FINISHED = r".*sca.*DEBUG: Finished dumping scan results to SCA DB for policy '(.*)'.*"
+CB_SCAN_RULE_RESULT = r".*sca.*wm_sca_hash_integrity.*DEBUG: ID: (\d+); Result: '(.*)'"
+CB_SCA_SCAN_EVENT = r".*sca_send_alert.*Sending event: (.*)"
 
-# Callbacks
+
+# Error Messages
+ERR_MSG_REGEX_ENGINE = "Did not receive the expected 'SCA will use '.*' engine to check the rules' event"
+ERR_MSG_ID_RESULTS = 'Expected sca_has_integrity result events not found'
+ERR_MSG_SCA_SUMMARY = 'Expected SCA Scan Summary type event not found.'
+
+
+# Callback functions
 def callback_scan_id_result(line):
     '''Callback that returns the ID an result of a SCA check
     Args:
         line (str): line string to check for match.
     '''
-    match = re.match(sca.CB_SCAN_RULE_RESULT, line)
+    match = re.match(CB_SCAN_RULE_RESULT, line)
     if match:
         return [match.group(1), match.group(2)]
 
@@ -27,13 +44,13 @@ def callback_detect_sca_scan_summary(line):
     Args:
         line (str): line string to check for match.
     '''
-    match = re.match(sca.CB_SCA_SCAN_EVENT, line)
+    match = re.match(CB_SCA_SCAN_EVENT, line)
     if match:
         if json.loads(match.group(1))['type'] == 'summary':
             return json.loads(match.group(1))
 
 
-# Event checkers
+# Event check functions
 def check_sca_event(file_monitor=None, callback='.*', error_message=None, update_position=False,
                     timeout=T_60, accum_results=1, file_to_monitor=LOG_FILE_PATH):
     """Check if a sca event occurs
@@ -60,7 +77,7 @@ def check_sca_enabled(file_monitor=None):
     Args:
         file_monitor (FileMonitor): FileMonitor object to monitor the file content.
     """
-    check_sca_event(callback=sca.CB_SCA_ENABLED, timeout=T_10, file_monitor=file_monitor)
+    check_sca_event(callback=CB_SCA_ENABLED, timeout=T_10, file_monitor=file_monitor)
 
 
 def check_sca_disabled(file_monitor=None):
@@ -68,7 +85,7 @@ def check_sca_disabled(file_monitor=None):
     Args:
         file_monitor (FileMonitor): FileMonitor object to monitor the file content.
     """
-    check_sca_event(callback=sca.CB_SCA_DISABLED, timeout=T_10, file_monitor=file_monitor)
+    check_sca_event(callback=CB_SCA_DISABLED, timeout=T_10, file_monitor=file_monitor)
 
 
 def check_sca_scan_started(file_monitor=None):
@@ -76,7 +93,7 @@ def check_sca_scan_started(file_monitor=None):
     Args:
         file_monitor (FileMonitor): FileMonitor object to monitor the file content.
     """
-    check_sca_event(callback=sca.CB_SCA_SCAN_STARTED, timeout=T_10, file_monitor=file_monitor)
+    check_sca_event(callback=CB_SCA_SCAN_STARTED, timeout=T_10, file_monitor=file_monitor)
 
 
 def check_sca_scan_ended(file_monitor=None):
@@ -84,7 +101,7 @@ def check_sca_scan_ended(file_monitor=None):
     Args:
         file_monitor (FileMonitor): FileMonitor object to monitor the file content.
     """
-    check_sca_event(callback=sca.CB_SCA_SCAN_ENDED, timeout=T_10, file_monitor=file_monitor)
+    check_sca_event(callback=CB_SCA_SCAN_ENDED, timeout=T_10, file_monitor=file_monitor)
 
 
 def check_scan_regex_engine(file_monitor=None):
@@ -93,8 +110,8 @@ def check_scan_regex_engine(file_monitor=None):
         file_monitor (FileMonitor): FileMonitor object to monitor the file content.
     """
     file_monitor = FileMonitor(LOG_FILE_PATH) if file_monitor is None else file_monitor
-    engine = file_monitor.start(callback=generate_monitoring_callback(sca.CB_SCA_OSREGEX_ENGINE), timeout=T_10,
-                                error_message=sca.ERR_MSG_REGEX_ENGINE, update_position=False).result()
+    engine = file_monitor.start(callback=generate_monitoring_callback(CB_SCA_OSREGEX_ENGINE), timeout=T_10,
+                                error_message=ERR_MSG_REGEX_ENGINE, update_position=False).result()
     return engine
 
 
@@ -106,7 +123,7 @@ def get_sca_scan_rule_id_results(file_monitor=None, results_num=1):
     """
     file_monitor = FileMonitor(LOG_FILE_PATH) if file_monitor is None else file_monitor
     results = file_monitor.start(callback=callback_scan_id_result, timeout=T_20, accum_results=results_num,
-                                 error_message=sca.ERR_MSG_ID_RESULTS).result()
+                                 error_message=ERR_MSG_ID_RESULTS).result()
     return results
 
 
@@ -117,5 +134,5 @@ def get_sca_scan_summary(file_monitor=None):
     """
     file_monitor = FileMonitor(LOG_FILE_PATH) if file_monitor is None else file_monitor
     results = file_monitor.start(callback=callback_detect_sca_scan_summary, timeout=T_20,
-                                 error_message=sca.ERR_MSG_SCA_SUMMARY).result()
+                                 error_message=ERR_MSG_SCA_SUMMARY).result()
     return results
