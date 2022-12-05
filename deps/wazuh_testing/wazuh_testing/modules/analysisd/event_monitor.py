e1739e473430b840ce7b996cd2554e634e2c3665
Deblintrake09 <amicalizzi2005@gmail.com>
2022-12-05 18:11:29 -0300

    feat(#3585): add new test module

--- deps/wazuh_testing/wazuh_testing/modules/analysisd/event_monitor.py
+++ deps/wazuh_testing/wazuh_testing/modules/analysisd/event_monitor.py
@@ -1,9 +1,18 @@
 import re
 
-from wazuh_testing import T_10, T_20, T_60
-from wazuh_testing.modules.analysisd import ANALYSISD_PREFIX, MAILD_PREFIX
+from wazuh_testing import T_10, T_60
+from wazuh_testing.modules.analysisd import ANALYSISD_PREFIX, MAILD_PREFIX, TESTRULE_PREFIX
 from wazuh_testing import LOG_FILE_PATH, ANALYSISD_STATE
-from wazuh_testing.tools.monitoring import FileMonitor, generate_monitoring_callback_groups
+from wazuh_testing.tools.monitoring import FileMonitor
+
+
+# Callback events
+CB_SID_NOT_FOUND = r".*Signature ID '(\d*)' was not found and will be ignored in the 'if_sid'.* of rule '(\d*)'"
+CB_EMPTY_IF_SID_RULE_IGNORED = fr"{TESTRULE_PREFIX}Empty 'if_sid' value. Rule '(\d*)' will be ignored.*"
+
+# Error Messages
+ERR_MSG_SID_NOT_FOUND = "Did not receive the expected 'Signature ID  not found...' event"
+ERR_MSG_EMPTY_IF_SID = "Did not receive the expected 'Empty 'if_sid' value. Rule '(\\d*)' will be ignored' event"
 
 
 def make_analysisd_callback(pattern, prefix=ANALYSISD_PREFIX):
