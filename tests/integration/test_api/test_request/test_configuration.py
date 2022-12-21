e4e920f9421613d56295e8a4afb624500f2ea097
Julia Magán <juliamgnr@gmail.com>
2022-12-21 15:47:52 +0100

    feat(#3604): add tests for analysisd configuration

--- tests/integration/test_api/test_request/test_configuration.py
+++ tests/integration/test_api/test_request/test_configuration.py
@@ -4,7 +4,6 @@ import requests
 
 from wazuh_testing.tools.configuration import load_configuration_template, get_test_cases_data
 
-
 pytestmark = [pytest.mark.server]
 
 
@@ -21,15 +20,69 @@ t1_cases_path = os.path.join(TEST_CASES_PATH, 'cases_default_configuration.yaml'
 t1_configuration_parameters, t1_configuration_metadata, t1_case_ids = get_test_cases_data(t1_cases_path)
 
 expected_default_conf_response = {
-    'remote': {
+    'request/remote': {
         'data': {'affected_items': [{'remote': [{'connection': 'secure', 'ipv6': 'no', 'protocol': ['TCP'],
-                 'port': '1514', 'queue_size': '131072'}]}], 'total_affected_items': 1, 'total_failed_items': 0,
-                 'failed_items': []}, 'message': 'Active configuration was successfully read', 'error': 0
+                                                 'port': '1514', 'queue_size': '131072'}]}], 'total_affected_items': 1,
+                 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
     },
-    'global': {
+    'request/global': {
         'data': {'affected_items': [{'global': {'remoted': {'agents_disconnection_alert_time': 0,
-                 'agents_disconnection_time': 600}}}], 'total_affected_items': 1, 'total_failed_items': 0,
-                 'failed_items': []}, 'message': 'Active configuration was successfully read', 'error': 0
+                                                            'agents_disconnection_time': 600}}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis/active_response': {
+        'data': {'affected_items': [{'active-response': []}], 'total_affected_items': 1,
+                 'total_failed_items': 0, 'failed_items': []}, 'message': 'Active configuration was successfully read',
+        'error': 0
+    },
+    'analysis/alerts': {
+        'data': {'affected_items': [{'alerts': {'email_alert_level': 12, 'log_alert_level': 3}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    #'analysis/decoders':{
+    #
+    #},
+    #'analysis/rules':{
+    #
+    #},
+    'analysis/command': {
+        'data': {'affected_items': [{'command': [{'name': 'disable-account', 'executable': 'disable-account',
+                                                  'timeout_allowed': 1},
+                                                 {'name': 'restart-wazuh', 'executable': 'restart-wazuh',
+                                                  'timeout_allowed': 0},
+                                                 {'name': 'firewall-drop', 'executable': 'firewall-drop',
+                                                  'timeout_allowed': 1},
+                                                 {'name': 'host-deny', 'executable': 'host-deny', 'timeout_allowed': 1},
+                                                 {'name': 'route-null', 'executable': 'route-null',
+                                                  'timeout_allowed': 1},
+                                                 {'name': 'win_route-null', 'executable': 'route-null.exe',
+                                                  'timeout_allowed': 1},
+                                                 {'name': 'netsh', 'executable': 'netsh.exe', 'timeout_allowed': 1}]}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    #'agent/labels':{
+    #
+    #},
+    'analysis/rule_test': {
+        'data': {'affected_items': [{'rule_test': {'enabled': 'yes', 'threads': 1, 'max_sessions': 64,
+                                                   'session_timeout': 900}}], 'total_affected_items': 1,
+                                                   'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis/global': {
+        'data': {'affected_items': [{'global': {'email_notification': 'no', 'logall': 'no', 'logall_json': 'no',
+                                                'integrity_checking': 8, 'rootkit_detection': 8, 'host_information': 8,
+                                                'prelude_output': 'no', 'zeromq_output': 'no', 'jsonout_output': 'yes',
+                                                'alerts_log': 'yes', 'stats': 8, 'memory_size': 8192,
+                                                'white_list': ['127.0.0.1', '10.0.2.3', 'localhost.localdomain'],
+                                                'rotate_interval': 0, 'max_output_size': 0, 'eps': {'maximum': 0,
+                                                'timeframe': 10}}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
     }
 }
 
@@ -56,6 +109,17 @@ expected_default_internal_conf_response = {
                                                               'tcp_keepintvl': 10, 'tcp_keepcnt': 3}}}],
                  'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
         'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis': {
+        'data': {'affected_items': [{'internal': {'analysisd': {'debug': 0, 'default_timeframe': 360,
+                                                                'stats_maxdiff': 999000, 'stats_mindiff': 1250,
+                                                                'stats_percent_diff': 150, 'fts_list_size': 32,
+                                                                'fts_min_size_for_str': 14, 'log_fw': 1,
+                                                                'decoder_order_size': 256, 'label_cache_maxage': 10,
+                                                                'show_hidden_labels': 0, 'rlimit_nofile': 458752,
+                                                                'min_rotate_interval': 600}}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
     }
 }
 
@@ -64,32 +128,75 @@ expected_default_internal_conf_response = {
 t3_configurations_path = os.path.join(CONFIGURATIONS_PATH, 'custom_configuration.yaml')
 t3_cases_path = os.path.join(TEST_CASES_PATH, 'cases_custom_configuration.yaml')
 
-# Enabled test configurations (t3)
+# Custom configuration test configurations (t3)
 t3_configuration_parameters, t3_configuration_metadata, t3_case_ids = get_test_cases_data(t3_cases_path)
 t3_configurations = load_configuration_template(t3_configurations_path, t3_configuration_parameters,
                                                 t3_configuration_metadata)
 
 expected_custom_conf_response = {
-    'remote': {
+    'request/remote': {
         'data': {'affected_items': [{'remote': [{'connection': 'secure', 'ipv6': 'no', 'protocol': ['UDP'],
-                 'port': '1514', 'queue_size': '131071'}]}], 'total_affected_items': 1, 'total_failed_items': 0,
-                 'failed_items': []}, 'message': 'Active configuration was successfully read', 'error': 0
+                                                 'port': '1514', 'queue_size': '131071'}]}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
     },
-    'global': {
+    'request/global': {
         'data': {'affected_items': [{'global': {'remoted': {'agents_disconnection_alert_time': 5,
-                 'agents_disconnection_time': 300}}}], 'total_affected_items': 1, 'total_failed_items': 0,
-                 'failed_items': []}, 'message': 'Active configuration was successfully read', 'error': 0
+                                                            'agents_disconnection_time': 300}}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis/active_response': {
+        'data': {'affected_items': [{'active-response': [{'command': 'restart-wazuh', 'timeout': 0, 'level': 0,
+                                                          'location': 'ALL_AGENTS'}]}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis/alerts': {
+        'data': {'affected_items': [{'alerts': {'email_alert_level': 6, 'log_alert_level': 5}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    #'analysis/decoders':{
+    #
+    #},
+    #'analysis/rules':{
+    #
+    #},
+    'analysis/command': {
+        'data': {'affected_items': [{'command': [{'name': 'restart-wazuh', 'executable': 'restart-wazuh',
+                                                  'timeout_allowed': 0}]}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    #'agent/labels':{
+    #
+    #},
+    'analysis/rule_test': {
+        'data': {'affected_items': [{'rule_test': {'enabled': 'no', 'threads': 1, 'max_sessions': 64,
+                                                   'session_timeout': 900}}], 'total_affected_items': 1,
+                                                   'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis/global': {
+        'data': {'affected_items': [{'global': {'email_notification': 'yes', 'logall': 'no', 'logall_json': 'no',
+                                                'integrity_checking': 8, 'rootkit_detection': 8, 'host_information': 8,
+                                                'prelude_output': 'no', 'zeromq_output': 'no', 'jsonout_output': 'yes',
+                                                'alerts_log': 'yes', 'stats': 8, 'memory_size': 8192,
+                                                'white_list': ['127.0.0.1', '10.0.2.3', 'localhost.localdomain'],
+                                                'rotate_interval': 0, 'max_output_size': 0, 'eps': {'maximum': 0,
+                                                'timeframe': 10}}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
     }
 }
-
-daemons_handler_configuration = {'daemons': ['wazuh-remoted']}
 
 
 # ----------------------------------------- TEST_CUSTOM_INTERNAL_CONFIGURATION -----------------------------------------
 # Configuration and cases data
 t4_cases_path = os.path.join(TEST_CASES_PATH, 'cases_custom_internal_configuration.yaml')
 
-# Enabled test configurations (t3)
+# Custom internal options test configurations (t4)
 t4_configuration_parameters, t4_configuration_metadata, t4_case_ids = get_test_cases_data(t4_cases_path)
 
 expected_custom_internal_conf_response = {
@@ -108,11 +215,22 @@ expected_custom_internal_conf_response = {
                                                               'tcp_keepintvl': 10, 'tcp_keepcnt': 3}}}],
                  'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
         'message': 'Active configuration was successfully read', 'error': 0
+    },
+    'analysis': {
+        'data': {'affected_items': [{'internal': {'analysisd': {'debug': 2, 'default_timeframe': 360,
+                                                                'stats_maxdiff': 999000, 'stats_mindiff': 1250,
+                                                                'stats_percent_diff': 150, 'fts_list_size': 32,
+                                                                'fts_min_size_for_str': 14, 'log_fw': 1,
+                                                                'decoder_order_size': 256, 'label_cache_maxage': 10,
+                                                                'show_hidden_labels': 1, 'rlimit_nofile': 458752,
+                                                                'min_rotate_interval': 600}}}],
+                 'total_affected_items': 1, 'total_failed_items': 0, 'failed_items': []},
+        'message': 'Active configuration was successfully read', 'error': 0
     }
 }
 
-local_internal_options = {'remoted.shared_reload': '20', 'remoted.request_timeout': '30'}
-daemons_handler_configuration = {'daemons': ['wazuh-remoted']}
+local_internal_options = {'remoted.shared_reload': '20', 'remoted.request_timeout': '30',
+                          'analysisd.show_hidden_labels': '1', 'analysisd.debug': '2'}
 
 
 @pytest.mark.tier(level=0)
