8a33cf7da3b338754f4e7575d5aebb8081353045
Julia Magán <juliamgnr@gmail.com>
2022-12-22 13:39:27 +0100

    fix(#3606): rename directory and configure limits

--- tests/integration/test_api/test_statistics/test_statistics_format.py
+++ tests/integration/test_api/test_statistics/test_statistics_format.py
@@ -3,7 +3,7 @@ import os
 import pytest
 import requests
 
-from wazuh_testing.tools.configuration import get_test_cases_data
+from wazuh_testing.tools.configuration import load_configuration_template, get_test_cases_data
 from wazuh_testing.tools.file import read_json_file
 
 pytestmark = [pytest.mark.server]
@@ -16,15 +16,19 @@ TEST_CASES_PATH = os.path.join(TEST_DATA_PATH, 'test_cases', 'statistics_format_
 
 # ----------------------------------------------- TEST_STATISTICS_FORMAT -----------------------------------------------
 # Configuration and cases data
+configurations_path = os.path.join(CONFIGURATIONS_PATH, 'configuration_statistics_format.yaml')
 t1_cases_path = os.path.join(TEST_CASES_PATH, 'cases_statistics_format.yaml')
 
-# Enabled test configurations (t1)
+# Statistics format test configurations (t1)
 t1_configuration_parameters, t1_configuration_metadata, t1_case_ids = get_test_cases_data(t1_cases_path)
+t1_configurations = load_configuration_template(configurations_path, t1_configuration_parameters,
+                                                t1_configuration_metadata)
 
 
 @pytest.mark.tier(level=0)
-@pytest.mark.parametrize('metadata', t1_configuration_metadata, ids=t1_case_ids)
-def test_statistics_format(metadata, get_api_details):
+@pytest.mark.parametrize('configuration, metadata', zip(t1_configurations, t1_configuration_metadata), ids=t1_case_ids)
+def test_enabled(configuration, metadata, load_wazuh_basic_configuration, set_wazuh_configuration,
+                 get_api_details, restart_wazuh_daemon_function):
 
     endpoint = metadata['endpoint']
     api_details = get_api_details()
