6caa6329e9e56610fe1e6c40d216a6b998ba88a0
Deblintrake09 <amicalizzi2005@gmail.com>
2022-12-22 09:38:25 -0300

    feat(#3652): create test template

---
+++ tests/integration/test_fim/test_files/test_windows_system_folder_redirection/test_windows_system_folder_redirection.py
@@ -0,0 +1,172 @@
+'''
+copyright: Copyright (C) 2015-2022, Wazuh Inc.
+
+           Created by Wazuh, Inc. <info@wazuh.com>.
+
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+type: integration
+
+brief: File Integrity Monitoring (FIM) system watches selected files and triggering alerts when these
+       files are modified. Specifically, these tests will check if FIM reports (or truncates if required)
+       the changes made in monitored files when it matches the 'nodiff' tag and vice versa when
+       the 'report_changes' option is enabled.
+       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks configured
+       files for changes to the checksums, permissions, and ownership.
+
+components:
+    - fim
+
+suite: files_report_changes
+
+targets:
+    - agent
+    - manager
+
+daemons:
+    - wazuh-syscheckd
+
+os_platform:
+    - linux
+
+os_version:
+    - Arch Linux
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - Debian Buster
+    - Red Hat 8
+    - Solaris 10
+    - Solaris 11
+    - macOS Catalina
+    - macOS Server
+    - Ubuntu Focal
+    - Ubuntu Bionic
+    - Windows 10
+    - Windows Server 2019
+    - Windows Server 2016
+
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html#diff
+    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html#nodiff
+
+pytest_args:
+    - fim_mode:
+        realtime: Enable real-time monitoring on Linux (using the 'inotify' system calls) and Windows systems.
+        whodata: Implies real-time monitoring but adding the 'who-data' information.
+    - tier:
+        0: Only level 0 tests are performed, they check basic functionalities and are quick to perform.
+        1: Only level 1 tests are performed, they check functionalities of medium complexity.
+        2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
+
+tags:
+    - fim_report_changes
+'''
+import os
+
+
+import pytest
+from wazuh_testing import global_parameters, LOG_FILE_PATH, T_10
+from wazuh_testing.tools import PREFIX, configuration
+from wazuh_testing.tools.monitoring import FileMonitor, generate_monitoring_callback
+from wazuh_testing.modules.fim import CB_FIM_PATH_CONVERTED, ERR_MSG_FIM_PATH_CONVERTED_EVENT
+from wazuh_testing.modules.fim import FIM_DEFAULT_LOCAL_INTERNAL_OPTIONS as local_internal_options
+from wazuh_testing.modules.fim.utils import regular_file_cud
+
+# Marks
+
+pytestmark = [pytest.mark.linux, pytest.mark.tier(level=1)]
+
+
+# Reference paths
+TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+CONFIGURATIONS_PATH = os.path.join(TEST_DATA_PATH, 'configuration_template')
+TEST_CASES_PATH = os.path.join(TEST_DATA_PATH, 'test_cases')
+
+# Configuration and cases data
+test_cases_path = os.path.join(TEST_CASES_PATH, 'cases_windows_system_folder_redirection.yaml')
+configurations_path = os.path.join(CONFIGURATIONS_PATH, 'configuration_windows_system_folder_redirection.yaml')
+
+
+# variables
+wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+
+
+# Test configurations
+configuration_parameters, configuration_metadata, test_case_ids = configuration.get_test_cases_data(test_cases_path)
+configurations = configuration.load_configuration_template(configurations_path, configuration_parameters,
+                                                           configuration_metadata)
+
+
+# tests
+@pytest.mark.parametrize('configuration, metadata', zip(configurations, configuration_metadata), ids=test_case_ids)
+def test_windows_folder_redirection(configuration, metadata, set_wazuh_configuration,
+                                    configure_local_internal_options_function, restart_syscheck_function,
+                                    wait_fim_start_function):
+    '''
+    description: Check if the 'wazuh-syscheckd' daemon reports the file changes (or truncates if required)
+                 in the generated events using the 'nodiff' tag and vice versa. For this purpose, the test
+                 will monitor a directory and make file operations inside it. Then, it will check if a
+                 'diff' file is created for the modified testing file. Finally, if the testing file matches
+                 the 'nodiff' tag, the test will verify that the FIM event generated contains in its
+                 'content_changes' field a message indicating that 'diff' is truncated because
+                 the 'nodiff' option is used.
+
+    wazuh_min_version: 4.5.0
+
+    tier: 1
+
+    parameters:
+        - configuration:
+            type: dict
+            brief: Configuration values for ossec.conf.
+        - metadata:
+            type: dict
+            brief: Test case data.
+        - set_wazuh_configuration:
+            type: fixture
+            brief: Set ossec.conf configuration.
+        - configure_local_internal_options_function:
+            type: fixture
+            brief: Set local_internal_options.conf file.
+        - restart_syscheck_function:
+            type: fixture
+            brief: restart syscheckd daemon, and truncate the ossec.log.
+        - create_monitored_folders
+            type: fixture
+            brief: Create folders to be monitored, delete after test.
+        - wait_for_fim_start_function:
+            type: fixture
+            brief: check that the starting fim scan is detected.
+
+    assertions:
+        - Verify that for each modified file a 'diff' file is generated.
+        - Verify that FIM events include the 'content_changes' field.
+        - Verify that FIM events truncate the modifications made in a monitored file
+          when it matches the 'nodiff' tag.
+        - Verify that FIM events include the modifications made in a monitored file
+          when it does not match the 'nodiff' tag.
+
+    input_description: A test case (ossec_conf_report) is contained in external YAML file (wazuh_conf.yaml)
+                       which includes configuration settings for the 'wazuh-syscheckd' daemon and, these
+                       are combined with the testing directory to be monitored defined in the module.
+
+    expected_output:
+        - r'.*Sending FIM event: (.+)$' ('added', 'modified', and 'deleted' events)
+
+    tags:
+        - diff
+        - scheduled
+    '''
+    file_list = [f"regular_file"]
+    folder = os.path.join(PREFIX, metadata['folder'])
+    wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+    
+    wazuh_log_monitor.start(timeout=T_10, callback=generate_monitoring_callback(CB_FIM_PATH_CONVERTED),
+                            error_message=ERR_MSG_FIM_PATH_CONVERTED_EVENT)
+    
+    
+    regular_file_cud(folder, wazuh_log_monitor, file_list=file_list, time_travel=False,
+                     min_timeout=global_parameters.default_timeout*4, triggers_event=True)
