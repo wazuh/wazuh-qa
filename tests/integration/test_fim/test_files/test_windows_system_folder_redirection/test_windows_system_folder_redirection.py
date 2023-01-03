9393bb9ae2e0d3ccbd329ad63672bacd272634ce
Deblintrake09 <amicalizzi2005@gmail.com>
2023-01-03 13:12:54 -0300

    docs(#3652): update documentation

--- tests/integration/test_fim/test_files/test_windows_system_folder_redirection/test_windows_system_folder_redirection.py
+++ tests/integration/test_fim/test_files/test_windows_system_folder_redirection/test_windows_system_folder_redirection.py
@@ -1,5 +1,5 @@
 '''
-copyright: Copyright (C) 2015-2022, Wazuh Inc.
+copyright: Copyright (C) 2015-2023, Wazuh Inc.
 
            Created by Wazuh, Inc. <info@wazuh.com>.
 
@@ -7,50 +7,32 @@ copyright: Copyright (C) 2015-2022, Wazuh Inc.
 
 type: integration
 
-brief: File Integrity Monitoring (FIM) system watches selected files and triggering alerts when these
-       files are modified. Specifically, these tests will check if FIM reports (or truncates if required)
-       the changes made in monitored files when it matches the 'nodiff' tag and vice versa when
-       the 'report_changes' option is enabled.
-       The FIM capability is managed by the 'wazuh-syscheckd' daemon, which checks configured
-       files for changes to the checksums, permissions, and ownership.
+brief: File Integrity Monitoring (FIM) system watches selected files and triggering alerts when these files are
+       modified. Specifically, these tests will check that FIM is able to monitor Windows system folders. FIM can
+       redirect %WINDIR%/Sysnative monitoring toward System32 folder, so the tests also check that when monitoring
+       Sysnative the path is converted to system32 and events are generated there properly.
 
 components:
     - fim
 
-suite: files_report_changes
+suite: windows_system_folder_redirection
 
 targets:
     - agent
-    - manager
 
 daemons:
     - wazuh-syscheckd
 
 os_platform:
-    - linux
+    - windows
 
 os_version:
-    - Arch Linux
-    - Amazon Linux 2
-    - Amazon Linux 1
-    - CentOS 8
-    - CentOS 7
-    - Debian Buster
-    - Red Hat 8
-    - Solaris 10
-    - Solaris 11
-    - macOS Catalina
-    - macOS Server
-    - Ubuntu Focal
-    - Ubuntu Bionic
     - Windows 10
     - Windows Server 2019
     - Windows Server 2016
 
 references:
     - https://documentation.wazuh.com/current/user-manual/capabilities/file-integrity/index.html
-    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html#diff
-    - https://documentation.wazuh.com/current/user-manual/reference/ossec-conf/syscheck.html#nodiff
 
 pytest_args:
     - fim_mode:
@@ -62,13 +44,13 @@ pytest_args:
         2: Only level 2 tests are performed, they check advanced functionalities and are slow to perform.
 
 tags:
-    - fim_report_changes
+    - windows_folder_redirection
 '''
 import os
 
 
 import pytest
-from wazuh_testing import global_parameters, LOG_FILE_PATH, T_10
+from wazuh_testing import LOG_FILE_PATH, T_10
 from wazuh_testing.tools import PREFIX, configuration
 from wazuh_testing.tools.monitoring import FileMonitor
 from wazuh_testing.modules.fim import FIM_DEFAULT_LOCAL_INTERNAL_OPTIONS as local_internal_options
@@ -104,13 +86,8 @@ def test_windows_folder_redirection(configuration, metadata, set_wazuh_configura
                                     configure_local_internal_options_function, restart_syscheck_function,
                                     wait_fim_start_function):
     '''
-    description: Check if the 'wazuh-syscheckd' daemon reports the file changes (or truncates if required)
-                 in the generated events using the 'nodiff' tag and vice versa. For this purpose, the test
-                 will monitor a directory and make file operations inside it. Then, it will check if a
-                 'diff' file is created for the modified testing file. Finally, if the testing file matches
-                 the 'nodiff' tag, the test will verify that the FIM event generated contains in its
-                 'content_changes' field a message indicating that 'diff' is truncated because
-                 the 'nodiff' option is used.
+    description: Check if the 'wazuh-syscheckd' monitors the windows system folders (System32 and SysWOW64) properly,
+    and that monitoring for Sysnative folder is redirected to System32 and works properly.
 
     wazuh_min_version: 4.5.0
 
@@ -144,23 +121,21 @@ def test_windows_folder_redirection(configuration, metadata, set_wazuh_configura
         - Verify that FIM events include the modifications made in a monitored file
           when it does not match the 'nodiff' tag.
 
-    input_description: A test case (ossec_conf_report) is contained in external YAML file (wazuh_conf.yaml)
-                       which includes configuration settings for the 'wazuh-syscheckd' daemon and, these
-                       are combined with the testing directory to be monitored defined in the module.
+    input_description: The file 'configuration_windows_system_folder_redirection.yaml' provides the configuration
+                       template.
+                       The file 'cases_windows_system_folder_redirection.yaml' provides the tes cases configuration
+                       details for each test case.
 
     expected_output:
-        - r'.*Sending FIM event: (.+)$' ('added', 'modified', and 'deleted' events)
-
-    tags:
-        - diff
-        - scheduled
+        - r'.*fim_adjust_path.*Convert '(.*) to '(.*)' to process the FIM events.'
+        - r'.*Sending FIM event: (.+)$' ('added', 'modified', and 'deleted' events)'
     '''
     file_list = [f"regular_file"]
     folder = os.path.join(PREFIX, 'windows', metadata['folder'])
     wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
-    
+
     if metadata['redirected']:
         check_fim_event(callback=CB_FIM_PATH_CONVERTED, timeout=T_10)
-    
+
     regular_file_cud(folder, wazuh_log_monitor, file_list=file_list, time_travel=False,
-                     min_timeout=300, triggers_event=True, escaped=True)
+                     min_timeout=90, triggers_event=True, escaped=True)
