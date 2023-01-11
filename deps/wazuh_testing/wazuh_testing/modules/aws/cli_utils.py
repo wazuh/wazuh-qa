d0638bec1435ff0985671a9eb1ad5ea36ee5be7a
Nico Stefani <nicolas.stefi@wazuh.com>
2023-01-11 09:07:12 -0300

    feat(#3335): Cloudtrail integration tests (#3624)
    * feat(#3335): add basic test for aws cloudtrail module
    
    * feat(#3335): add utils for handle files in S3 buckets
    
    * feat(#3335): add remove_from_bucket test for aws cloudtrail module
    
    * feat(#3335): add fixture for delete s3_cloudtrail.db
    
    * refactor(#3335): rename modules
    
    * refactor(#3335): change the return of upload_file function
    
    * refactor(#3335): Fix docstring and rename constant
    
    * refactor(#3335): improve default test catching parser and module errors
    
    * feat(#3335): add only_logs_from test for aws cloudtrail module
    
    * feat(#3335): add path tests for aws cloudtrail module
    
    * feat(#3335): add path_suffix tests for aws cloudtrail module
    
    * feat(#3335): add regions tests for aws cloudtrail module
    
    * refactor(#3335): Move wazuh_log_monitor to a fixture
    
    * feat(#3335): add discard_regex tests for aws cloudtrail module
    
    * feat(#3335): add only_logs_after tier_1 test for aws cloudtrail module
    
    * refactor(#3335): Improve name of test cases
    
    * refactor(#3335): Improve tests assertions
    
    * refactor(#3335): Improve tier 1 test
    
    * refactor(#3335): Implement new design for Tier 1 test
    
    * style(#3335): Changed the extension from .yml to .yaml
    
    * style(#3335): Fixed linter issues
    
    * style(#3335): Apply suggestions from code review
    
    Co-authored-by: José Luis López Sánchez <joseluis.lopez@wazuh.com>
    
    * refactor(#3335): Add path parameter to get_db_connection
    
    * style(#3335): Fix quotes use according to QA style
    
    * style(#3335): Use google docstring style
    
    * refactor(#3335): Add missing information about fixtures
    
    * refactor(#3335): Change sqlite table name for backward compatibility
    
    * style(#3335): Fix docstrings punctuation and capitalization
    
    * refactor(#3335): Apply suggestions from code review
    
    * style(#3335): Apply suggestions from code review
    
    Fix punctuation in docstrings.
    
    Co-authored-by: David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
    
    * style(#3335): Apply suggestions from code review
    
    Co-authored-by: David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>
    
    Co-authored-by: José Luis López Sánchez <joseluis.lopez@wazuh.com>
    Co-authored-by: David Jose Iglesias Lopez <davidj.iglesias@wazuh.com>

---
+++ deps/wazuh_testing/wazuh_testing/modules/aws/cli_utils.py
@@ -0,0 +1,66 @@
+import subprocess
+from pathlib import Path
+from typing import Callable
+
+from wazuh_testing import logger
+
+from .constants import AWS_MODULE_PATH
+
+AWS_BINARY_PATH = Path(AWS_MODULE_PATH, "aws-s3")
+
+
+class OutputAnalysisError(Exception):
+    pass
+
+
+def call_aws_module(*parameters) -> str:
+    """Given some parameters call the AWS module and return the output.
+
+    Returns:
+        str: The command output.
+    """
+    command = [AWS_BINARY_PATH, *parameters]
+    logger.debug("Calling AWS module with: '%s'", command)
+    command_result = subprocess.run(command, capture_output=True)
+
+    return command_result.stdout.decode()
+
+
+def _default_callback(line: str) -> str:
+    print(line)
+    return line
+
+
+def analyze_command_output(
+    command_output: str, callback: Callable = _default_callback, expected_results: int = 1, error_message: str = ''
+):
+    """Analyze the given command output searching for a pattern.
+
+    Args:
+        command_output (str): The output to analyze.
+        callback (Callable, optional): A callback to process each line. Defaults to _default_callback.
+        expected_results (int, optional): Number of expected results. Defaults to 1.
+        error_message (str, optional): Message to show with the exception. Defaults to ''.
+
+    Raises:
+        OutputAnalysisError: When the expected results are not correct.
+    """
+
+    results = []
+
+    for line in command_output.splitlines():
+        logger.debug(line)
+        item = callback(line)
+
+        if item is not None:
+            results.append(item)
+
+    results_len = len(results)
+
+    if results_len != expected_results:
+        if error_message:
+            logger.error(error_message)
+            logger.error('Results found: %s', results_len)
+            logger.error('Results expected: %s', expected_results)
+            raise OutputAnalysisError(error_message)
+        raise OutputAnalysisError()
