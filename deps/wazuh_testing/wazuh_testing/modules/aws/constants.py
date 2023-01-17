896867619f58e3f4cdf0c51d76041f66a7bb1ef1
Nicolas Stefani <nicolas.stefi@wazuh.com>
2023-01-17 13:50:35 -0300

    feat(#3344): Add schema for ServerAccess table

--- deps/wazuh_testing/wazuh_testing/modules/aws/constants.py
+++ deps/wazuh_testing/wazuh_testing/modules/aws/constants.py
@@ -13,6 +13,7 @@ VPC_FLOW_LOGS = "vpcflowlogs"
 FLOW_LOG_ID = "fl-0754d951c16f517fa"
 CONFIG = 'Config'
 ELASTIC_LOAD_BALANCING = 'elasticloadbalancing'
+SERVER_ACCESS_TABLE_NAME = 's3_server_access'
 
 EVENT_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 PATH_DATE_FORMAT = '%Y/%m/%d'
