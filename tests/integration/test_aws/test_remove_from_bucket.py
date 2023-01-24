3fc5f7476e96fe69c0aa18639f8bed462d3d78f7
Nicolas Stefani <nicolas.stefi@wazuh.com>
2023-01-24 16:21:37 -0300

    feat(#3346): Add remove_log_streams test for aws cloudwatch integration

--- tests/integration/test_aws/test_remove_from_bucket.py
+++ tests/integration/test_aws/test_remove_from_bucket.py
@@ -4,6 +4,7 @@ import pytest
 from wazuh_testing import global_parameters
 from wazuh_testing.modules.aws import event_monitor
 from wazuh_testing.modules.aws.s3_utils import file_exists
+from wazuh_testing.modules.aws.cloudwatch_utils import log_stream_exists
 from wazuh_testing.tools.configuration import (
     get_test_cases_data,
     load_configuration_template,
@@ -116,3 +117,103 @@ def test_remove_from_bucket(
     ).result()
 
     assert not file_exists(filename=metadata['uploaded_file'], bucket_name=bucket_name)
+
+
+# ---------------------------------------------------- TEST_REMOVE_LOG_STREAM ------------------------------------------
+# Configuration and cases data
+configurations_path = os.path.join(CONFIGURATIONS_PATH, 'configuration_remove_log_stream.yaml')
+cases_path = os.path.join(TEST_CASES_PATH, 'cases_remove_log_streams.yaml')
+
+configuration_parameters, configuration_metadata, case_ids = get_test_cases_data(cases_path)
+configurations = load_configuration_template(configurations_path, configuration_parameters, configuration_metadata)
+
+
+@pytest.mark.tier(level=0)
+@pytest.mark.parametrize('configuration, metadata', zip(configurations, configuration_metadata), ids=case_ids)
+def test_remove_log_stream(
+    configuration, metadata, create_log_stream, load_wazuh_basic_configuration, set_wazuh_configuration,
+    clean_aws_services_db, configure_local_internal_options_function, truncate_monitored_files, restart_wazuh_function,
+    wazuh_log_monitor
+):
+    """
+    description: The created log stream was removed after the execution.
+    test_phases:
+        - setup:
+            - Load Wazuh light configuration.
+            - Apply ossec.conf configuration changes according to the configuration template and use case.
+            - Apply custom settings in local_internal_options.conf.
+            - Truncate wazuh logs.
+            - Restart wazuh-manager service to apply configuration changes.
+        - test:
+            - Check in the ossec.log that a line has appeared calling the module with correct parameters.
+            - Check that the created log stream was removed by the module after the execution.
+        - teardown:
+            - Truncate wazuh logs.
+            - Restore initial configuration, both ossec.conf and local_internal_options.conf.
+    wazuh_min_version: 4.5.0
+    parameters:
+        - configuration:
+            type: dict
+            brief: Get configurations from the module.
+        - metadata:
+            type: dict
+            brief: Get metadata from the module.
+        - create_log_stream:
+            type: fixture
+            brief: Create a log stream with events for the day of execution.
+        - load_wazuh_basic_configuration:
+            type: fixture
+            brief: Load basic wazuh configuration.
+        - set_wazuh_configuration:
+            type: fixture
+            brief: Apply changes to the ossec.conf configuration.
+        - clean_aws_services_db:
+            type: fixture
+            brief: Delete the DB file before and after the test execution.
+        - configure_local_internal_options_function:
+            type: fixture
+            brief: Apply changes to the local_internal_options.conf configuration.
+        - truncate_monitored_files:
+            type: fixture
+            brief: Truncate wazuh logs.
+        - restart_wazuh_daemon_function:
+            type: fixture
+            brief: Restart the wazuh service.
+        - wazuh_log_monitor:
+            type: fixture
+            brief: Return a `ossec.log` monitor.
+    assertions:
+        - Check in the log that the module was called with correct parameters.
+        - Check in the log group that the created stream was removed.
+    input_description:
+        - The `configuration_defaults` file provides the module configuration for this test.
+        - The `cases_defaults` file provides the test cases.
+    """
+    service_type = metadata['service_type']
+    log_group_name = metadata['log_group_name']
+
+    parameters = [
+        'wodles/aws/aws-s3',
+        '--service', service_type,
+        '--aws_profile', 'qa',
+        '--regions', 'us-east-1',
+        '--aws_log_groups', log_group_name,
+        '--remove-log-streams',
+        '--debug', '2'
+    ]
+
+    # Check AWS module started
+    wazuh_log_monitor.start(
+        timeout=global_parameters.default_timeout,
+        callback=event_monitor.callback_detect_aws_module_start,
+        error_message='The AWS module did not start as expected',
+    ).result()
+
+    # Check command was called correctly
+    wazuh_log_monitor.start(
+        timeout=global_parameters.default_timeout,
+        callback=event_monitor.callback_detect_aws_module_called(parameters),
+        error_message='The AWS module was not called with the correct parameters',
+    ).result()
+
+    assert not log_stream_exists(log_stream=metadata['log_stream'], log_group=log_group_name)
