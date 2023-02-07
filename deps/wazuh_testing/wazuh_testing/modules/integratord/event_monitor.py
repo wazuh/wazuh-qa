1202581bd132f05fa989f798d4a562b295f75a31
mauromalara <mmalara@outlook.es>
2023-02-07 14:09:53 +0000

    refactor(#3298): apply multiple changes to integratord monitor.

--- deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
+++ deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
@@ -5,14 +5,14 @@ copyright: Copyright (C) 2015-2022, Wazuh Inc.
 '''
 import re
 
-from wazuh_testing import T_20, T_30
+from wazuh_testing import T_5, T_20, T_30
 from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.tools.monitoring import FileMonitor
-from wazuh_testing.modules.integratord import INTEGRATORD_PREFIX
+from wazuh_testing.modules import integratord
 
 
-def make_integratord_callback(pattern, prefix=INTEGRATORD_PREFIX):
-    """Create a callback function from a text pattern.
+def make_integratord_callback(pattern, prefix=integratord.INTEGRATORD_PREFIX):
+    '''Create a callback function from a text pattern.
 
     It already contains the vulnerability-detector prefix.
 
@@ -25,7 +25,7 @@ def make_integratord_callback(pattern, prefix=INTEGRATORD_PREFIX):
 
     Examples:
         >>> callback_bionic_update_started = make_vuln_callback("Starting Ubuntu Bionic database update")
-    """
+    '''
     pattern = r'\s+'.join(pattern.split())
     regex = re.compile(r'{}{}'.format(prefix, pattern))
 
@@ -33,8 +33,9 @@ def make_integratord_callback(pattern, prefix=INTEGRATORD_PREFIX):
 
 
 def check_integratord_event(file_monitor=None, callback='', error_message=None, update_position=True,
-                            timeout=T_30, accum_results=1, file_to_monitor=LOG_FILE_PATH, prefix=INTEGRATORD_PREFIX):
-    """Check if an event occurs
+                            timeout=T_30, accum_results=1, file_to_monitor=LOG_FILE_PATH,
+                            prefix=integratord.INTEGRATORD_PREFIX):
+    '''Check if an event occurs
     Args:
         file_monitor (FileMonitor): FileMonitor object to monitor the file content.
         callback (str): log regex to check in Wazuh log
@@ -43,7 +44,7 @@ def check_integratord_event(file_monitor=None, callback='', error_message=None, 
         timeout (str): timeout to check the event in Wazuh log
         accum_results (int): Accumulation of matches.
         prefix (str): log pattern regex
-    """
+    '''
     file_monitor = FileMonitor(file_to_monitor) if file_monitor is None else file_monitor
     error_message = f"Could not find this event in {file_to_monitor}: {callback}" if error_message is None else \
         error_message
@@ -52,90 +53,70 @@ def check_integratord_event(file_monitor=None, callback='', error_message=None, 
                        callback=make_integratord_callback(callback, prefix), error_message=error_message)
 
 
-def check_integratord_thread_ready(file_monitor=None, timeout=T_20):
-    """Check if a local requests thread ready in the logs.
+def check_integratord_thread_ready(file_monitor=None, timeout=T_5):
+    '''Check if a local requests thread ready in the logs.
 
     Args:
         log_monitor (FileMonitor): Log monitor.
         timeout (int): Event timeout.
-    """
+    '''
     check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback='DEBUG: Local requests thread ready',
-                            error_message='Did not receive the expected "Enabling integration for slack"')
-
-
-def check_send_new_alers(file_monitor=None, timeout=T_20, update_position=False):
-    """Check for sending new alert in the logs.
-
-    Args:
-        log_monitor (FileMonitor): Log monitor.
-        timeout (int): Event timeout.
-        update_position (boolean): filter configuration parameter to search in Wazuh log
-    """
-    check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback='DEBUG: sending new alert',
-                            error_message='Did not receive the expected "...sending new alert" event',
-                            update_position=update_position)
+                            callback=integratord.CB_INTEGRATORD_THREAD_IS_READY,
+                            error_message='Did not receive the expected "Local requests thread ready"')
 
 
 def check_file_inode_changed(file_monitor=None, timeout=T_20):
-    """Check for Alert file inode changed in the logs.
+    '''Check for Alert file inode changed in the logs.
 
     Args:
         log_monitor (FileMonitor): Log monitor.
         timeout (int): Event timeout.
-    """
-    check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback=r".*DEBUG: jqueue_next.*Alert file inode changed.*",
+    '''
+    check_integratord_event(file_monitor=file_monitor, timeout=timeout, callback=integratord.CB_INODE_CHANGED,
                             error_message='Did not receive the expected "...Alert file inode changed..." event')
 
 
-def check_process_alert(file_monitor=None, timeout=T_20):
-    """Check for Processing alert in the logs.
+def check_alert_processing(file_monitor=None, timeout=T_20):
+    '''Check for Processing alert message in the logs.
 
     Args:
         log_monitor (FileMonitor): Log monitor.
         timeout (int): Event timeout.
-    """
-    check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback=r".*Processing alert.*",
+    '''
+    check_integratord_event(file_monitor=file_monitor, timeout=timeout, callback=integratord.CB_PROCESSING_ALERT,
                             error_message='Did not receive the expected Slack alert in alerts.json')
 
 
-def check_response(file_monitor=None, timeout=T_20):
-    """Check for response in the logs.
+def check_third_party_response(file_monitor=None, timeout=T_20):
+    '''Check for response message in the logs.
 
     Args:
         log_monitor (FileMonitor): Log monitor.
         timeout (int): Event timeout.
-    """
-    check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback=r".*<Response \[200\]>",
+    '''
+    check_integratord_event(file_monitor=file_monitor, timeout=timeout, callback=integratord.CB_THIRD_PARTY_RESPONSE,
                             error_message='Could not send the alert to slack')
 
 
-def check_alert_read(file_monitor=None, timeout=T_20, callback='', error_message=None):
-    """Check for alert read in the logs.
+def check_invalid_alert_read(file_monitor=None, timeout=T_20, callback='', error_message=None):
+    '''Check for invalid alert read message in the logs.
 
     Args:
         log_monitor (FileMonitor): Log monitor.
         timeout (int): Event timeout.
         callback (str): log regex to check in Wazuh log
         error_message (str): error message to show in case of expected event does not occur
-    """
-    check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback=callback,
-                            error_message=error_message)
+    '''
+    check_integratord_event(file_monitor=file_monitor, timeout=timeout, callback=callback, error_message=error_message)
 
 
 def check_file_warning(file_monitor=None, timeout=T_20):
-    """Check for information of file in the logs.
+    '''Check for information of file in the logs.
 
     Args:
         log_monitor (FileMonitor): Log monitor.
         timeout (int): Event timeout.
-    """
+    '''
     check_integratord_event(file_monitor=file_monitor, timeout=timeout,
-                            callback=fr".*{INTEGRATORD_PREFIX}.*WARNING.*Could not retrieve information of file.*"\
-                                     r'alerts\.json.*No such file.*',
+                            callback=integratord.CB_ALERT_JSON_FILE_NOT_FOUND,
                             error_message='Did not receive the expected "...Could not retrieve information/open file"')
