9d14514805848dcca85a92217f12ed40c5e8611f
Nico Stefani <nicolas.stefi@wazuh.com>
2023-02-08 05:57:42 -0300

    AWS Config integration tests (#3705)
    * feat(#3337): Add basic test for aws config integration
    
    * feat(#3337): Add remove_from_bucket test for aws config integration
    
    * feat(#3337): Add only_logs_after tests for aws config integration
    
    * feat(#3337): Add path tests for aws config integration
    
    * feat(#3337): Add path_suffix tests for aws config integration
    
    * feat(#3337): Add regions tests for aws config integration
    
    * feat(#3337): Add discard_regex tests for aws config integration
    
    * feat(#3337): Add only_logs_after tier_1 tests for aws config integration
    
    * feat(#3337): Use non padaded month format
    
    * feat(#3337): Use join in path builds
    
    * feat(#3337): Improve message for check_non_processed_logs_from_output
    
    * fix(#3337): Add minor fixes

--- deps/wazuh_testing/wazuh_testing/modules/aws/constants.py
+++ deps/wazuh_testing/wazuh_testing/modules/aws/constants.py
@@ -10,9 +10,11 @@ RANDOM_ACCOUNT_ID = '819751203818'
 CLOUDTRAIL = 'CloudTrail'
 VPC_FLOW_LOGS = "vpcflowlogs"
 FLOW_LOG_ID = "fl-0754d951c16f517fa"
+CONFIG = 'Config'
 
 EVENT_TIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 PATH_DATE_FORMAT = '%Y/%m/%d'
+PATH_DATE_NO_PADED_FORMAT = '%Y/%-m/%-d'
 FILENAME_DATE_FORMAT = '%Y%m%dT%H%MZ'
 
 US_EAST_1_REGION = 'us-east-1'
@@ -23,3 +25,4 @@ LOG_EXT = '.log'
 # Bucket types
 CLOUD_TRAIL_TYPE = 'cloudtrail'
 VPC_FLOW_TYPE = 'vpcflow'
+CONFIG_TYPE = 'config'
