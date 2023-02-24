28b5003a55e12ae724eda45da0d54c185a6fb1a5
Nico Stefani <nicolas.stefi@wazuh.com>
2023-02-24 11:41:41 -0300

    feat(#3343): WAF integration tests (#3763)
    * feat(#3343): Add basic test for aws WAF integration
    
    * feat(#3343): Add data generator for WAF type
    
    * feat(#3343): Add remove_from_bucket test for aws WAF integration
    
    * feat(#3343): Add schema for WAF table
    
    * feat(#3343): Add only_logs_after tests for aws WAF integration
    
    * feat(#3343): Add path tests for aws WAF integration
    
    * feat(#3343): Add discard_regex tests for aws WAF integration
    
    * feat(#3343): Add only_logs_after tier 1 for aws WAF integration
    
    * style(#3343): Apply suggestions from code review
    
    Co-authored-by: Facundo Dalmau <facundo.dalmau@wazuh.com>
    
    * refactor(#3343): Improve discard_regex test findings
    
    ---------
    
    Co-authored-by: Facundo Dalmau <facundo.dalmau@wazuh.com>

--- deps/wazuh_testing/wazuh_testing/modules/aws/db_utils.py
+++ deps/wazuh_testing/wazuh_testing/modules/aws/db_utils.py
@@ -12,6 +12,7 @@ from .constants import (
     NLB_TYPE,
     S3_CLOUDTRAIL_DB_PATH,
     VPC_FLOW_TYPE,
+    WAF_TYPE
 )
 
 SELECT_QUERY_TEMPLATE = 'SELECT * FROM {table_name}'
@@ -36,6 +37,10 @@ S3GuardDutyRow = namedtuple(
     'S3GuardDutyRow', 'bucket_path aws_account_id log_key processed_date created_date'
 )
 
+S3WAFRow = namedtuple(
+    'S3WAFRow', 'bucket_path aws_account_id log_key processed_date created_date'
+)
+
 s3_rows_map = {
     CLOUD_TRAIL_TYPE: S3CloudTrailRow,
     VPC_FLOW_TYPE: S3VPCFlowRow,
@@ -43,7 +48,8 @@ s3_rows_map = {
     CLB_TYPE: S3ALBRow,
     NLB_TYPE: S3ALBRow,
     CUSTOM_TYPE: S3CustomRow,
-    GUARD_DUTY_TYPE: S3GuardDutyRow
+    GUARD_DUTY_TYPE: S3GuardDutyRow,
+    WAF_TYPE: S3WAFRow
 }
 
 
