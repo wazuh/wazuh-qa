c8796c2543b288ea11a4734e4ffa4f959bb2a57e
Julia <juliamgnr@gmail.com>
2023-03-28 15:55:04 +0200

    feat(#3912): add test cases

--- tests/system/test_cluster/test_agent_groups/test_remove_group.py
+++ tests/system/test_cluster/test_agent_groups/test_remove_group.py
@@ -1,11 +1,14 @@
 import os
 import pytest
+from time import sleep
 
-from system.test_cluster.test_agent_groups.common import register_agent
+import wazuh_testing as fw
 from system import (ERR_MSG_CLIENT_KEYS_IN_MASTER_NOT_FOUND, check_agent_groups, check_keys_file,
                     create_new_agent_group, assign_agent_to_new_group, restart_cluster, execute_wdb_query, get_group_id)
-from wazuh_testing.tools.system import HostManager
+from system.test_cluster.test_agent_groups.common import register_agent
 from wazuh_testing.tools.configuration import get_test_cases_data
+from wazuh_testing.tools.monitoring import HostMonitor
+from wazuh_testing.tools.system import HostManager
 
 pytestmark = [pytest.mark.cluster, pytest.mark.enrollment_cluster_env]
 
@@ -20,6 +23,7 @@ host_manager = HostManager(inventory_path)
 local_path = os.path.dirname(os.path.abspath(__file__))
 tmp_path = os.path.join(local_path, 'tmp')
 t1_cases_path = os.path.join(data_path, 'cases_remove_group.yaml')
+messages_path = os.path.join(data_path, 'remove_group_messages.yaml')
 
 # Variables
 t1_configuration_parameters, t1_configuration_metadata, t1_case_ids = get_test_cases_data(t1_cases_path)
@@ -55,24 +59,64 @@ def pre_configured_groups(target_node, group):
 # Tests
 @pytest.mark.parametrize('metadata', t1_configuration_metadata, ids=t1_case_ids)
 @pytest.mark.parametrize('group', ['group_test'])
-@pytest.mark.parametrize('target_node', ['wazuh-master'])
+@pytest.mark.parametrize('target_node', ['wazuh-master', 'wazuh-worker1'])
 def test_remove_group(metadata, group, target_node, pre_configured_groups, clean_environment):
     '''
-    description:
+    description: Check that when a group is deleted using different methods, it is deleted on all nodes, except when
+                 the group folder is deleted in a worker.
     wazuh_min_version: 4.4.0
     parameters:
+        - metadata:
+            type: dict
+            brief: Get metadata from the module.
+        - group:
+            type: string
+            brief: Group name.
+        - target_node:
+            type: string
+            brief: Name of the host where the agent will register.
+        - pre_configured_groups:
+            type: fixture
+            brief: Create group, register and assign agent during the setup.
+        - clean_environment:
+            type: fixture
+            brief: Reset the wazuh log files at the start of the test. Remove all registered agents from master.
     assertions:
+        - Verify [Agent-groups send full] task finished when the group folder is removed in a worker node.
+        - Verify group name is present in agent table when the group folder is removed in a worker node.
+        - Verify group id is present in agent table when the group folder is removed in a worker node.
+        - Verify group name is not present in agent table when group is deleted.
+        - Verify group id is not present in agent table when group is deleted.
     '''
-    # Get group ID
-    group_id = get_group_id(group, test_infra_managers[0], host_manager)
+    # Get group IDs
+    group_ids = {}
+    for manager in test_infra_managers:
+        group_ids[manager] = str(get_group_id(group, manager, host_manager))
 
     # Delete group
     exec(metadata['method'])
+    sleep(fw.T_10)
 
-    # Check group is deleted in agent table in every node
+    if not metadata['deleted']:
+        HostMonitor(inventory_path=inventory_path,
+                    messages_path=messages_path,
+                    tmp_path=tmp_path).run(update_position=True)
+
+        for manager in test_infra_managers:
+            group_ids[manager] = str(get_group_id(group, manager, host_manager))
+
+    # Check group is deleted or not if exepceted in agent table in every node
     for query in queries:
         for manager in test_infra_managers:
             global_query = f"global '{query}'"
             response = execute_wdb_query(global_query, manager, host_manager)
-            print(manager)
-            assert group and str(group_id) not in response, 'Group not deleted correctly'
+            if metadata['deleted']:
+                if 'id_group' in query:
+                    assert group_ids[manager] not in response, f"Group not deleted correctly in {manager}"
+                else:
+                    assert group not in response, f"Group not deleted correctly in {manager}"
+            else:
+                if 'id_group' in query:
+                    assert group_ids[manager] in response, f"Group deleted in {manager}"
+                else:
+                    assert group in response, f"Group deleted in {manager}"
