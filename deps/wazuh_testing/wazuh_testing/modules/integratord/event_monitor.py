e6448a337a68a87945dd672d9a8a778f7f1152b9
Deblintrake09 <amicalizzi2005@gmail.com>
2023-04-17 14:08:30 -0300

    feat(#3928): add new integratord callbacks

--- deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
+++ deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
@@ -8,6 +8,11 @@ from wazuh_testing.tools import LOG_FILE_PATH
 from wazuh_testing.tools.monitoring import FileMonitor
 
 
+# Callback string
+CB_OPTIONS_NON_EXISTENT = ".*OS_IntegratorD.*JSON file  doesn't exist"
+
+
+# Functions
 def check_integratord_event(file_monitor=None, callback='', error_message=None, update_position=True,
                             timeout=30, accum_results=1, file_to_monitor=LOG_FILE_PATH):
     """Check if an event occurs
@@ -25,3 +30,36 @@ def check_integratord_event(file_monitor=None, callback='', error_message=None, 
 
     file_monitor.start(timeout=timeout, update_position=update_position, accum_results=accum_results,
                        callback=callback, error_message=error_message)
+
+
+
+# Callback functions
+def detect_integration_enabled(integration, file_monitor=None):
+    """Detects integration has been enabled.
+
+    Args:
+        integration (str): The integratio that is being checked. Ex: Slack, Pagerduty and Shuffle
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    callback = fr".*Enabling integration for: '{integration}'."
+    check_integratord_event(file_monitor=file_monitor, callback=callback)
+
+
+def detect_unable_to_run_integration(integration, file_monitor=None):
+    """Detects is unable to be executed.
+
+    Args:
+        integration (str): The integration that is being checked. Ex: Slack, Pagerduty and Shuffle
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    callback = fr".*ERROR: Unable to run integration for {integration} -> integrations"
+    check_integratord_event(file_monitor=file_monitor, callback=callback)
+
+
+def detect_options_json_file_does_not_exist(file_monitor=None):
+    """Detects if JSON options file does not exist
+
+    Args:
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    check_integratord_event(file_monitor=file_monitor, callback=CB_OPTIONS_NON_EXISTENT)
