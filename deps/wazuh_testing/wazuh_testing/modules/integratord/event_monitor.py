eada35f8ebea003e46fb774206b6500138a67096
Deblintrake09 <amicalizzi2005@gmail.com>
2023-04-18 17:04:32 -0300

    feat(#3928): new callbacks for slack integration

--- deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
+++ deps/wazuh_testing/wazuh_testing/modules/integratord/event_monitor.py
@@ -4,10 +4,15 @@ copyright: Copyright (C) 2015-2022, Wazuh Inc.
            This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 '''
 
-from wazuh_testing.tools import LOG_FILE_PATH
-from wazuh_testing.tools.monitoring import FileMonitor
+from wazuh_testing import LOG_FILE_PATH, T_10
+from wazuh_testing.tools.monitoring import FileMonitor, generate_monitoring_callback
 
 
+# Callback string
+CB_OPTIONS_FILE_DOES_NOT_EXISTENT = ".*OS_IntegratorD.*(JSON file for options  doesn't exist)"
+
+
+# Functions
 def check_integratord_event(file_monitor=None, callback='', error_message=None, update_position=True,
                             timeout=30, accum_results=1, file_to_monitor=LOG_FILE_PATH):
     """Check if an event occurs
@@ -25,3 +30,63 @@ def check_integratord_event(file_monitor=None, callback='', error_message=None, 
 
     file_monitor.start(timeout=timeout, update_position=update_position, accum_results=accum_results,
                        callback=callback, error_message=error_message)
+
+
+# Callback functions
+def detect_integration_enabled(integration, file_monitor=None):
+    """Detects integration has been enabled.
+    Args:
+        integration (str): The integratio that is being checked. Ex: Slack, Pagerduty and Shuffle
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    callback = fr".*(Enabling integration for: '{integration}')."
+    check_integratord_event(file_monitor=file_monitor, callback=generate_monitoring_callback(callback),
+                            error_message="Could not find the expected 'Enabling integration for...' event")
+
+
+def detect_unable_to_run_integration(integration, file_monitor=None):
+    """Detects is unable to be executed.
+    Args:
+        integration (str): The integration that is being checked. Ex: Slack, Pagerduty and Shuffle
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    callback = fr".*ERROR: Unable to run integration for ({integration}) -> integrations"
+    check_integratord_event(file_monitor=file_monitor, callback=generate_monitoring_callback(callback),
+                            error_message="Could not find the expected 'Unable to run integration for...' event")
+
+
+def detect_options_json_file_does_not_exist(file_monitor=None):
+    """Detects if JSON options file does not exist
+    Args:
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    check_integratord_event(file_monitor=file_monitor, timeout=T_10,
+                            callback=generate_monitoring_callback(CB_OPTIONS_FILE_DOES_NOT_EXISTENT),
+                            error_message="Could not find the expected 'JSON file doesn't exist...' event")
+
+
+def detect_integration_response_code(response='200', file_monitor=None):
+    """Detects the response code for the integration.
+    Args:
+        response (str): the response code for the integration. Defaults to 200
+        file_monitor (FileMonitor): file log monitor to detect events
+    """
+    callback = fr'.*Response received.* \[({response})\].*'
+    check_integratord_event(file_monitor=file_monitor, callback=generate_monitoring_callback(callback),
+                            error_message="Could not find the expected 'Response received...' event")
+
+
+def get_message_sent(integration, file_monitor):
+    """Gets the message that is being sent to the integration.
+    Args:
+        integration (str): The integration that is being checked. Ex: Slack, Pagerduty and Shuffle
+        file_monitor (FileMonitor): file log monitor to detect events
+    Returns:
+        string: Returns the message JSON string that was sent.
+    """
+    callback = fr'.*Sending message (.*) to {integration} server'
+
+    result = file_monitor.start(timeout=T_10, update_position=True, accum_results=1,
+                                callback=generate_monitoring_callback(callback),
+                                error_message="Could not find the expected 'Sending message...' event").result()
+    return result
