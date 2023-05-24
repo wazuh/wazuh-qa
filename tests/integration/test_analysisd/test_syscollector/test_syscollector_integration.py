b9f270e53b0ce565b9238ffe9a015faa4a1f16c8
quebim <kevin.ledesma@wazuh.com>
2023-05-24 18:23:48 -0300

    fix(#3541): adapt cases to new expected payload

--- tests/integration/test_analysisd/test_syscollector/test_syscollector_integration.py
+++ tests/integration/test_analysisd/test_syscollector/test_syscollector_integration.py
@@ -1,8 +1,35 @@
+'''
+copyright: Copyright (C) 2015-2023, Wazuh Inc.
+           Created by Wazuh, Inc. <info@wazuh.com>.
+           This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+type: integration
+brief: These tests will check if Analysisd handle Syscollector deltas
+       properly by generating alerts.
+components:
+    - analysisd
+suite: syscollector
+targets:
+    - manager
+daemons:
+    - wazuh-analysisd
+os_platform:
+    - linux
+os_version:
+    - Amazon Linux 2
+    - Amazon Linux 1
+    - CentOS 8
+    - CentOS 7
+    - Ubuntu Focal
+    - Ubuntu Bionic
+references:
+    - https://documentation.wazuh.com/current/user-manual/capabilities/syscollector.html\
+        #using-syscollector-information-to-trigger-alerts
+'''
 import os
 import pytest
 
 from wazuh_testing.tools.configuration import get_test_cases_data
-from wazuh_testing.tools import (ANALYSISD_QUEUE_SOCKET_PATH, ALERT_FILE_PATH)
+from wazuh_testing.tools import ANALYSISD_QUEUE_SOCKET_PATH, ALERT_FILE_PATH
 from wazuh_testing.analysis import CallbackWithContext, callback_check_syscollector_alert
 
 pytestmark = [pytest.mark.server]
@@ -11,6 +38,7 @@ pytestmark = [pytest.mark.server]
 TEST_DATA_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
 TEST_CASES_PATH = os.path.join(TEST_DATA_PATH, 'test_cases')
 TEST_RULES_PATH = os.path.join(TEST_DATA_PATH, 'rules')
+
 local_internal_options = {'analysisd.debug': '2'}
 receiver_sockets_params = [(ANALYSISD_QUEUE_SOCKET_PATH, 'AF_UNIX', 'UDP')]
 receiver_sockets = None
@@ -87,7 +115,7 @@ def test_syscollector_integration(metadata, configure_local_internal_options_mod
 
     # Create full message by header and payload concatenation
     test_msg = event_header + metadata['event_payload']
-
+    print(test_msg)
     # Send delta to analysisd queue
     receiver_sockets[0].send(test_msg)
 
