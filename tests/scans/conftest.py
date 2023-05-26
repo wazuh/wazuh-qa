45b6a0d3d782254eaa8130969938c0d34b5f0206
Miguel Verdaguer Velázquez <verdx@riseup.net>
2023-05-26 15:01:36 +0200

    feat(#5139): chagne test_python_flaws to accept both commit and branch in the same argument, reference
    Signed-off-by: Miguel Verdaguer Velázquez <verdx@riseup.net>

--- tests/scans/conftest.py
+++ tests/scans/conftest.py
@@ -3,9 +3,7 @@ DEFAULT_REPOSITORY = 'wazuh'
 
 
 def pytest_addoption(parser):
-    parser.addoption("--branch", action="store", default=DEFAULT_BRANCH,
-                     help=f"Set the repository used. Default: {DEFAULT_REPOSITORY}")
+    parser.addoption("--reference", action="store", default=DEFAULT_BRANCH,
+                     help=f"Set the repository reference(branch or commit). Default: {DEFAULT_REPOSITORY}")
     parser.addoption("--repo", action="store", default=DEFAULT_REPOSITORY,
-                     help=f"Set the repository branch. Default: {DEFAULT_BRANCH}")
-    parser.addoption("--commit", action="store", default=None,
-                     help=f"Set the repository commit. Default: None")
+                     help=f"Set the repository used. Default: {DEFAULT_BRANCH}")
