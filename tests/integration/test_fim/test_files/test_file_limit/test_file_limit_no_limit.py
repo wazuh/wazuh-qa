3261284c7ef0df98da3c5bd54fb0cc1ececac059
Deblintrake09 <amicalizzi2005@gmail.com>
2023-06-05 18:17:14 -0300

    fix(#3963):  update test_file_limit confs

--- tests/integration/test_fim/test_files/test_file_limit/test_file_limit_no_limit.py
+++ tests/integration/test_fim/test_files/test_file_limit/test_file_limit_no_limit.py
@@ -93,9 +93,8 @@ pytestmark = [TIER1]
 test_directories = [os.path.join(PREFIX, 'testdir1')]
 
 directory_str = ','.join(test_directories)
-wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
 test_data_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
-configurations_path = os.path.join(test_data_path, 'wazuh_conf.yaml')
+configurations_path = os.path.join(test_data_path, 'wazuh_conf_no_limit.yaml')
 testdir1 = test_directories[0]
 
 
@@ -156,7 +155,8 @@ def test_file_limit_no_limit(configure_local_internal_options_module, get_config
         - realtime
         - who_data
     '''
-
+    wazuh_log_monitor = FileMonitor(LOG_FILE_PATH)
+    
     wazuh_log_monitor.start(timeout=global_parameters.default_timeout,
                             callback=generate_monitoring_callback(CB_FILE_LIMIT_DISABLED),
                             error_message=ERR_MSG_FILE_LIMIT_DISABLED)
