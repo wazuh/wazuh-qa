bd4784fa822202b25624d8ac4db9552376d7f9ac
Julia <juliamgnr@gmail.com>
2023-07-03 13:07:37 +0200

    feat(#3878): add delete and unassign test cases

--- tests/system/test_cluster/test_agent_groups/test_group_hash.py
+++ tests/system/test_cluster/test_agent_groups/test_group_hash.py
@@ -6,8 +6,8 @@ import pytest
 
 import wazuh_testing as fw
 from system.test_cluster.test_agent_groups.common import register_agent
-from system import (create_new_agent_group, delete_group_of_agents, assign_agent_to_new_group, restart_cluster,
-                    execute_wdb_query)
+from system import (create_new_agent_group, delete_agent_group, assign_agent_to_new_group, restart_cluster,
+                    execute_wdb_query, remove_cluster_agents, unassign_agent_from_group)
 from wazuh_testing.tools.system import HostManager
 
 
@@ -15,13 +15,17 @@ pytestmark = [pytest.mark.cluster, pytest.mark.enrollment_cluster_env]
 
 # Hosts
 test_infra_managers = ["wazuh-master", "wazuh-worker1", "wazuh-worker2"]
-test_infra_agents = ["wazuh-agent1"]
+test_infra_agents = ["wazuh-agent1", "wazuh-agent2"]
 
 inventory_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
                               'provisioning', 'enrollment_cluster', 'inventory.yml')
 data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 host_manager = HostManager(inventory_path)
 local_path = os.path.dirname(os.path.abspath(__file__))
+
+# Global hash query
+command = 'sync-agent-groups-get {"condition": "sync_status", "set_synced": false, "get_global_hash": true}'
+query = f"global '{command}'"
 
 
 # Functions
@@ -66,13 +70,14 @@ def configure_groups(group):
 
     # Delete group
     if group != 'default':
-        delete_group_of_agents(test_infra_managers[0], group, host_manager)
+        delete_agent_group(test_infra_managers[0], group, host_manager)
 
 
 # Tests
+@pytest.mark.parametrize('n_agents', [1, 2])
 @pytest.mark.parametrize('group', ['default', 'multigroup1'])
 @pytest.mark.parametrize('target_node', ['wazuh-master', 'wazuh-worker1'])
-def test_assign_agent_to_a_group(target_node, group, configure_groups, clean_environment):
+def test_group_hash(target_node, group, n_agents, configure_groups, clean_environment):
     '''
     description: Check that when an agent registered in the manager and assigned to a group, the global hash is the
                  the expected one.
@@ -95,16 +100,19 @@ def test_assign_agent_to_a_group(target_node, group, configure_groups, clean_env
         - Verify that the global hash is the same in every node.
     '''
     # Register agent with agent-auth
-    agent_ip, agent_id, agent_name, manager_ip = register_agent(test_infra_agents[0], target_node,
-                                                                host_manager)
+    agents_data = []
+    for agent in range(n_agents):
+        agent_data = register_agent(test_infra_agents[agent], target_node, host_manager)
+        agents_data.append(agent_data)
 
     # Restart agent
-    restart_cluster([test_infra_agents[0]], host_manager)
+    restart_cluster(test_infra_agents, host_manager)
     time.sleep(fw.T_10)
 
     # Assing group for multigroups case
-    if group != 'default':
-        assign_agent_to_new_group(test_infra_managers[0], group, agent_id, host_manager)
+    for agent in range(n_agents):
+        if group != 'default':
+            assign_agent_to_new_group(test_infra_managers[0], group, agents_data[agent][1], host_manager)
     time.sleep(fw.T_10)
 
     # Calculate global hash
@@ -113,8 +121,35 @@ def test_assign_agent_to_a_group(target_node, group, configure_groups, clean_env
 
     for node in test_infra_managers:
         # Get global hash
-        command = 'sync-agent-groups-get {"condition": "sync_status", "set_synced": false, "get_global_hash": true}'
-        query = f"global '{command}'"
         obtained_global_hash = json.loads(execute_wdb_query(query, node, host_manager))[0]['hash']
 
         assert expected_global_hash == obtained_global_hash, f"{node} reported different global group hash"
+
+    if group != 'default':
+        # Unassign one agent from group
+
+        unassign_agent_from_group(test_infra_managers[0], group, agents_data[0][1], host_manager)
+        time.sleep(fw.T_10)
+
+        # Calculate global hash
+        expected_global_hash = calculate_global_hash(test_infra_managers[0], host_manager)
+        assert expected_global_hash is not None, 'No group assigned'
+
+        for node in test_infra_managers:
+            # Get global hash
+            obtained_global_hash = json.loads(execute_wdb_query(query, node, host_manager))[0]['hash']
+            assert expected_global_hash == obtained_global_hash, f"{node} reported different global group hash"
+
+    if n_agents >= 2:
+        # Delete one agent
+        remove_cluster_agents(test_infra_managers[0], [test_infra_agents[0]], host_manager, [agents_data[0][1]])
+        time.sleep(fw.T_10)
+
+        # Calculate global hash
+        expected_global_hash = calculate_global_hash(test_infra_managers[0], host_manager)
+        assert expected_global_hash is not None, 'No group assigned'
+
+        for node in test_infra_managers:
+            # Get global hash
+            obtained_global_hash = json.loads(execute_wdb_query(query, node, host_manager))[0]['hash']
+            assert expected_global_hash == obtained_global_hash, f"{node} reported different global group hash"
