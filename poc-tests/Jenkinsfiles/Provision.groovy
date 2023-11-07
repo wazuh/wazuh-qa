a4efc3eca7fca56d5bbe689d72aff06bd6e57bef
fcaffieri <federico.caffieri@wazuh.com>
2023-11-07 12:31:26 -0300

    Add new ips into inventory for testing purpose, and add JENKINS_REFERENCE parameter to pipeline

--- poc-tests/Jenkinsfiles/Provision.groovy
+++ poc-tests/Jenkinsfiles/Provision.groovy
@@ -3,13 +3,14 @@
 String provision_path = "scripts/provision"
 String provision_script = "provisioner.py"
 String inventory = "inventory.yaml"
+String jenkins_reference = params.getOrDefault('JENKINS_REFERENCE', 'enhancement/4665-dtt1-poc')
 
 // Jenkinsfile
 
 node {
 
   stage('Clone Repo') {
-    git branch: 'enhancement/4524-dtt1-poc', url: 'https://github.com/wazuh/wazuh-qa.git'
+    git branch: ${JENKINS_REFERENCE}, url: 'https://github.com/wazuh/wazuh-qa.git'
   }
 
   stage('Provision') {
