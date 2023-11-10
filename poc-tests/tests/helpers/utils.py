6c88f70d31fc20ba85bba7b6248095183d9fe713
quebim <kevin.ledesma@wazuh.com>
2023-11-10 10:17:08 -0300

    Add get_service function to get the current wazuh service

--- poc-tests/tests/helpers/utils.py
+++ poc-tests/tests/helpers/utils.py
@@ -3,22 +3,34 @@ import subprocess
 from constants import WAZUH_CONTROL
 
 
-def get_services_status() -> dict:
+def get_service() -> str:
     """
-    Get the status of the Wazuh services.
+    Retrieves the name of the Wazuh service running on the current platform.
+
+    Returns:
+        str: The name of the Wazuh service.
+
+    """
+    control_output = subprocess.check_output([WAZUH_CONTROL, "info", "-t"], stderr=subprocess.PIPE)
+    return control_output.decode('utf-8').strip()
+
+
+def get_daemons_status() -> dict:
+    """
+    Get the status of the Wazuh daemons.
     
     Return: 
-        dict: The services (keys) and their status(values).
+        dict: The daemons (keys) and their status(values).
     """
-    services_status = {}
+    daemons_status = {}
 
     control_output = subprocess.run([WAZUH_CONTROL, "status"], stdout=subprocess.PIPE)
     control_output_decoded = control_output.stdout.decode('utf-8')
 
     for line in control_output_decoded.split('\n'):
         if "running" in line:
-            service_name = line.split(' ')[0]
-            status = line.replace(service_name, '').replace('.', '').lstrip()
-            services_status[service_name] = status
+            daemon_name = line.split(' ')[0]
+            status = line.replace(daemon_name, '').replace('.', '').lstrip()
+            daemons_status[daemon_name] = status
 
-    return services_status
+    return daemons_status
