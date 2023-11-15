38268e39c971e5f00c1a27be030194a33f6679cf
quebim <kevin.ledesma@wazuh.com>
2023-11-15 15:35:21 -0300

    Add test_stop python testfile and playbook with its provision

---
+++ poc-tests/tests/test_stop.py
@@ -0,0 +1,18 @@
+from helpers import utils
+from helpers.constants import DELETING_RESPONSES, RELEASING_RESOURCES, WAZUH_LOG
+
+
+# Actual running service.
+service = utils.get_service()
+
+
+def test_release_resources_shutdown_log_raised():
+    assert utils.file_monitor(WAZUH_LOG, RELEASING_RESOURCES), "Release resources log not found."
+
+
+def test_deleting_responses_shutdown_log_raised():
+    assert utils.file_monitor(WAZUH_LOG, DELETING_RESPONSES), "Deleting responses log not found."
+
+
+def test_service_started():
+    assert utils.get_service_status() == "inactive", "Service is active."
