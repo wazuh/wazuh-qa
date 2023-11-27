3d269841bc0403304bc5f076f6472ab027843675
fcaffieri <federico.caffieri@wazuh.com>
2023-11-27 10:23:30 -0300

    Fix venv path

--- poc-tests/scripts/provision/provision.py
+++ poc-tests/scripts/provision/provision.py
@@ -57,9 +57,7 @@ def run(provision):
 def install_dependencies():
   venv_path = 'venv'
   if not os.path.exists(venv_path):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "virtualenv"])
-    subprocess.check_call([sys.executable, "-m", "virtualenv", venv_path])
-
+      subprocess.run(['python3', '-m', 'venv', venv_path], check=True)
   activate_script = os.path.join(venv_path, 'bin', 'activate')
   activate_command = f"source {activate_script}" if sys.platform != 'win32' else f"call {activate_script}"
   subprocess.run(activate_command, shell=True)
