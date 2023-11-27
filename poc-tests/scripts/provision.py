cd3937a3dfe0e0ba7165b9cba38f86ebc103351d
fcaffieri <federico.caffieri@wazuh.com>
2023-11-27 17:49:51 -0300

    Fix activate virtual env into jenkins node

--- poc-tests/scripts/provision.py
+++ poc-tests/scripts/provision.py
@@ -60,7 +60,7 @@ def install_dependencies():
       subprocess.run(['python3', '-m', 'venv', venv_path], check=True)
   activate_script = os.path.join(venv_path, 'bin', 'activate')
   activate_command = f"source {activate_script}" if sys.platform != 'win32' else f"call {activate_script}"
-  subprocess.run(activate_command, shell=True)
+  subprocess.run(activate_command, shell=True, executable="/bin/bash")
   subprocess.run(['python3', '-m', 'pip', 'install', '--upgrade', 'pip'], check=True)
   subprocess.run(['pip', 'install', '-r', 'utils/requirements.txt'], check=True)
 
