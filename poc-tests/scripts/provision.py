efa52b227cef8bea152e47a509e983b22711a8e7
fcaffieri <federico.caffieri@wazuh.com>
2023-11-27 17:39:17 -0300

    Fix activate virtual env into jenkins node

--- poc-tests/scripts/provision.py
+++ poc-tests/scripts/provision.py
@@ -55,11 +55,7 @@ def run(provision):
 # ----------------------------------------------
 
 def install_dependencies():
-  #try:
-  #    subprocess.check_call(["apt-get", "install", "pip", "python3.10-venv", "-y"])
-  #except subprocess.CalledProcessError:
-  #    print(f"Package pip already installed")
-  venv_path = 'venv'
+  venv_path = CURRENT_DIR + 'venv'
   if not os.path.exists(venv_path):
       subprocess.run(['python3', '-m', 'venv', venv_path], check=True)
   activate_script = os.path.join(venv_path, 'bin', 'activate')
