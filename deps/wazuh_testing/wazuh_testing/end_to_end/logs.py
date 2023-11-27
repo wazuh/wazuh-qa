e0a7933b090ab581352ed7785e3a725ad3a6cd65
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2023-11-27 11:26:52 +0000

    refac: logs E2E module
    Co-authored-by Julia Magan <juliamgnr@gmail.com>

--- deps/wazuh_testing/wazuh_testing/end_to_end/logs.py
+++ deps/wazuh_testing/wazuh_testing/end_to_end/logs.py
@@ -6,62 +6,35 @@ Description:
     This module provides functions for truncating logs and alerts for Wazuh agents and managers.
 
 Functions:
-    - truncate_agents_logs: Truncate logs for Wazuh agents.
-    - truncate_managers_logs: Truncate logs for Wazuh managers.
-    - truncate_logs: Truncate logs for both Wazuh agents and managers.
-    - truncate_alerts: Truncate Wazuh alerts.
+    - truncate_remote_host_group_files: Truncate the specified files in all the host of a group
 
 
 Copyright (C) 2015, Wazuh Inc.
 Created by Wazuh, Inc. <info@wazuh.com>.
 This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
 """
-
+from wazuh_testing import ALERTS_JSON_PATH
 from wazuh_testing.end_to_end import logs_filepath_os
 from wazuh_testing.tools.system import HostManager
 
 
-def truncate_agents_logs(host_manager: HostManager) -> None:
+def truncate_remote_host_group_files(host_manager: HostManager, host_group: str,
+                                     file_to_truncate: str='logs'):
     """
-    Truncate logs for Wazuh agents.
+    Truncate log or alert files on remote hosts in a specified host group.
 
-    Args:
-        host_manager: An instance of the HostManager class containing information about hosts.
+    Parameters:
+    - host_manager (HostManager): An instance of the HostManager class for managing remote hosts.
+    - host_group (str): The name of the host group where the files will be truncated.
+    - file_to_truncate (str, optional): The type of file to truncate. Default is 'logs'.
+      Possible values are 'logs' for log files or 'alerts' for alert files.
     """
-    for agent in host_manager.get_group_hosts('agent'):
-        host_os_name = host_manager.get_host_variables(agent)['os_name']
-        host_manager.truncate_file(agent, logs_filepath_os[host_os_name])
+    for host in host_manager.get_group_hosts(host_group):
+        log_file_path = None
+        if file_to_truncate == 'logs':
+            host_os_name = host_manager.get_host_variables(host)['os_name']
+            log_file_path = logs_filepath_os[host_os_name]
+        elif file_to_truncate == 'alerts':
+            log_file_path = ALERTS_JSON_PATH
 
-
-def truncate_managers_logs(host_manager: HostManager) -> None:
-    """
-    Truncate logs for Wazuh managers.
-
-    Args:
-        host_manager: An instance of the HostManager class containing information about hosts.
-    """
-    for manager in host_manager.get_group_hosts('manager'):
-        host_os_name = host_manager.get_host_variables(manager)['os_name']
-        host_manager.truncate_file(manager, logs_filepath_os[host_os_name])
-
-
-def truncate_logs(host_manager: HostManager) -> None:
-    """
-    Truncate logs for both Wazuh agents and managers.
-
-    Args:
-        host_manager: An instance of the HostManager class containing information about hosts.
-    """
-    truncate_managers_logs(host_manager)
-    truncate_agents_logs(host_manager)
-
-
-def truncate_alerts(host_manager: HostManager) -> None:
-    """
-    Truncate Wazuh alerts.
-
-    Args:
-        host_manager: An instance of the HostManager class containing information about hosts.
-    """
-    for manager in host_manager.get_group_hosts('manager'):
-        host_manager.truncate_file(manager, '/var/ossec/logs/alerts/alerts.json')
+        host_manager.truncate_file(host, log_file_path)
