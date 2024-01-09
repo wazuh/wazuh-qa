4dd4e52cd83f7db8862deed88736f09492fece3a
fcaffieri <federico.caffieri@wazuh.com>
2024-01-08 22:29:13 -0300

    Fix task of tasjflow, fix provision launcher, fix shcema validator, fix yaml example for PoC

--- deployability/launchers/provision.py
+++ deployability/launchers/provision.py
@@ -1,7 +1,8 @@
-# Python
 import argparse
 import os
 import sys
+import ast
+import json
 
 # ---------------- Vars ------------------------
 
@@ -12,11 +13,13 @@ from modules.provision import Provision, models
 
 # ---------------- Methods ---------------------
 
+
 def parse_arguments():
   parser = argparse.ArgumentParser(description="Provision infraestructure tool")
   parser.add_argument("--inventory-agent", required=False, help="Inventory with agent host information")
   parser.add_argument("--inventory-manager", required=False, help="Inventory with manager host information")
-  parser.add_argument("--install", type=str, required=False, help='Componentes to be installed')
+  parser.add_argument('--install', type=lambda x: ast.literal_eval(x), default=[],
+                        help='List of dictionaries for installation.')
   parser.add_argument("--custom-credentials", required=False, default=None)
   return parser.parse_args()
 
--- deployability/launchers/workflow_engine.py
+++ deployability/launchers/workflow_engine.py
@@ -10,7 +10,7 @@ import colorlog
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(project_root)
 from modules.workflow_engine.workflow_processor import WorkflowProcessor
-from modules import SchemaValidator
+from modules.generic import SchemaValidator
 
 
 
