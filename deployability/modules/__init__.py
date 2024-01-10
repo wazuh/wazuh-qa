e730a7ab36f8a496df3eeeb88328d0778a85fef0
fcaffieri <federico.caffieri@wazuh.com>
2024-01-10 00:17:30 -0300

    Solve conflixts with enhancement/4751-dtt1-iteration-2-poc

--- deployability/modules/__init__.py
+++ deployability/modules/__init__.py
@@ -2,8 +2,4 @@ from .provision import Provision
 from .generic import Ansible
 from .allocation.vagrant import VagrantProvider
 from .allocation.aws import AWSProvider
-<<<<<<< HEAD
 from .generic import SchemaValidator
-=======
-from .generic import SchemaValidator
->>>>>>> enhancement/4751-dtt1-iteration-2-poc
