7e0f2b20c5a551c805aaf6cb0a291a136f4c7987
quebim <kevin.ledesma@wazuh.com>
2024-01-31 15:32:11 -0300

    Fix path to logger config file

--- deployability/modules/generic/logger/logger.py
+++ deployability/modules/generic/logger/logger.py
@@ -1,5 +1,8 @@
 import logging
+import logging.config
 import yaml
+
+from pathlib import Path
 
 
 class Logger:
@@ -24,7 +27,8 @@ class Logger:
         """
         Loads the logging configuration from 'config.yaml' file.
         """
-        with open('config.yaml', 'r') as f:
+        config_path = Path(__file__).parent / 'config.yaml'
+        with open(config_path, 'r') as f:
             config = yaml.safe_load(f.read())
             logging.config.dictConfig(config)
 
