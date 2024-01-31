6b04d000f4d9f39c0afba504d64a5a7ef0cff551
quebim <kevin.ledesma@wazuh.com>
2024-01-31 16:01:24 -0300

    Styling improvements

--- deployability/modules/generic/utils.py
+++ deployability/modules/generic/utils.py
@@ -1,44 +1,47 @@
-import os, subprocess, sys
-from pathlib import Path
+import os
 import yaml
 
+from pathlib import Path
+
+
 class Utils:
-  @staticmethod
-  def get_template_list(path, custom_order=None) -> list[Path]:
-    """
-    Get the list of templates in the path.
+    @staticmethod
+    def get_template_list(path, custom_order=None) -> list[Path]:
+        """
+        Get the list of templates in the path.
 
-    Args:
-        path: Path to the templates.
-    """
-    list_tasks = []
-    with os.scandir(path) as entries:
-        for entry in entries:
-            if entry.is_file():
-                list_tasks.append(entry.name)
+        Args:
+            path: Path to the templates.
+        """
+        list_tasks = []
+        with os.scandir(path) as entries:
+            for entry in entries:
+                if entry.is_file():
+                    list_tasks.append(entry.name)
 
-    if custom_order:
-        sorted_list = sorted(list_tasks, key=lambda x: custom_order.index(x) if x in custom_order else float('inf'))
-    else:
-      sorted_list = list_tasks
+        if custom_order:
+            sorted_list = sorted(list_tasks, key=lambda x: custom_order.index(
+                x) if x in custom_order else float('inf'))
+        else:
+            sorted_list = list_tasks
 
-    return sorted_list
+        return sorted_list
 
-  @staticmethod
-  def load_from_yaml(file_path, map_keys=None, specific_key=None):
-    """
-    Load data from a yaml file.
+    @staticmethod
+    def load_from_yaml(file_path, map_keys=None, specific_key=None):
+        """
+        Load data from a yaml file.
 
-    Args:
-        file_path: Path to the yaml file.
-        map_keys: Map of keys to change.
-    """
-    data = yaml.safe_load(open(file_path))
+        Args:
+            file_path: Path to the yaml file.
+            map_keys: Map of keys to change.
+        """
+        data = yaml.safe_load(open(file_path))
 
-    if map_keys:
-      data = {map_keys[k]: v for k, v in data.items() if k in map_keys}
+        if map_keys:
+            data = {map_keys[k]: v for k, v in data.items() if k in map_keys}
 
-    if specific_key:
-      return data.get(specific_key)
+        if specific_key:
+            return data.get(specific_key)
 
-    return data
+        return data
