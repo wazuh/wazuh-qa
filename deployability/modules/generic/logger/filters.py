92d9d29f6500a7248b463ac8b7c7c45e8cbae4b7
quebim <kevin.ledesma@wazuh.com>
2024-02-01 16:57:56 -0300

    Add docstrings

--- deployability/modules/generic/logger/filters.py
+++ deployability/modules/generic/logger/filters.py
@@ -2,6 +2,18 @@ import logging
 
 
 class UppercaseNameFilter(logging.Filter):
-    def filter(self, record):
+    """
+    A filter that uppercases the name of the log record.
+    """
+    def filter(self, record: str) -> bool:
+        """
+        Filters the log record to uppercase the name.
+        
+        Args:
+            record (LogRecord): The log record to filter.
+        
+        Returns:
+            bool: True if the record should be logged, False otherwise.
+        """
         record.name = record.name.upper()
         return True
