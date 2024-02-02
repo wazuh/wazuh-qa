faec511aca86f9f1a4d620a1bc1e87d271744377
marcos <marcos.sanchez@wazuh.com>
2024-02-02 12:56:19 +0100

    fix: Kibana references removed

--- deps/wazuh_testing/wazuh_testing/scripts/simulate_api_load.py
+++ deps/wazuh_testing/wazuh_testing/scripts/simulate_api_load.py
@@ -11,16 +11,12 @@ def get_arguments():
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-f', '--foreground', dest='foreground', action='store_true', default=False,
                         help='Enable logging in foreground mode')
-    parser.add_argument('-fr', '--frequency', dest='frequency', action='store', default=60, type=int,
-                        help='Insert Kibana API requests interval')
     parser.add_argument('-t', '--time', dest='time', action='store', default=60, type=int, required=True,
                         help='Time in seconds for the simulation')
     parser.add_argument('-l', '--log-path', dest='log_path', action='store', default='/tmp/wazuh_api_simulator.log',
                         required=True, type=str, help='Log path file destination')
     parser.add_argument('-c', '--configuration', dest='configuration', action='store', required=True, type=str,
                         help='Path to the configuration file')
-    parser.add_argument('-kt', '--kibana-template', dest='kibana_template', action='store', required=True, type=str,
-                        help='Path to the Kibana request template')
     parser.add_argument('-et', '--extraload-template', dest='extraload_template', action='store', required=True,
                         type=str, help='Path to the ExtraLoad request template')
 
@@ -55,17 +51,6 @@ def main():
             thread_list.append(extra_load_thread)
         except Exception as extra_exception:
             extra_logger.error(f'Unhandled exception: {extra_exception}')
-
-    if configuration['kibana']['enabled']:
-        kibana_logger = CustomLogger('kibana_thread', file_path=options.log_path, foreground=options.foreground,
-                                     tag='Kibana').get_logger()
-        try:
-            kibana_thread = APISimulator(HOST, PORT, request_template=options.kibana_template,
-                                         frequency=options.frequency, external_logger=kibana_logger)
-            kibana_thread.start()
-            thread_list.append(kibana_thread)
-        except Exception as kibana_exception:
-            kibana_logger.error(f'Unhandled exception: {kibana_exception}')
 
     sleep(options.time)
     for thread in thread_list:
