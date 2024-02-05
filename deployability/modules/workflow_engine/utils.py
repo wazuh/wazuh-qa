bb20c1c0513cc6e91ad1313903dcedf8a07543b4
quebim <kevin.ledesma@wazuh.com>
2024-02-05 09:10:45 -0300

    Update workflow_engine removing its dependency from the generic module

--- deployability/modules/workflow_engine/utils.py
+++ deployability/modules/workflow_engine/utils.py
@@ -1,3 +1,6 @@
-from modules.generic.logger import Logger
+import logging
 
-logger = Logger("workflow_enine")
+# The logging module will use the generic logger anyway
+# that is because the logging module is a singleton.
+# So, we can just use it this way here, and it will work.
+logger = (lambda: logging.getLogger("workflow_engine"))()
