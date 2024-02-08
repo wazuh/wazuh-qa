54f71104fbdf3d4e8f585315f7154665a5c87316
quebim <kevin.ledesma@wazuh.com>
2024-02-08 09:56:35 -0300

    Styling and docstrings to install.py file

--- deployability/modules/provision/install.py
+++ deployability/modules/provision/install.py
@@ -1,88 +1,103 @@
 from modules.generic import Ansible
 from abc import ABC, abstractmethod
 
+
 class Install(ABC):
-
-  def __init__(self, ansible_data, component_information):
-    self.ansible = Ansible(ansible_data)
-    self.install_type = component_information.get('install_type')
-
-    provision_template_path = ''
-    list_template_order = ''
-
-    match self.install_type:
-      case "package":
-        provision_template_path = 'provision/wazuh/package'
-        list_template_order = ["set_repo.j2", "install.j2", "register.j2", "service.j2"]
-      case "aio":
-        provision_template_path = 'provision/wazuh/aio'
-        list_template_order = ["download.j2", "install.j2", "register", "service"]
-      case "deps":
-        provision_template_path = 'provision/deps'
-      case _ :
-        provision_template_path = 'provision/generic'
-
-    component_information["templates_path"] = provision_template_path
-    component_information["list_template_order"] = list_template_order
-
-    self.component_information = component_information
-
-  @abstractmethod
-  def install_component(self, ansible_data, playbooks_variables):
     """
-    Install component on host.
-
-    Args:
-        ansible_data: Data with the ansible configuration.
-        info_component_install: Data with the installation configuration.
+    Install class used to install a component on a host.
     """
-    pass
 
-  @abstractmethod
-  def set_playbooks_variables(self, install_info):
-    """
-    Set extra variables for the installation.
+    def __init__(self, ansible_data: dict, component_information: dict) -> None:
+        """
+        Initialize the Install class.
 
-    Args:
-        install_info: Data with the installation configuration.
-    """
-    pass
+        Args:
+            ansible_data: Data with the ansible configuration.
+            component_information: Data with the installation configuration.
+        """
+        self.ansible = Ansible(ansible_data)
+        self.install_type = component_information.get('install_type')
+
+        list_template_order = ''
+
+        match self.install_type:
+            case "package":
+                provision_template_path = 'provision/wazuh/package'
+                list_template_order = ["set_repo.j2",
+                                       "install.j2", "register.j2", "service.j2"]
+            case "aio":
+                provision_template_path = 'provision/wazuh/aio'
+                list_template_order = ["download.j2",
+                                       "install.j2", "register", "service"]
+            case "deps":
+                provision_template_path = 'provision/deps'
+            case _:
+                provision_template_path = 'provision/generic'
+
+        component_information["templates_path"] = provision_template_path
+        component_information["list_template_order"] = list_template_order
+
+        self.component_information = component_information
+
+    @abstractmethod
+    def install_component(self, ansible_data: dict, playbooks_variables: dict) -> dict:
+        """
+        Install component on host.
+
+        Args:
+            ansible_data: Data with the ansible configuration.
+            info_component_install: Data with the installation configuration.
+        """
+        pass
+
+    @abstractmethod
+    def set_playbooks_variables(self, install_info: dict) -> dict:
+        """
+        Set extra variables for the installation.
+
+        Args:
+            install_info: Data with the installation configuration.
+        """
+        pass
+
 
 class InstallComponent(Install):
-
-  def install_component(self):
     """
-    Install component on host.
-
-    Args:
-        ansible_data: Data with the ansible configuration.
-        info_component_install: Data with the installation configuration.
+    InstallComponent class used to install a component on a host.
     """
-    status = {}
 
-    #vars = self.set_playbooks_variables(playbooks_variables)
+    def install_component(self):
+        """
+        Install component on host.
 
-    tasks = self.ansible.render_playbooks(self.component_information)
+        Args:
+            ansible_data: Data with the ansible configuration.
+            info_component_install: Data with the installation configuration.
+        """
+        status = {}
 
-    playbook = {
-      'hosts': self.ansible.ansible_host,
-      'become': True,
-      'gather_facts': True,
-      'tasks': tasks
-    }
+        # vars = self.set_playbooks_variables(playbooks_variables)
 
-    status = self.ansible.run_playbook(playbook)
+        tasks = self.ansible.render_playbooks(self.component_information)
 
-    return status
+        playbook = {
+            'hosts': self.ansible.ansible_host,
+            'become': True,
+            'gather_facts': True,
+            'tasks': tasks
+        }
 
-  def set_playbooks_variables(self, vars):
-    """
-    Set extra variables for the installation.
+        status = self.ansible.run_playbook(playbook)
 
-    Args:
-        install_info: Data with the installation configuration.
-    """
-    variables_values = {}
+        return status
 
+    def set_playbooks_variables(self, vars):
+        """
+        Set extra variables for the installation.
 
-    return variables_values
+        Args:
+            install_info: Data with the installation configuration.
+        """
+        variables_values = {}
+
+        return variables_values
