4b44fa77c305161861640bc86f5b6b61e53b1a98
Antonio <antonio.kim@wazuh.com>
2024-02-12 16:05:23 +0100

    enhacement(#4858): removing some prints and fixture comments

--- deployability/modules/provision/provision.py
+++ deployability/modules/provision/provision.py
@@ -47,7 +47,6 @@ class Provision(ProvisionModule):
   
 
     for item in self.component_info:
-      #print(item)
       action_class = Action(self.action, item, self.ansible_data)
       status = action_class.execute()
 
