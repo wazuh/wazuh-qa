d2f6a993be6c0485689b25f5cf0126461b984003
c-bordon <carlos.e.bordon@gmail.com>
2024-02-14 17:03:33 -0300

    Added custom key support for Vagrant deploy

--- deployability/modules/allocation/vagrant/provider.py
+++ deployability/modules/allocation/vagrant/provider.py
@@ -23,7 +23,7 @@ class VagrantProvider(Provider):
     provider_name = 'vagrant'
 
     @classmethod
-    def _create_instance(cls, base_dir: Path, params: CreationPayload, config: VagrantConfig = None, public_key: str = None) -> VagrantInstance:
+    def _create_instance(cls, base_dir: Path, params: CreationPayload, config: VagrantConfig = None, ssh_key: str = None) -> VagrantInstance:
         """
         Creates a Vagrant instance.
 
@@ -31,7 +31,7 @@ class VagrantProvider(Provider):
             base_dir (Path): The base directory for the instance.
             params (CreationPayload): The parameters for instance creation.
             config (VagrantConfig, optional): The configuration for the instance. Defaults to None.
-            public_key (str, optional): Public key for the instance. Defaults to None.
+            ssh_key (str, optional): Public or private key for the instance. For example, we assume that if the public key is provided, the private key is located in the same directory and has the same name as the public key. Defaults to None.
 
         Returns:
             VagrantInstance: The created Vagrant instance.
@@ -44,11 +44,12 @@ class VagrantProvider(Provider):
         if not config:
             logger.debug(f"No config provided. Generating from payload")
             # Keys.
-            if not public_key:
+            if not ssh_key:
                 logger.debug(f"Generating new key pair")
                 credentials.generate(instance_dir, 'instance_key')
             else:
-                logger.debug(f"Using provided public key")
+                logger.debug(f"Using provided key pair")
+                public_key = credentials.ssh_key_interpreter(ssh_key)
                 credentials.load(public_key)
             # Parse the config if it is not provided.
             config = cls.__parse_config(params, credentials, instance_id)
@@ -75,7 +76,7 @@ class VagrantProvider(Provider):
         return VagrantInstance(instance_dir, identifier)
 
     @classmethod
-    def _destroy_instance(cls, instance_dir: Path, identifier: str) -> None:
+    def _destroy_instance(cls, instance_dir: Path, identifier: str, key_path: str) -> None:
         """
         Destroys a Vagrant instance.
 
@@ -87,6 +88,8 @@ class VagrantProvider(Provider):
             None
         """
         instance = VagrantInstance(instance_dir, identifier)
+        if os.path.dirname(key_path) != str(instance_dir):
+            logger.debug(f"The key {key_path} will not be deleted. It is the user's responsibility to delete it.")
         logger.debug(f"Destroying instance {identifier}")
         instance.delete()
 
