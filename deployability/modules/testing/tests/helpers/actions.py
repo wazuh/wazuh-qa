0fe9d6bee017248ba6b96d93def5dfe38859f3b6
Antonio <antonio.kim@wazuh.com>
2024-02-15 10:14:38 +0100

    enhacement(#4943): Adding install and chefiles methods still in dev

---
+++ deployability/modules/testing/tests/helpers/actions.py
@@ -0,0 +1,312 @@
+from . import utils
+import time
+import subprocess
+from pathlib import Path
+import subprocess
+
+def install_wazuh_agent(os_type, wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip, type_os=None, architecture=None):
+    """
+    Install Wazuh agent based on the provided OS type and parameters.
+
+    Args:
+        os_type (str): The target operating system ('linux', 'windows', 'macos').
+        wazuh_version (str): The version of Wazuh agent.
+        wazuh_revision (str): The revision of Wazuh agent.
+        aws_s3 (str): AWS S3 base URL.
+        repository (str): Wazuh repository URL.
+        dependency_ip (str): IP address of the Wazuh manager.
+        type_os (str): Type of linux os (rpm, deb).
+        architecture: (str): Type of architecture (aarch64, amd64, intel, apple).
+
+    Returns:
+        None
+    """
+    if os_type == 'linux':
+        install_linux_agent(wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip, type_os, architecture)
+    elif os_type == 'windows':
+        install_windows_agent(wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip)
+    elif os_type == 'macos':
+        install_macos_agent(wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip, architecture)
+    else:
+        print("Unsupported operating system.")
+
+def install_linux_agent(wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip, type_os, architecture):
+    """
+    Install Wazuh agent on Linux.
+
+    Args:
+        wazuh_version (str): The version of Wazuh agent.
+        wazuh_revision (str): The revision of Wazuh agent.
+        aws_s3 (str): AWS S3 base URL.
+        repository (str): Wazuh repository URL.
+        dependency_ip (str): IP address of the Wazuh manager.
+        type_os (str): Type of linux os (rpm, deb).
+        architecture: (str): Type of architecture (aarch64, amd64, intel, apple).
+
+    Returns:
+        None
+    """
+
+    # Define common parts of the URL and file names
+    base_url = f"https://{aws_s3}/{repository}/yum/wazuh-agent-{wazuh_version}-{wazuh_revision}"
+
+    # Map architectures to their corresponding suffixes
+    architecture_suffix = {'amd64': 'x86_64', 'aarch64': 'aarch64'}
+
+    # Construct URL, download, and install commands
+    url = f"{base_url}.{architecture_suffix.get(architecture)}.rpm"
+    download_command = f'wget {url} -O wazuh-agent_{wazuh_version}-{wazuh_revision}.{architecture}.rpm'
+    install_command = f"sudo WAZUH_MANAGER='{dependency_ip}' rpm -ihv wazuh-agent-{wazuh_version}-{wazuh_revision}.{architecture}.rpm"
+
+    # Adjust for Debian-based systems
+    if type_os == 'deb':
+        architecture_suffix['amd64'] = 'amd64'
+        url = f"https://{aws_s3}.wazuh.com/{repository}/apt/pool/main/w/wazuh-agent/wazuh-agent_{wazuh_version}-{wazuh_revision}_{architecture_suffix.get(architecture)}.deb"
+        download_command = f'wget {url} -O wazuh-agent_{wazuh_version}-{wazuh_revision}_{architecture}.deb'
+        install_command = f"sudo WAZUH_MANAGER='{dependency_ip}' dpkg -i ./wazuh-agent_{wazuh_version}-{wazuh_revision}_{architecture}.deb"
+
+    # Download agent
+    try:
+        subprocess.run(download_command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar el comando: {e}")
+
+    time.sleep(2)
+    # Install agent
+    #subprocess.run("tree", shell=True, check=True)
+    try:
+        subprocess.run(install_command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing the command: {e}")
+
+    # Post-installation steps for all Linux OS
+    post_install_commands = [
+        "sudo systemctl daemon-reload",
+        "sudo systemctl enable wazuh-agent",
+        "sudo systemctl start wazuh-agent"
+    ]
+
+    for command in post_install_commands:
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error executing the command: {e}")
+
+def install_windows_agent(wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip):
+    """
+    Install Wazuh agent on Windows.
+
+    Args:
+        wazuh_version (str): The version of Wazuh agent.
+        wazuh_revision (str): The revision of Wazuh agent.
+        aws_s3 (str): AWS S3 base URL.
+        repository (str): Wazuh repository URL.
+        dependency_ip (str): IP address of the Wazuh manager.
+
+    Returns:
+        None
+    """
+    # Replace placeholders in the command with actual values
+    install_command = f"Invoke-WebRequest -Uri {aws_s3}/{repository}/windows/wazuh-agent-{wazuh_version}-{wazuh_revision}.msi -OutFile $env:tmp\\wazuh-agent; msiexec.exe /i $env:tmp\\wazuh-agent /q WAZUH_MANAGER='{dependency_ip}' WAZUH_REGISTRATION_SERVER='{dependency_ip}'"
+
+    # Run installation command
+    utils.run_command(install_command)
+
+    # Post-installation steps for all Windows OS
+    post_install_command = "NET START WazuhSvc"
+    utils.run_command(post_install_command)
+
+def install_macos_agent(wazuh_version, wazuh_revision, aws_s3, repository, dependency_ip, architecture):
+    """
+    Install Wazuh agent on MacOS.
+
+    Args:
+        wazuh_version (str): The version of Wazuh agent.
+        wazuh_revision (str): The revision of Wazuh agent.
+        aws_s3 (str): AWS S3 base URL.
+        repository (str): Wazuh repository URL.
+        dependency_ip (str): IP address of the Wazuh manager.
+        architecture: (str): Type of architecture (aarch64, amd64, intel, apple).
+
+    Returns:
+        None
+    """
+    # Replace placeholders in the commands with actual values
+    if architecture == 'Intel':
+        command = f"curl -so wazuh-agent.pkg {aws_s3}/{repository}/macos/wazuh-agent-{wazuh_version}-{wazuh_revision}.intel64.pkg && echo 'WAZUH_MANAGER='{dependency_ip}'' > /tmp/wazuh_envs && sudo installer -pkg ./wazuh-agent.pkg -target /"
+    elif architecture == 'Apple':
+        command = f"curl -so wazuh-agent.pkg {aws_s3}/{repository}/macos/wazuh-agent-{wazuh_version}-{wazuh_revision}.arm64.pkg && echo 'WAZUH_MANAGER='{dependency_ip}'' > /tmp/wazuh_envs && sudo installer -pkg ./wazuh-agent.pkg -target /"
+
+    # Run installation commands
+    utils.run_command(command)
+
+    # Post-installation steps for all MacOS OS
+    post_install_command = "sudo /Library/Ossec/bin/wazuh-control start"
+    utils.run_command(post_install_command)
+
+def uninstall_wazuh_agent(os_type, wazuh_version, wazuh_revision, type_os):
+    """
+    Uninstall Wazuh agent based on the provided OS type and parameters.
+
+    Args:
+        os_type (str): The target operating system ('linux', 'windows', 'macos').
+        wazuh_version (str): The version of Wazuh agent.
+        wazuh_revision (str): The revision of Wazuh agent.
+        type_os (str): Type of linux os (rpm, deb).
+
+    Returns:
+        None
+    """
+    if os_type == 'linux':
+        uninstall_linux_agent(type_os)
+    elif os_type == 'windows':
+        uninstall_windows_agent(wazuh_version, wazuh_revision)
+    elif os_type == 'macos':
+        uninstall_macos_agent()
+    else:
+        print("Unsupported operating system.")
+
+def uninstall_linux_agent(type_os):
+    """
+    Uninstall Wazuh agent on Linux.
+
+    type_os (str): Type of linux os (rpm, deb).
+    Returns:
+        None
+    """
+    # Linux uninstallation commands
+    
+    if type_os == 'rpm':
+        uninstall_commands = ["yum remove wazuh-agent"]
+    elif type_os == 'deb':
+        uninstall_commands = [
+        "sudo apt-get remove -y wazuh-agent",
+        "sudo apt-get remove -y --purge wazuh-agent"
+        ]
+
+    # Run uninstallation commands
+    for command in uninstall_commands:
+        print(command)
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error al ejecutar el comando: {e}")
+        
+    # Post-uninstallation steps for all Linux OS
+    post_uninstall_commands = [
+        "systemctl disable wazuh-agent",
+        "systemctl daemon-reload"
+    ]
+
+    # Run post-uninstallation commands
+    for command in post_uninstall_commands:
+        try:
+            subprocess.run(command, shell=True, check=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Error al ejecutar el comando: {e}")
+
+def uninstall_windows_agent(wazuh_version, wazuh_revision):
+    """
+    Uninstall Wazuh agent on Windows.
+
+    Args:
+        wazuh_version (str): The version of Wazuh agent.
+        wazuh_revision (str): The revision of Wazuh agent.
+
+    Returns:
+        None
+    """
+    # Replace placeholders in the command with actual values
+    uninstall_command = f"msiexec.exe /x wazuh-agent-{wazuh_version}-{wazuh_revision}.msi /qn"
+
+    # Run uninstallation command
+    utils.run_command(uninstall_command)
+
+def uninstall_macos_agent():
+    """
+    Uninstall Wazuh agent on MacOS.
+
+    Returns:
+        None
+    """
+    # MacOS uninstallation commands
+    uninstall_commands = [
+        "/Library/Ossec/bin/wazuh-control stop",
+        "/bin/rm -r /Library/Ossec",
+        "/bin/launchctl unload /Library/LaunchDaemons/com.wazuh.agent.plist",
+        "/bin/rm -f /Library/LaunchDaemons/com.wazuh.agent.plist",
+        "/bin/rm -rf /Library/StartupItems/WAZUH",
+        "/usr/bin/dscl . -delete \"/Users/wazuh\"",
+        "/usr/bin/dscl . -delete \"/Groups/wazuh\"",
+        "/usr/sbin/pkgutil --forget com.wazuh.pkg.wazuh-agent"
+    ]
+
+    # Run uninstallation commands
+    for command in uninstall_commands:
+        utils.run_command(command)
+
+def checkfiles(os_type):
+    if os_type == 'linux' or os_type == 'macos':
+        command = "sudo find /var/ossec -type f -o -type d  | wc -l"
+    elif os_type == 'windows':
+        command = 'dir /a-d /b /s | findstr /v /c:"\\.$" /c:"\\..$"| find /c ":"'
+    else:
+        print("Unsupported operating system.")
+        return None
+
+    result = subprocess.run(command, shell=True, executable="/bin/bash", stdout=subprocess.PIPE, text=True)
+    
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        print(f"Error executing command. Return code: {result.returncode}")
+        return None
+    
+import os
+import hashlib
+
+def scan_directory(directory):
+    file_list = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_list.append((file_path, get_file_hash(file_path)))
+    return file_list
+
+def get_file_hash(file_path):
+    hasher = hashlib.md5()
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, 'rb') as f:
+                while chunk := f.read(8192):
+                    hasher.update(chunk)
+        except OSError as e:
+            print(f"Failure opening the file {file_path}: {e}")
+    else:
+        print(f"The {file_path} does not exist.")
+        return None
+    return hasher.hexdigest()
+
+def compare_directories(old_scan, new_scan):
+    changed_files = []
+
+    # Identify modified and added files
+    for new_file, new_hash in new_scan:
+        file_found = False
+        for old_file, old_hash in old_scan:
+            if os.path.relpath(old_file) == os.path.relpath(new_file):
+                file_found = True
+                if old_hash != new_hash:
+                    changed_files.append((new_file, old_hash, new_hash))
+
+        # If the file is not found in the initial scan, consider it added
+        if not file_found:
+            changed_files.append((new_file, 'Added', new_hash))
+
+    # Identify deleted files
+    for old_file, _ in old_scan:
+        if os.path.relpath(old_file) not in (os.path.relpath(new_file) for new_file, _ in new_scan):
+            changed_files.append((old_file, 'Deleted', None))
+
+    return changed_files
+
--- deployability/modules/testing/tests/test_agent/test_install.py
+++ deployability/modules/testing/tests/test_agent/test_install.py
@@ -1,13 +1,34 @@
 import grp
 import pwd
 
+
+from ..helpers import actions
 from ..helpers import constants, utils
 
 
+def test_installation():
+    scan_directory = "/var"
+    initial_scan = actions.scan_directory(scan_directory)
+    print("Initial scan:")
+    print(initial_scan)
+    
+    actions.checkfiles('linux')
+    #actions.install_wazuh_agent('linux', '4.7.2', '1', 'packages', '4.x', '127.0.0.1', 'deb', 'amd64')
+    actions.uninstall_wazuh_agent('linux', '4.7.2', '1','deb')
+    
+    second_scan = actions.scan_directory(scan_directory)
+    print("\nPost scan:")
+    print(second_scan)
+    
+    changes = actions.compare_directories(initial_scan, second_scan)
+    print("\nDetected changes:")
+    print(changes)
+
+    
+""" 
 def test_wazuh_user():
     all_users = [x[0] for x in pwd.getpwall()]
     assert constants.WAZUH_USER in all_users, "Wazuh user not found."
-
 
 def test_wazuh_group():
     all_groups = [x[0] for x in grp.getgrall()]
@@ -35,3 +56,4 @@ def test_wazuh_daemons():
 
     for daemon in expected_daemons:
         assert daemon in actual_daemons.keys(), f"{daemon} not found."
+"""
