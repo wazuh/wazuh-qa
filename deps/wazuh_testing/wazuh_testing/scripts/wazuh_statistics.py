b573fd23a3d7acf090fe94b2a63f195a56aa40e6
Andres Carmelo Micalizzi Casali <amicalizzi2005@gmail.com>
2024-02-16 09:40:36 +0100

    add params to wazuh_statistics

--- deps/wazuh_testing/wazuh_testing/scripts/wazuh_statistics.py
+++ deps/wazuh_testing/wazuh_testing/scripts/wazuh_statistics.py
@@ -26,13 +26,15 @@ def get_script_arguments():
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-t', '--target', dest='target_list', required=True, type=str, nargs='+', action='store',
                         help='Type the statistics target to collect separated by whitespace. '
-                             'Targets: agent, logcollector, remote and analysis.')
+                             'Targets: agent, logcollector, remoted, analysis-events analysisd-state.')
     parser.add_argument('-s', '--sleep', dest='sleep_time', type=float, default=5, action='store',
                         help='Type the time in seconds between each entry.')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False,
                         help='Enable debug level logging.')
     parser.add_argument('--store', dest='store_path', action='store', default=gettempdir(),
                         help=f"Path to store the CSVs with the data. Default {gettempdir()}.")
+    parser.add_argument('-a', '--use_api', dest='use_api', type=bool, action='store', default=False,
+                        help="Determine if the API should be used to collect the data. Default False.")
 
     return parser.parse_args()
 
@@ -50,10 +52,12 @@ def main():
     options.debug and logger.setLevel(logging.DEBUG)
     logger.info(f'Started new session: {CURRENT_SESSION}')
 
+    print("Target list: ", options.target_list)
     for target in options.target_list:
+        print("Target:", target)
         monitor = StatisticMonitor(target=target, time_step=options.sleep_time, dst_dir=options.store_path)
-        monitor.start()
         MONITOR_LIST.append(monitor)
+        monitor.start()
 
 
 if __name__ == '__main__':
