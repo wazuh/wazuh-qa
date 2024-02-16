a404dee7c08d24c9d03bc9691f7db3d6800a0f52
Andres Carmelo Micalizzi Casali <amicalizzi2005@gmail.com>
2024-02-16 16:34:45 +0100

    add params to wazuh_statistics

--- deps/wazuh_testing/wazuh_testing/scripts/wazuh_statistics.py
+++ deps/wazuh_testing/wazuh_testing/scripts/wazuh_statistics.py
@@ -26,13 +26,16 @@ def get_script_arguments():
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-t', '--target', dest='target_list', required=True, type=str, nargs='+', action='store',
                         help='Type the statistics target to collect separated by whitespace. '
-                             'Targets: agent, logcollector, remote and analysis.')
+                             'Targets: agent, logcollector, remoted, analysis-events and analysisd-state.')
     parser.add_argument('-s', '--sleep', dest='sleep_time', type=float, default=5, action='store',
                         help='Type the time in seconds between each entry.')
     parser.add_argument('-d', '--debug', dest='debug', action='store_true', default=False,
                         help='Enable debug level logging.')
     parser.add_argument('--store', dest='store_path', action='store', default=gettempdir(),
                         help=f"Path to store the CSVs with the data. Default {gettempdir()}.")
+    parser.add_argument('-a', '--use_api', dest='use_api', type=bool, action='store', default=False,
+                        help="Determine if the API should be used to collect the data. Default False."
+                             "For remoted set to True to get data from API. analysis_events uses API by default.")
 
     return parser.parse_args()
 
@@ -51,9 +54,9 @@ def main():
     logger.info(f'Started new session: {CURRENT_SESSION}')
 
     for target in options.target_list:
-        monitor = StatisticMonitor(target=target, time_step=options.sleep_time, dst_dir=options.store_path)
-        monitor.start()
+        monitor = StatisticMonitor(target=target, time_step=options.sleep_time, dst_dir=options.store_path, use_api=options.use_api)
         MONITOR_LIST.append(monitor)
+        monitor.start()
 
 
 if __name__ == '__main__':
