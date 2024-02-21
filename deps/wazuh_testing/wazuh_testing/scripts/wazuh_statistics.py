8128e8a9a2702d28ba4216603d8539104506be48
Andres Carmelo Micalizzi Casali <amicalizzi2005@gmail.com>
2024-02-21 13:27:52 +0100

    add port and ip for API

--- deps/wazuh_testing/wazuh_testing/scripts/wazuh_statistics.py
+++ deps/wazuh_testing/wazuh_testing/scripts/wazuh_statistics.py
@@ -36,6 +36,10 @@ def get_script_arguments():
     parser.add_argument('-a', '--use_api', dest='use_api', type=bool, action='store', default=False,
                         help="Determine if the API should be used to collect the data. Default False."
                              "For remoted set to True to get data from API. analysis_events uses API by default.")
+    parser.add_argument('-i', '--ip' dest='ip', action='store', default='localhost',
+                        help=f"IP for the API. Default localhost.")
+    parser.add_argument('-p', '--port' dest='port', action='store', default='55000',
+                        help=f"port for the API. Default localhost.")
 
     return parser.parse_args()
 
@@ -54,7 +58,8 @@ def main():
     logger.info(f'Started new session: {CURRENT_SESSION}')
 
     for target in options.target_list:
-        monitor = StatisticMonitor(target=target, time_step=options.sleep_time, dst_dir=options.store_path, use_api=options.use_api)
+        monitor = StatisticMonitor(target=target, time_step=options.sleep_time, dst_dir=options.store_path,
+                                   use_api=options.use_api, ip=options.ip, port=options.port)
         MONITOR_LIST.append(monitor)
         monitor.start()
 
