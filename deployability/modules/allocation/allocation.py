ffc950ef49a4b9dc805a89fc743d999325e7a3fb
c-bordon <carlos.e.bordon@gmail.com>
2024-03-06 16:39:41 -0300

    Fixed inventory-output and track-output arguments

--- deployability/modules/allocation/allocation.py
+++ deployability/modules/allocation/allocation.py
@@ -54,8 +54,8 @@ class Allocator:
         instance.start()
         logger.info(f"Instance {instance.identifier} started.")
         # Generate the inventory and track files.
-        cls.__generate_inventory(instance)
-        cls.__generate_track_file(instance, payload.provider)
+        cls.__generate_inventory(instance, payload.inventory_output)
+        cls.__generate_track_file(instance, payload.provider, payload.track_output)
 
     @classmethod
     def __delete(cls, payload: models.DeletionPayload) -> None:
@@ -97,7 +97,7 @@ class Allocator:
         return config
 
     @staticmethod
-    def __generate_inventory(instance: Instance) -> None:
+    def __generate_inventory(instance: Instance, inventory_path: Path) -> None:
         """
         Generates an inventory file.
 
@@ -105,7 +105,10 @@ class Allocator:
             instance (Instance): The instance for which the inventory file is generated.
             inventory_path (Path): The path where the inventory file will be generated.
         """
-        inventory_path = Path(instance.path, 'inventory.yml')
+        if inventory_path is None:
+            inventory_path = Path(instance.path, 'inventory.yml')
+        else:
+            inventory_path = Path(inventory_path, 'inventory.yml')
         if not inventory_path.parent.exists():
             inventory_path.parent.mkdir(parents=True, exist_ok=True)
         ssh_config = instance.ssh_connection_info()
@@ -118,7 +121,7 @@ class Allocator:
         logger.info(f"Inventory file generated at {inventory_path}")
 
     @staticmethod
-    def __generate_track_file(instance: Instance, provider_name: str) -> None:
+    def __generate_track_file(instance: Instance, provider_name: str,  track_path: Path) -> None:
         """
         Generates a track file.
 
@@ -127,7 +130,10 @@ class Allocator:
             provider_name (str): The name of the provider.
             track_path (Path): The path where the track file will be generated.
         """
-        track_path = Path(instance.path, 'track.yml')
+        if track_path is None:
+            track_path = Path(instance.path, 'track.yml')
+        else:
+            track_path = Path(track_path, 'track.yml')
         if not track_path.parent.exists():
             track_path.parent.mkdir(parents=True, exist_ok=True)
         track = models.TrackOutput(identifier=instance.identifier,
