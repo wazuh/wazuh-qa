dca3507ff0e773f63047425d2154451f5e16c68d
Antonio <antonio.kim@wazuh.com>
2024-03-12 22:36:40 +0100

    enhancement(#4844): Adding filters to checkfiles

--- deployability/modules/testing/tests/test_manager/test_install.py
+++ deployability/modules/testing/tests/test_manager/test_install.py
@@ -2,11 +2,39 @@ import pytest
 import json
 
 from ..helpers.manager import WazuhManager
-from ..helpers.generic import HostConfiguration, CheckFiles
+from ..helpers.generic import HostConfiguration, CheckFiles, HostInformation
 
 wazuh_manager = WazuhManager()
 host_configuration = HostConfiguration()
 checkfiles = CheckFiles()
+host_information = HostInformation()
+
+def install_manager_callback(wazuh_params, manager_name, manager_params):
+    wazuh_manager.install_manager(manager_params, manager_name, wazuh_params['wazuh_version'])
+
+def perform_action_and_scan_for_manager(manager_params, manager_name, wazuh_params):
+    result = checkfiles.perform_action_and_scan(manager_params, lambda: install_manager_callback(wazuh_params, manager_name, manager_params))
+    categories = ['/root', '/usr/bin', '/usr/sbin', '/boot']
+    actions = ['added', 'modified', 'removed']
+
+    # Selecting filter
+    os_name = host_information.get_os_name_from_inventory(manager_params)
+    if 'debian' in os_name:
+        filter_data= {'/boot': {'added': [], 'removed': [], 'modified': ['grubenv']}, '/usr/bin': {'added': ['unattended-upgrade', 'gapplication', 'add-apt-repository', 'gpg-wks-server', 'pkexec', 'gpgsplit', 'watchgnupg', 'pinentry-curses', 'gpg-zip', 'gsettings', 'gpg-agent', 'gresource', 'gdbus', 'gpg-connect-agent', 'gpgconf', 'gpgparsemail', 'lspgpot', 'pkaction', 'pkttyagent', 'pkmon', 'dirmngr', 'kbxutil', 'migrate-pubring-from-classic-gpg', 'gpgcompose', 'pkcheck', 'gpgsm', 'gio', 'pkcon', 'gpgtar', 'dirmngr-client', 'gpg', 'filebeat', 'gawk', 'curl', 'update-mime-database', 'dh_installxmlcatalogs', 'appstreamcli','lspgpot'], 'removed': [], 'modified': []}, '/root': {'added': ['trustdb.gpg'], 'removed': [], 'modified': []}, '/usr/sbin': {'added': ['update-catalog', 'applygnupgdefaults', 'addgnupghome', 'install-sgmlcatalog', 'update-xmlcatalog'], 'removed': [], 'modified': []}}
+    else:
+        filter_data = {'/boot': {'added': [], 'removed': [], 'modified': ['grubenv']}, '/usr/bin': {'added': ['filebeat'], 'removed': [], 'modified': []}, '/root': {'added': ['trustdb.gpg'], 'removed': [], 'modified': []}, '/usr/sbin': {'added': [], 'removed': [], 'modified': []}}
+
+    # Use of filters
+    for directory, changes in result.items():
+        if directory in filter_data:
+            for change, files in changes.items():
+                if change in filter_data[directory]:
+                    result[directory][change] = [file for file in files if file.split('/')[-1] not in filter_data[directory][change]]
+
+    # Testing the results
+    for category in categories:
+        for action in actions:
+            assert result[category][action] == []
 
 @pytest.fixture
 def wazuh_params(request):
@@ -44,24 +72,9 @@ def test_installation(wazuh_params):
     host_configuration.certs_create(wazuh_params['wazuh_version'], wazuh_params['master'], wazuh_params['dashboard'], wazuh_params['indexers'], wazuh_params['workers'])
     host_configuration.scp_to(wazuh_params['master'], wazuh_params['workers'][0], 'wazuh-install-files.tar')
 
-
-    def install_manager_callback(wazuh_params, manager_name, manager_params):
-        wazuh_manager.install_manager(manager_params, manager_name, wazuh_params['wazuh_version'])
-
-    def perform_action_and_scan_for_manager(manager_params, manager_name):
-        result = checkfiles.perform_action_and_scan(manager_params, lambda: install_manager_callback(wazuh_params, manager_name, manager_params))
-        print(manager_name)
-        print(result)
-        categories = ['/root', '/usr/bin', '/usr/sbin', '/boot']
-        actions = ['added', 'modified', 'removed']
-
-        #for category in categories:
-        #    for action in actions:
-        #        assert result[category][action] == []
-
-    # Combine the code using a loop over managers
+    # Install managers and perform checkfile testing
     for manager_name, manager_params in managers.items():
-        perform_action_and_scan_for_manager(manager_params, manager_name)
+        perform_action_and_scan_for_manager(manager_params, manager_name, wazuh_params)
 
     # Configuring cluster for all managers
     hex16_code = 'eecda366dded9b32bcfbf3b057bf3ede'
--- deployability/modules/testing/tests/test_manager/test_uninstall.py
+++ deployability/modules/testing/tests/test_manager/test_uninstall.py
@@ -3,10 +3,12 @@ import json
 
 
 from ..helpers.manager import WazuhManager
-from ..helpers.generic import HostConfiguration, CheckFiles
+from ..helpers.generic import HostConfiguration, CheckFiles, HostInformation
+
 wazuh_manager = WazuhManager()
 host_configuration = HostConfiguration()
 checkfiles = CheckFiles()
+host_information = HostInformation()
 
 @pytest.fixture
 def wazuh_params(request):
@@ -20,11 +22,36 @@ def wazuh_params(request):
         'wazuh_revision': wazuh_revision,
         'dependencies': json.loads(dependencies.replace("{", "{\"").replace(":", "\":\"").replace(",", "\",\"").replace("}", "\"}").replace(' ', '')),
         'inventory': inventory
-
     }
 
     return params
 
+def uninstall_manager_callback(manager_params):
+    wazuh_manager.uninstall_manager(manager_params)
+
+def perform_action_and_scan_for_manager(manager_params):
+    result = checkfiles.perform_action_and_scan(manager_params, lambda: uninstall_manager_callback(manager_params))
+    categories = ['/root', '/usr/bin', '/usr/sbin', '/boot']
+    actions = ['added', 'modified', 'removed']
+
+    # Selecting filter
+    os_name = host_information.get_os_name_from_inventory(manager_params)
+    if 'debian' in os_name:
+        filter_data= {'/boot': {'added': [], 'removed': [], 'modified': ['grubenv']}, '/usr/bin': {'added': ['unattended-upgrade', 'gapplication', 'add-apt-repository', 'gpg-wks-server', 'pkexec', 'gpgsplit', 'watchgnupg', 'pinentry-curses', 'gpg-zip', 'gsettings', 'gpg-agent', 'gresource', 'gdbus', 'gpg-connect-agent', 'gpgconf', 'gpgparsemail', 'lspgpot', 'pkaction', 'pkttyagent', 'pkmon', 'dirmngr', 'kbxutil', 'migrate-pubring-from-classic-gpg', 'gpgcompose', 'pkcheck', 'gpgsm', 'gio', 'pkcon', 'gpgtar', 'dirmngr-client', 'gpg', 'filebeat', 'gawk', 'curl', 'update-mime-database', 'dh_installxmlcatalogs', 'appstreamcli','lspgpot'], 'removed': [], 'modified': []}, '/root': {'added': ['trustdb.gpg'], 'removed': [], 'modified': []}, '/usr/sbin': {'added': ['update-catalog', 'applygnupgdefaults', 'addgnupghome', 'install-sgmlcatalog', 'update-xmlcatalog'], 'removed': [], 'modified': []}}
+    else:
+        filter_data = {'/boot': {'added': [], 'removed': [], 'modified': ['grubenv']}, '/usr/bin': {'added': ['filebeat'], 'removed': [], 'modified': []}, '/root': {'added': ['trustdb.gpg'], 'removed': [], 'modified': []}, '/usr/sbin': {'added': [], 'removed': [], 'modified': []}}
+
+    # Use of filters
+    for directory, changes in result.items():
+        if directory in filter_data:
+            for change, files in changes.items():
+                if change in filter_data[directory]:
+                    result[directory][change] = [file for file in files if file.split('/')[-1] not in filter_data[directory][change]]
+
+    # Testing the results
+    for category in categories:
+        for action in actions:
+            assert result[category][action] == []
 
 @pytest.fixture(autouse=True)
 def setup_test_environment(wazuh_params):
@@ -39,18 +66,6 @@ def test_uninstall(wazuh_params):
         'wazuh-1': wazuh_params['master'],
         'wazuh-2': wazuh_params['workers'][0]
     }
-    def uninstall_manager_callback(manager_params):
-        wazuh_manager.uninstall_manager(manager_params)
-
-    def perform_action_and_scan_for_manager(manager_params):
-        result = checkfiles.perform_action_and_scan(manager_params, lambda: uninstall_manager_callback(manager_params))
-
-        categories = ['/root', '/usr/bin', '/usr/sbin', '/boot']
-        actions = ['added', 'modified', 'removed']
-
-        for category in categories:
-            for action in actions:
-                assert result[category][action] == []
 
     for manager_name, manager_params in managers.items():
         perform_action_and_scan_for_manager(manager_params)
