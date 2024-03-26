48c6d93a524bb16c6cb2ac7b4b6591d47437bf3d
quebim <kevin.ledesma@wazuh.com>
2024-03-26 10:56:28 -0300

    Remove space after copyright header to follow the convenience of the rest of modules

--- deployability/modules/workflow_engine/__init__.py
+++ deployability/modules/workflow_engine/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2015, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is a free software; you can redistribute it and/or modify it under the terms of GPLv2
-
 from .workflow_processor import WorkflowProcessor
