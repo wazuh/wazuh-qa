4636e350b910fbc56c8b066d6833cd91905af6ac
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2024-04-02 11:33:50 +0100

    style: waiters style errors

--- deps/wazuh_testing/wazuh_testing/end_to_end/waiters.py
+++ deps/wazuh_testing/wazuh_testing/end_to_end/waiters.py
@@ -32,7 +32,7 @@ from wazuh_testing.modules.syscollector import TIMEOUT_SYSCOLLECTOR_SHORT_SCAN
 
 
 VD_FEED_UPDATE_TIMEOUT = 600
-VD_INITIAL_SCAN_PER_AGENT_TIMEOUT = 15
+VD_INITIAL_SCAN_PER_AGENT_TIMEOUT = 10
 
 
 def wait_until_vd_is_updated(host_manager: HostManager) -> None:
@@ -44,7 +44,8 @@ def wait_until_vd_is_updated(host_manager: HostManager) -> None:
     """
     monitoring_data = generate_monitoring_logs(host_manager, ["Vulnerability scanner module started"],
                                                [VD_FEED_UPDATE_TIMEOUT], host_manager.get_group_hosts('manager'))
-    monitoring_events_multihost(host_manager, monitoring_data)
+
+    monitoring_events_multihost(host_manager, monitoring_data, ignore_timeout_error=False)
 
 
 def wait_until_vuln_scan_agents_finished(host_manager: HostManager) -> None:
@@ -54,12 +55,13 @@ def wait_until_vuln_scan_agents_finished(host_manager: HostManager) -> None:
     Args:
         host_manager (HostManager): Host manager instance to handle the environment.
     """
-    final_timeout = VD_INITIAL_SCAN_PER_AGENT_TIMEOUT * len(get_agents_id(host_manager))
+    final_timeout = VD_INITIAL_SCAN_PER_AGENT_TIMEOUT * len(host_manager.get_group_hosts('agent'))
+
     time.sleep(final_timeout)
 
 
-def wait_syscollector_and_vuln_scan(host_manager: HostManager, host: str,  operation_data: Dict,
-                                    current_datetime: str = '') -> None:
+def wait_syscollector_and_vuln_scan(host_manager: HostManager, syscollector_scan: int, 
+                                    greater_than_timestamp: str = '') -> None:
     """
     Wait until syscollector and vulnerability scans are finished for a specific host.
 
@@ -69,24 +71,18 @@ def wait_syscollector_and_vuln_scan(host_manager: HostManager, host: str,  opera
         operation_data (Dict): Dictionary with the operation data.
         current_datetime (str): Current datetime to use in the operation.
     """
-    logging.info(f"Waiting for syscollector scan to finish on {host}")
-
-    timeout_syscollector_scan = TIMEOUT_SYSCOLLECTOR_SHORT_SCAN if 'timeout_syscollector_scan' not in \
-        operation_data else operation_data['timeout_syscollector_scan']
+    logging.info(f"Waiting for syscollector scan to finish in all hosts")
 
     # Wait until syscollector
     monitoring_data = generate_monitoring_logs(host_manager,
                                                [get_event_regex({'event': 'syscollector_scan_start'}),
                                                 get_event_regex({'event': 'syscollector_scan_end'})],
-                                               [timeout_syscollector_scan, timeout_syscollector_scan],
-                                               host_manager.get_group_hosts('agent'))
-
-    truncate_remote_host_group_files(host_manager, host_manager.get_group_hosts('agent'))
-
+                                               [syscollector_scan, syscollector_scan],
+                                               host_manager.get_group_hosts('agent'),
+                                               greater_than_timestamp=greater_than_timestamp)
     monitoring_events_multihost(host_manager, monitoring_data)
 
-    logging.info(f"Waiting for vulnerability scan to finish on {host}")
+    logging.info(f"Waiting for vulnerability scan to finish")
 
     wait_until_vuln_scan_agents_finished(host_manager)
 
-    logging.info(f"Checking agent vulnerability on {host}")
