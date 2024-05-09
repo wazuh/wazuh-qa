6467edab2b3a232c8cfa59b380bebba8f1dfe8e0
c-bordon <carlos.e.bordon@gmail.com>
2024-05-09 11:21:09 -0300

    Vagrant status None output is improved

--- deployability/modules/allocation/vagrant/instance.py
+++ deployability/modules/allocation/vagrant/instance.py
@@ -120,9 +120,10 @@ class VagrantInstance(Instance):
         output = self.__run_vagrant_command('status')
         vagrant_status = self.__parse_vagrant_status(output)
         if vagrant_status == None:
-            if VagrantUtils.remote_command(f"ls {self.host_instance_dir} > /dev/null 2>&1", self.remote_host_parameters):
-                if VagrantUtils.remote_command(f"prlctl list -a | grep {self.identifier} > /dev/null 2>&1", self.remote_host_parameters):
-                    return "running"
+            if VagrantUtils.remote_command(f"sudo ls {self.host_instance_dir} > /dev/null 2>&1", self.remote_host_parameters):
+                if VagrantUtils.remote_command(f"sudo /usr/local/bin/prlctl list -a | grep {self.identifier} > /dev/null 2>&1", self.remote_host_parameters):
+                    logger.warning(f"The instance was found, it will be deleted. The creation of the instance must be restarted again.")
+                    self.delete()
                 else:
                     VagrantUtils.remote_command(f"sudo rm -rf {self.host_instance_dir}", self.remote_host_parameters)
                     raise ValueError(f"Instance {self.identifier} is not running, remote instance dir {self.host_instance_dir} was removed.")
