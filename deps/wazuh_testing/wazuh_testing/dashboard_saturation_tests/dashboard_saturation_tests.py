4fee113a6384ad0a9d5dd204ecf37708f0632be5
rafabailon <rafael.bailon@wazuh.com>
2024-08-02 12:54:47 +0200

    fix: changes  according to the PEP 8 standard

--- deps/wazuh_testing/wazuh_testing/dashboard_saturation_tests/dashboard_saturation_tests.py
+++ deps/wazuh_testing/wazuh_testing/dashboard_saturation_tests/dashboard_saturation_tests.py
@@ -10,41 +10,54 @@ from subprocess import run
 
 # Global Configuration Variables
 artillery_result_type = [
-    'aggregate',   # Test-wide statistics, which correspond to the final statistics printed to the console at the end of a test
-    'intermediate' # Statistics that print to the console while a test is running
+    # Final statistics printed to the console at the end of a test
+    'aggregate',
+    # Statistics that print to the console while a test is running
+    'intermediate'
 ]
 
+
 def check_artillery_result_types(types):
-    """Check that a valid type of Artillery result has been chosen to generate the CSV.
+    """Check that a valid type of Artillery result has been
+    chosen to generate the CSV.
+
     Args:
         types (list): List of types.
     """
     for value in types:
         if value not in artillery_result_type:
-            msg = f'The Artillery result type to generate the CSV is not valid.'
-            accepted_values = f'Accepted Values: {artillery_result_type}.'
-            received_value = f'Value received {value}.'
+            msg = f'The Artillery result type to generate the CSV is not ' \
+                'valid. Accepted Values: {artillery_result_type}. ' \
+                'Value received {value}.'
 
-            raise Exception(f'{msg} {accepted_values} {received_value}') 
+            raise Exception(msg)
+
 
 def format_directory(directory):
     """Check the paths of the directories and format them if necessary.
+
     Args:
         args (string): Path of a directory.
+
     Returns:
         string: Formatted path.
     """
     return join(directory, '')
-  
+
+
 def create_directories(directory):
     """Create directory that does not exist.
+
     Args:
         directory (str): Path of directory.
     """
     makedirs(directory, exist_ok=True)
 
+
 def provide_directories(args):
-    """Format the paths of the directories and create them (if they do not exist).
+    """Format the paths of the directories and create them
+    (if they do not exist).
+
     Args:
         args (ArgumentParser): Script parameters.
     """
@@ -60,8 +73,10 @@ def provide_directories(args):
     create_directories(args.csv)
     create_directories(args.session)
 
+
 def create_session_file(path, user):
     """Create file to save the browser session.
+
     Args:
         path (str): Path of directory.
         user (str): Username.
@@ -71,8 +86,10 @@ def create_session_file(path, user):
     with open(file_path, 'w') as file:
         file.write('{}')
 
+
 def process_script_arguments(args):
     """Process script arguments, create folders and generate necessary files.
+
     Args:
         args (ArgumentParser): Script parameters.
     """
@@ -85,20 +102,26 @@ def process_script_arguments(args):
     # Create Session File
     create_session_file(args.session, args.user)
 
+
 def format_artillery_params(key, value):
     """Format options for Artillery.
+
     Args:
         key (str): Name of the Artillery option.
         value (str): Value of the Artillery option.
+
     Returns:
         str: Formatted Artillery option.
     """
     return f"\"{key}\": \"{value}\""
 
+
 def gen_artillery_params(args):
     """Format all parameters for Artillery.
+
     Args:
         args (ArgumentParser): Script parameters.
+
     Returns:
         str: Formatted Artillery parameters.
     """
@@ -111,19 +134,25 @@ def gen_artillery_params(args):
 
     return params
 
+
 def gen_log_filename(log_path):
     """Generate log file name.
+
     Args:
         log_path (str): Path of the logs.
+
     Returns:
         str: File name of the log (include path).
     """
     return log_path + datetime.now().strftime(f"log-%Y%m%d%H%M%S.log")
 
+
 def gen_url(ip):
     """Generate dashboard url.
+
     Args:
         ip (str): IP of the dashboard.
+
     Returns:
         str: Complete url of the dashboard.
     """
@@ -131,18 +160,23 @@ def gen_url(ip):
 
     return f'{url_format}{ip}'
 
+
 def gen_csv_filename(csv_path, type):
     """Generate csv file name (per type).
+
     Args:
         csv_path (str): Path of the CSVs.
         type (str): CSV data type.
+
     Returns:
         str: File name of the csv (include type and path).
     """
     return csv_path + datetime.now().strftime(f"{type}-%Y%m%d%H%M%S.csv")
 
+
 def convert_json_to_csv(args, json_output):
     """Convert data from JSON format to CSV format.
+
     Args:
         args (ArgumentParser): Script parameters.
         json_output (str): Path and file name of the log.
@@ -156,8 +190,10 @@ def convert_json_to_csv(args, json_output):
         df = pd.json_normalize(data[type])
         df.to_csv(csv_filename, index=False)
 
+
 def run_artillery(args):
     """Execute Artillery tests.
+
     Args:
         args (ArgumentParser): Script parameters.
     """
@@ -180,58 +216,119 @@ def run_artillery(args):
     run(command, shell=True)
     convert_json_to_csv(args, json_filename)
 
+
 def get_script_arguments():
-    parser = argparse.ArgumentParser(usage='%(prog)s [options]', 
-                                     description='Script to Run Dashboard Saturation Tests',
-                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [options]',
+        description='Script to Run Dashboard Saturation Tests',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
 
-    parser.add_argument('-l', '--log', dest='logs', default='logs/',
-                        help=f'Directory to store the logs. Default "logs".')
-    
-    parser.add_argument('-s', '--screenshots', dest='screenshots', default='screenshots/',
-                        help=f'Directory to store the screenshots. Default "screenshot".')
-    
-    parser.add_argument('-c', '--csv', dest='csv', default='csv/',
-                        help=f'Directory to store the CSVs. Default "csv".')
+    parser.add_argument(
+        '-l', '--log',
+        dest='logs',
+        default='logs/',
+        help=f'Directory to store the logs. Default "logs".'
+    )
 
-    parser.add_argument('-o', '--session', dest='session', default='.auth/',
-                        help=f'Directory to store the Sessions. Default ".auth".')
+    parser.add_argument(
+        '-s', '--screenshots',
+        dest='screenshots',
+        default='screenshots/',
+        help=f'Directory to store the screenshots. Default "screenshot".')
 
-    parser.add_argument('-a', '--artillery', dest='artillery', type=str, default="artillery.yml",
-                        help=f'Path to the Artillery Script. Default "artillery.yml".')
+    parser.add_argument(
+        '-c', '--csv',
+        dest='csv',
+        default='csv/',
+        help=f'Directory to store the CSVs. Default "csv".'
+    )
 
-    parser.add_argument('-u', '--user', dest='user', type=str, default= 'admin',
-                        help=f'Wazuh User for the Dashboard. Default "admin".')
+    parser.add_argument(
+        '-o', '--session',
+        dest='session',
+        default='.auth/',
+        help=f'Directory to store the Sessions. Default ".auth".'
+    )
 
-    parser.add_argument('-p', '--password', dest='password', type=str, required=True,
-                        help=f'Wazuh Password for the Dashboard.')
-    
-    parser.add_argument('-q', '--iterations', dest='iterations', type=int, default=1,
-                        help=f'Number of Tests to Run. Default {1}')
-    
-    parser.add_argument('-i', '--ip', dest='ip', type=str, required=True,
-                        help=f'Set the Dashboard IP.')
+    parser.add_argument(
+        '-a', '--artillery',
+        dest='artillery',
+        type=str,
+        default="artillery.yml",
+        help=f'Path to the Artillery Script. Default "artillery.yml".'
+    )
 
-    parser.add_argument('-t', '--type', dest='type', type=str, nargs='+', action='store',
-                        default=['aggregate', 'intermediate'],
-                        help=f'JSON data to create the CSV.')
-    
-    parser.add_argument('-w', '--wait', dest='wait', type=int, default=5,
-                        help=f'Waiting Time between Executions.')
-    
-    parser.add_argument('-d', '--debug', dest='debug', action='store_true', required=False,
-                        default=False, help='Enable Debug Mode')
+    parser.add_argument(
+        '-u', '--user',
+        dest='user',
+        type=str,
+        default='admin',
+        help=f'Wazuh User for the Dashboard. Default "admin".'
+    )
+
+    parser.add_argument(
+        '-p', '--password',
+        dest='password',
+        type=str,
+        required=True,
+        help=f'Wazuh Password for the Dashboard.'
+    )
+
+    parser.add_argument(
+        '-q', '--iterations',
+        dest='iterations',
+        type=int,
+        default=1,
+        help=f'Number of Tests to Run. Default {1}'
+    )
+
+    parser.add_argument(
+        '-i', '--ip',
+        dest='ip',
+        type=str,
+        required=True,
+        help=f'Set the Dashboard IP.'
+    )
+
+    parser.add_argument(
+        '-t', '--type',
+        dest='type',
+        type=str,
+        nargs='+',
+        action='store',
+        default=['aggregate', 'intermediate'],
+        help=f'JSON data to create the CSV.'
+    )
+
+    parser.add_argument(
+        '-w', '--wait',
+        dest='wait',
+        type=int,
+        default=5,
+        help=f'Waiting Time between Executions.'
+    )
+
+    parser.add_argument(
+        '-d', '--debug',
+        dest='debug',
+        action='store_true',
+        required=False,
+        default=False, help='Enable Debug Mode'
+    )
 
     return parser.parse_args()
+
 
 def main():
     script_args = get_script_arguments()
 
     process_script_arguments(script_args)
 
-    for n in range(0, script_args.iterations):    
+    for n in range(0, script_args.iterations):
         run_artillery(script_args)
         time.sleep(script_args.wait)
+
 
 if __name__ == "__main__":
     main()
