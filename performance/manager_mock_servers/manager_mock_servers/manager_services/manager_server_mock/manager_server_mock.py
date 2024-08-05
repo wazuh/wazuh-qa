cc7cf4895ef39a5055b9030288ba789415147657
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2024-08-05 08:37:53 +0100

    docs: change module docstring

--- performance/manager_mock_servers/manager_mock_servers/manager_services/manager_server_mock/manager_server_mock.py
+++ performance/manager_mock_servers/manager_mock_servers/manager_services/manager_server_mock/manager_server_mock.py
@@ -16,20 +16,7 @@ Issue:
 
 Todo
 # app = FastAPI(
-#     title="ChimichangApp",
-#     description=description,
-#     summary="Deadpool's favorite app. Nuff said.",
-#     version="0.0.1",
-#     terms_of_service="http://example.com/terms/",
-#     contact={
-#         "name": "Deadpoolio the Amazing",
-#         "url": "http://x-force.example.com/contact/",
-#         "email": "dp@x-force.example.com",
-#     },
-#     license_info={
-#         "name": "Apache 2.0",
-#         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-#     },
+#     title=
 # )
 
 Logging
