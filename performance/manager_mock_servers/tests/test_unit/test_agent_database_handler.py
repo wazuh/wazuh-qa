56a88fc3041f1c3b02272f14173d135c84d339f4
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2024-08-08 10:59:35 +0100

    style: include typpe hint to test database handler

--- performance/manager_mock_servers/tests/test_unit/test_agent_database_handler.py
+++ performance/manager_mock_servers/tests/test_unit/test_agent_database_handler.py
@@ -1,43 +1,46 @@
-import pytest
+"""Tests for agent database handler."""
 import os
 import sqlite3
-from manager_mock_servers.utils.agent_database_handler import insert_new_agent, check_if_agent_exists, check_if_uuid_exists, \
-    create_agents_database
+from typing import Generator
 
+import pytest
 from _pytest.tmpdir import TempPathFactory
-from typing import Protocol
+
+from manager_mock_servers.utils.agent_database_handler import (
+    check_if_agent_exists,
+    check_if_uuid_exists,
+    create_agents_database,
+    insert_new_agent,
+)
 
 
 @pytest.fixture
-def temp_db_path(tmpdir):
-    """Fixture to provide a temporary path for a test SQLite database.
-
-    Uses the pytest 'tmpdir' fixture to create a temporary directory for database storage.
-    Returns the path as a string.
-    """
-    db_path = tmpdir.mkdir("db")
-    return str(db_path)
-
-
-@pytest.fixture
-def create_agent_database(tmp_path_factory: TempPathFactory):
+def create_agent_database(tmp_path_factory: TempPathFactory) -> Generator:
     """Fixture to create a new SQLite database for testing with the 'agents' table.
 
-    Uses the pytest 'tmp_path_factory' fixture to create a temporary database directory.
-    Calls the 'create_agents_database' function to initialize the database.
-    Yields the path to the created database file.
+    Args:
+        tmp_path_factory (TempPathFactory): Factory for creating temporary directories.
+
+    Yields:
+        str: Path of the database path
     """
     agent_database_path = tmp_path_factory.mktemp('db')
-    db_path = create_agents_database(agent_database_path)
+    db_path = create_agents_database(str(agent_database_path))
 
     yield db_path
 
 
-def test_create_agents_database(create_agent_database: Protocol):
+def test_create_agents_database(create_agent_database: str):
     """Verify that the agents database is created with the correct table structure.
 
     Asserts that the database file exists and that the 'agents' table contains the
     expected columns: 'id', 'uuid', 'credential', and 'name'.
+
+    Args:
+        create_agent_database (str): Path with the database for testing.
+
+    Assertions:
+        Asserts that created database exists and match the expected.
     """
     assert os.path.exists(create_agent_database)
 
@@ -49,12 +52,19 @@ def test_create_agents_database(create_agent_database: Protocol):
         expected_columns = ['id', 'uuid', 'credential', 'name']
         assert set(columns) == set(expected_columns)
 
-def test_check_if_agent_exists(create_agent_database: Protocol):
+
+def test_check_if_agent_exists(create_agent_database: str):
     """Verify the functionality of the 'check_if_agent_exists' function.
 
     Inserts a new agent into the database and checks that the agent can be
     correctly retrieved by its name. Also verifies that a non-existent agent
     returns None.
+
+    Args:
+        create_agent_database (str): Path with the database for testing.
+
+    Assertions:
+        Asserts that inserted agent is correctly detected by agent name
     """
     insert_new_agent(create_agent_database, 'uuid-1234', 'key-1234', 'Agent1')
 
@@ -68,11 +78,18 @@ def test_check_if_agent_exists(create_agent_database: Protocol):
     result = check_if_agent_exists(create_agent_database, 'NonExistentAgent')
     assert result is None
 
-def test_check_if_uuid_exists(create_agent_database: Protocol):
+
+def test_check_if_uuid_exists(create_agent_database: str):
     """Verify the functionality of the 'check_if_uuid_exists' function.
 
     Inserts a new agent into the database and checks that the UUID can be
     correctly retrieved. Also verifies that a non-existent UUID returns None.
+
+    Args:
+        create_agent_database (str): Path with the database for testing.
+
+    Assertions:
+        Asserts that inserted agent is correctly detected by uuid
     """
     insert_new_agent(create_agent_database, 'uuid-5678', 'key-5678', 'Agent2')
 
@@ -86,12 +103,19 @@ def test_check_if_uuid_exists(create_agent_database: Protocol):
     result = check_if_uuid_exists(create_agent_database, 'uuid-9999')
     assert result is None
 
-def test_insert_new_agent(create_agent_database: Protocol):
+
+def test_insert_new_agent(create_agent_database: str):
     """Verify the functionality of the 'insert_new_agent' function.
 
     Inserts a new agent into the database and checks that the agent is
     correctly retrievable by its name. Ensures that all the details match
     what was inserted.
+
+    Args:
+        create_agent_database (str): Path with the database for testing.
+
+    Assertions:
+        Asserts that inserted agent is correctly initilized in the database.
     """
     insert_new_agent(create_agent_database, 'uuid-91011', 'key-91011', 'Agent3')
 
