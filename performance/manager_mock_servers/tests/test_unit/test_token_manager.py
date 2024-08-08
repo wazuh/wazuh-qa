bd45b0ba4027fa2d2fe20a686fe028aaff46c7f4
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2024-08-08 10:09:18 +0100

    style: pep8 tests token manager

--- performance/manager_mock_servers/tests/test_unit/test_token_manager.py
+++ performance/manager_mock_servers/tests/test_unit/test_token_manager.py
@@ -1,12 +1,15 @@
-import pytest
+"""Test module for TokenManager class."""
 from datetime import datetime, timedelta
+
+import pytest
 from fastapi import HTTPException
+
 from manager_mock_servers.utils.token_manager import TokenManager
+
 
 @pytest.fixture
 def token_data():
-    """
-    Fixture that provides a dictionary of token data for testing.
+    """Fixture that provides a dictionary of token data for testing.
 
     This fixture generates and returns a dictionary containing the necessary data
     to create and verify tokens. The dictionary includes issuer, audience, issue time,
@@ -25,9 +28,9 @@ def token_data():
         'credential': "test_secret"
     }
 
+
 def test_generate_token(token_data):
-    """
-    Test that the `generate_token` method creates a token of type string.
+    """Test that the `generate_token` method creates a token of type string.
 
     This test verifies that the `TokenManager.generate_token` method returns a string
     when provided with valid token data. It does not check the content of the token,
@@ -45,9 +48,9 @@ def test_generate_token(token_data):
     )
     assert isinstance(token, str)
 
+
 def test_decode_token(token_data):
-    """
-    Test that the `decode_token` method correctly decodes a valid token.
+    """Test that the `decode_token` method correctly decodes a valid token.
 
     This test checks that the `TokenManager.decode_token` method can decode a token
     generated with the `TokenManager.generate_token` method, and that the decoded
@@ -73,9 +76,9 @@ def test_decode_token(token_data):
     assert decoded_token['exp'] == token_data['exp']
     assert decoded_token['uuid'] == token_data['uuid']
 
+
 def test_decode_token_expired(token_data):
-    """
-    Test that the `decode_token` method raises an HTTPException for an expired token.
+    """Test that the `decode_token` method raises an HTTPException for an expired token.
 
     This test checks that the `TokenManager.decode_token` method raises an HTTPException
     with a 401 status code and the message "Token has expired" when attempting to decode
@@ -101,9 +104,9 @@ def test_decode_token_expired(token_data):
     assert exc_info.value.status_code == 401
     assert exc_info.value.detail == "Token has expired"
 
+
 def test_decode_token_invalid(token_data):
-    """
-    Test that the `decode_token` method raises an HTTPException for a token with an invalid secret.
+    """Test that the `decode_token` method raises an HTTPException for a token with an invalid secret.
 
     This test verifies that the `TokenManager.decode_token` method raises an HTTPException
     with a 401 status code and the message "Invalid token" when decoding a token using
