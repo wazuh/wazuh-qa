6d11a09fe97e26dbe0133f97636b54bafda54370
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2024-08-09 11:19:53 +0100

    docs: update dosctring

--- performance/manager_mock_servers/manager_mock_servers/manager_services/agent_comm_mock/middlewares/brotli.py
+++ performance/manager_mock_servers/manager_mock_servers/manager_services/agent_comm_mock/middlewares/brotli.py
@@ -4,6 +4,8 @@ This module defines the `BrotliMiddleware` class, which is a custom middleware f
 using Brotli compression. It checks if the client supports Brotli (indicated by the 'accept-encoding' header
 containing 'br') and compresses the response body if the response status code is 200.
 """
+from typing import Callable
+
 import brotli
 from fastapi.responses import Response
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -16,19 +18,17 @@ class BrotliMiddleware(BaseHTTPMiddleware):
     This middleware checks if the incoming request's 'accept-encoding' header includes 'br' (Brotli).
     If so, and if the response status code is 200, it compresses the response body using Brotli
     compression and updates the response headers to reflect the encoding.
-
-    Methods:
-        dispatch(request: Request, call_next) -> Response:
-            Processes the incoming request and response, applying Brotli compression if applicable.
-
-    Args:
-        request (Request): The incoming HTTP request.
-        call_next (Callable): The next function to call in the request chain, which returns the response.
-
-    Returns:
-        Response: The HTTP response, potentially with Brotli compression applied.
     """
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(self, request: Request, call_next: Callable):
+        """Processes the incoming request and response, applying Brotli compression if applicable.
+
+        Args:
+            request (Request): The incoming HTTP request.
+            call_next (Callable): The next function to call in the request chain, which returns the response.
+
+        Returns:
+            Request: The HTTP response, potentially with Brotli compression applied.
+        """
         response = await call_next(request)
         if 'accept-encoding' in request.headers and 'br' in request.headers['accept-encoding']:
             if response.status_code == 200:
@@ -39,4 +39,3 @@ class BrotliMiddleware(BaseHTTPMiddleware):
                 headers['Content-Length'] = str(len(compressed_content))
                 return Response(content=compressed_content, headers=headers, status_code=response.status_code)
         return response
-
