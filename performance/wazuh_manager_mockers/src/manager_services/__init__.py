ea018f2233800482f1960035172b389d4fabb84d
Víctor Rebollo Pérez <victorrebollop@gmail.com>
2024-08-12 09:34:06 +0100

    docs: include basic readme for wazuh services mockers

---
+++ performance/wazuh_manager_mockers/src/manager_services/__init__.py
@@ -0,0 +1,8 @@
+"""Mock Services Manager.
+
+This package provides tools for managing and running the agent comm and management services of a Wazuh manager.
+
+### Features:
+- Agent Comm simulator
+- Management API simulator
+"""
