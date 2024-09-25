24dc63523c233f94cc7823e263db2baf2ac8c0a3
Juan Manuel Segura <juanmanuel.segura@wazuh.com>
2024-09-25 18:02:55 +0200

    feat: splitted script into separate scripts

--- deps/wazuh_testing/wazuh_testing/process_resource_monitoring/src/wazuh_disk_metrics.py
+++ deps/wazuh_testing/wazuh_testing/process_resource_monitoring/src/wazuh_disk_metrics.py
@@ -2,12 +2,11 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-"""Script to monitor Wazuh central components resource usage."""
+"""Script to monitor Wazuh central components disk usage."""
 
 
 import argparse
 import logging
-from collections import defaultdict
 from datetime import datetime
 from os import makedirs
 from os.path import join
@@ -15,46 +14,16 @@ from signal import SIGINT, SIGTERM, signal
 from tempfile import gettempdir
 from time import sleep, time_ns
 from types import FrameType
-from typing import Dict, List, Optional
+from typing import Dict, Optional
 
 from process_resource_monitoring._logger import logger
 from process_resource_monitoring.disk_usage_tracker import DiskUsageTracker
-from process_resource_monitoring.monitor import Monitor
 
 # Constant variables
 METRICS_FOLDER: str = join(gettempdir(), 'process_metrics')
 CURRENT_SESSION: str = join(METRICS_FOLDER, datetime.now().strftime('%d-%m-%Y'), str(time_ns() // 10**9))
-PROCESS_MAPPING: Dict[str, str] = {
-    # Manager processes
-    'agentlessd': 'wazuh-agentlessd',
-    'analysisd': 'wazuh-analysisd',
-    'apid': 'wazuh_apid.py',
-    'authd': 'wazuh-authd',
-    'clusterd': 'wazuh_clusterd.py',
-    'csyslogd': 'wazuh-csyslogd',
-    'db': 'wazuh-db',
-    'dbd': 'wazuh-dbd',
-    'execd': 'wazuh-execd',
-    'integratord': 'wazuh-integratord',
-    'logcollector': 'wazuh-logcollector',
-    'maild': 'wazuh-maild',
-    'modulesd': 'wazuh-modulesd',
-    'monitord': 'wazuh-monitord',
-    'remoted': 'wazuh-remoted',
-    'wazuh-syscheckd': 'syscheckd',
-
-    # Indexer
-    'indexer': 'wazuh-indexer',
-
-    # Dashboard
-    'dashboard': 'wazuh-dashboard',
-
-    # Agent
-    'agentd': 'wazuh-agentd',
-}
 
 # Global variables
-active_monitors: Dict[str, List[Monitor]] = defaultdict(list)
 active_disk_trackers: Dict[str, DiskUsageTracker] = {}
 session_active: bool = True
 
@@ -66,23 +35,14 @@ def get_script_arguments() -> argparse.Namespace:
         (argparse.Namespace): object containing the script options.
     """
     parser = argparse.ArgumentParser(
-        usage='%(prog)s [options] process_list',
-        description='Monitor the resource usage of processes in process_list',
+        usage='%(prog)s [options] files_list',
+        description='Monitor the resource usage of files and directories in files_list',
         formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
-        dest='process_name_list',
-        type=str,
-        nargs='+',
-        choices=list(PROCESS_MAPPING.keys()),
-        action='store',
-        help='Name of process/processes to monitor separated by whitespace.',
-    )
-    parser.add_argument(
-        '--disk',
         dest='disk_paths',
         type=str,
-        nargs='*',
+        nargs='+',
         action='store',
         help='Paths of the files/dirs to monitor their disk usage.',
     )
@@ -98,27 +58,11 @@ def get_script_arguments() -> argparse.Namespace:
     parser.add_argument(
         '-u',
         '--units',
-        dest='data_unit',
-        default='KB',
-        choices=['B', 'KB', 'MB'],
-        type=str,
-        help='Unit for the bytes-related values. Default: \'KB\'.',
-    )
-    parser.add_argument(
-        '--disk-unit',
         dest='disk_unit',
         default='GB',
         choices=['B', 'KB', 'MB', 'GB', 'TB'],
         type=str,
         help='Unit for the disk usage related values. Default \'GB\'',
-    )
-    parser.add_argument(
-        '-v',
-        '--version',
-        dest='version',
-        default=None,
-        type=str,
-        help='Version of the binaries. Default: None.',
     )
     parser.add_argument(
         '-d',
@@ -147,15 +91,6 @@ def get_script_arguments() -> argparse.Namespace:
         help='Number of reconnection retries before aborting the monitoring process. Default: 10.',
     )
     parser.add_argument(
-        '--store-process',
-        dest='store_process_path',
-        action='store',
-        default=CURRENT_SESSION,
-        type=str,
-        help=f'Path to store the CSVs with the process resource usage data. Default: \'{METRICS_FOLDER}/<id>\'.\n'
-        + 'Where <id> is the number of seconds since the epoch when the monitoring started.',
-    )
-    parser.add_argument(
         '--store-disk',
         dest='store_disk_path',
         action='store',
@@ -180,10 +115,6 @@ def shutdown_threads(signal_number: int, frame: Optional[FrameType]) -> None:
     global session_active
     session_active = False
 
-    # Shutdown all possible monitors
-    for monitor in sum(active_monitors.values(), []):
-        monitor.shutdown()
-
     # Shutdown all disk trackers
     for tracker in active_disk_trackers.values():
         tracker.shutdown()
@@ -201,41 +132,6 @@ def check_monitors_health(options) -> bool:
         (bool): False if there were any errors. True otherwise.
     """
     healthy = True
-    for process, monitors in active_monitors.items():
-        # Check if there is any unhealthy monitor
-        if any(filter(lambda monitor: monitor.is_event_set(), monitors)):
-            logger.warning(f'Monitoring of {process} failed. Attempting to create new monitor instances')
-
-            try:
-                # Try to get new PIDs
-                process_pids = Monitor.get_process_pids(process)
-                # Shutdown all the related monitors to the failed process (necessary for multiprocessing)
-                for monitor in monitors:
-                    monitor.shutdown()
-            except ValueError:
-                healthy = False
-                logger.warning(f'Could not create new monitor instances for {process}')
-                continue
-
-            for i, pid in enumerate(process_pids):
-                # Attempt to create new monitor instances for the process
-                p_name = process if i == 0 else f'{process}_child_{i}'
-                monitor = Monitor(
-                    process_name=p_name,
-                    pid=pid,
-                    value_unit=options.data_unit,
-                    time_step=options.sleep_time,
-                    version=options.version,
-                    dst_dir=options.store_process_path,
-                )
-                monitor.start()
-
-                try:
-                    # Replace old monitors for new ones
-                    active_monitors[process][i] = monitor
-                except IndexError:
-                    active_monitors[process].append(monitor)
-
     # Check health of disk trackers
     for path, tracker in active_disk_trackers.items():
         if tracker.is_event_set():
@@ -274,18 +170,16 @@ def monitors_healthcheck(options) -> None:
 
 
 def main():
-    """Create Monitor and DiskUsageTracker instances and start monitoring."""
+    """Create DiskUsageTracker instances and start monitoring."""
     # Execute `shutdown_threads` function whenever any of these signals are received externally (e.g. SIGINT=Ctrl+C)
     signal(SIGTERM, shutdown_threads)
     signal(SIGINT, shutdown_threads)
 
     options = get_script_arguments()
 
-    makedirs(CURRENT_SESSION)
-    if options.disk_paths:
-        makedirs(f'{CURRENT_SESSION}/files')
+    makedirs(f'{CURRENT_SESSION}/files')
     logging.basicConfig(
-        filename=join(METRICS_FOLDER, 'wazuh-metrics.log'),
+        filename=join(METRICS_FOLDER, 'wazuh-disk-metrics.log'),
         filemode='a',
         format='%(asctime)s %(levelname)s: %(message)s',
         datefmt='%Y/%m/%d %H:%M:%S',
@@ -298,23 +192,6 @@ def main():
 
     logger.info(f'Started new session: {CURRENT_SESSION}')
 
-    # Start monitoring for processes
-    process_list = map((lambda p: PROCESS_MAPPING[p]), options.process_name_list)
-    for process in process_list:
-        # Launch a monitor for every possible child process
-        for i, pid in enumerate(Monitor.get_process_pids(process)):
-            p_name = process if i == 0 else f'{process}_child_{i}'
-            monitor = Monitor(
-                process_name=p_name,
-                pid=pid,
-                value_unit=options.data_unit,
-                time_step=options.sleep_time,
-                version=options.version,
-                dst_dir=options.store_process_path,
-            )
-            monitor.start()
-            active_monitors[process].append(monitor)
-
     # Start monitoring for disk usage of files
     if options.disk_paths:
         for path in options.disk_paths:
@@ -323,7 +200,7 @@ def main():
                 value_unit=options.disk_unit,
                 time_step=options.sleep_time,
                 dst_dir=options.store_disk_path,
-                )
+            )
             tracker.start()
             active_disk_trackers[path] = tracker
 
