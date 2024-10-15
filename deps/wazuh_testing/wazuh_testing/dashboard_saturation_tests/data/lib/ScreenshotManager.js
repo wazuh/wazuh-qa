32b772c97cd442fe8dad8fed00013530f5e9773b
rafabailon <rafael.bailon@wazuh.com>
2024-10-15 14:35:18 +0200

    feat: add date and time to screenshots name

--- deps/wazuh_testing/wazuh_testing/dashboard_saturation_tests/data/lib/ScreenshotManager.js
+++ deps/wazuh_testing/wazuh_testing/dashboard_saturation_tests/data/lib/ScreenshotManager.js
@@ -14,7 +14,37 @@ class ScreenshotManager {
      */
     constructor(page, filepath) {
         this.page = page;
-        this.filepath = filepath
+        this.filepath = filepath;
+    }
+
+    /**
+     * Get the current date and time
+     * @returns {String} Returns the current date and time
+     */
+    get_date_and_time() {
+        date_and_time =  Date().toLocaleString(
+            'es-ES',
+            {
+                year: 'numeric', 
+                month: '2-digit', 
+                day: '2-digit',
+                hour: '2-digit', 
+                minute: '2-digit', 
+                second: '2-digit',
+                fractionalSecondDigits: 3
+            }
+        );
+
+        return date_and_time.replace(',', '-');
+    }
+
+    /**
+     * Generate the full name of the screenshot
+     * @param {String} name - Screenshot name
+     * @returns {String} Returns the full name of the screenshot
+     */
+    get_complete_name(name) {
+        return this.get_date_and_time() + '-' + name + this.extension;
     }
 
     /**
@@ -23,7 +53,7 @@ class ScreenshotManager {
      */
     async takeAnScreenshot(name) {
         // Generate the Complete Path to Store the Screenshots
-        let full_path = this.filepath + name + this.extension;
+        let full_path = this.filepath + this.get_complete_name(name);
 
         // Take a Browser Screenshot
         await this.page.screenshot({ path: full_path, fullPage: true });
