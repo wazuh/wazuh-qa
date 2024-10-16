39a127231d9e3b6a3fc49d5b960777844008633f
rafabailon <rafael.bailon@wazuh.com>
2024-10-16 15:51:00 +0200

    feat: improve screenshot manager

--- deps/wazuh_testing/wazuh_testing/dashboard_saturation_tests/data/lib/ScreenshotManager.js
+++ deps/wazuh_testing/wazuh_testing/dashboard_saturation_tests/data/lib/ScreenshotManager.js
@@ -6,6 +6,8 @@ class ScreenshotManager {
     extension = `.png`;
     // Page provides methods to interact with a single tab in a Browser
     page = null;
+    // User ID to group screenshots
+    user_id = null;
 
     /**
      * Class constructor
@@ -15,6 +17,7 @@ class ScreenshotManager {
     constructor(page, filepath) {
         this.page = page;
         this.filepath = filepath;
+        this.user_id = this.get_user_id();
     }
 
     /**
@@ -22,20 +25,16 @@ class ScreenshotManager {
      * @returns {String} Returns the current date and time
      */
     get_date_and_time() {
-        date_and_time =  Date().toLocaleString(
-            'es-ES',
-            {
-                year: 'numeric', 
-                month: '2-digit', 
-                day: '2-digit',
-                hour: '2-digit', 
-                minute: '2-digit', 
-                second: '2-digit',
-                fractionalSecondDigits: 3
-            }
-        );
-
-        return date_and_time.replace(',', '-');
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const day = String(now.getDate()).padStart(2, '0');
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const milliseconds = String(now.getMilliseconds()).padStart(3, '0');
+        
+        return `${year}${month}${day}_${hours}${minutes}${seconds}_${milliseconds}`;
     }
 
     /**
@@ -44,7 +43,15 @@ class ScreenshotManager {
      * @returns {String} Returns the full name of the screenshot
      */
     get_complete_name(name) {
-        return this.get_date_and_time() + '-' + name + this.extension;
+        return this.user_id + '-' + name + '_' + this.get_date_and_time() + this.extension;
+    }
+
+    /**
+     * Generate random user id
+     * @returns {String} Returns the random user id
+     */
+    get_user_id() {
+        return Math.random().toString(36).substring(7);
     }
 
     /**
@@ -53,7 +60,7 @@ class ScreenshotManager {
      */
     async takeAnScreenshot(name) {
         // Generate the Complete Path to Store the Screenshots
-        let full_path = this.filepath + this.get_complete_name(name);
+        const full_path = this.filepath + this.get_complete_name(name);
 
         // Take a Browser Screenshot
         await this.page.screenshot({ path: full_path, fullPage: true });
